dc0fea8441c62882693717e1eabd0a5ef5cb06db
Alex Curran <alexcurran.va@gmail.com>
2021-11-22 11:41:10 -0600

    Add second solution for concat arr

--- easy/concatenation-of-arr.js
+++ easy/concatenation-of-arr.js
@@ -1,12 +1,32 @@
 "use strict";
 
-/** O(n)
+/**
+ * Solution 1 -- O(n)
  * @param {number[]} nums
  * @return {number[]}
  */
 function getConcatenation(nums) {
     return [...nums, ...nums];
 }
+
+// /**
+//  * Solution 2 -- O(n)
+//  * @param {number[]} nums
+//  * @return {number[]}
+//  */
+// function getConcatenation(nums) {
+//     let resultArr = [];
+
+//     for (let num of nums) {
+//         resultArr.push(num);
+//     }
+
+//     for (let num of nums) {
+//         resultArr.push(num);
+//     }
+
+//     return resultArr;
+// }
 
 console.log( getConcatenation([1,2,1]) ); // [1,2,1,1,2,1]
 console.log( getConcatenation([1,3,2,1]) ); // [1,3,2,1,1,3,2,1]
