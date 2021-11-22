26e462e457e3b00d628153b8c7fe9c64c3288bd3
Alex Curran <alexcurran.va@gmail.com>
2021-11-22 11:51:35 -0600

    Working solution for palindrome-num algo

---
+++ easy/palindrome-num.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * Solution 1 -- O(n)
+ * @param {number} x
+ * @return {boolean}
+ */
+function isPalindrome(x) {
+    const xStr = String(x);
+    let reversedXStr = "";
+
+    for (let i = xStr.length - 1; i >= 0; i--) {
+        reversedXStr += xStr[i];
+    }
+
+    return +reversedXStr === x;
+}
+
+console.log( isPalindrome(121) ); // true -> 121 reads the same left to right and right to left
+console.log( isPalindrome(-121) ); // true -> 121 reads the same left to right and right to left
+console.log( isPalindrome(10) ); // true -> 121 reads the same left to right and right to left
+console.log( isPalindrome(-101) ); // true -> 121 reads the same left to right and right to left
