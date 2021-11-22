22ef04e592487fcfeff0825bc38a645d7115f483
Alex Curran <alexcurran.va@gmail.com>
2021-11-22 11:56:29 -0600

    Add second solution

--- easy/palindrome-num.js
+++ easy/palindrome-num.js
@@ -1,19 +1,34 @@
 "use strict";
 
+// /**
+//  * Solution 1 -- O(n)
+//  * @param {number} x
+//  * @return {boolean}
+//  */
+// function isPalindrome(x) {
+//     const xStr = String(x);
+//     let reversedXStr = "";
+
+//     for (let i = xStr.length - 1; i >= 0; i--) {
+//         reversedXStr += xStr[i];
+//     }
+
+//     return +reversedXStr === x;
+// }
+
 /**
- * Solution 1 -- O(n)
+ * Solution 2 -- O(n)
  * @param {number} x
  * @return {boolean}
  */
 function isPalindrome(x) {
-    const xStr = String(x);
-    let reversedXStr = "";
+    const reversedXStr = x
+                            .toString()
+                            .split("")
+                            .reverse()
+                            .join("");
 
-    for (let i = xStr.length - 1; i >= 0; i--) {
-        reversedXStr += xStr[i];
-    }
-
-    return +reversedXStr === x;
+    return Number(reversedXStr) === x;
 }
 
 console.log( isPalindrome(121) ); // true -> 121 reads the same left to right and right to left
