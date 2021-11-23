104fbdb0ac2cde5dd6497061c5f6e4114c0d5d2e
Alex Curran <alexcurran.va@gmail.com>
2021-11-23 15:30:48 -0600

    Solution 2 with O(1) runtime for power of two algo

--- easy/power-of-two.js
+++ easy/power-of-two.js
@@ -1,17 +1,31 @@
 "use strict";
 
+// /**
+//  * Solution 1 -- O(log n)
+//  * @param {number} n
+//  * @return {boolean}
+//  */
+// function isPowerOfTwo(n) {
+//     if (n === 1) return true;
+
+//     if (n === 0 || n % 2 !== 0) return false;
+
+//     // Recursively call func with new n val after dividing
+//     return isPowerOfTwo(n / 2);
+// }
+
 /**
- * O(log n)
+ * Solution 2 -- O(1)
  * @param {number} n
  * @return {boolean}
  */
 function isPowerOfTwo(n) {
-    if (n === 1) return true;
+    if (n === 0) return false;
 
-    if (n === 0 || n % 2 !== 0) return false;
-
-    // Recursively call func with new n val after dividing
-    return isPowerOfTwo(n / 2);
+    const baseTwoLog = Math.floor(Math.log2(n));
+    const expectedExponentiation = 2 ** baseTwoLog;
+    
+    return expectedExponentiation === n;
 }
 
 console.log( isPowerOfTwo(16) ); // true -> 2^4 === 16
