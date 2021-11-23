1af1e8cdb27b3e713021642d6a2ca40d7870cc9d
Alex Curran <alexcurran.va@gmail.com>
2021-11-23 12:35:54 -0600

    Working solution for length of last word algo

---
+++ easy/length-of-last-word.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/**
+ * O(n)
+ * @param {string} str
+ * @return {number}
+ */
+function lengthOfLastWord(str) {
+    const trimmedStr = str.trim();
+    const lastWord = trimmedStr
+                        .split(" ")
+                        .slice(-1)[0];
+
+    return lastWord.length;
+}
+
+console.log( lengthOfLastWord("Hello World") ); // 5
+console.log( lengthOfLastWord("   fly me   to   the moon  ") ); // 4
