b48ad44d631f4aa8559fbfe513523e353b58a07e
Alex Curran <alexcurran.va@gmail.com>
2021-11-24 10:57:40 -0600

    Working solution for valid-perfect-square algo

---
+++ easy/valid-perfect-square.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+function isPerfectSquare(num) {
+    const sqrtNum = Math.sqrt(num);
+    
+    return sqrtNum % 1 === 0 ? true : false;
+}
+
+console.log( isPerfectSquare(16) ); // true
+console.log( isPerfectSquare(14) ); // false
