1536c09c6367ae028af796989343564f97897e3e
Alex Curran <alexcurran.va@gmail.com>
2021-11-24 12:55:39 -0600

    Update fibonacci-number.js to include another solution with O(n) time complexity

--- easy/fibonacci-number.js
+++ easy/fibonacci-number.js
@@ -1,7 +1,7 @@
 "use strict";
 
 /**
- * O(1)
+ * Solution 1 -- O(n)
  * @param {number} n 
  * @returns {number}
  */
@@ -9,10 +9,31 @@ function fib(n) {
     if (n === 0) return 0;
     if (n === 1) return 1;
 
-    const sqrt5 = Math.sqrt(5);
+    let a = 1;
+    let b = 1;
 
-    return Math.round((((1 + sqrt5) ** n) - ((1 - sqrt5) ** n)) / ((2 ** n) * sqrt5));
+    for (let i = 2; i < n; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return b;
 }
+
+// /**
+//  * Solution 2 -- O(1)
+//  * @param {number} n 
+//  * @returns {number}
+//  */
+// function fib(n) {
+//     if (n === 0) return 0;
+//     if (n === 1) return 1;
+
+//     const sqrt5 = Math.sqrt(5);
+
+//     return Math.round((((1 + sqrt5) ** n) - ((1 - sqrt5) ** n)) / ((2 ** n) * sqrt5));
+// }
 
 console.log(fib(2)); // 1
 console.log(fib(3)); // 2
