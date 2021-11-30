5155a388bef6c3bfb06def0932175669beab6c43
Alex Curran <alexcurran.va@gmail.com>
2021-11-30 12:55:32 -0600

    Working solution for duplicate-zeros

---
+++ easy/duplicate-zeros.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/**
+ * @param {number[]} arr
+ * @return {void} Do not return anything, modify arr in-place instead.
+ */
+function duplicateZeros(arr) {
+    const originalArrLength = arr.length;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 0) {
+            arr.splice(i, 0, 0);
+            i = i + 1;
+        }
+    }
+
+    arr.splice(originalArrLength);
+}
+
+console.log( duplicateZeros([1,0,2,3,0,4,5,0]) ); // [1,0,0,2,3,0,0,4]
