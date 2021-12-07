ec4cf07b259b2bb6cf9cd852050a297d53b3de2b
Alex Curran <alexcurran.va@gmail.com>
2021-12-07 10:49:58 -0600

    Working solution for sort-arr-by-parity

---
+++ easy/sort-arr-by-parity.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * Given an integer array nums,
+ * move all the even integers at the beginning of the array followed by all the odd integers.
+ * 
+ * Solution 1
+ * Time: O(n)
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+function sortArrayByParity(nums) {
+    return [...nums].sort(compareNums);
+}
+
+function compareNums(a, b) {
+    // If num is even, place at the front of arr
+    if (a % 2 === 0) {
+        return -1;
+    }
+
+    return 1;
+}
+console.log( sortArrayByParity([3,1,2,4]) ); // [2, 4, 3, 1]
