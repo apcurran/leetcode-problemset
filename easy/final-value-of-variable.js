9dd7e504fa97e9e97e9a7393f68a9b4fcf3c153f
Alex Curran <alexcurran.va@gmail.com>
2021-12-13 16:58:15 -0600

    Working solution for final-value-of-variable.js

---
+++ easy/final-value-of-variable.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ * @param {string[]} operations
+ * @return {number}
+ */
+function finalValueAfterOperations(operations) {
+    let xVal = 0;
+
+    for (let op of operations) {
+        if (op === "--X" || op === "X--") {
+            xVal--;
+        } else {
+            xVal++;
+        }
+    }
+
+    return xVal;
+}
+
+console.log( finalValueAfterOperations(["--X","X++","X++"]) ); // 1
+console.log( finalValueAfterOperations(["++X","++X","X++"]) ); // 3
+console.log( finalValueAfterOperations(["X++","++X","--X","X--"]) ); // 0
