262a8c9233053ef37f922550d134c38fa2d9de16
Alex Curran <alexcurran.va@gmail.com>
2021-12-13 17:08:32 -0600

    Working solution for shuffle-the-array.js

---
+++ easy/shuffle-the-array.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * Given the array nums consisting of 2n elements in the form [x1,x2,...,xn,y1,y2,...,yn],
+ * return the array in the form [x1,y1,x2,y2,...,xn,yn].
+ * @param {number[]} nums
+ * @param {number} n
+ * @return {number[]}
+ */
+function shuffle(nums, n) {
+    let resArr = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i === n) break;
+
+        const currX = nums[i];
+        const currY = nums[i + n];
+        resArr.push(currX, currY);
+    }
+
+    return resArr;
+}
+
+console.log( shuffle([2,5,1,3,4,7], 3) ); // [2,3,5,4,1,7]
