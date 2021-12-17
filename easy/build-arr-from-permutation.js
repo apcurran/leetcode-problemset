ebf40d2bdca8a61c5ebbbd8b5465d4fbe6afe7b8
Alex Curran <alexcurran.va@gmail.com>
2021-12-17 16:00:57 -0600

    Working solution for build-arr-from-permutation.js

---
+++ easy/build-arr-from-permutation.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+function buildArray(nums) {
+    let resArr = [];
+
+    for (let i = 0; i < nums.length; i++) {
+        const newResNum = nums[nums[i]];
+        resArr.push(newResNum);
+    }
+
+    return resArr;
+}
+
+console.log( buildArray([0,2,1,5,3,4]) ); // [0,1,2,4,5,3]
+console.log( buildArray([5,0,1,2,3,4]) ); // [4,5,0,1,2,3]
