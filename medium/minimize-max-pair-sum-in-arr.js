f437a7bee5595a53235a9fc578024442e58796d4
Alex Curran <alexcurran.va@gmail.com>
2021-12-22 09:42:05 -0600

    Solution for min-max-pair-sum-in-arr.js

---
+++ medium/minimize-max-pair-sum-in-arr.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+function minPairSum(nums) {
+    const n = nums.length;
+    nums.sort((a, b) => a - b);
+
+    let best = 0;
+
+    for (let i = 0; i < n; i++) {
+        const minNum = nums[i];
+        const maxNum = nums[n - 1 - i];
+        
+        best = Math.max(best, minNum + maxNum);
+    }
+
+    return best;
+}
+
+console.log( minPairSum([3,5,2,3]) ); // 7
+console.log( minPairSum([3,5,4,2,4,6]) ); // 8
