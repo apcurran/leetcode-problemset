274555de2e0699fdd861bbfc1a25d482485df88b
Alex Curran <alexcurran.va@gmail.com>
2021-12-27 16:22:39 -0600

    O(n) time and O(1) space for find-all-duplicated-in-arr.js

--- medium/find-all-duplicates-in-arr.js
+++ medium/find-all-duplicates-in-arr.js
@@ -1,22 +1,56 @@
+"use strict";
+
+// /**
+//  * Solution 1
+//  * 
+//  * Time: O(n)
+//  * Space: O(n)
+//  * @param {number[]} nums
+//  * @return {number[]}
+//  */
+// function findDuplicates(nums) {
+//     let numsMap = new Map();
+//     let dupsArr = [];
+
+//     for (let num of nums) {
+//         if (!numsMap.has(num)) {
+//             numsMap.set(num, false);
+//         } else {
+//             numsMap.set(num, true);
+//         }
+//     }
+
+//     for (let [num, isDup] of numsMap) {
+//         if (isDup) {
+//             dupsArr.push(num);
+//         }
+//     }
+
+//     return dupsArr;
+// }
+
 /**
+ * Solution 2
+ * 
+ * Time: O(n)
+ * Space: O(1)
  * @param {number[]} nums
  * @return {number[]}
  */
 function findDuplicates(nums) {
-    let numsMap = new Map();
+    nums.sort((a, b) => a - b);
     let dupsArr = [];
+    // Each num in arr should only appear once in order
+    // If the current num is the same as the prev num, then it is a dup
+    // Dup may be added to dupsArr
 
-    for (let num of nums) {
-        if (!numsMap.has(num)) {
-            numsMap.set(num, false);
-        } else {
-            numsMap.set(num, true);
-        }
-    }
+    for (let i = 0; i < nums.length; i++) {
+        const currNum = nums[i];
+        const prevNum = nums[i - 1];
 
-    for (let [num, isDup] of numsMap) {
-        if (isDup) {
-            dupsArr.push(num);
+        if (currNum === prevNum) {
+            // Dup
+            dupsArr.push(currNum);
         }
     }
 
@@ -24,3 +58,5 @@ function findDuplicates(nums) {
 }
 
 console.log( findDuplicates([4,3,2,7,8,2,3,1]) ); // [2, 3]
+console.log( findDuplicates([1,1,2]) ); // [1]
+console.log( findDuplicates([1]) ); // []
