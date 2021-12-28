1e9289dfffc998a72dab37c82c6ee9e525cc839b
Alex Curran <alexcurran.va@gmail.com>
2021-12-28 09:42:11 -0600

    Third solution, fastest time and slightly less space than prev O(n) algo

--- medium/find-all-duplicates-in-arr.js
+++ medium/find-all-duplicates-in-arr.js
@@ -29,32 +29,57 @@
 //     return dupsArr;
 // }
 
+// /**
+//  * Solution 2
+//  * 
+//  * Time: O(n log n) - due to JS sorting algo
+//  * Space: O(1)
+//  * @param {number[]} nums
+//  * @return {number[]}
+//  */
+// function findDuplicates(nums) {
+//     nums.sort((a, b) => a - b);
+//     let dupsArr = [];
+//     // Each num in arr should only appear once in order
+//     // If the current num is the same as the prev num, then it is a dup
+//     // Dup may be added to dupsArr
+
+//     for (let i = 0; i < nums.length; i++) {
+//         const currNum = nums[i];
+//         const prevNum = nums[i - 1];
+
+//         if (currNum === prevNum) {
+//             // Dup
+//             dupsArr.push(currNum);
+//         }
+//     }
+
+//     return dupsArr;
+// }
+
 /**
- * Solution 2
+ * Solution 3
  * 
- * Time: O(n log n) - due to JS sorting algo
- * Space: O(1)
+ * This solution eliminates the second for loop, and uses slightly less memory
+ * 
+ * Time: O(n)
+ * Space: O(n)
  * @param {number[]} nums
  * @return {number[]}
  */
 function findDuplicates(nums) {
-    nums.sort((a, b) => a - b);
-    let dupsArr = [];
-    // Each num in arr should only appear once in order
-    // If the current num is the same as the prev num, then it is a dup
-    // Dup may be added to dupsArr
+    let numsMap = new Map();
+    let resArr = [];
 
-    for (let i = 0; i < nums.length; i++) {
-        const currNum = nums[i];
-        const prevNum = nums[i - 1];
-
-        if (currNum === prevNum) {
-            // Dup
-            dupsArr.push(currNum);
+    for (let num of nums) {
+        if (!numsMap.has(num)) {
+            numsMap.set(num, false);
+        } else {
+            resArr.push(num);
         }
     }
 
-    return dupsArr;
+    return resArr;
 }
 
 console.log( findDuplicates([4,3,2,7,8,2,3,1]) ); // [2, 3]
