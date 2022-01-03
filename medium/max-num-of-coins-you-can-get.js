96c90c92b071e0a5f357ae161dd262ea57fd945a
Alex Curran <alexcurran.va@gmail.com>
2022-01-03 10:39:13 -0600

    Slight optimizations for reducing execution time and memory

--- medium/max-num-of-coins-you-can-get.js
+++ medium/max-num-of-coins-you-can-get.js
@@ -12,21 +12,21 @@
 function maxCoins(piles) {
     // Sort in desc order
     piles.sort((a, b) => b - a);
-    // Init arr to store my coin piles
-    let myMaxCoinsArr = [];
+    // Init var to track total num of coins
+    let myCoinsTotal = 0;
 
     while (piles.length) {
         // Biggest num of triplet goes to Alice
         piles.shift();
         // Second highest num of triplet goes to me
         const myNum = piles.shift();
-        myMaxCoinsArr.push(myNum);
+        myCoinsTotal += myNum;
         // Third num should come from the end of the sorted arr (give smallest num to Bob)
         piles.pop();
     }
 
-    // Return sum of all nums I get
-    return myMaxCoinsArr.reduce((total, curr) => total + curr, 0);
+    // Return coins total
+    return myCoinsTotal;
 }
 
 console.log( maxCoins([2,4,1,2,7,8]) ); // 9
