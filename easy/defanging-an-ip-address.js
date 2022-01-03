ca07bfb6253d013784ac44130e03b7bde0dcac5e
Alex Curran <alexcurran.va@gmail.com>
2022-01-03 09:34:43 -0600

    Working O(n) solution for defanging-an-ip-address.js

---
+++ easy/defanging-an-ip-address.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/**
+ * A defanged IP address replaces every period "." with "[.]".
+ * 
+ * Time: O(n) - Iteration of a string under the hood is linear time
+ * Space: O(n)
+ * 
+ * @param {string} address
+ * @return {string}
+ */
+function defangIPaddr(address) {
+    const myRegex = /\./g;
+
+    return address.replace(myRegex, "[.]");
+}
+
+console.log( defangIPaddr("1.1.1.1") ); // "1[.]1[.]1[.]1"
+console.log( defangIPaddr("255.100.50.0") ); // "255[.]100[.]50[.]0"
