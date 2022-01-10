1779d3d9be74a57aaaec8cd0551a70fc71b4334c
Alex Curran <alexcurran.va@gmail.com>
2022-01-10 09:55:29 -0600

    Third solution -- optimized for space complexity

--- easy/sub-prod-and-sum-of-digits-of-an-int.js
+++ easy/sub-prod-and-sum-of-digits-of-an-int.js
@@ -18,26 +18,49 @@
 //     return digitsProd - digitsSum; 
 // }
 
+// /**
+//  * Solution 2 -- slight execution time improvement, fewer iterations
+//  * Time: O(n)
+//  * Space: O(n) - due to String primitive O(n) space complexity
+//  * 
+//  * @param {number} n
+//  * @return {number}
+//  */
+// function subtractProductAndSum(n) {
+//     const numDigitsStr = String(n);
+//     let prod = 1;
+//     let sum = 0;
+
+//     for (let num of numDigitsStr) {
+//         const numDigit = Number(num);
+//         prod *= numDigit;
+//         sum += numDigit;
+//     }
+    
+//     return prod - sum; 
+// }
+
 /**
- * Solution 2 -- slight execution time improvement, fewer iterations
+ * Solution 3 -- (optimized approach) single iteration without any extra space being used
  * Time: O(n)
- * Space: O(n) - due to String primitive O(n) space complexity
+ * Space: O(1)
  * 
  * @param {number} n
  * @return {number}
  */
 function subtractProductAndSum(n) {
-    const numDigitsStr = String(n);
     let prod = 1;
     let sum = 0;
 
-    for (let num of numDigitsStr) {
-        const numDigit = Number(num);
-        prod *= numDigit;
-        sum += numDigit;
+    while (n) {
+        const mod = n % 10;
+        prod *= mod;
+        sum += mod;
+
+        n = Math.floor(n / 10);
     }
-    
-    return prod - sum; 
+
+    return prod - sum;
 }
 
 console.log( subtractProductAndSum(234) ); // 15
