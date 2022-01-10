c13da2bfcdc3a96b1e2a9976b19b1385b989a67a
Alex Curran <alexcurran.va@gmail.com>
2022-01-10 09:43:36 -0600

    Remove another iteration -- .split("")

--- easy/sub-prod-and-sum-of-digits-of-an-int.js
+++ easy/sub-prod-and-sum-of-digits-of-an-int.js
@@ -1,21 +1,43 @@
 "use strict";
 
+// /**
+//  * Solution 1
+//  * Time: O(n)
+//  * Space: O(n)
+//  * 
+//  * @param {number} n
+//  * @return {number}
+//  */
+// function subtractProductAndSum(n) {
+//     const numDigitsArr = String(n)
+//                             .split("")
+//                             .map(Number);
+//     const digitsProd = numDigitsArr.reduce((total, curr) => total * curr, 1);
+//     const digitsSum = numDigitsArr.reduce((total, curr) => total + curr, 0);
+    
+//     return digitsProd - digitsSum; 
+// }
+
 /**
- * Solution 1
+ * Solution 2 -- slight execution time improvement, fewer iterations
  * Time: O(n)
- * Space: O(n)
+ * Space: O(n) - due to String primitive O(n) space complexity
  * 
  * @param {number} n
  * @return {number}
  */
 function subtractProductAndSum(n) {
-    const numDigitsArr = String(n)
-                            .split("")
-                            .map(Number);
-    const digitsProd = numDigitsArr.reduce((total, curr) => total * curr, 1);
-    const digitsSum = numDigitsArr.reduce((total, curr) => total + curr, 0);
+    const numDigitsStr = String(n);
+    let prod = 1;
+    let sum = 0;
+
+    for (let num of numDigitsStr) {
+        const numDigit = Number(num);
+        prod *= numDigit;
+        sum += numDigit;
+    }
     
-    return digitsProd - digitsSum; 
+    return prod - sum; 
 }
 
 console.log( subtractProductAndSum(234) ); // 15
