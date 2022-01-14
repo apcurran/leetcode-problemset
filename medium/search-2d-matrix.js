70dd222523089cb1a28f9a66da9988540448224b
Alex Curran <alexcurran.va@gmail.com>
2022-01-14 15:27:19 -0600

    Second solution

--- medium/search-2d-matrix.js
+++ medium/search-2d-matrix.js
@@ -1,8 +1,23 @@
 "use strict";
 
+// /**
+//  * Solution 1
+//  * Time: O(n)
+//  * Space: O(n)
+//  * 
+//  * @param {number[][]} matrix
+//  * @param {number} target
+//  * @return {boolean}
+//  */
+// function searchMatrix(matrix, target) {
+//     const flattenedArr = matrix.flat();
+
+//     return flattenedArr.includes(target);
+// }
+
 /**
- * Solution 1
- * Time: O(n)
+ * Solution 2 -- Execution time improvement, uses a bit more memory
+ * Time: O(n) -- Set object's .has() method is faster than array.includes()
  * Space: O(n)
  * 
  * @param {number[][]} matrix
@@ -11,8 +26,9 @@
  */
 function searchMatrix(matrix, target) {
     const flattenedArr = matrix.flat();
+    const matrixElemsSet = new Set(flattenedArr);
 
-    return flattenedArr.includes(target);
+    return matrixElemsSet.has(target);
 }
 
 console.log( searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 3) ); // true
