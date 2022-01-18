fe76778506dd015ac10431b450896455f82e1e40
Alex Curran <alexcurran.va@gmail.com>
2022-01-18 12:30:00 -0600

    More solutions

--- medium/sort-an-arr.js
+++ medium/sort-an-arr.js
@@ -1,7 +1,48 @@
 "use strict";
 
+// /**
+//  * Solution 1 -- Quicksort ES6
+//  * Time: O(n^2)
+//  * Space: O(n^2)
+//  * 
+//  * @param {number[]} nums
+//  * @return {number[]}
+//  */
+// function sortArray(nums) {
+//     if (nums.length <= 1) return nums;
+
+//     const arrCopy = [...nums];
+//     const pivot = arrCopy[arrCopy.length - 1]; // Choose last elem
+//     let left = [];
+//     let right = [];
+
+//     for (let i = 0; i < arrCopy.length - 1; i++) {
+//         const currNum = arrCopy[i];
+
+//         if (currNum < pivot) {
+//             left.push(currNum);
+//         } else {
+//             right.push(currNum);
+//         }
+//     }
+
+//     return [...sortArray(left), pivot, ...sortArray(right)];
+// }
+
+// /**
+//  * Solution 2 -- built-in array.sort() method
+//  * Time: O(n log n)
+//  * Space: O(1)
+//  * 
+//  * @param {number[]} nums
+//  * @return {number[]}
+//  */
+// function sortArray(nums) {
+//     return nums.sort((a, b) => a - b);
+// }
+
 /**
- * Solution 1
+ * Solution 2 -- recursive
  * Time: O(n^2)
  * Space: O(n^2)
  * 
@@ -11,13 +52,12 @@
 function sortArray(nums) {
     if (nums.length <= 1) return nums;
 
-    const arrCopy = [...nums];
-    const pivot = arrCopy[arrCopy.length - 1]; // Choose last elem
+    let pivot = nums[0];
     let left = [];
     let right = [];
 
-    for (let i = 0; i < arrCopy.length - 1; i++) {
-        const currNum = arrCopy[i];
+    for (let i = 1; i < nums.length; i++) {
+        const currNum = nums[i];
 
         if (currNum < pivot) {
             left.push(currNum);
@@ -26,7 +66,7 @@ function sortArray(nums) {
         }
     }
 
-    return [...sortArray(left), pivot, ...sortArray(right)];
+    return sortArray(left).concat(pivot, sortArray(right));
 }
 
-console.log( sortArray([5,2,3,1]) ); // [1,2,3,5]
+console.log( sortArray([5, 2, 3, 1]) ); // [1, 2, 3, 5]
