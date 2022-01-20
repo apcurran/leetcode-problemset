4ff7b2c35a1eedb67a3f658eb5289603b80fd9f1
Alex Curran <alexcurran.va@gmail.com>
2022-01-20 10:10:21 -0600

    Working solution for truncate-sentence.js

---
+++ easy/truncate-sentence.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/**
+ * Solution 1
+ * Time: O(n)
+ * Space: O(n)
+ * 
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+function truncateSentence(s, k) {
+    return s
+            .split(" ")
+            .slice(0, k)
+            .join(" "); 
+}
+
+console.log( truncateSentence("Hello how are you Contestant", 4) ); // "Hello how are you"
