ee994cff6c63e64a455c1934c24ca6c695d17916
Alex Curran <alexcurran.va@gmail.com>
2022-01-27 10:20:02 -0600

    Working solution for find-all-nums-disappeared-in-an-arr.js

---
+++ easy/find-all-nums-disappeared-in-an-arr.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * Solution 1
+ * time: O(n log n)
+ * space: O(n)
+ * 
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+function findDisappearedNumbers(nums) {
+    let resArr = [];
+    const sortedArr = new Set(nums);
+
+    for (let i = 1; i <= nums.length; i++) {
+        if (!sortedArr.has(i)) {
+            resArr.push(i);
+        }
+    }
+
+    return resArr;
+}
+
+console.log( findDisappearedNumbers([4,3,2,7,8,2,3,1]) ); // [5, 6]
+console.log( findDisappearedNumbers([1, 1]) ); // [2]
