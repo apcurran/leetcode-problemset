b856a6173e2f1c41d8dd2d9700758900300ce17e
Alex Curran <alexcurran.va@gmail.com>
2022-01-31 15:39:21 -0600

    Reverse linked list algo

---
+++ easy/top-interview-qs/linked-list/reverse-linked-list.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// Definition for singly-linked list.
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val);
+    this.next = (next === undefined ? null : next);
+}
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+function reverseList(head) {
+    let prev = null;
+    let current = head;
+
+    while (current !== null) {
+        const next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
+    }
+
+    return prev;
+}
