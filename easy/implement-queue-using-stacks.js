82d94c09452319ec691bea5d33c550b85d6c2b0d
Alex Curran <alexcurran.va@gmail.com>
2022-02-11 13:44:12 -0600

    Add JSDoc params and complexities for each method

--- easy/implement-queue-using-stacks.js
+++ easy/implement-queue-using-stacks.js
@@ -5,20 +5,45 @@ class MyQueue {
         this.stack = [];
     }
 
+    /**
+     * time: O(1)
+     * space: O(1)
+     * 
+     * @param {number} x 
+     * @returns {void}
+     */
     push(x) {
         this.stack.push(x);
     }
 
+    /**
+     * time: O(n) - shifting removes from front of stack, which causes all other elems to move over by one
+     * space: O(1)
+     * 
+     * @returns {number}
+     */
     pop() {
         const poppedElem = this.stack.shift();
 
         return poppedElem;
     }
 
+    /**
+     * time: O(1)
+     * space: O(1)
+     * 
+     * @returns {number}
+     */
     peek() {
         return this.stack[0];
     }
 
+    /**
+     * time: O(1)
+     * space: O(1)
+     * 
+     * @returns {boolean}
+     */
     empty() {
         return this.stack.length === 0;
     }
