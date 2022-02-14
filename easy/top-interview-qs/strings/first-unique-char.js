439cb13495749142515c7fb8df344cf1feadcc64
Alex Curran <alexcurran.va@gmail.com>
2022-02-14 11:22:35 -0600

    Update comments

--- easy/top-interview-qs/strings/first-unique-char.js
+++ easy/top-interview-qs/strings/first-unique-char.js
@@ -28,41 +28,8 @@
 //     return -1;
 // }
 
-// /**
-//  * Solution 2
-//  * time: O(n^2)
-//  * space: O(n)
-//  * 
-//  * @param {string} str
-//  * @return {number}
-//  */
-// function firstUniqChar(str) {
-//     let charMap = new Map();
-//     // Iterate through string
-//     for (let char of str) {
-//         if (!charMap.has(char)) {
-//             // Store curr char in a map obj
-//             charMap.set(char, 1);
-//         } else {
-//             // Incr the count of the char occurrences
-//             const prevCharVal = charMap.get(char);
-//             charMap.set(char, prevCharVal + 1);
-//         }
-//     }
-
-//     // Check for a char in the map with an occurrence val of 1
-//     for (let [char, charVal] of charMap) {
-//         if (charVal === 1) {
-//             // return index of char
-//             return str.indexOf(char);
-//         }
-//     }
-//     // If none exist with an occurrence of 1, then return -1
-//     return -1;
-// }
-
 /**
- * Solution 2
+ * Solution 2 -- fastest submission
  * time: O(n)
  * space: O(n)
  * 
@@ -72,35 +39,68 @@
 function firstUniqChar(str) {
     let charMap = new Map();
     // Iterate through string
-    for (let i = 0; i < str.length; i++) {
-        const currChar = str[i];
-
-        if (!charMap.has(currChar)) {
+    for (let char of str) {
+        if (!charMap.has(char)) {
             // Store curr char in a map obj
-            // Store index with char occurrences as val to fetch later!
-            // Ex. val = 0:1 -- index 0 and occurrences of 1
-            const charVal = `${i}:1`;
-            charMap.set(currChar, charVal);
+            charMap.set(char, 1);
         } else {
             // Incr the count of the char occurrences
-            // Parse str for char val first
-            const prevCharVal = Number(charMap.get(currChar).split(":")[1]);
-            const incrementedCharVal = prevCharVal + 1;
-
-            charMap.set(currChar, `${i}:${incrementedCharVal}`);
+            const prevCharVal = charMap.get(char);
+            charMap.set(char, prevCharVal + 1);
         }
     }
 
     // Check for a char in the map with an occurrence val of 1
-    for (let charVal of charMap.values()) {
-        const charValIndex = Number(charVal.split(":")[0]);
-        const charValNum = Number(charVal.split(":")[1]);
-
-        if (charValNum === 1) return charValIndex;
+    for (let [char, charVal] of charMap) {
+        if (charVal === 1) {
+            // return index of char -- not O(n^2) operation, since it only executes when charVal === 1 (which should only run max once)
+            return str.indexOf(char);
+        }
     }
     // If none exist with an occurrence of 1, then return -1
     return -1;
 }
+
+// /**
+//  * Solution 3
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {string} str
+//  * @return {number}
+//  */
+// function firstUniqChar(str) {
+//     let charMap = new Map();
+//     // Iterate through string
+//     for (let i = 0; i < str.length; i++) {
+//         const currChar = str[i];
+
+//         if (!charMap.has(currChar)) {
+//             // Store curr char in a map obj
+//             // Store index with char occurrences as val to fetch later!
+//             // Ex. val = 0:1 -- index 0 and occurrences of 1
+//             const charVal = `${i}:1`;
+//             charMap.set(currChar, charVal);
+//         } else {
+//             // Incr the count of the char occurrences
+//             // Parse str for char val first
+//             const prevCharVal = Number(charMap.get(currChar).split(":")[1]);
+//             const incrementedCharVal = prevCharVal + 1;
+
+//             charMap.set(currChar, `${i}:${incrementedCharVal}`);
+//         }
+//     }
+
+//     // Check for a char in the map with an occurrence val of 1
+//     for (let charVal of charMap.values()) {
+//         const charValIndex = Number(charVal.split(":")[0]);
+//         const charValNum = Number(charVal.split(":")[1]);
+
+//         if (charValNum === 1) return charValIndex;
+//     }
+//     // If none exist with an occurrence of 1, then return -1
+//     return -1;
+// }
 
 console.log(firstUniqChar("leetcode")); // 0
 console.log(firstUniqChar("loveleetcode")); // 2
