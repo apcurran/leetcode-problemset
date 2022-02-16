62e5e8e944b8f1d6e1586f6a981d72a4e481f15d
Alex Curran <alexcurran.va@gmail.com>
2022-02-16 10:41:16 -0600

    Working solution (but times out in LeetCode submission) for best-time-to-buy-and-sell-stock.js

---
+++ easy/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/**
+ * Solution 1
+ * time: O(n^2)
+ * space: O(1)
+ * 
+ * @param {number[]} prices
+ * @return {number}
+ */
+function maxProfit(prices) {
+    let globalMaxProfit = 0;
+
+    for (let i = 0; i < prices.length; i++) {
+        const outerCurrNum = prices[i];
+
+        for (let j = i + 1; j < prices.length; j++) {
+            const innerCurrNum = prices[j];
+            const testMaxProfit = innerCurrNum - outerCurrNum;
+
+            if (testMaxProfit > globalMaxProfit) {
+                globalMaxProfit = testMaxProfit;
+            }
+        }
+    }
+
+    if (globalMaxProfit > 0) return globalMaxProfit;
+
+    return 0;
+}
+
+console.log( maxProfit([7, 1, 5, 3, 6, 4]) ); // 5 -- max profit
+console.log( maxProfit([7, 6, 4, 3, 1]) ); // 0 -- no profit
