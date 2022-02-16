892d96cbdc3005343b1843d088f8920430a4dcc6
Alex Curran <alexcurran.va@gmail.com>
2022-02-16 12:49:09 -0600

    Working solution for sorting-the-sent.js

---
+++ easy/sorting-the-sent.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ * Solution 1
+ * time: O(n log n) -- due to sorting
+ * space: O(n)
+ * @param {string} sentence
+ * @return {string}
+ */
+function sortSentence(sentence) {
+    const wordsArr = sentence.split(" ");
+    const wordNumRegex = /\d/;
+
+    wordsArr.sort((word1, word2) => {
+        const word1Num = Number(word1.match(wordNumRegex)[0]);
+        const word2Num = Number(word2.match(wordNumRegex)[0]);
+        
+        return word1Num - word2Num;
+    });
+
+    const wordsSent = wordsArr.join(" ");
+
+    return wordsSent.replace(/\d/g, "");
+}
+
+console.log( sortSentence("is2 sentence4 This1 a3") ); // "This is a sentence"
