b636271aca47b98b4cbbc6c4f1cb5c5f20dc6fdf
Alex Curran <alexcurran.va@gmail.com>
2022-02-21 13:40:10 -0600

    Working solution for range-sum-of-bst.js

---
+++ easy/range-sum-of-bst.js
@@ -0,0 +1,43 @@
+"use strict";
+
+
+// Definition for a binary tree node.
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val);
+    this.left = (left === undefined ? null : left);
+    this.right = (right === undefined ? null : right);
+}
+
+/**
+ * Solution 1 -- iterative
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+function rangeSumBST(root, low, high) {
+    let nodeStack = [root];
+    let rangeSum = 0;
+
+    while (nodeStack.length > 0) {
+        const current = nodeStack.pop();
+        const currVal = current.val;
+
+        if (currVal >= low && currVal <= high) {
+            rangeSum += currVal;
+        }
+
+        if (current.right) {
+            nodeStack.push(current.right);
+        }
+
+        if (current.left) {
+            nodeStack.push(current.left);
+        }
+    }
+
+    return rangeSum;
+}
