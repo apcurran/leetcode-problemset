cbdc667734ebf9a05d7163fe26ca452047ccc65d
Alex Curran <alexcurran.va@gmail.com>
2022-02-22 09:36:24 -0600

    Working recursive solution for max-depth-of-bin-tree.js

---
+++ easy/top-interview-qs/trees/max-depth-of-bin-tree.js
@@ -0,0 +1,27 @@
+"use strict";
+
+
+// Definition for a binary tree node.
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val);
+    this.left = (left === undefined ? null : left);
+    this.right = (right === undefined ? null : right);
+}
+
+/**
+ * Solution 1 -- recursion
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {TreeNode} root
+ * @return {number}
+ */
+function maxDepth(root) {
+    if (root === null) return 0;
+
+    const leftDepth = maxDepth(root.left);
+    const rightDepth = maxDepth(root.right);
+    const currMaxDepth = Math.max(leftDepth, rightDepth);
+
+    return 1 + currMaxDepth;
+}
