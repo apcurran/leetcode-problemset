2fe5dd48f3e6cde2ed649312fbfc54aa2e98e321
Alex Curran <alexcurran.va@gmail.com>
2022-03-07 10:46:48 -0600

    Working solution for create-target-arr-in-the-given-order.js

---
+++ easy/create-target-arr-in-the-given-order.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n^2) -- due to splice possibly forcing shifting of elems' positions
+ * space: O(n)
+ * 
+ * @param {number[]} numsArr
+ * @param {number[]} indexArr
+ * @return {number[]}
+ */
+function createTargetArray(numsArr, indexArr) {
+    let targetArr = [];
+
+    for (let i = 0; i < numsArr.length; i++) {
+        const currNumsVal = numsArr[i];
+        const currIndexVal = indexArr[i];
+
+        targetArr.splice(currIndexVal, 0, currNumsVal);
+    }
+
+    return targetArr;
+}
+
+console.log( createTargetArray([0,1,2,3,4], [0,1,2,2,1]) ); // [0,4,1,3,2]
