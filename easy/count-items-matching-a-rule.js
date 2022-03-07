76de27de40d1e8bfd93330b8700e4714745d7a7e
Alex Curran <alexcurran.va@gmail.com>
2022-03-07 17:15:46 -0600

    Working solution for count-items-matching-a-rule.js

---
+++ easy/count-items-matching-a-rule.js
@@ -0,0 +1,59 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {string[][]} items
+ * @param {string} ruleKey
+ * @param {string} ruleValue
+ * @return {number}
+ */
+function countMatches(items, ruleKey, ruleValue) {
+    let itemCount = 0;
+    let ruleIndex;
+
+    switch (ruleKey) {
+        case "type":
+            ruleIndex = 0;
+            break;
+        case "color":
+            ruleIndex = 1;
+            break;
+        case "name":
+            ruleIndex = 2;
+            break;
+    }
+    // Iterate through arr calc ruleKey and rulevalue to check
+    for (let item of items) {
+        const itemVal = item[ruleIndex];
+
+        if (itemVal === ruleValue) {
+            // If a match is found, incr the resVal
+            itemCount++;
+        }
+    }
+    // Return the resVal for number of matching items based upon the given rule
+    return itemCount;
+}
+
+console.log( countMatches(
+    [
+        ["phone", "blue", "pixel"],
+        ["computer", "silver", "lenovo"],
+        ["phone", "gold", "iphone"]
+    ],
+    "color",
+    "silver"
+) ); // 1
+
+console.log( countMatches(
+    [
+        ["phone", "blue", "pixel"],
+        ["computer", "silver", "phone"],
+        ["phone", "gold", "iphone"]
+    ],
+    "type",
+    "phone"
+) ); // 2
