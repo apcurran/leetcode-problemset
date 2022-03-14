af032f6e5af9a38f24f99846e26aff738c8686b3
Alex Curran <alexcurran.va@gmail.com>
2022-03-14 16:49:55 -0500

    Working math logarithm solution to power-of-four.js

--- easy/power-of-four.js
+++ easy/power-of-four.js
@@ -1,21 +1,39 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(log n)
+//  * space: O(log n)
+//  * 
+//  * @param {number} n
+//  * @return {boolean}
+//  */
+// function isPowerOfFour(n) {
+//     // base case
+//     if (n === 1) return true;
+    
+//     // base case
+//     if (n === 0 || n % 2 !== 0) return false;
+
+//     return isPowerOfFour(n / 4);
+// }
+
 /**
  * solution 1
- * time: O(log n)
- * space: O(log n)
+ * time: O(1)
+ * space: O(1)
  * 
  * @param {number} n
  * @return {boolean}
- */
+*/
 function isPowerOfFour(n) {
-    // base case
-    if (n === 1) return true;
-    
-    // base case
-    if (n === 0 || n % 2 !== 0) return false;
+    if (n === 0) return false;
 
-    return isPowerOfFour(n / 4);
+    const baseFourLog = Math.log(n) / Math.log(4);
+
+    if (!Number.isInteger(baseFourLog)) return false;
+
+    return n === 4 ** baseFourLog;
 }
 
 console.log( isPowerOfFour(16) ); // true
