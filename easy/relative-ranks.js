81093e38179b5c4543bc6e1e0093bc210b9a6e1b
Alex Curran <alexcurran.va@gmail.com>
2022-03-22 09:59:43 -0500

    Working solution for relative-ranks.js

---
+++ easy/relative-ranks.js
@@ -0,0 +1,40 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n^2)
+ * space: O(n)
+ * 
+ * @param {number[]} score
+ * @return {string[]}
+ */
+function findRelativeRanks(scores) {
+    // sort list in desc order first
+    const sortedScoresArrCopy = [...scores].sort((a, b) => b - a);
+    let resArr = [];
+    // iterate through original score arr
+    for (let score of scores) {
+        // findIndex on the curr score
+        const sortedScoresIdx = sortedScoresArrCopy.findIndex((num) => num === score);
+        // add one to index res (so 1-based index instead of 0-based)
+        const scoreRank = sortedScoresIdx + 1;
+        // check if num is 1st -> gold, 2nd -> silver, etc.
+        let finalRank;
+
+        if (scoreRank === 1) {
+            finalRank = "Gold Medal";
+        } else if (scoreRank === 2) {
+            finalRank = "Silver Medal";
+        } else if (scoreRank === 3) {
+            finalRank = "Bronze Medal";
+        } else {
+            finalRank = String(scoreRank);
+        }
+        // add str num or medal type str to res arr
+        resArr.push(finalRank);
+    }
+    // return res arr
+    return resArr;
+}
+
+console.log( findRelativeRanks([10, 3, 8, 9, 4]) ); // ["Gold Medal", "5", "Bronze Medal", "Silver Medal", "4"]
