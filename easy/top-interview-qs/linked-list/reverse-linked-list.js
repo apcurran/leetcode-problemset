ac7ff173de9c4a19a1b9e19152c73e253a30c90f
Alex Curran <alexcurran.va@gmail.com>
2022-04-06 09:45:57 -0500

    Uncomment first solution

--- easy/top-interview-qs/linked-list/reverse-linked-list.js
+++ easy/top-interview-qs/linked-list/reverse-linked-list.js
@@ -6,49 +6,49 @@ function ListNode(val, next) {
     this.next = (next === undefined ? null : next);
 }
 
-// /**
-//  * solution 1 -- iterative
-//  * time: O(n)
-//  * space: O(1)
-//  * 
-//  * @param {ListNode} head
-//  * @return {ListNode}
-//  */
-// function reverseList(head) {
-//     let prev = null;
-//     let current = head;
-
-//     while (current !== null) {
-//         const next = current.next;
-//         current.next = prev;
-//         prev = current;
-//         current = next;
-//     }
-
-//     return prev;
-// }
-
 /**
- * solution 2 -- recursive
+ * solution 1 -- iterative
  * time: O(n)
- * space: O(n)
+ * space: O(1)
  * 
  * @param {ListNode} head
  * @return {ListNode}
  */
 function reverseList(head) {
-    // base case
-    if (head === null) return null;
+    let prev = null;
+    let current = head;
 
-    let newHead = head;
-
-    if (head.next) {
-        newHead = reverseList(head.next);
-        // reverse link
-        head.next.next = head;
+    while (current !== null) {
+        const next = current.next;
+        current.next = prev;
+        prev = current;
+        current = next;
     }
 
-    head.next = null;
-
-    return newHead;
+    return prev;
 }
+
+// /**
+//  * solution 2 -- recursive
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {ListNode} head
+//  * @return {ListNode}
+//  */
+// function reverseList(head) {
+//     // base case
+//     if (head === null) return null;
+
+//     let newHead = head;
+
+//     if (head.next) {
+//         newHead = reverseList(head.next);
+//         // reverse link
+//         head.next.next = head;
+//     }
+
+//     head.next = null;
+
+//     return newHead;
+// }
