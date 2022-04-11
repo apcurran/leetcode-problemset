0e48e4a240e8c1710b413add9ae7c680ba91f37a
Alex Curran <alexcurran.va@gmail.com>
2022-04-11 14:56:22 -0500

    Improved bin search and O(m * log n) solution

--- medium/search-2d-matrix.js
+++ medium/search-2d-matrix.js
@@ -31,37 +31,73 @@
 //     return matrixElemsSet.has(target);
 // }
 
+// /**
+//  * Solution 3 -- binary search
+//  * Time: O(n) -- .flat()? or O(log n) for bin search
+//  * Space: O(n) -- flattened arr copy
+//  * 
+//  * @param {number[][]} matrix
+//  * @param {number} target
+//  * @return {boolean}
+//  */
+// function searchMatrix(matrix, target) {
+//     const flattenedArr = matrix.flat();
+
+//     // impl bin search
+//     let startIndex = 0;
+//     let endIndex = flattenedArr.length - 1;
+//     let middleIndex = 0;
+
+//     while (startIndex <= endIndex) {
+//         middleIndex = Math.floor((startIndex + endIndex) / 2);
+
+//         if (flattenedArr[middleIndex] === target) return true;
+
+//         if (flattenedArr[middleIndex] < target) {
+//             // search right half
+//             startIndex = middleIndex + 1;
+//         } else {
+//             // search left half
+//             endIndex = middleIndex - 1;
+//         }
+//     }
+
+//     return false;
+// }
+
 /**
- * Solution 3 -- binary search
- * Time: O(n) -- .flat()? or O(log n) for bin search
- * Space: O(n) -- flattened arr copy
+ * Solution 4 -- binary search
+ * m = num of matrix rows
+ * n = num of elems in a row
+ * Time: O(m * log n)
+ * Space: O(1)
  * 
  * @param {number[][]} matrix
  * @param {number} target
  * @return {boolean}
  */
 function searchMatrix(matrix, target) {
-    const flattenedArr = matrix.flat();
-
-    // impl bin search
-    let startIndex = 0;
-    let endIndex = flattenedArr.length - 1;
-    let middleIndex = 0;
-
-    while (startIndex <= endIndex) {
-        middleIndex = Math.floor((startIndex + endIndex) / 2);
-
-        if (flattenedArr[middleIndex] === target) return true;
-
-        if (flattenedArr[middleIndex] < target) {
-            // search right half
-            startIndex = middleIndex + 1;
-        } else {
-            // search left half
-            endIndex = middleIndex - 1;
+    for (let row of matrix) {
+        // impl bin search
+        let startIndex = 0;
+        let endIndex = row.length - 1;
+        let middleIndex = 0;
+    
+        while (startIndex <= endIndex) {
+            middleIndex = Math.floor((startIndex + endIndex) / 2);
+    
+            if (row[middleIndex] === target) return true;
+    
+            if (row[middleIndex] < target) {
+                // search right half
+                startIndex = middleIndex + 1;
+            } else {
+                // search left half
+                endIndex = middleIndex - 1;
+            }
         }
     }
-
+    
     return false;
 }
 
