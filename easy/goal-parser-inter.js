67d454f8ac011ce18d77a0f89b8b1b184cb7c7a5
Alex Curran <alexcurran.va@gmail.com>
2022-04-14 16:53:30 -0500

    Working O(n) solution for goal-parser-inter.js

---
+++ easy/goal-parser-inter.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/**
+ * solution 1 -- regex replaceAll() method
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {string} command
+ * @return {string}
+ */
+function interpret(command) {
+    command = command.replaceAll("()", "o");
+    command = command.replaceAll("(al)", "al");
+
+    return command;
+}
+
+console.log( interpret("G()(al)") ); // "Goal"
