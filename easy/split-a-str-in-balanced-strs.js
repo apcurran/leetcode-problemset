e8ca794d49a44ded04303035bbedbd4d998aa601
Alex Curran <alexcurran.va@gmail.com>
2022-04-15 16:32:30 -0500

    Working solution for split-a-str-in-balanced-strs.js

---
+++ easy/split-a-str-in-balanced-strs.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/**
+ * solution 1 -- iterative
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {string} str
+ * @return {number}
+ */
+function balancedStringSplit(str) {
+    let balanceVal = 0;
+    let matchesAmt = 0;
+
+    for (let char of str) {
+        if (char === "L") {
+            balanceVal++;
+        } else {
+            balanceVal--;
+        }
+
+        if (balanceVal === 0) matchesAmt++;
+    }
+
+    return matchesAmt;
+}
+
+console.log( balancedStringSplit("RLRRLLRLRL") ); // 4
