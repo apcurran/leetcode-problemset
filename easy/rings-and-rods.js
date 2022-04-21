ee4a08b51866e627123ef3f386419e1f6bbb3985
Alex Curran <alexcurran.va@gmail.com>
2022-04-21 15:25:24 -0500

    Working solution variant

--- easy/rings-and-rods.js
+++ easy/rings-and-rods.js
@@ -1,7 +1,41 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {string} rings
+//  * @return {number}
+//  */
+// function countPoints(rings) {
+//     const splitRingsTuplesArr = rings.match(/.{1,2}/g);
+//     let rodsMap = new Map();
+
+//     for (let ring of splitRingsTuplesArr) {
+//         const [ringColorLetter, rodNum] = ring;
+        
+//         if (!rodsMap.has(rodNum)) {
+//             // init Set obj to hold colors
+//             rodsMap.set(rodNum, new Set());
+//         }
+
+//         rodsMap
+//             .get(rodNum)
+//             .add(ringColorLetter);
+//     }
+
+//     let resCount = 0;
+
+//     for (let ringColorSet of rodsMap.values()) {
+//         if (ringColorSet.size === 3) resCount++;
+//     }
+
+//     return resCount;
+// }
+
 /**
- * solution 1
+ * solution 1 -- skips creation of new split arr
  * time: O(n)
  * space: O(n)
  * 
@@ -9,17 +43,17 @@
  * @return {number}
  */
 function countPoints(rings) {
-    const splitRingsTuplesArr = rings.match(/.{1,2}/g);
     let rodsMap = new Map();
 
-    for (let ring of splitRingsTuplesArr) {
-        const [ringColorLetter, rodNum] = ring;
+    for (let i = 0; i < rings.length; i += 2) {
+        const ringColorLetter = rings[i];
+        const rodNum = rings[i + 1];
         
         if (!rodsMap.has(rodNum)) {
             // init Set obj to hold colors
             rodsMap.set(rodNum, new Set());
         }
-
+    
         rodsMap
             .get(rodNum)
             .add(ringColorLetter);
@@ -34,5 +68,5 @@ function countPoints(rings) {
     return resCount;
 }
 
-console.log( countPoints("B0B6G0R6R0R6G9") ); // 1
+console.log( countPoints("B6B0G0R6R0R6G9") ); // 1
 console.log( countPoints("B0R0G0R9R0B0G0") ); // 1
