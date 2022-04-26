c6d06c09634c0a41da83b0aace010432a33023ff
Alex Curran <alexcurran.va@gmail.com>
2022-04-26 16:47:18 -0500

    Working O(n) solution for Math.max() and Math.min() calls

--- easy/max-prod-of-two-elems-in-an-arr.js
+++ easy/max-prod-of-two-elems-in-an-arr.js
@@ -1,18 +1,39 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n * log n) -- due to sorting first
+//  * space: O(1)
+//  * 
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// function maxProduct(nums) {
+//     nums.sort((a, b) => b - a);
+//     const largestNum = nums[0];
+//     const nextLargestNum = nums[1];
+
+//     return (largestNum - 1) * (nextLargestNum - 1);
+// }
+
 /**
- * solution 1
- * time: O(n * log n) -- due to sorting first
+ * solution 2
+ * time: O(n)
  * space: O(1)
  * 
  * @param {number[]} nums
  * @return {number}
  */
 function maxProduct(nums) {
-    nums.sort((a, b) => b - a);
-    const largestNum = nums[0];
-    const nextLargestNum = nums[1];
-    
+    let largestNum = 0;
+    let nextLargestNum = 0;
+
+    for (let num of nums) {
+        // requires that nextLargestNum be calculated first
+        nextLargestNum = Math.max(nextLargestNum, Math.min(largestNum, num));
+        largestNum = Math.max(largestNum, num);
+    }
+
     return (largestNum - 1) * (nextLargestNum - 1);
 }
 
