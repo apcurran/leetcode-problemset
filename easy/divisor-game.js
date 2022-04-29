dbed40d24d5165109431a292b906428621388da6
Alex Curran <alexcurran.va@gmail.com>
2022-04-29 10:41:45 -0500

    Working solution to divisor-game.js

---
+++ easy/divisor-game.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**
+ * solution 1 -- modulus op
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {number} n
+ * @return {boolean}
+ */
+function divisorGame(n) {
+    return n % 2 === 0;
+}
