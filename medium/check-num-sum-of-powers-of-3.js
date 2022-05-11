23f879abcb8760118ee07497eb0f7e197285c23e
Alex Curran <alexcurran.va@gmail.com>
2022-05-11 09:46:50 -0500

    Working solution for check-num-sum-of-powers-of-3.js

---
+++ medium/check-num-sum-of-powers-of-3.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(log n)
+ * space: O(1)
+ * 
+ * @param {number} n
+ * @return {boolean}
+ */
+function checkPowersOfThree(n) {
+    while (n) {
+        // a remainder of 2 can't be a power of 3
+        if (n % 3 === 2) return false;
+
+        n = Math.floor(n / 3);
+    }
+
+    return true;
+}
+
+console.log( checkPowersOfThree(12) ); // true
+// console.log( checkPowersOfThree(21) ); // false
