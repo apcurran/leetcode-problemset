99c037a27a836be15a5f9b786fd7d81b6248aa49
Alex Curran <alexcurran.va@gmail.com>
2022-05-17 09:55:30 -0500

    Working solution for count-num-of-pairs-with-abs-diff-k.js

---
+++ easy/count-num-of-pairs-with-abs-diff-k.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/**
+ * solution 1 -- nested for loops
+ * time: O(n^2)
+ * space: O(1)
+ * 
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+function countKDifference(nums, k) {
+    let pairCount = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            const outerNum = nums[i];
+            const innerNum = nums[j];
+            const absDiff = Math.abs(outerNum - innerNum);
+
+            if (absDiff === k) pairCount++;
+        }
+    }
+
+    return pairCount;
+}
+
+console.log( countKDifference([1, 2, 2, 1], 1) ); // 4
