95bc616e1af98fc2d9dcb303c6592c8a141a161d
Alex Curran <alexcurran.va@gmail.com>
2022-05-17 17:08:44 -0500

    Working linear time/space solution for determine-if-str-halves-are-alike.js

---
+++ easy/determine-if-str-halves-are-alike.js
@@ -0,0 +1,46 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} str
+ * @return {boolean}
+ */
+function halvesAreAlike(str) {
+    const halfwayIndex = str.length / 2;
+    const firstHalf = str.slice(0, halfwayIndex);
+    const secondHalf = str.slice(halfwayIndex);
+    let firstHalfVowelCount = 0;
+    let secondHalfVowelCount = 0;
+
+    for (let char of firstHalf) {
+        const lowerChar = char.toLowerCase();
+
+        if (lowerChar === "a" ||
+            lowerChar === "e" ||
+            lowerChar === "i" ||
+            lowerChar === "o" ||
+            lowerChar === "u") {
+            firstHalfVowelCount++;
+        }
+    }
+
+    for (let char of secondHalf) {
+        const lowerChar = char.toLowerCase();
+
+        if (lowerChar === "a" ||
+            lowerChar === "e" ||
+            lowerChar === "i" ||
+            lowerChar === "o" ||
+            lowerChar === "u") {
+            secondHalfVowelCount++;
+        }
+    }
+
+    return firstHalfVowelCount === secondHalfVowelCount;
+}
+
+console.log(halvesAreAlike("book")); // true -> first half has 1 vowel & second half has 1 vowel
+console.log(halvesAreAlike("textbook")); // false
