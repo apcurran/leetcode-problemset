e8e6e419a17c14e1775e75eef86e5ca5f7571e82
Alex Curran <alexcurran.va@gmail.com>
2022-05-24 10:32:26 -0500

    Working solution for find-n-unique-ints-sum-to-zero.js

---
+++ easy/find-n-unique-ints-sum-to-zero.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ * solution 1 -- for loop
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number} n
+ * @return {number[]}
+ */
+function sumZero(n) {
+    let resArr = [];
+    
+    if (n % 2 !== 0) {
+        // if odd, append 0
+        resArr.push(0);
+    }
+
+    for (let i = 1; i <= n / 2; i++) {
+        resArr.push(i, -i);
+    }
+
+    return resArr;
+}
+
+console.log( sumZero(5) );
