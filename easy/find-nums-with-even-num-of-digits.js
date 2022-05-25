b9d743e59a67cc8921ebe4a36427c976e6ee3982
Alex Curran <alexcurran.va@gmail.com>
2022-05-25 10:47:40 -0500

    Working solution for find-nums-with-even-num-of-digits.js

---
+++ easy/find-nums-with-even-num-of-digits.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ * solution 1
+ * n = len of nums arr
+ * m = len of digits in each num
+ * time: O(n * m)
+ * space: O(m)
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
+function findNumbers(nums) {
+    let evenDigitAmtCounter = 0;
+
+    for (let num of nums) {
+        const numDigitsCount = [...String(num)].length;
+        const isDigitCountEven = numDigitsCount % 2 === 0;
+        
+        if (isDigitCountEven) evenDigitAmtCounter++;
+    }
+
+    return evenDigitAmtCounter;
+}
+
+console.log( findNumbers([12, 345, 2, 6, 7896]) ); // 2
