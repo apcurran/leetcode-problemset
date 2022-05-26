35dbcd95737da260f149c9097cb964050da0fe9c
Alex Curran <alexcurran.va@gmail.com>
2022-05-26 09:43:41 -0500

    array.reduce() version

--- easy/arr-partition-1.js
+++ easy/arr-partition-1.js
@@ -1,7 +1,28 @@
 "use strict";
 
+// /**
+//  * solution 1 -- iterative
+//  * time: O(n * log n)
+//  * space: O(1)
+//  * 
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// function arrayPairSum(nums) {
+//     nums.sort((a, b) => a - b);
+//     let resMax = 0;
+    
+//     for (let [index, num] of nums.entries()) {
+//         if (index % 2 === 0) {
+//             resMax += num;
+//         }
+//     }
+
+//     return resMax;
+// }
+
 /**
- * solution 1 -- iterative
+ * solution 2 -- iterative
  * time: O(n * log n)
  * space: O(1)
  * 
@@ -10,15 +31,12 @@
  */
 function arrayPairSum(nums) {
     nums.sort((a, b) => a - b);
-    let resMax = 0;
     
-    for (let [index, num] of nums.entries()) {
-        if (index % 2 === 0) {
-            resMax += num;
-        }
-    }
+    return nums.reduce((sum, currNum, index) => {
+        if (index % 2 === 0) return sum + currNum;
 
-    return resMax;
+        return sum;
+    }, 0);
 }
 
 console.log( arrayPairSum([1, 4, 3, 2]) ); // 4
