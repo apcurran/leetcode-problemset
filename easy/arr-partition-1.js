292417c6eedec822ab9884cedb296c61ac05058b
Alex Curran <alexcurran.va@gmail.com>
2022-05-26 10:25:31 -0500

    Slightly optimized third solution

--- easy/arr-partition-1.js
+++ easy/arr-partition-1.js
@@ -21,9 +21,27 @@
 //     return resMax;
 // }
 
+// /**
+//  * solution 2 -- iterative
+//  * time: O(n * log n)
+//  * space: O(1)
+//  * 
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// function arrayPairSum(nums) {
+//     nums.sort((a, b) => a - b);
+    
+//     return nums.reduce((sum, currNum, index) => {
+//         if (index % 2 === 0) return sum + currNum;
+
+//         return sum;
+//     }, 0);
+// }
+
 /**
- * solution 2 -- iterative
- * time: O(n * log n)
+ * solution 3 -- iterative
+ * time: O(n * log n) -> slightly less work with half the iterations in the lower for loop
  * space: O(1)
  * 
  * @param {number[]} nums
@@ -31,12 +49,13 @@
  */
 function arrayPairSum(nums) {
     nums.sort((a, b) => a - b);
-    
-    return nums.reduce((sum, currNum, index) => {
-        if (index % 2 === 0) return sum + currNum;
+    let max = 0;
 
-        return sum;
-    }, 0);
+    for (let i = 0; i < nums.length; i += 2) {
+        max += nums[i];
+    }
+
+    return max;
 }
 
 console.log( arrayPairSum([1, 4, 3, 2]) ); // 4
