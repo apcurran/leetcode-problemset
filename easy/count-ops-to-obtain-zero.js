6b501cf5f4e65b41544b09debdf3593ed55feec0
Alex Curran <alexcurran.va@gmail.com>
2022-05-31 16:52:26 -0500

    Working solution for count-ops-to-obtain-zero.js

---
+++ easy/count-ops-to-obtain-zero.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ * solution 1
+ * 
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+function countOperations(num1, num2) {
+    let counter = 0;
+
+    while (num1 !== 0 && num2 !== 0) {
+        if (num1 >= num2) {
+            const revisedNum1 = num1 - num2;
+            num1 = revisedNum1;
+        } else {
+            const revisedNum2 = num2 - num1;
+            num2 = revisedNum2;
+        }
+
+        counter++;
+    }
+
+    return counter;
+}
+
+console.log( countOperations(2, 3) ); // 3
+console.log( countOperations(10, 10) ); // 1
