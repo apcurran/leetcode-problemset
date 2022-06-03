e7da4c16c0a7dd5e3974d64e0eb043f39debb596
Alex Curran <alexcurran.va@gmail.com>
2022-06-03 16:56:49 -0500

    Working solution for reverse-prefix-of-word.js

---
+++ easy/reverse-prefix-of-word.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} word
+ * @param {string} ch
+ * @return {string}
+ */
+function reversePrefix(word, ch) {
+    if (!word.includes(ch)) return word;
+
+    const chFirstIndex = word.indexOf(ch);
+    const wordPrefix = word.slice(0, chFirstIndex + 1);
+    const wordEnding = word.slice(chFirstIndex + 1);
+    const reversedWordPrefix = wordPrefix.split("").reverse().join("");
+    
+    return reversedWordPrefix + wordEnding;
+}
+
+console.log( reversePrefix("abcdefd", "d") ); // "dcbaefd"
+console.log( reversePrefix("abcd", "z") ); // "abcd"
