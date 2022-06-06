1419aac7589462b2c070e42e491df29837d5c235
Alex Curran <alexcurran.va@gmail.com>
2022-06-06 09:22:52 -0500

    Working solution for partition-arr-according-to-given-pivot.js

---
+++ medium/partition-arr-according-to-given-pivot.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number[]} nums
+ * @param {number} pivot
+ * @return {number[]}
+ */
+function pivotArray(nums, pivot) {
+    const smallerThanPivotArr = nums.filter((num) => num < pivot);
+    const equalToPivotArr = nums.filter((num) => num === pivot);
+    const greaterThanPivotArr = nums.filter((num) => num > pivot);
+    
+    return [...smallerThanPivotArr, ...equalToPivotArr, ...greaterThanPivotArr];
+}
+
+console.log( pivotArray([9, 12, 5, 10, 14, 3, 10], 10) ); // [9, 5, 3, 10, 10, 12, 14]
+console.log( pivotArray([-3, 4, 3, 2], 2) ); // [-3, 2, 4, 3]
