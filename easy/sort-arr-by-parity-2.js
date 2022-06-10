0eb041361d015f2b2d5db989ed70364781577db1
Alex Curran <alexcurran.va@gmail.com>
2022-06-10 11:00:24 -0500

    Three working variations for sort-arr-by-parity-2.js

--- easy/sort-arr-by-parity-2.js
+++ easy/sort-arr-by-parity-2.js
@@ -27,8 +27,35 @@
 //     return resArr;
 // }
 
+// /**
+//  * solution 2 -- slight variation w/ loop
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {number[]} nums
+//  * @return {number[]}
+//  */
+// function sortArrayByParityII(nums) {
+//     const evenNums = nums.filter((num) => num % 2 === 0);
+//     const oddNums = nums.filter((num) => num % 2 !== 0);
+
+//     let resArr = [];
+
+//     for (let i = 0; i < nums.length; i++) {
+//         if (i % 2 === 0) {
+//             const evenNum = evenNums.pop();
+//             resArr.push(evenNum);
+//         } else {
+//             const oddNum = oddNums.pop();
+//             resArr.push(oddNum);
+//         }
+//     }
+
+//     return resArr;
+// }
+
 /**
- * solution 1
+ * solution 3
  * time: O(n)
  * space: O(n)
  * 
@@ -36,18 +63,19 @@
  * @return {number[]}
  */
 function sortArrayByParityII(nums) {
-    const evenNums = nums.filter((num) => num % 2 === 0);
-    const oddNums = nums.filter((num) => num % 2 !== 0);
-
     let resArr = [];
+    let even = 0;
+    let odd = 1;
 
     for (let i = 0; i < nums.length; i++) {
-        if (i % 2 === 0) {
-            const evenNum = evenNums.pop();
-            resArr.push(evenNum);
+        const currNum = nums[i];
+
+        if (currNum % 2 === 0) {
+            resArr[even] = currNum;
+            even += 2;
         } else {
-            const oddNum = oddNums.pop();
-            resArr.push(oddNum);
+            resArr[odd] = currNum;
+            odd += 2;
         }
     }
 
