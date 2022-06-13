63e680e28665ce65fed4affb2a4fb85e12ff26fb
Alex Curran <alexcurran.va@gmail.com>
2022-06-13 15:59:14 -0500

    Optimize iterative solution

--- easy/replace-elems-w-greatest-elem-on-right.js
+++ easy/replace-elems-w-greatest-elem-on-right.js
@@ -1,27 +1,48 @@
 "use strict";
 
+// /**
+//  * solution 1 -- iterative
+//  * time: O(n^2)
+//  * space: O(n)
+//  * 
+//  * @param {number[]} arr
+//  * @return {number[]}
+//  */
+// function replaceElements(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (i === arr.length -1) {
+//             arr[i] = -1;
+//             break;
+//         }
+
+//         const elemsToRight = arr.slice(i + 1);
+//         const greatestElemToRight = Math.max(...elemsToRight);
+//         arr[i] = greatestElemToRight;
+//     }
+
+//     return arr;
+// }
+
 /**
- * solution 1 -- iterative
- * time: O(n^2)
- * space: O(n)
+ * solution 2 -- iterative (optimized)
+ * time: O(n)
+ * space: O(1)
  * 
  * @param {number[]} arr
  * @return {number[]}
  */
 function replaceElements(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        if (i === arr.length -1) {
-            arr[i] = -1;
-            break;
-        }
+    let lastMaxNum = -1;
 
-        const elemsToRight = arr.slice(i + 1);
-        const greatestElemToRight = Math.max(...elemsToRight);
-        arr[i] = greatestElemToRight;
+    // iterate backwards from end
+    for (let i = arr.length - 1; i >= 0; i--) {
+        const currNum = arr[i];
+        arr[i] = lastMaxNum;
+        lastMaxNum = Math.max(lastMaxNum, currNum);
     }
 
     return arr;
 }
 
-console.log( replaceElements([17,18,5,4,6,1]) ); // [18,6,6,6,1,-1]
+console.log( replaceElements([17, 18, 5, 4, 6, 1]) ); // [18, 6, 6, 6, 1, -1]
 console.log( replaceElements([400]) ); // [-1]
