acc21facb8c9895376ccd8a52392ce9fa53682ce
Alex Curran <alexcurran.va@gmail.com>
2022-06-13 14:51:56 -0500

    Working solution for max-consec-ones-3.js

---
+++ medium/max-consec-ones-3.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/**
+ * solution 1 -- sliding window
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+function longestOnes(nums, k) {
+    let endPointer = 0;
+    let beginningPointer = 0;
+
+    while (endPointer < nums.length) {
+        if (nums[endPointer] === 0) k--;
+
+        // move sliding window forward to check remaining nums
+        if (k < 0) {
+            if (nums[beginningPointer] === 0) k++;
+
+            beginningPointer++;
+        }
+
+        endPointer++;
+    }
+
+    return endPointer - beginningPointer;
+}
+
+console.log( longestOnes([1,1,1,0,0,0,1,1,1,1,0], 2) ); // 6
+console.log( longestOnes([0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1], 3) ); // 10
