444f4ae83a970f40ba3fe6ecfe8d235cd887b27d
Alex Curran <alexcurran.va@gmail.com>
2022-06-16 09:41:13 -0500

    Working solution for same-tree.js

---
+++ easy/same-tree.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+
+/**
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+function isSameTree(p, q) {
+    if (p === null && q === null) return true;
+
+    if (p === null || q === null) return false;
+
+    if (p.val !== q.val) return false;
+
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+}
