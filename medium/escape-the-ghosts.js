7b3b4ab61154d7dfb8e91c9bf06c40a2b2609f1b
Alex Curran <alexcurran.va@gmail.com>
2022-06-27 10:41:01 -0500

    Working solution for escape-the-ghosts.js

---
+++ medium/escape-the-ghosts.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number[][]} ghosts
+ * @param {number[]} target
+ * @return {boolean}
+ */
+function escapeGhosts(ghosts, target) {
+    const [targetX, targetY] = target;
+    const playerDistance = Math.abs(targetX) + Math.abs(targetY);
+
+    for (let ghost of ghosts) {
+        const [ghostX, ghostY] = ghost;
+        const ghostDistance = Math.abs(ghostX - targetX) + Math.abs(ghostY - targetY);
+
+        if (ghostDistance <= playerDistance) return false;
+    }
+
+    return true;
+}
