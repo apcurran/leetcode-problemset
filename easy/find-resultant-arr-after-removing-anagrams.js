181b82ea13326e77319675ed18d3073ae9a1dd08
Alex Curran <alexcurran.va@gmail.com>
2022-06-30 11:08:07 -0500

    Working solution for find-resultant-arr-after-removing-anagrams.js

---
+++ easy/find-resultant-arr-after-removing-anagrams.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ * solution 1
+ * n = arr len
+ * m = word len
+ * time: O(n * (m * log m))
+ * space: O(n)
+ * 
+ * @param {string[]} words
+ * @return {string[]}
+ */
+function removeAnagrams(words) {
+    let resArr = [];
+    let prevSortedWord = "";
+
+    for (let word of words) {
+        const currSortedWord = word.split("").sort().join("");
+
+        if (currSortedWord === prevSortedWord) continue;
+
+        prevSortedWord = currSortedWord;
+        resArr.push(word);
+    }
+
+    return resArr;
+}
+
+console.log( removeAnagrams(["abba","baba","bbaa","cd","cd"]) ); // ["abba","cd"]
