f9636c3bc3304498825f4529407e143ef0c6f436
Alex Curran <alexcurran.va@gmail.com>
2022-07-08 16:39:45 -0500

    Working recursive solution for keep-mult-found-vals-by-two.js

---
+++ easy/keep-mult-found-vals-by-two.js
@@ -0,0 +1,40 @@
+"use strict";
+
+// /**
+//  * solution -- recursive
+//  * time: O(n^2)
+//  * space: O(n) -- due to call stack
+//  * 
+//  * @param {number[]} nums
+//  * @param {number} original
+//  * @return {number}
+//  */
+// function findFinalValue(nums, original) {
+//     for (let num of nums) {
+//         if (num !== original) continue;
+
+//         const newNum = num * 2;
+//         return findFinalValue(nums, newNum);
+//     }
+
+//     return original;
+// }
+
+/**
+ * solution -- recursive (cleaned up)
+ * time: O(n^2)
+ * space: O(n) -- due to call stack
+ * 
+ * @param {number[]} nums
+ * @param {number} original
+ * @return {number}
+ */
+function findFinalValue(nums, original) {
+    if (!nums.includes(original)) return original;
+
+    return findFinalValue(nums, original * 2);
+}
+
+console.log( findFinalValue([2, 7, 9], 4) ); // 4
+console.log( findFinalValue([5, 3, 6, 1, 12], 3) ); // 24
+console.log( findFinalValue([8, 19, 4, 2, 15, 3], 2) ); // 16
