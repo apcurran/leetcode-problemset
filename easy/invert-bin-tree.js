d7b6fffcc433481112ba1b71ab3c4c68a31d252d
Alex Curran <alexcurran.va@gmail.com>
2022-08-02 13:32:09 -0500

    Working recursive and BFS versions

--- easy/invert-bin-tree.js
+++ easy/invert-bin-tree.js
@@ -9,8 +9,57 @@
  * }
  */
 
+// /**
+//  * solution 1 -- DFS stack
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {TreeNode} root
+//  * @return {TreeNode}
+//  */
+// function invertTree(root) {
+//     if (root === null) return null;
+
+//     let stack = [root];
+
+//     while (stack.length > 0) {
+//         const currentNode = stack.pop();
+
+//         // destructured swap
+//         [currentNode.left, currentNode.right] = [currentNode.right, currentNode.left];
+
+//         if (currentNode.left !== null) stack.push(currentNode.left);
+
+//         if (currentNode.right !== null) stack.push(currentNode.right);
+//     }
+
+//     return root;
+// }
+
+// /**
+//  * solution 2 -- Recursive DFS stack
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {TreeNode} root
+//  * @return {TreeNode}
+//  */
+// function invertTree(root) {
+//     // base case
+//     if (root === null) return null;
+
+//     // recursive cases
+//     let left = invertTree(root.left);
+//     let right = invertTree(root.right);
+//     // swap
+//     root.left = right;
+//     root.right = left;
+
+//     return root;
+// }
+
 /**
- * solution1 -- DFS stack
+ * solution 3 -- BFS queue
  * time: O(n)
  * space: O(n)
  * 
@@ -18,19 +67,20 @@
  * @return {TreeNode}
  */
 function invertTree(root) {
+    // base case
     if (root === null) return null;
 
-    let stack = [root];
+    let queue = [root];
 
-    while (stack.length > 0) {
-        const currentNode = stack.pop();
+    while (queue.length > 0) {
+        const currNode = queue.shift();
 
-        // destructured swap
-        [currentNode.left, currentNode.right] = [currentNode.right, currentNode.left];
+        // destructure swap
+        [currNode.left, currNode.right] = [currNode.right, currNode.left];
 
-        if (currentNode.left !== null) stack.push(currentNode.left);
+        if (currNode.left) queue.push(currNode.left);
 
-        if (currentNode.right !== null) stack.push(currentNode.right);
+        if (currNode.right) queue.push(currNode.right);
     }
 
     return root;
