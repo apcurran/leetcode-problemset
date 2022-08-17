0b5b41da32ee641b9706c7a9137fa8dc59d1bff0
Alex Curran <alexcurran.va@gmail.com>
2022-08-17 09:51:23 -0500

    Update

--- easy/sql/customer-placing-largest-num-of-orders.sql
+++ easy/sql/customer-placing-largest-num-of-orders.sql
@@ -1,9 +1,5 @@
--- SELECT customer_number
--- FROM Orders
--- GROUP BY customer_number
--- ORDER BY COUNT(*) DESC
--- LIMIT 1;
-
-SELECT MAX(COUNT(*)) AS customer_number
+SELECT customer_number
 FROM Orders
-GROUP BY customer_number;
+GROUP BY customer_number
+ORDER BY COUNT(*) DESC
+LIMIT 1;
