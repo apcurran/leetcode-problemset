22f8bf634ffdc177f17d183ed66cdf011d247fdf
Alex Curran <alexcurran.va@gmail.com>
2022-08-24 10:35:51 -0500

    Working solution for power-of-three.js

---
+++ easy/power-of-three.js
@@ -0,0 +1,41 @@
+"use strict";
+
+// /**
+//  * solution 1 -- iterative
+//  * b = 3
+//  * time: O(logb * n)
+//  * space: O(1)
+//  * 
+//  * @param {number} n
+//  * @return {boolean}
+//  */
+// function isPowerOfThree(n) {
+//     if (n < 1) return false;
+
+//     while (n % 3 === 0) {
+//         n /= 3;
+//     }
+
+//     return n === 1;
+// }
+
+/**
+ * solution 2 -- recursive
+ * b = 3
+ * time: O(logb * n)
+ * space: O(logb * n)
+ * 
+ * @param {number} n
+ * @return {boolean}
+ */
+function isPowerOfThree(n) {
+    if (n === 1) return true;
+
+    if (n < 1) return false;
+
+    return isPowerOfThree(n / 3);
+}
+
+console.log( isPowerOfThree(27) ); // true
+console.log( isPowerOfThree(0) ); // false
+console.log( isPowerOfThree(9) ); // true
