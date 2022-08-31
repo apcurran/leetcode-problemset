d71dc5b2c1718f273d4206f6efd5000ea311c576
Alex Curran <alexcurran.va@gmail.com>
2022-08-31 10:32:11 -0500

    Add time/space complex

--- easy/top-interview-qs/strings/valid-palindrome.js
+++ easy/top-interview-qs/strings/valid-palindrome.js
@@ -1,6 +1,10 @@
 "use strict";
 
 /**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
  * A phrase is a palindrome if,
  * after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters,
  * it reads the same forward and backward.
