8ec9731d252131ba26cad2e92fa38c2e9ae885a1
Alex Curran <alexcurran.va@gmail.com>
2022-09-21 11:00:47 -0500

    Add cache to func params with a default val

--- easy/top-interview-qs/dynamic-programming/climbing-stairs.js
+++ easy/top-interview-qs/dynamic-programming/climbing-stairs.js
@@ -4,9 +4,7 @@
  * @param {number} n
  * @return {number}
  */
-let cache = new Map();
-
-function climbStairs(n) {
+function climbStairs(n, cache = new Map()) {
     if (n === 1) {
         return 1;
     } else if (n === 2) {
@@ -17,7 +15,7 @@ function climbStairs(n) {
 
     if (cache.has(n)) return cache.get(n);
 
-    result = climbStairs(n - 1) + climbStairs(n - 2);
+    result = climbStairs(n - 1, cache) + climbStairs(n - 2, cache);
     cache.set(n, result);
 
     return result;
