0d483efd467c2a3a09981c6acbd98cd1f70f0f92
Alex Curran <alexcurran.va@gmail.com>
2022-09-21 11:07:08 -0500

    Working iterative solution with O(n) time and O(1) space

--- easy/top-interview-qs/dynamic-programming/climbing-stairs.js
+++ easy/top-interview-qs/dynamic-programming/climbing-stairs.js
@@ -1,26 +1,47 @@
 "use strict";
 
+// /**
+//  * solution 1 -- recursion with cache (memo)
+//  * 
+//  * @param {number} n
+//  * @return {number}
+//  */
+// function climbStairs(n, cache = new Map()) {
+//     if (n === 1) {
+//         return 1;
+//     } else if (n === 2) {
+//         return 2;
+//     }
+
+//     let result = 0;
+
+//     if (cache.has(n)) return cache.get(n);
+
+//     result = climbStairs(n - 1, cache) + climbStairs(n - 2, cache);
+//     cache.set(n, result);
+
+//     return result;
+// }
+
 /**
- * solution 1 -- recursion with cache (memo)
+ * solution 2 -- iterative DP bottom-up
+ * time: O(n)
+ * space: O(1)
  * 
  * @param {number} n
  * @return {number}
  */
-function climbStairs(n, cache = new Map()) {
-    if (n === 1) {
-        return 1;
-    } else if (n === 2) {
-        return 2;
+function climbStairs(n) {
+    let a = 1;
+    let b = 1;
+
+    for (let i = 2; i <= n; i++) {
+        const temp = a;
+        a = a + b;
+        b = temp;
     }
 
-    let result = 0;
-
-    if (cache.has(n)) return cache.get(n);
-
-    result = climbStairs(n - 1, cache) + climbStairs(n - 2, cache);
-    cache.set(n, result);
-
-    return result;
+    return a;
 }
 
 console.log(climbStairs(2)); // 2
