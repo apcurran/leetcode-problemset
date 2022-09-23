b81bffc1d8a4dc5a043f2782feed3c639de7f8f8
Alex Curran <alexcurran.va@gmail.com>
2022-09-23 16:22:20 -0500

    Working solution for jump-game.js

---
+++ medium/jump-game.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+function canJump(nums) {
+    let goal = nums.length - 1;
+
+    for (let i = nums.length - 1; i >= 0; i--) {
+        const currNum = nums[i];
+
+        if (i + currNum >= goal) {
+            goal = i;
+        }
+    }
+
+    return goal === 0;
+}
+
+console.log( canJump([2, 3, 1, 1, 4]) ); // true
+console.log( canJump([3, 2, 1, 0, 4]) ); // false
