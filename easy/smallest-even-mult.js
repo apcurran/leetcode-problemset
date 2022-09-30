6fcaf3b6cb8870d4b177bd9847aa72286cf73a0a
Alex Curran <alexcurran.va@gmail.com>
2022-09-30 10:46:29 -0500

    Math formula solution

--- easy/smallest-even-mult.js
+++ easy/smallest-even-mult.js
@@ -18,18 +18,33 @@
 //     return twoNum;
 // }
 
+// /**
+//  * solution 2 -- recursive
+//  * time: O(n)
+//  * space: O(n) -- due to call stack mem
+//  * 
+//  * @param {number} n
+//  * @return {number}
+//  */
+// function smallestEvenMultiple(n, twoCounter = 2) {
+//     if (twoCounter % n === 0) return twoCounter;
+    
+//     return smallestEvenMultiple(n, twoCounter += 2);
+// }
+
 /**
- * solution 1 -- recursive
- * time: O(n)
- * space: O(n) -- due to call stack mem
+ * solution 3 -- math formula
+ * time: O(1)
+ * space: O(1)
  * 
  * @param {number} n
  * @return {number}
  */
-function smallestEvenMultiple(n, twoCounter = 2) {
-    if (twoCounter % n === 0) return twoCounter;
-    
-    return smallestEvenMultiple(n, twoCounter += 2);
+function smallestEvenMultiple(n) {
+    // if num is divisible by two (even number) then n is the first even multiple
+    if (n % 2 === 0) return n;
+    // if n is odd, then the sum of two odd nums is always even, which is therefore divisible by two
+    return n * 2;
 }
 
 console.log( smallestEvenMultiple(2) ); // 2
