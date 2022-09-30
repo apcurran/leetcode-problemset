0c7b48138b3489cff960e3912a910f8fe0373983
Alex Curran <alexcurran.va@gmail.com>
2022-09-30 14:42:27 -0500

    Working solution for largest-local-vals-in-a-mat.js

---
+++ easy/largest-local-vals-in-a-mat.js
@@ -0,0 +1,45 @@
+"use strict";
+
+/**
+ * @param {number[][]} grid
+ * @return {number[][]}
+ */
+function largestLocal(grid) {
+    let resArr = [];
+
+    for (let r = 1; r < grid.length - 1; r++) {
+        let resRow = [];
+        // For each 3 x 3 window,
+        // iterate through the values to get the maximum value within the window.
+        for (let c = 1; c < grid[r].length - 1; c++) {
+            const localMax = Math.max(
+                grid[r - 1][c - 1], // upper-left
+                grid[r - 1][c],
+                grid[r - 1][c + 1],
+
+                grid[r][c - 1],
+                grid[r][c],
+                grid[r][c + 1],
+
+                grid[r + 1][c - 1],
+                grid[r + 1][c],
+                grid[r + 1][c + 1],
+            );
+
+            resRow.push(localMax);
+        }
+
+        resArr.push(resRow);
+    }
+
+    return resArr;
+}
+
+console.log( largestLocal(
+    [
+        [9, 9, 8, 1],
+        [5, 6, 2, 6],
+        [8, 2, 6, 4],
+        [6, 2, 2, 2]
+    ]
+) );
