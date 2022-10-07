6a79f36d99d7a2f9c230843dd4c61c9f36e03b36
Alex Curran <alexcurran.va@gmail.com>
2022-10-07 16:03:53 -0500

    Recursive solution

--- easy/remove-linked-list-elems.js
+++ easy/remove-linked-list-elems.js
@@ -1,41 +1,62 @@
 "use strict";
 
-
 // Definition for singly-linked list.
 function ListNode(val, next) {
     this.val = (val===undefined ? 0 : val);
     this.next = (next===undefined ? null : next);
 }
 
+// /**
+//  * solution 1 -- iterative
+//  * time: O(n)
+//  * space: O(1)
+//  * 
+//  * @param {ListNode} head
+//  * @param {number} targetVal
+//  * @return {ListNode}
+//  */
+// function removeElements(head, targetVal) {
+//     // next ptr is pointing to head
+//     let dummy = new ListNode(0, head);
+//     let prev = dummy;
+//     let curr = head;
+
+//     while (curr !== null) {
+//         const next = curr.next;
+
+//         if (curr.val === targetVal) {
+//             // skip over to delete node from list
+//             prev.next = next;
+//         } else {
+//             // shift to right
+//             prev = curr;
+//         }
+
+//         // always traverse to the next node
+//         curr = next;
+//     }
+
+//     return dummy.next;
+// }
+
 /**
- * solution 1 -- iterative
+ * solution 2 -- recursive
  * time: O(n)
- * space: O(1)
+ * space: O(n)
  * 
  * @param {ListNode} head
  * @param {number} targetVal
  * @return {ListNode}
  */
 function removeElements(head, targetVal) {
-    // next ptr is pointing to head
-    let dummy = new ListNode(0, head);
-    let prev = dummy;
-    let curr = head;
+    // base case 1
+    if (head === null) return head;
 
-    while (curr !== null) {
-        const next = curr.next;
+    // recursive case
+    // traverse to next node
+    head.next = removeElements(head.next, targetVal);
 
-        if (curr.val === targetVal) {
-            // skip over to delete node from list
-            prev.next = next;
-        } else {
-            // shift to right
-            prev = curr;
-        }
-
-        // always traverse to the next node
-        curr = next;
-    }
-
-    return dummy.next;
+    // base case 2
+    // if curr val is equal to targetVal, skip node (thereby deleting it)
+    return head.val === targetVal ? head.next : head;
 }
