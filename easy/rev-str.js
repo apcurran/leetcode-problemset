938025d51b231d06872e3d959b2e503cd741f573
Alex Curran <alexcurran.va@gmail.com>
2022-10-12 14:25:07 -0500

    Working O(1) mem solution

---
+++ easy/rev-str.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/**
+ * solution 1 -- two-pointer technique
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {string[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+function reverseString(s) {
+    let leftPointer = 0;
+    let rightPointer = s.length - 1;
+    // 0 -> 4
+    // 1 -> 3
+    // 2 -> 2
+    // 3 -> 1
+    // 4 -> 0
+    while (leftPointer < rightPointer) {
+        const temp = s[leftPointer];
+        // left char
+        s[leftPointer] = s[rightPointer];
+        // right char
+        s[rightPointer] = temp;
+
+        // move pointers
+        leftPointer++;
+        rightPointer--;
+    }
+}
+
+console.log( reverseString(["h","e","l","l","o"]) ); // ["o","l","l","e","h"]
