6d599468facfcf70b86d8abfb89c7c8e70bd3726
Alex Curran <alexcurran.va@gmail.com>
2022-10-18 14:48:23 -0500

    Optimal O(log n) bin search solution

--- easy/valid-perfect-square.js
+++ easy/valid-perfect-square.js
@@ -33,28 +33,58 @@
 //     return num === 0;
 // }
 
+// /**
+//  * solution 3 -- squaring solution
+//  * time: O(sqrt(n))
+//  * space: O(1)
+//  * 
+//  * @param {number} num
+//  * @return {boolean}
+//  */
+//  function isPerfectSquare(num) {
+//     for (let i = 1; i <= num; i++) {
+//         const squaredNum = i * i;
+
+//         if (squaredNum === num) {
+//             return true;
+//         }
+
+//         if (squaredNum > num) {
+//             return false;
+//         }
+//     }
+// }
+
 /**
- * solution 3 -- squaring solution
- * time: O(sqrt(n))
+ * solution 4 -- 
+ * time: O(log n)
  * space: O(1)
  * 
  * @param {number} num
  * @return {boolean}
  */
- function isPerfectSquare(num) {
-    for (let i = 1; i <= num; i++) {
-        const squaredNum = i * i;
+function isPerfectSquare(num) {
+    let left = 1;
+    let right = num;
 
-        if (squaredNum === num) {
+    while (left <= right) {
+        const mid = Math.floor(left + (right - left) / 2);
+        const midSquared = mid * mid;
+
+        if (midSquared > num) {
+            // search left half
+            right = mid - 1;
+        } else if (midSquared < num) {
+            // search right half
+            left = mid + 1;
+        } else {
+            // equal to num
             return true;
         }
-
-        if (squaredNum > num) {
-            return false;
-        }
     }
-}
 
+    return false;
+}
 
 console.log( isPerfectSquare(16) ); // true
 console.log( isPerfectSquare(14) ); // false
