9b7ead6e521a132dbbbddbf1dfe2289213674520
Alex Curran <alexcurran.va@gmail.com>
2022-10-18 14:04:05 -0500

    Working math solution O(sqrt(n)) time

--- easy/valid-perfect-square.js
+++ easy/valid-perfect-square.js
@@ -1,17 +1,36 @@
 "use strict";
 
+// /**
+//  * solution 1 -- using built-in sqrt func (not meeting problem constraints)
+//  * time: O(1)
+//  * space: O(1)
+//  * 
+//  * @param {number} num
+//  * @return {boolean}
+//  */
+// function isPerfectSquare(num) {
+//     const sqrtNum = Math.sqrt(num);
+    
+//     return Number.isInteger(sqrtNum);
+// }
+
 /**
- * solution 1 -- using built-in sqrt func (not meeting problem constraints)
- * time: O(1)
+ * solution 2 -- valid math solution
+ * time: O(sqrt(n))
  * space: O(1)
  * 
  * @param {number} num
  * @return {boolean}
  */
 function isPerfectSquare(num) {
-    const sqrtNum = Math.sqrt(num);
-    
-    return Number.isInteger(sqrtNum);
+    let i = 1;
+
+    while (num > 0) {
+        num -= i;
+        i += 2;
+    }
+
+    return num === 0;
 }
 
 console.log( isPerfectSquare(16) ); // true
