14c9598601014e511f4ca67340d9a4856f870b41
Alex Curran <alexcurran.va@gmail.com>
2022-10-18 13:51:16 -0500

    Add time/space complexity

--- easy/valid-perfect-square.js
+++ easy/valid-perfect-square.js
@@ -1,14 +1,17 @@
 "use strict";
 
 /**
- * O(1)
+ * solution 1 -- using built-in sqrt func (not meeting problem constraints)
+ * time: O(1)
+ * space: O(1)
+ * 
  * @param {number} num
  * @return {boolean}
  */
 function isPerfectSquare(num) {
     const sqrtNum = Math.sqrt(num);
     
-    return sqrtNum % 1 === 0 ? true : false;
+    return Number.isInteger(sqrtNum);
 }
 
 console.log( isPerfectSquare(16) ); // true
