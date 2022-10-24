7b0d3cad72ef70b2bf4b27e786c296a7ff7da668
Alex Curran <alexcurran.va@gmail.com>
2022-10-24 16:50:12 -0500

    Working solution for boats-to-save-people.js

---
+++ medium/boats-to-save-people.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/**
+ * solution 1 -- two-pointer technique
+ * time: O(n * log n)
+ * space: O(1)
+ * 
+ * @param {number[]} people
+ * @param {number} limit
+ * @return {number}
+ */
+function numRescueBoats(people, limit) {
+    // sort input arr
+    people.sort((a, b) => a - b);
+
+    let boatsCount = 0;
+    // use two pointer technique
+    let leftPointer = 0;
+    let rightPointer = people.length - 1;
+
+    while (leftPointer <= rightPointer) {
+        // sub heaviest person from limit
+        const remainingWeight = limit - people[rightPointer];
+        rightPointer--;
+        boatsCount++;
+
+        // there is a person left and enough weight to hold the person
+        if (leftPointer <= rightPointer && remainingWeight >= people[leftPointer]) {
+            leftPointer++;
+        }
+    }
+
+    return boatsCount;
+}
