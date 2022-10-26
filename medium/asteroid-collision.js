0599cce27f38ce195a1c40ac1aa5334833e8d6ff
Alex Curran <alexcurran.va@gmail.com>
2022-10-26 14:43:38 -0500

    Alt conditional logic

--- medium/asteroid-collision.js
+++ medium/asteroid-collision.js
@@ -1,7 +1,38 @@
 "use strict";
 
+// /**
+//  * solution 1 -- stack
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {number[]} asteroids
+//  * @return {number[]}
+//  */
+// function asteroidCollision(asteroids) {
+//     let stack = [];
+
+//     for (let i = 0; i < asteroids.length; i++) {
+//         const stackTop = stack[stack.length - 1];
+//         const curr = asteroids[i];
+
+//         if (stack.length === 0 || stackTop < 0 || curr > 0) {
+//             // stack is empty, and last/curr are not colliding
+//             stack.push(curr);
+//         } else if (stackTop + curr === 0) {
+//             // last and curr collide -> both destroyed
+//             stack.pop();
+//         } else if (Math.abs(stackTop) < Math.abs(curr)) {
+//             // destroy smaller stackTop asteroid
+//             stack.pop();
+//             i--;
+//         }
+//     }
+
+//     return stack;
+// }
+
 /**
- * solution 1 -- stack
+ * solution 2 -- stack (slightly more readable)
  * time: O(n)
  * space: O(n)
  * 
@@ -12,19 +43,21 @@ function asteroidCollision(asteroids) {
     let stack = [];
 
     for (let i = 0; i < asteroids.length; i++) {
-        const stackTop = stack[stack.length - 1];
+        const last = stack[stack.length - 1];
         const curr = asteroids[i];
 
-        if (stack.length === 0 || stackTop < 0 || curr > 0) {
-            // stack is empty, and last/curr are not colliding
+        if (last > 0 && curr < 0) {
+            // collision
+            if (last + curr === 0) {
+                // destroy both asteroids
+                stack.pop();
+            } else if (Math.abs(last) < Math.abs(curr)) {
+                stack.pop();
+                i--;
+            }
+        } else {
+            // no collision
             stack.push(curr);
-        } else if (stackTop + curr === 0) {
-            // last and curr collide -> both destroyed
-            stack.pop();
-        } else if (Math.abs(stackTop) < Math.abs(curr)) {
-            // destroy smaller stackTop asteroid
-            stack.pop();
-            i--;
         }
     }
 
