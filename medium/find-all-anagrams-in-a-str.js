80a242d6011a9a305b388fdf92c5330da40c67c7
Alex Curran <alexcurran.va@gmail.com>
2022-11-01 11:41:50 -0500

    Correct, but time limit exceeding solution for find-all-anagrams-in-a-str.js

---
+++ medium/find-all-anagrams-in-a-str.js
@@ -0,0 +1,53 @@
+"use strict";
+
+/**
+ * solution 1 -- map cache (correct, but TLE)
+ * time: O(s * p)
+ * space: O(s * p)
+ * 
+ * @param {string} s
+ * @param {string} p
+ * @return {number[]}
+ */
+function findAnagrams(s, p) {
+    let anagramIndices = [];
+    let pMap = new Map();
+
+    for (let char of p) {
+        const prevFreq = pMap.get(char) || 0;
+        pMap.set(char, prevFreq + 1);
+    }
+
+    sLoop:
+    for (let i = 0; i < s.length - p.length + 1; i++) {
+        // for each char in s string
+        // console.log("index:", i);
+        // console.log("p str len:", i + p.length);
+        
+        const sSlice = s.slice(i, i + p.length);
+        // console.log(sSlice);
+        // check if sSlice is an anagram of p str
+        let sSliceMap = new Map();
+
+        for (let char of sSlice) {
+            const prevFreq = sSliceMap.get(char) || 0;
+            sSliceMap.set(char, prevFreq + 1);
+        }
+
+        for (let [sChar, sCharFreq] of sSliceMap) {
+            // not the same letter
+            if (!pMap.has(sChar)) continue sLoop;
+
+            const pCharFreq = pMap.get(sChar);
+            // not the same letter frequency counts
+            if (sCharFreq !== pCharFreq) continue sLoop;
+        }
+        // if we make it here, this is an anagram slice
+        anagramIndices.push(i);
+    }
+
+    return anagramIndices;
+}
+
+console.log( findAnagrams("cbaebabacd", "abc") ); // [0, 6]
+console.log( findAnagrams("abab", "ab") ); // [0, 1, 2]
