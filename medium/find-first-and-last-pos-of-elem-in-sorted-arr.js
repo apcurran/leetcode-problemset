47cf9ecde3e35035bc8d5728a1d19f094af6f480
Alex Curran <alexcurran.va@gmail.com>
2022-11-04 17:04:01 -0500

    Slightly optimize O(n) solution

--- medium/find-first-and-last-pos-of-elem-in-sorted-arr.js
+++ medium/find-first-and-last-pos-of-elem-in-sorted-arr.js
@@ -12,29 +12,38 @@
 function searchRange(nums, target) {
     let resArr = [-1, -1];
 
-    if (!nums.includes(target)) return resArr;
-
-    // left index
     for (let i = 0; i < nums.length; i++) {
         const currNum = nums[i];
-
-        if (currNum === target) {
+        
+        // left index
+        if (currNum === target && resArr[0] === -1) {
             resArr[0] = i;
-            break;
         }
-    }
 
-    // right index
-    for (let i = nums.length - 1; i >= 0; i--) {
-        const currNum = nums[i];
-
-        if (currNum === target) {
+        // right index
+        if (currNum === target && resArr[0] !== -1) {
             resArr[1] = i;
-            break;
         }
     }
 
     return resArr;
 }
+
+// /**
+//  * solution 2 -- binary search
+//  * time: O(log n)
+//  * space: O(1)
+//  * 
+//  * @param {number[]} nums arr sorted in non-decreasing order
+//  * @param {number} target
+//  * @return {number[]}
+//  */
+// function searchRange(nums, target) {
+//     let resArr = [-1, -1];
+
+
+
+//     return resArr;
+// }
 
 console.log( searchRange([5,7,7,8,8,10], 8) ); // [3, 4]
