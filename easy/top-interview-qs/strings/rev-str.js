2628a8cebcd2b06b16afa5aca9aefc4f571c17ee
Alex Curran <alexcurran.va@gmail.com>
2022-11-15 15:10:34 -0600

    Working solution with O(n) time and O(1) space

---
+++ easy/top-interview-qs/strings/rev-str.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ * solution 1 -- two-pointer technique
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {string[]} str
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+function reverseString(str) {
+    let leftPtr = 0;
+    let rightPtr = str.length - 1;
+
+    while (leftPtr < rightPtr) {
+        // save refs
+        const leftChar = str[leftPtr];
+        const rightChar = str[rightPtr];
+        // swap vals
+        str[leftPtr] = rightChar;
+        str[rightPtr] = leftChar;
+        // move pointers
+        leftPtr++;
+        rightPtr--;
+    }
+}
+
+console.log( reverseString(["h","e","l","l","o"]) ); // ["o","l","l","e","h"]
+console.log( reverseString(["H","a","n","n","a","h"]) ); // ["h","a","n","n","a","H"]
