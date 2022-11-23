1c839f812a5287e4ccbfda97e0bb568663fbf3fc
Alex Curran <alexcurran.va@gmail.com>
2022-11-23 09:58:12 -0600

    Working solution for group-anagrams.js

---
+++ medium/top-interview-qs/array-and-strs/group-anagrams.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n * (k * log k))
+ * space: O(n * k)
+ * 
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+function groupAnagrams(strs) {
+    let anagramsMap = new Map();
+
+    for (let str of strs) {
+        // key for map val
+        const sortedStr = str
+                            .split("")
+                            .sort()
+                            .join("");
+        // map val
+        const anagramGroup = anagramsMap.get(sortedStr) || [];
+        // add curr anagram to str anagram list
+        anagramGroup.push(str);
+        // set new val
+        anagramsMap.set(sortedStr, anagramGroup);
+    }
+
+    return [...anagramsMap.values()];
+}
+
+console.log( groupAnagrams(["eat","tea","tan","ate","nat","bat"]) );
+// [["bat"],["nat","tan"],["ate","eat","tea"]]
