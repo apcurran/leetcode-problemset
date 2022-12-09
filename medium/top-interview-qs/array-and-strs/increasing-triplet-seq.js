3e65c2e10bdd906c23728a2d5c5f9bfc0b90f8d5
Alex Curran <alexcurran.va@gmail.com>
2022-12-09 10:46:50 -0600

    Working O(n) time solution

--- medium/top-interview-qs/array-and-strs/increasing-triplet-seq.js
+++ medium/top-interview-qs/array-and-strs/increasing-triplet-seq.js
@@ -1,27 +1,53 @@
 "use strict";
 
+// /**
+//  * solution 1 -- correct, but TLE
+//  * time: O(n^3)
+//  * space: O(1)
+//  * 
+//  * @param {number[]} nums
+//  * @return {boolean}
+//  */
+// function increasingTriplet(nums) {
+//     // indices DO NOT have to be near each other (touching)
+//     // but order must be maintained (no re-sorting)
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             for (let k = j + 1; k < nums.length; k++) {
+//                 const firstNum = nums[i];
+//                 const secondNum = nums[j];
+//                 const thirdNum = nums[k];
+
+//                 if (firstNum < secondNum && secondNum < thirdNum) {
+//                     return true;
+//                 }
+//             }
+//         }
+//     }
+
+//     return false;
+// }
+
 /**
- * solution 1 -- correct, but TLE
- * time: O(n^3)
+ * solution 2
+ * time: O(n)
  * space: O(1)
  * 
  * @param {number[]} nums
  * @return {boolean}
  */
 function increasingTriplet(nums) {
-    // indices DO NOT have to be near each other (touching)
-    // but order must be maintained (no re-sorting)
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            for (let k = j + 1; k < nums.length; k++) {
-                const firstNum = nums[i];
-                const secondNum = nums[j];
-                const thirdNum = nums[k];
+    let firstNumber = Infinity;
+    let secondNumber = Infinity;
 
-                if (firstNum < secondNum && secondNum < thirdNum) {
-                    return true;
-                }
-            }
+    for (let thirdNumber of nums) {
+        if (thirdNumber > secondNumber && thirdNumber > firstNumber) {
+            return true;
+        } else if (thirdNumber > firstNumber) {
+            secondNumber = thirdNumber;
+        } else {
+            // current number is smaller than both numbers -> set first num to current num
+            firstNumber = thirdNumber;
         }
     }
 
