5bbba6da5a183ab1caa015b2c06ec889da1b31cf
Alex Curran <alexcurran.va@gmail.com>
2022-12-22 17:03:01 -0600

    Working solution for gen-parens.js

---
+++ medium/gen-parens.js
@@ -0,0 +1,36 @@
+"use strict";
+
+/**
+ * solution 1 -- recursion
+ * 
+ * @param {number} n
+ * @return {string[]}
+ */
+function generateParenthesis(n) {
+    let results = [];
+
+    function recurse(openN, closedN, parenthesisStr) {
+        // only add if openN is greater -- this prevents invalid paren combos
+        if (openN > closedN) return;
+
+        if (openN === 0 && closedN === 0) {
+            results.push(parenthesisStr);
+
+            return;
+        }
+
+        if (openN > 0) {
+            recurse(openN - 1, closedN, parenthesisStr + "(");
+        }
+
+        if (closedN > 0) {
+            recurse(openN, closedN - 1, parenthesisStr + ")");
+        }
+    }
+
+    recurse(n, n, "");
+
+    return results;
+}
+
+console.log( generateParenthesis(3) ); // ["((()))","(()())","(())()","()(())","()()()"]
