727e60afe302620176207664f4f6001960ca5cb6
Alex Curran <alexcurran.va@gmail.com>
2022-12-28 12:08:43 -0600

    Working solution for copy-list-with-random-pointer.js

---
+++ medium/copy-list-with-random-pointer.js
@@ -0,0 +1,44 @@
+"use strict";
+
+function Node(val, next, random) {
+    this.val = val;
+    this.next = next;
+    this.random = random;
+}
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {Node} head
+ * @return {Node}
+ */
+function copyRandomList(head) {
+    // set null val to map to null
+    let nodesCache = new Map([
+        [null, null]
+    ]);
+    let current = head;
+
+    while (current !== null) {
+        const nodeCopy = new Node(current.val);
+        nodesCache.set(current, nodeCopy);
+        // move pointer
+        current = current.next;
+    }
+
+    // reset curr to beginning of the list
+    current = head;
+    // second pass
+    while (current !== null) {
+        const nodeCopy = nodesCache.get(current);
+        nodeCopy.next = nodesCache.get(current.next);
+        nodeCopy.random = nodesCache.get(current.random);
+        // move pointer
+        current = current.next;
+    }
+
+    // return head of copied list
+    return nodesCache.get(head);
+}
