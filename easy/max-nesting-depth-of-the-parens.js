fa19a162b8b8be91daf2d35b99a917b8b4e8bb1c
Alex Curran <alexcurran.va@gmail.com>
2023-01-03 15:47:13 -0600

    Stack-based solution for max-nesting-depth-of-the-parens.js

---
+++ easy/max-nesting-depth-of-the-parens.js
@@ -0,0 +1,42 @@
+"use strict";
+
+/**
+ * solution 1 -- stack
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} str
+ * @return {number}
+ */
+function maxDepth(str) {
+    let openParensStack = [];
+    let maxNestingCount = 0;
+    // iterate string chars
+    for (let character of str) {
+        // check for open paren -> add to stack AND
+        if (character === "(") {
+            // push on to stack
+            openParensStack.push("(");
+            // peek at last elem on top of the stack -- if it is a ( open paren
+            const topOfStack = openParensStack[openParensStack.length - 1];
+
+            if (topOfStack === "(") {
+                // then increment counter
+                maxNestingCount = Math.max(maxNestingCount, openParensStack.length);
+            }
+        }
+        
+        if (character === ")") {
+            // check for closing paren -> pop off stack
+            openParensStack.pop();
+            // skip addition of current paren to stack
+        }
+    }
+
+    // return nesting depth counter
+    return maxNestingCount;
+}
+
+console.log(maxDepth("(1+(2*3)+((8)/4))+1")); // 3
+console.log(maxDepth("()()")); // 1
+console.log(maxDepth("()(()())")); // 2
