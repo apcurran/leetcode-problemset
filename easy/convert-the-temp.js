e0fbd1638cc3079ba5e3b45f0f1b461905f209b7
Alex Curran <alexcurran.va@gmail.com>
2023-01-03 12:22:18 -0600

    Working solution for convert-the-temp.js

---
+++ easy/convert-the-temp.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {number} celsius
+ * @return {number[]}
+ */
+function convertTemperature(celsius) {
+    const kelvinTemperature = celsius + 273.15;
+    const fahrenheitTemperature = celsius * 1.8 + 32;
+
+    return [kelvinTemperature, fahrenheitTemperature];
+}
+
+console.log( convertTemperature(36.5) ); // [309.65000, 97.70000]
