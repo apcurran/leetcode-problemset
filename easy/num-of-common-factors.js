c6fcf341689e4ed4081a04ccce3a75b3e9e91a72
Alex Curran <alexcurran.va@gmail.com>
2023-01-04 14:48:46 -0600

    Working iterative solution for num-of-common-factors.js

---
+++ easy/num-of-common-factors.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+function commonFactors(a, b) {
+    const smallerNumber = Math.min(a, b);
+    let commonFactorCount = 0;
+    // take smallest number
+    // iterate up to smallest
+    for (let i = 1; i <= smallerNumber; i++) {
+        // check if this value divides into a and b evenly
+        const isADivisibleByValue = Number.isInteger(a / i);
+        const isBDivisibleByValue = Number.isInteger(b / i);
+        // if so, increment counter
+        if (isADivisibleByValue && isBDivisibleByValue) {
+            commonFactorCount++;
+        }
+    }
+
+    return commonFactorCount;
+}
+
+console.log( commonFactors(12, 6) ); // 4
+console.log( commonFactors(25, 30) ); // 2
