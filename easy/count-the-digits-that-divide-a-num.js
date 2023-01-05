17b863d867f4acd670df1b3db8129b54a71c1bb1
Alex Curran <alexcurran.va@gmail.com>
2023-01-05 10:03:07 -0600

    Working solution for count-the-digits-that-divide-a-num.js

---
+++ easy/count-the-digits-that-divide-a-num.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number} num
+ * @return {number}
+ */
+function countDigits(num) {    
+    const digitsStr = num
+                        .toString()
+                        .split("");
+    let divisibleCounter = 0;
+    
+    for (let digitStr of digitsStr) {
+        const currentDigit = Number(digitStr);
+        const isNumDivisibleByDigit = num % currentDigit === 0;
+        
+        if (isNumDivisibleByDigit) divisibleCounter++;
+    }
+
+    return divisibleCounter;
+}
+
+console.log( countDigits(121) ); // 2
+console.log( countDigits(7) ); // 1
+console.log( countDigits(1248) ); // 4
+
