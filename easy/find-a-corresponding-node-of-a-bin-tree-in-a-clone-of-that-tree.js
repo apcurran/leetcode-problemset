2460e49fe6ee2aaa2bc2d8d236214257ee930d0c
Alex Curran <alexcurran.va@gmail.com>
2023-01-11 11:29:36 -0600

    Working solution for find-a-corresponding-node-of-a-bin-tree-in-a-clone-of-that-tree.js

---
+++ easy/find-a-corresponding-node-of-a-bin-tree-in-a-clone-of-that-tree.js
@@ -0,0 +1,43 @@
+"use strict";
+
+// Definition for a binary tree node.
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
+/**
+ * solution 1 -- DFS
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {TreeNode} original
+ * @param {TreeNode} cloned
+ * @param {TreeNode} target
+ * @return {TreeNode}
+ */
+function getTargetCopy(original, cloned, target) {
+    const neededTargetValue = target.val;
+    let stack = [cloned];
+
+    while (stack.length > 0) {
+        const currentNode = stack.pop();
+
+        // handle current node value
+        if (currentNode.val === neededTargetValue) {
+            return currentNode;
+        }
+
+        // check for left
+        if (currentNode.left) {
+            // if exists, add
+            stack.push(currentNode.left);
+        }
+
+        // check for right
+        if (currentNode.right) {
+            // if exists, add
+            stack.push(currentNode.right);
+        }
+    }
+}
