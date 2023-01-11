eea0b0b8967cf837effad93840556612ee0465b4
Alex Curran <alexcurran.va@gmail.com>
2023-01-11 15:34:04 -0600

    Slightly optimize solution

--- easy/merge-similar-items.js
+++ easy/merge-similar-items.js
@@ -1,5 +1,40 @@
+// /**
+//  * solution 1
+//  * n = Max(items1, items2)
+//  * time: O(n * log n)
+//  * space: O(n)
+//  * 
+//  * @param {number[][]} items1
+//  * @param {number[][]} items2
+//  * @return {number[][]}
+//  */
+// function mergeSimilarItems(items1, items2) {
+//     // create a cache for val -> combinedWeight
+//     let itemsCache = new Map();
+
+//     for (let [itemValue, itemWeight] of items1) {
+//         const itemPreviousWeight = itemsCache.get(itemValue) || 0;
+//         itemsCache.set(itemValue, itemPreviousWeight + itemWeight);
+//     }
+
+//     for (let [itemValue, itemWeight] of items2) {
+//         const itemPreviousWeight = itemsCache.get(itemValue) || 0;
+//         itemsCache.set(itemValue, itemPreviousWeight + itemWeight);
+//     }
+
+//     const resultsSortedByValue = [...itemsCache.entries()].sort((arrA, arrB) => {
+//         if (arrA[0] > arrB[0]) {
+//             return 1;
+//         } else {
+//             return -1;
+//         }
+//     });
+
+//     return resultsSortedByValue;
+// }
+
 /**
- * solution 1
+ * solution 2
  * n = Max(items1, items2)
  * time: O(n * log n)
  * space: O(n)
@@ -10,27 +45,14 @@
  */
 function mergeSimilarItems(items1, items2) {
     // create a cache for val -> combinedWeight
-    let itemsCache = new Map();
-
-    for (let [itemValue, itemWeight] of items1) {
-        const itemPreviousWeight = itemsCache.get(itemValue) || 0;
-        itemsCache.set(itemValue, itemPreviousWeight + itemWeight);
-    }
+    let itemsCache = new Map(items1);
 
     for (let [itemValue, itemWeight] of items2) {
         const itemPreviousWeight = itemsCache.get(itemValue) || 0;
         itemsCache.set(itemValue, itemPreviousWeight + itemWeight);
     }
 
-    const resultsSortedByValue = [...itemsCache.entries()].sort((arrA, arrB) => {
-        if (arrA[0] > arrB[0]) {
-            return 1;
-        } else {
-            return -1;
-        }
-    });
-
-    return resultsSortedByValue;
+    return [...itemsCache.entries()].sort((arrA, arrB) => arrA[0] - arrB[0]);
 }
 
 console.log( mergeSimilarItems([[1,1],[4,5],[3,8]], [[3,1],[1,5]]) ); // [[1,6],[3,9],[4,5]]
