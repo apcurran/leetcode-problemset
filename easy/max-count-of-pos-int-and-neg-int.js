ed783784b37528e50a137be3712981b8c0cd3579
Alex Curran <alexcurran.va@gmail.com>
2023-01-24 15:18:49 -0600

    Working solution for max-count-of-pos-int-and-neg-int.js

---
+++ easy/max-count-of-pos-int-and-neg-int.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
+function maximumCount(nums) {
+    let negativeNumsCount = 0;
+    let positiveNumsCount = 0;
+
+    for (let num of nums) {
+        if (num < 0) {
+            negativeNumsCount++;
+        }
+
+        if (num > 0) {
+            positiveNumsCount++;
+        }
+    }
+
+    return Math.max(negativeNumsCount, positiveNumsCount);
+}
+
+console.log( maximumCount([-2,-1,-1,1,2,3]) ); // 3
+console.log( maximumCount([-3,-2,-1,0,0,1,2]) ); // 3
