4078150acc3e92b11089ddaea92bf1ff50258994
Alex Curran <alexcurran.va@gmail.com>
2023-01-25 10:28:27 -0600

    Working solution for univalued-bin-tree.js

---
+++ easy/univalued-bin-tree.js
@@ -0,0 +1,42 @@
+"use strict";
+
+// Definition for a binary tree node.
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val);
+    this.left = (left===undefined ? null : left);
+    this.right = (right===undefined ? null : right);
+}
+
+/**
+ * solution 1 -- iterative
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+function isUnivalTree(root) {
+    // create a Set object to track unique node values
+    let uniqueValues = new Set();
+    // traverse binary tree
+    let stack = [root];
+
+    while (stack.length > 0) {
+        // pop off top node
+        const currentNode = stack.pop();
+        uniqueValues.add(currentNode.val);
+
+        // check if node's right property has a node
+        if (currentNode.right) {
+            // if so, add it to the stack
+            stack.push(currentNode.right);
+        }
+        // check if node's left property has a node
+        if (currentNode.left) {
+            // if so, add it to the stack
+            stack.push(currentNode.left);
+        }
+    }
+
+    return uniqueValues.size === 1;
+}
