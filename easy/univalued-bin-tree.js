4254b4b0d928603355a0c77d5c44a11d292962ab
Alex Curran <alexcurran.va@gmail.com>
2023-01-25 10:35:20 -0600

    Recursive solution

--- easy/univalued-bin-tree.js
+++ easy/univalued-bin-tree.js
@@ -7,36 +7,55 @@ function TreeNode(val, left, right) {
     this.right = (right===undefined ? null : right);
 }
 
+// /**
+//  * solution 1 -- iterative
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {TreeNode} root
+//  * @return {boolean}
+//  */
+// function isUnivalTree(root) {
+//     // create a Set object to track unique node values
+//     let uniqueValues = new Set();
+//     // traverse binary tree
+//     let stack = [root];
+
+//     while (stack.length > 0) {
+//         // pop off top node
+//         const currentNode = stack.pop();
+//         uniqueValues.add(currentNode.val);
+
+//         // check if node's right property has a node
+//         if (currentNode.right) {
+//             // if so, add it to the stack
+//             stack.push(currentNode.right);
+//         }
+//         // check if node's left property has a node
+//         if (currentNode.left) {
+//             // if so, add it to the stack
+//             stack.push(currentNode.left);
+//         }
+//     }
+
+//     return uniqueValues.size === 1;
+// }
+
 /**
- * solution 1 -- iterative
+ * solution 2 -- recursive
  * time: O(n)
- * space: O(n)
+ * space: O(h)
  * 
  * @param {TreeNode} root
  * @return {boolean}
  */
-function isUnivalTree(root) {
-    // create a Set object to track unique node values
-    let uniqueValues = new Set();
-    // traverse binary tree
-    let stack = [root];
+function isUnivalTree(root, value = root.val) {
+    if (root === null) return true;
 
-    while (stack.length > 0) {
-        // pop off top node
-        const currentNode = stack.pop();
-        uniqueValues.add(currentNode.val);
+    if (root.val !== value) return false;
 
-        // check if node's right property has a node
-        if (currentNode.right) {
-            // if so, add it to the stack
-            stack.push(currentNode.right);
-        }
-        // check if node's left property has a node
-        if (currentNode.left) {
-            // if so, add it to the stack
-            stack.push(currentNode.left);
-        }
-    }
+    const isLeftSideUnival = isUnivalTree(root.left, value);
+    const isRightSideUnival = isUnivalTree(root.right, value);
 
-    return uniqueValues.size === 1;
+    return isLeftSideUnival && isRightSideUnival;
 }
