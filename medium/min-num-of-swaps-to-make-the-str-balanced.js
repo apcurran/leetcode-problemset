040263204c321a437bf8b44cf3943f6c6d100f02
Alex Curran <alexcurran.va@gmail.com>
2023-01-26 14:20:22 -0600

    Working solution for min-num-of-swaps-to-make-the-str-balanced.js

---
+++ medium/min-num-of-swaps-to-make-the-str-balanced.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {string} s
+ * @return {number}
+ */
+function minSwaps(s) {
+    let currentExtraClosingBracketsCount = 0;
+    let maxExtraClosingBracketsCount = 0;
+
+    for (let bracket of s) {
+        if (bracket === "[") {
+            currentExtraClosingBracketsCount--;
+        } else {
+            currentExtraClosingBracketsCount++;
+        }
+
+        // always set max extras to current max
+        maxExtraClosingBracketsCount = Math.max(maxExtraClosingBracketsCount, currentExtraClosingBracketsCount);
+    }
+
+    return Math.floor((maxExtraClosingBracketsCount + 1) / 2);
+}
+
+console.log( minSwaps("][][") ); // 1
+console.log( minSwaps("]]][[[") ); // 2
