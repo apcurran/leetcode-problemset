2b7f37c80a610409207d034de4da0a74a99e4d24
Alex Curran <alexcurran.va@gmail.com>
2023-01-27 12:15:03 -0600

    Working solution for count-pairs-of-similar-strs.js

---
+++ easy/count-pairs-of-similar-strs.js
@@ -0,0 +1,43 @@
+"use strict";
+
+/**
+ * solution 1
+ * 
+ * @param {string[]} words
+ * @return {number}
+ */
+function similarPairs(words) {
+    let similarPairsCount = 0;
+
+    for (let i = 0; i < words.length; i++) {
+        const currentWord = words[i];
+        const currentWordUniqueLetters = new Set([...currentWord]);
+
+        followingWordsLoop: for (let j = i + 1; j < words.length; j++) {
+            const followingWord = words[j];
+            const followingWordUniqueLetters = new Set([...followingWord]);
+            // iterate Set currentWord
+            for (let letter of currentWordUniqueLetters) {
+                if (!followingWordUniqueLetters.has(letter)) {
+                    continue followingWordsLoop;
+                }
+            }
+
+            // iterate Set followingWord
+            for (let letter of followingWordUniqueLetters) {
+                if (!currentWordUniqueLetters.has(letter)) {
+                    continue followingWordsLoop;
+                }
+            }
+
+            // successful match
+            similarPairsCount++;
+        }
+    }
+
+    return similarPairsCount;
+}
+
+console.log( similarPairs(["aba","aabb","abcd","bac","aabc"]) ); // 2
+console.log( similarPairs(["aabb","ab","ba"]) ); // 3
+console.log( similarPairs(["nba","cba","dba"]) ); // 0
