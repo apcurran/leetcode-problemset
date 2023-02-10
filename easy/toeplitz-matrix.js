65d87edf03f61c343126cb4c4a9d6aa58b669caa
Alex Curran <alexcurran.va@gmail.com>
2023-02-10 14:07:09 -0600

    Working solution for toeplitz-matrix.js

---
+++ easy/toeplitz-matrix.js
@@ -0,0 +1,38 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(m * n)
+ * space: O(1)
+ * 
+ * @param {number[][]} matrix
+ * @return {boolean}
+ */
+function isToeplitzMatrix(matrix) {
+    // skip first row
+    for (let row = 1; row < matrix.length; row++) {
+        // skip first column
+        for (let column = 1; column < matrix[row].length; column++) {
+            const currentCell = matrix[row][column];
+            const previousDiagonalCell = matrix[row - 1][column - 1];
+
+            if (currentCell !== previousDiagonalCell) return false;
+        }
+    }
+
+    return true;
+}
+
+console.log( isToeplitzMatrix(
+    [
+        [1,2,3,4],
+        [5,1,2,3],
+        [9,5,1,2]
+    ]
+) ); // true
+console.log( isToeplitzMatrix(
+    [
+        [1,2],
+        [2,2]
+    ]
+) ); // false
