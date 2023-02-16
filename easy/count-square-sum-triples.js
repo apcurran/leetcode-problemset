758e7490f8192a9524724a0bbdfae9b7cbb091c7
Alex Curran <alexcurran.va@gmail.com>
2023-02-16 15:55:00 -0600

    Working solution for count-square-sum-triples.js

--- easy/count-square-sum-triples.js
+++ easy/count-square-sum-triples.js
@@ -1,8 +1,32 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n^3)
+//  * space: O(1)
+//  * 
+//  * @param {number} n
+//  * @return {number}
+//  */
+// function countTriples(n) {
+//     let triplesCount = 0;
+
+//     for (let a = 1; a <= n; a++) {
+//         for (let b = 2; b <= n; b++) {
+//             for (let c = 3; c <= n; c++) {
+//                 if ((a * a) + (b * b) === (c * c)) {
+//                     triplesCount++;
+//                 }
+//             }
+//         }
+//     }
+
+//     return triplesCount;
+// }
+
 /**
- * solution 1
- * time: O(n^3)
+ * solution 2
+ * time: O(n^2)
  * space: O(1)
  * 
  * @param {number} n
@@ -13,10 +37,10 @@ function countTriples(n) {
 
     for (let a = 1; a <= n; a++) {
         for (let b = 2; b <= n; b++) {
-            for (let c = 3; c <= n; c++) {
-                if ((a * a) + (b * b) === (c * c)) {
-                    triplesCount++;
-                }
+            const c = Math.trunc( Math.sqrt((a * a) + (b * b)) );
+
+            if ((a * a) + (b * b) === (c * c) && c <= n) {
+                triplesCount++;
             }
         }
     }
