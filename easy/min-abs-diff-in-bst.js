54156f3cf4cd4a19028a50dc04d0d97e70a99818
Alex Curran <alexcurran.va@gmail.com>
2023-02-16 13:30:16 -0600

    Working solution for min-abs-diff-in-bst.js

---
+++ easy/min-abs-diff-in-bst.js
@@ -0,0 +1,47 @@
+"use strict";
+
+// Definition for a binary tree node.
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val);
+    this.left = (left===undefined ? null : left);
+    this.right = (right===undefined ? null : right);
+}
+
+/**
+ * solution 1 -- iterative DFS
+ * time: O(n * log n) -- due to sorting of array
+ * space: O(n)
+ * 
+ * @param {TreeNode} root
+ * @return {number}
+ */
+function getMinimumDifference(root) {
+    let stack = [root];
+    let nodeValues = [];
+
+    while (stack.length > 0) {
+        const currentNode = stack.pop();
+        nodeValues.push(currentNode.val);
+
+        if (currentNode.right) {
+            stack.push(currentNode.right);
+        }
+
+        if (currentNode.left) {
+            stack.push(currentNode.left);
+        }
+    }
+
+    nodeValues = nodeValues.sort((a, b) => a - b);
+
+    let minimumDifference = Infinity;
+
+    for (let i = 0; i < nodeValues.length - 1; i++) {
+        const currentValue = nodeValues[i];
+        const nextValue = nodeValues[i + 1];
+        const currentDifference = Math.abs(currentValue - nextValue);
+        minimumDifference = Math.min(minimumDifference, currentDifference);
+    }
+
+    return minimumDifference;
+}
