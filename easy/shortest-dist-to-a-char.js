c34896d19065c6cf94e50f4d43998254a0755674
Alex Curran <alexcurran.va@gmail.com>
2023-02-22 14:59:00 -0600

    Working solution for shortest-dist-to-a-char.js

---
+++ easy/shortest-dist-to-a-char.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/**
+ * @param {string} str
+ * @param {string} char
+ * @return {number[]}
+ */
+function shortestToChar(str, char) {
+    let charPositionsInStr = [];
+    let shortestDistancesToChar = [];
+
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === char) charPositionsInStr.push(i);
+    }
+
+    for (let i = 0; i < str.length; i++) {
+        let minDistanceToChar = Infinity;
+
+        for (let j = 0; j < charPositionsInStr.length; j++) {
+            const currentCharPosition = charPositionsInStr[j];
+            const positionsDifference = Math.abs(i - currentCharPosition);
+            minDistanceToChar = Math.min(minDistanceToChar, positionsDifference);
+        }
+        
+        shortestDistancesToChar.push(minDistanceToChar);        
+    }
+
+    return shortestDistancesToChar;
+}
+
+console.log( shortestToChar("loveleetcode", "e") ); // [3,2,1,0,1,0,0,1,2,2,1,0]
