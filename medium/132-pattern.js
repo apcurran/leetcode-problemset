9455c35924150030abbe3fd21177e424626add6a
Alex Curran <alexcurran.va@gmail.com>
2023-03-02 11:15:13 -0600

    Working stack solution for 132-pattern.js

---
+++ medium/132-pattern.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/**
+ * solution 1 -- stack
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+function find132pattern(nums) {
+    let stack = [];
+    let currentMin = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        const num = nums[i];
+
+        while (stack.length > 0 && num >= stack[stack.length - 1][0]) {
+            stack.pop();
+        }
+
+        if (stack.length > 0 && num > stack[stack.length - 1][1]) {
+            return true;
+        }
+
+        stack.push([num, currentMin]);
+        currentMin = Math.min(currentMin, num);
+    }
+
+    return false;
+}
+
+console.log( find132pattern([1,2,3,4]) ); // false
+console.log( find132pattern([3,1,4,2]) ); // true
