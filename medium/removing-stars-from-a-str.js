1c7766f2959c6cc7d2fa71a94eef6c72a2745b54
Alex Curran <alexcurran.va@gmail.com>
2023-03-06 15:51:21 -0600

    Working solution with O(n) memory

--- medium/removing-stars-from-a-str.js
+++ medium/removing-stars-from-a-str.js
@@ -1,7 +1,31 @@
 "use strict";
 
+// /**
+//  * solution 1 -- recursion (stack overflow on large inputs)
+//  * time: O(n^2)
+//  * space: O(n^2)
+//  * 
+//  * @param {string} str
+//  * @return {string}
+//  */
+// function removeStars(str) {
+//     const starIndex = str.indexOf("*");
+
+//     // base case
+//     if (starIndex === -1) {
+//         return str;
+//     }
+
+//     // cut parts up to, but not including the letter to the left of star, then
+//     // ADD this substring to the characters to the right of string
+//     const updatedText = str.slice(0, starIndex - 1) + str.slice(starIndex + 1);
+
+//     // recusive case
+//     return removeStars(updatedText);
+// }
+
 /**
- * solution 1 -- recursion (stack overflow on large inputs)
+ * solution 2
  * time: O(n^2)
  * space: O(n)
  * 
@@ -9,19 +33,17 @@
  * @return {string}
  */
 function removeStars(str) {
-    const starIndex = str.indexOf("*");
+    let updatedText = str;
 
-    // base case
-    if (starIndex === -1) {
-        return str;
+    while (true) {
+        const starIndex = updatedText.indexOf("*");
+
+        if (starIndex === -1) break;
+
+        updatedText = updatedText.slice(0, starIndex - 1) + updatedText.slice(starIndex + 1);
     }
 
-    // cut parts up to, but not including the letter to the left of star, then
-    // ADD this substring to the characters to the right of string
-    const updatedText = str.slice(0, starIndex - 1) + str.slice(starIndex + 1);
-
-    // recusive case
-    return removeStars(updatedText);
+    return updatedText;
 }
 
 console.log( removeStars("leet**cod*e") ); // "lecoe"
