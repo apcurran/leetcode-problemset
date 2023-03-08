6e2fb1071f21e40aecd3b1b7ec5fc07d17bf4b41
Alex Curran <alexcurran.va@gmail.com>
2023-03-08 15:50:41 -0600

    Working solution for convert-bin-num-in-a-linked-list-to-int.js

---
+++ easy/convert-bin-num-in-a-linked-list-to-int.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// Definition for singly-linked list.
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val);
+    this.next = (next===undefined ? null : next);
+}
+
+/**
+ * solution 1 -- iterative
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {ListNode} head
+ * @return {number}
+ */
+function getDecimalValue(head) {
+    let binaryStr = "";
+    // iterate each value in list
+    while (head !== null) {
+        // add bin val to string
+        binaryStr += head.val;
+        // move pointer to next link in list
+        head = head.next;
+    }
+
+    // convert bin string to number with parseInt(str, 2)
+    return parseInt(binaryStr, 2);
+}
