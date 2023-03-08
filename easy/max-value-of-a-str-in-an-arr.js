92d14c2a8839e4a637ff4be750d3921fdcfc65e1
Alex Curran <alexcurran.va@gmail.com>
2023-03-08 15:08:21 -0600

    Working solution for max-value-of-a-str-in-an-arr.js

---
+++ easy/max-value-of-a-str-in-an-arr.js
@@ -0,0 +1,36 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n * m)
+ * space: O(1)
+ * 
+ * @param {string[]} strs
+ * @return {number}
+ */
+function maximumValue(strs) {
+    const allStrDigits = new Set(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]);
+    let maxVal = -Infinity;
+
+    strsLoop: for (let str of strs) {
+        // check if string has any character that is not in str digits Set,
+        for (let char of str) {
+            if (!allStrDigits.has(char)) {
+                // if so, get the length of the string
+                maxVal = Math.max(maxVal, str.length);
+
+                continue strsLoop;
+            }
+        }
+
+        // otherwise, get the numberic val of the string
+        const numericValue = Number(str);
+        // reset maxVal to Max(maxVal, currentString)
+        maxVal = Math.max(maxVal, numericValue);
+    }
+
+    return maxVal;
+}
+
+console.log( maximumValue(["alic3","bob","3","4","00000"]) ); // 5
+console.log( maximumValue(["1","01","001","0001"]) ); // 1
