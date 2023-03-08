a087a49d2fb19d2b5e91a8918faf20e0c432dbe5
Alex Curran <alexcurran.va@gmail.com>
2023-03-08 15:12:57 -0600

    Working solution for max-value-of-a-str-in-an-arr.js

--- easy/max-value-of-a-str-in-an-arr.js
+++ easy/max-value-of-a-str-in-an-arr.js
@@ -1,32 +1,57 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n * m)
+//  * space: O(1)
+//  * 
+//  * @param {string[]} strs
+//  * @return {number}
+//  */
+// function maximumValue(strs) {
+//     const allStrDigits = new Set(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]);
+//     let maxVal = -Infinity;
+
+//     strsLoop: for (let str of strs) {
+//         // check if string has any character that is not in str digits Set,
+//         for (let char of str) {
+//             if (!allStrDigits.has(char)) {
+//                 // if so, get the length of the string
+//                 maxVal = Math.max(maxVal, str.length);
+
+//                 continue strsLoop;
+//             }
+//         }
+
+//         // otherwise, get the numberic val of the string
+//         const numericValue = Number(str);
+//         // reset maxVal to Max(maxVal, currentString)
+//         maxVal = Math.max(maxVal, numericValue);
+//     }
+
+//     return maxVal;
+// }
+
 /**
- * solution 1
- * time: O(n * m)
+ * solution 2
+ * time: O(n)
  * space: O(1)
  * 
  * @param {string[]} strs
  * @return {number}
  */
 function maximumValue(strs) {
-    const allStrDigits = new Set(["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]);
     let maxVal = -Infinity;
 
-    strsLoop: for (let str of strs) {
-        // check if string has any character that is not in str digits Set,
-        for (let char of str) {
-            if (!allStrDigits.has(char)) {
-                // if so, get the length of the string
-                maxVal = Math.max(maxVal, str.length);
+    for (let str of strs) {
+        const strConvertedToNumber = Number(str);
+        const isConvertedNumberNotValid = Number.isNaN(strConvertedToNumber);
 
-                continue strsLoop;
-            }
+        if (isConvertedNumberNotValid) {
+            maxVal = Math.max(maxVal, str.length);
+        } else {
+            maxVal = Math.max(maxVal, strConvertedToNumber);
         }
-
-        // otherwise, get the numberic val of the string
-        const numericValue = Number(str);
-        // reset maxVal to Max(maxVal, currentString)
-        maxVal = Math.max(maxVal, numericValue);
     }
 
     return maxVal;
