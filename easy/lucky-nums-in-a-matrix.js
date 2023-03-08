12863a8ee16c3e1b6d521cb958d7dbb925fe58c0
Alex Curran <alexcurran.va@gmail.com>
2023-03-08 10:55:30 -0600

    Working solution for lucky-nums-in-a-matrix.js

---
+++ easy/lucky-nums-in-a-matrix.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n * m)
+ * space: O(n)
+ * 
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+function luckyNumbers(matrix) {
+    let luckyValues = [];
+
+    for (let i = 0; i < matrix.length; i++) {
+        const row = matrix[i];
+        const minCellInRow = Math.min(...row);
+        const minCellInRowIndex = row.indexOf(minCellInRow);
+        const isMinCellInRowMaxInColumn = matrix.every((row) => row[minCellInRowIndex] <= minCellInRow);
+
+        if (isMinCellInRowMaxInColumn) {
+            luckyValues.push(minCellInRow);
+        }
+    }
+
+    return luckyValues;
+}
+
+console.log( luckyNumbers(
+    [
+        [3,   7,  8],
+        [9,  11, 13],
+        [15, 16, 17]
+    ]
+) ); // [15]
