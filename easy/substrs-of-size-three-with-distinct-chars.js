9e2ec9550c103b8d4e624c952e51c80463a5ab15
Alex Curran <alexcurran.va@gmail.com>
2023-03-10 10:46:53 -0600

    Working solution for substrs-of-size-three-with-distinct-chars.js

---
+++ easy/substrs-of-size-three-with-distinct-chars.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ * solution 1 -- Set cache
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {string} str
+ * @return {number} count of "good" strings without repeated chars
+ */
+function countGoodSubstrings(str) {
+    let goodStrsCount = 0;
+
+    for (let i = 0; i < str.length - 2; i++) {
+        const chunk = str.slice(i, i + 3);
+        const chunkUniqueChars = new Set(chunk);
+
+        if (chunkUniqueChars.size === 3) {
+            goodStrsCount++;
+        }
+    }
+
+    return goodStrsCount;
+}
+
+console.log( countGoodSubstrings("xyzzaz") );
