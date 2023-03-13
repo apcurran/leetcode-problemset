ab418f92c1204585853de4ba04dd1a59d0401e8e
Alex Curran <alexcurran.va@gmail.com>
2023-03-13 14:28:50 -0500

    Working solution for min-dist-to-the-target-elem.js

---
+++ easy/min-dist-to-the-target-elem.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number[]} nums
+ * @param {number} target
+ * @param {number} start
+ * @return {number}
+ */
+function getMinDistance(nums, target, start) {
+    let minOverallDistance = Infinity;
+
+    for (let i = 0; i < nums.length; i++) {
+        const currentNum = nums[i];
+        const currentDistance = Math.abs(i - start);
+
+        if (currentNum === target && currentDistance < minOverallDistance) {
+            minOverallDistance = currentDistance;
+        }
+    }
+
+    return minOverallDistance;
+}
+
+console.log( getMinDistance([1,2,3,4,5], 5, 3) ); // 1
