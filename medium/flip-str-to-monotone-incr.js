ce522cae9633f0f3a3ecab5ff5b413ec0ca4a6bd
Alex Curran <alexcurran.va@gmail.com>
2023-03-17 15:15:39 -0500

    Working solution for flip-str-to-monotone-incr.js

---
+++ medium/flip-str-to-monotone-incr.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {string} binStr
+ * @return {number} flips count
+ */
+function minFlipsMonoIncr(binStr) {
+    let onesCount = 0;
+    let flipsCount = 0;
+
+    for (let char of binStr) {
+        if (char === "1") {
+            onesCount++;
+        } else {
+            flipsCount = Math.min(flipsCount + 1, onesCount);
+        }
+    }
+
+    return flipsCount;
+}
+
+console.log( minFlipsMonoIncr("010110") ); // 2
