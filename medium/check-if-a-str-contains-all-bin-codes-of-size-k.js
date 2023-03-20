842926ced5c77206221bd637801cf65cbef95b42
Alex Curran <alexcurran.va@gmail.com>
2023-03-20 13:11:06 -0500

    Working solution for check-if-a-str-contains-all-bin-codes-of-size-k.js

---
+++ medium/check-if-a-str-contains-all-bin-codes-of-size-k.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n * k)
+ * space: O(n * k)
+ * 
+ * @param {string} str
+ * @param {number} k
+ * @return {boolean}
+ */
+function hasAllCodes(str, k) {
+    const totalPossibleBinStrCombos = 2 ** k;
+    let uniqueBinStrs = new Set();
+
+    for (let i = 0; i < str.length - k + 1; i++) {
+        const binStr = str.slice(i, i + k);
+        uniqueBinStrs.add(binStr);
+    }
+
+    return uniqueBinStrs.size === totalPossibleBinStrCombos;
+}
+
+console.log( hasAllCodes("00110110", 2) ); // true
