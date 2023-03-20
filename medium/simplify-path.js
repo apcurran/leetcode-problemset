8cbdb654bcb3d009eceae7e75d3e1b206bf711ea
Alex Curran <alexcurran.va@gmail.com>
2023-03-20 17:05:06 -0500

    Working solution for simplify-path.js

---
+++ medium/simplify-path.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/**
+ * solution 1 -- stack
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} path
+ * @return {string}
+ */
+function simplifyPath(path) {
+    const splitText = path.split("/");
+    let stack = [];
+
+    for (let segment of splitText) {
+        if (segment === "..") {
+            if (stack.length > 0) {
+                stack.pop();
+            }
+        } else if (segment !== "" && segment !== ".") {
+            stack.push(segment);
+        }
+    }
+
+    const revisedPath = stack.join("/");
+
+    return `/${revisedPath}`;
+}
+
+console.log( simplifyPath("/home//foo/") ); // "/home/foo"
+console.log( simplifyPath("/../") ); // "/"
