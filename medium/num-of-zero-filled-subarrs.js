4980f8bbc4694702cdcad905074430f9d97b4adf
Alex Curran <alexcurran.va@gmail.com>
2023-03-21 09:35:12 -0500

    Working solution for num-of-zero-filled-subarrs.js

---
+++ medium/num-of-zero-filled-subarrs.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
+function zeroFilledSubarray(nums) {
+    let currentConsecutiveZerosCount = 0;
+    let totalZeroSubarrsCount = 0;
+
+    for (let num of nums) {
+        if (num === 0) {
+            currentConsecutiveZerosCount++;
+            totalZeroSubarrsCount += currentConsecutiveZerosCount;
+        } else {
+            // reset counter
+            currentConsecutiveZerosCount = 0;
+        }
+    }
+
+    return totalZeroSubarrsCount;
+}
+
+console.log( zeroFilledSubarray([1,3,0,0,2,0,0,4]) ); // 6
