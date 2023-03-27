95d6c4176a6da2bf85fe9be5dadfe91eb4491f9c
Alex Curran <alexcurran.va@gmail.com>
2023-03-27 10:35:07 -0500

    Working solution for replace-all-digits-with-chars.js

---
+++ easy/replace-all-digits-with-chars.js
@@ -0,0 +1,40 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} str
+ * @return {string}
+ */
+function replaceDigits(str) {
+    let chars = [...str];
+
+    for (let i = 1; i < chars.length; i++) {
+        const previousChar = chars[i - 1];
+        const currentDigit = Number(chars[i]);
+
+        // skip non-digits (chars)
+        if (Number.isNaN(currentDigit)) continue;
+
+        const replacementChar = shift(previousChar, currentDigit);
+        chars[i] = replacementChar;
+    }
+
+    return chars.join("");
+}
+
+console.log( replaceDigits("a1b2c3d4e") ); // "abbdcfdhe"
+
+/**
+ * @param {string} char 
+ * @param {number} digit 
+ * @returns {string} replacement char
+ */
+function shift(char, digit) {
+    const charCode = char.charCodeAt(0);
+    const replacementCharCode = charCode + digit;
+
+    return String.fromCharCode(replacementCharCode);
+}
