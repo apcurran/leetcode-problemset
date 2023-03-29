3d6df293710417f8e36563a40f57bcaad931af07
Alex Curran <alexcurran.va@gmail.com>
2023-03-29 15:30:22 -0500

    Working solution for remove-all-adjacent-duplicates-in-str-2.js

---
+++ medium/remove-all-adjacent-duplicates-in-str-2.js
@@ -0,0 +1,38 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n * m)
+ * space: O(n)
+ * 
+ * @param {string} str
+ * @param {number} k
+ * @return {string}
+ */
+function removeDuplicates(str, k) {
+    let stack = [];
+
+    for (let char of str) {
+        if (stack.length > 0 && stack[stack.length - 1][0] === char) {
+            stack[stack.length - 1][1] += 1;
+        } else {
+            // create new tuple pair
+            stack.push([char, 1]);
+        }
+
+        if (stack[stack.length - 1][1] === k) {
+            stack.pop();
+        }
+    }
+
+    let result = "";
+
+    for (let [char, count] of stack) {
+        const chunk = char.repeat(count);
+        result += chunk;
+    }
+
+    return result;
+}
+
+console.log( removeDuplicates("deeedbbcccbdaa", 3) ); // "aa"
