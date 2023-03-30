0c4fb064e46b6a6fe65a8fb57719e68f1bad0961
Alex Curran <alexcurran.va@gmail.com>
2023-03-30 15:29:56 -0500

    Median solution for min-moves-to-equal-arr-elems-2.js

---
+++ medium/min-moves-to-equal-arr-elems-2.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n * log n) -- due to sorting
+ * space: O(1)
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
+function minMoves2(nums) {
+    nums.sort((a, b) => a - b);
+    const median = nums[Math.floor(nums.length / 2)];
+    let totalMoves = 0;
+    
+    for (let num of nums) {
+        const diff = Math.abs(num - median);
+        totalMoves += diff;
+    }
+
+    return totalMoves;
+}
+
+console.log( minMoves2([1, 10, 2, 9]) ); // 16
