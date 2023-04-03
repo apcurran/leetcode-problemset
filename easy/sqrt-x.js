1038fa473987f82de2a6944df87b20632907e7e8
Alex Curran <alexcurran.va@gmail.com>
2023-04-03 11:56:18 -0500

    Binary search algorithm still TLE, something may be wrong with LeetCode

--- easy/sqrt-x.js
+++ easy/sqrt-x.js
@@ -1,33 +1,7 @@
 "use strict";
 
-/**
- * solution 1 -- correct, but TLE
- * time: O(sqrt(n))
- * space: O(1)
- * 
- * @param {number} x
- * @return {number}
- */
-function mySqrt(x) {
-    let squareRootRoundedDown = 0;
-
-    for (let i = 0; i <= x / 2 + 1; i++) {
-        const squaredValue = i * i;
-
-        if (squaredValue > squareRootRoundedDown * squareRootRoundedDown && squaredValue <= x) {
-            squareRootRoundedDown = i;
-
-            if (squaredValue === x) {
-                break;
-            }
-        }
-    }
-
-    return squareRootRoundedDown;
-}
-
 // /**
-//  * solution 2
+//  * solution 1
 //  * time: O(sqrt(n))
 //  * space: O(1)
 //  * 
@@ -35,7 +9,42 @@ function mySqrt(x) {
 //  * @return {number}
 //  */
 // function mySqrt(x) {
-    
+//     for (let i = 0; i <= x / 2 + 1; i++) {
+//         const squaredValue = i * i;
+
+//         if (squaredValue > x) return i - 1;
+//     }
+
+//     return 1;
 // }
 
+/**
+ * solution 2
+ * time: O(log n)
+ * space: O(1)
+ * 
+ * @param {number} x
+ * @return {number}
+ */
+function mySqrt(x) {
+    let left = 1;
+    let right = Math.ceil(x / 2);
+    
+    while (left <= right) {
+        const middle = left + ( Math.floor(right - left / 2) );
+        const squaredMiddle = middle * middle;
+
+        if (squaredMiddle > x) {
+            right = middle - 1;
+        } else if (squaredMiddle < x) {
+            left = middle + 1;
+        } else {
+            return middle;
+        }
+    }
+
+    return right;
+}
+
+console.log( mySqrt(1) ); // 1
 console.log( mySqrt(4) ); // 2
