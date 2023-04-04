d36129fd8acafb25d6a733eac651ed4d7ff73f40
Alex Curran <alexcurran.va@gmail.com>
2023-04-04 14:45:42 -0500

    Acceptable, but slow solution for successful-pairs-of-spells-and-potions.js

--- medium/successful-pairs-of-spells-and-potions.js
+++ medium/successful-pairs-of-spells-and-potions.js
@@ -1,10 +1,42 @@
 "use strict";
 
+// /**
+//  * solution 1 -- correct, but TLE
+//  * n = spells length
+//  * m = potions length
+//  * time: O(n * m)
+//  * space: O(n)
+//  * 
+//  * @param {number[]} spells
+//  * @param {number[]} potions
+//  * @param {number} success
+//  * @return {number[]}
+//  */
+// function successfulPairs(spells, potions, success) {
+//     let successfulPairs = [];
+
+//     for (let i = 0; i < spells.length; i++) {
+//         const currentSpell = spells[i];
+//         let successfulLocalPairsCount = 0;
+
+//         for (let j = 0; j < potions.length; j++) {
+//             const currentPotion = potions[j];
+//             const spellPotionProduct = currentSpell * currentPotion;
+
+//             if (spellPotionProduct >= success) successfulLocalPairsCount++;
+//         }
+
+//         successfulPairs.push(successfulLocalPairsCount);
+//     }
+
+//     return successfulPairs;
+// }
+
 /**
- * solution 1 -- correct, but TLE
+ * solution 2 -- slight optimization (passes, but slow)
  * n = spells length
  * m = potions length
- * time: O(n * m)
+ * time: O((n * m) + (m * log m))
  * space: O(n)
  * 
  * @param {number[]} spells
@@ -13,20 +45,28 @@
  * @return {number[]}
  */
 function successfulPairs(spells, potions, success) {
+    potions.sort((a, b) => a - b);
+
     let successfulPairs = [];
 
-    for (let i = 0; i < spells.length; i++) {
+    spellsLoop: for (let i = 0; i < spells.length; i++) {
         const currentSpell = spells[i];
-        let successfulLocalPairsCount = 0;
 
         for (let j = 0; j < potions.length; j++) {
             const currentPotion = potions[j];
-            const spellPotionProduct = currentSpell * currentPotion;
+            const product = currentSpell * currentPotion;
 
-            if (spellPotionProduct >= success) successfulLocalPairsCount++;
+            if (product >= success) {
+                // calc diff and skip iteration of subsequent potions
+                const diff = potions.length - j;
+                successfulPairs.push(diff);
+
+                continue spellsLoop;
+            }
         }
 
-        successfulPairs.push(successfulLocalPairsCount);
+        // fallback
+        successfulPairs.push(0);
     }
 
     return successfulPairs;
