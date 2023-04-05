083c4739f79c6f22262f0b998803d3955234061b
Alex Curran <alexcurran.va@gmail.com>
2023-04-05 11:31:18 -0500

    Working solution using distX and distY for min-time-visiting-all-points.js

---
+++ easy/min-time-visiting-all-points.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number[][]} points
+ * @return {number}
+ */
+function minTimeToVisitAllPoints(points) {
+    let totalSeconds = 0;
+
+    for (let i = 0; i < points.length - 1; i++) {
+        const currentPoint = points[i];
+        const nextPoint = points[i + 1];
+
+        const[currentPointX, currentPointY] = currentPoint;
+        const[nextPointX, nextPointY] = nextPoint;
+        const distanceX = Math.abs(currentPointX - nextPointX);
+        const distanceY = Math.abs(currentPointY - nextPointY);
+
+        totalSeconds += Math.max(distanceX, distanceY);
+    }
+
+    return totalSeconds;
+}
+
+console.log( minTimeToVisitAllPoints([[1,1], [3,4], [-1,0]]) ); // 7
