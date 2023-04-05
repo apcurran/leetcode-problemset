59234d7625ecd5c8e6e10edf3c9dd653a3e93151
Alex Curran <alexcurran.va@gmail.com>
2023-04-05 15:34:22 -0500

    O(log n) time solution for find-peak-elem.js

--- medium/find-peak-elem.js
+++ medium/find-peak-elem.js
@@ -1,34 +1,62 @@
 "use strict";
 
+// /**
+//  * A peak element is an element that is strictly greater than its neighbors.
+//  * Given an integer array nums, find a peak element, and return its index.
+//  * If the array contains multiple peaks, return the index to any of the peaks.
+//  * 
+//  * Time: O(n)
+//  * Space: O(1)
+//  * 
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// function findPeakElement(nums) {
+//     const n = nums.length;
+//     nums[n] = -Infinity;
+//     nums[-1] = nums[n];
+
+//     for (let i = 0; i < n; i++) {
+//         const currNum = nums[i];
+//         const nextNum = nums[i + 1];
+
+//         if (currNum > nextNum) {
+//             return i;
+//         }
+//     }
+
+//     return 0;
+// }
 
 /**
  * A peak element is an element that is strictly greater than its neighbors.
  * Given an integer array nums, find a peak element, and return its index.
  * If the array contains multiple peaks, return the index to any of the peaks.
  * 
- * Time: O(n)
+ * Time: O(log n)
  * Space: O(1)
  * 
  * @param {number[]} nums
  * @return {number}
  */
 function findPeakElement(nums) {
-    const n = nums.length;
-    nums[n] = -Infinity;
-    nums[-1] = nums[n];
+    let left = 0;
+    let right = nums.length - 1;
 
-    for (let i = 0; i < n; i++) {
-        const currNum = nums[i];
-        const nextNum = nums[i + 1];
+    while (left <= right) {
+        const middleIndex = left + ( Math.floor((right - left) / 2) );
+        const middleValue = nums[middleIndex];
 
-        if (currNum > nextNum) {
-            return i;
+        if (middleIndex > 0 && middleValue < nums[middleIndex - 1]) {
+            right = middleIndex - 1;
+        } else if (middleIndex < nums.length - 1 && middleValue < nums[middleIndex + 1]) {
+            left = middleIndex + 1;
+        } else {
+            return middleIndex;
         }
     }
-
-    return 0;
 }
 
 console.log( findPeakElement([1, 2, 3, 1]) ); // 2
-console.log( findPeakElement([1, 2, 1, 3, 5, 6, 4]) ); // 5 or 1
+console.log( findPeakElement([1, 2, 1, 3, 5, 6, 4]) ); // 5 (or 1)
 console.log( findPeakElement([1, 2, 3]) ); // 2
