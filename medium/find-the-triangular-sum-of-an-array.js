f8cccc1a7e0746b85d541ff66a9e46b349178f0d
Alex Curran <alexcurran.va@gmail.com>
2023-05-01 14:19:09 -0500

    O(n) space -- iterative solution

--- medium/find-the-triangular-sum-of-an-array.js
+++ medium/find-the-triangular-sum-of-an-array.js
@@ -1,28 +1,55 @@
 "use strict";
 
+// /**
+//  * solution 1 -- recursion
+//  * time: O(n^2)
+//  * space: O(n^2)
+//  * 
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// function triangularSum(nums) {
+//     // base case
+//     if (nums.length === 1) return nums[0];
+
+//     let updatedList = [];
+
+//     for (let i = 0; i < nums.length - 1; i++) {
+//         const currentNum = nums[i];
+//         const nextNum = nums[i + 1];
+//         const sum = (currentNum + nextNum) % 10;
+//         updatedList.push(sum);
+//     }
+
+//     // recursive case
+//     return triangularSum(updatedList);
+// }
+
 /**
- * solution 1 -- recursion
+ * solution 2 -- iterative
  * time: O(n^2)
- * space: O(n^2)
+ * space: O(n)
  * 
  * @param {number[]} nums
  * @return {number}
  */
 function triangularSum(nums) {
-    // base case
     if (nums.length === 1) return nums[0];
 
-    let updatedList = [];
+    while (nums.length > 1) {
+        let updatedList = [];
 
-    for (let i = 0; i < nums.length - 1; i++) {
-        const currentNum = nums[i];
-        const nextNum = nums[i + 1];
-        const sum = (currentNum + nextNum) % 10;
-        updatedList.push(sum);
+        for (let i = 0; i < nums.length - 1; i++) {
+            const currentNum = nums[i];
+            const nextNum = nums[i + 1];
+            const sum = (currentNum + nextNum) % 10;
+            updatedList.push(sum);
+        }
+
+        nums = updatedList;
     }
 
-    // recursive case
-    return triangularSum(updatedList);
+    return nums[0];
 }
 
 console.log( triangularSum([1,2,3,4,5]) ); // 8
