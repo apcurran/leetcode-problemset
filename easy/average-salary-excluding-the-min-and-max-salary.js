7fe4dc82feac933d9301b645841f0bc1d808f8c1
Alex Curran <alexcurran.va@gmail.com>
2023-05-01 09:01:31 -0500

    Working solution for average-salary-excluding-the-min-and-max-salary.js

---
+++ easy/average-salary-excluding-the-min-and-max-salary.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n * log n) -- sort
+ * space: O(1)
+ * 
+ * @param {number[]} salary
+ * @return {number}
+ */
+function average(salary) {
+    salary.sort((a, b) => a - b);
+    let salarySum = 0;
+    
+    // iterate through salary, skip first (min) and last (max)
+    for (let i = 1; i < salary.length - 1; i++) {
+        salarySum += salary[i];
+    }
+
+    const average = salarySum / (salary.length - 2); // remove two for the min and max exclusions
+
+    return average;
+}
+
+console.log( average([4000,3000,1000,2000]) ); // 2500
