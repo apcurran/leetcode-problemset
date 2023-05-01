70836e1341554735562d82241cbd5f03db1bb4a1
Alex Curran <alexcurran.va@gmail.com>
2023-05-01 14:10:31 -0500

    Working recursive solution for find-the-triangular-sum-of-an-array.js

---
+++ medium/find-the-triangular-sum-of-an-array.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/**
+ * solution 1 -- recursion
+ * time: O(n^2)
+ * space: O(n^2)
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
+function triangularSum(nums) {
+    // base case
+    if (nums.length === 1) return nums[0];
+
+    let updatedList = [];
+
+    for (let i = 0; i < nums.length - 1; i++) {
+        const currentNum = nums[i];
+        const nextNum = nums[i + 1];
+        const sum = (currentNum + nextNum) % 10;
+        updatedList.push(sum);
+    }
+
+    // recursive case
+    return triangularSum(updatedList);
+}
+
+console.log( triangularSum([1,2,3,4,5]) ); // 8
