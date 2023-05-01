58e7f1a2b9883fc4363230323b5dd4a2e4ea805f
Alex Curran <alexcurran.va@gmail.com>
2023-05-01 09:10:04 -0500

    O(n) time and O(1) space solution

--- easy/average-salary-excluding-the-min-and-max-salary.js
+++ easy/average-salary-excluding-the-min-and-max-salary.js
@@ -1,25 +1,50 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n * log n) -- sort
+//  * space: O(1)
+//  * 
+//  * @param {number[]} salary
+//  * @return {number}
+//  */
+// function average(salary) {
+//     salary.sort((a, b) => a - b);
+//     let salarySum = 0;
+    
+//     // iterate through salary, skip first (min) and last (max)
+//     for (let i = 1; i < salary.length - 1; i++) {
+//         salarySum += salary[i];
+//     }
+
+//     const average = salarySum / (salary.length - 2); // remove two for the min and max exclusions
+
+//     return average;
+// }
+
 /**
- * solution 1
- * time: O(n * log n) -- sort
+ * solution 2
+ * time: O(n)
  * space: O(1)
  * 
  * @param {number[]} salary
  * @return {number}
  */
 function average(salary) {
-    salary.sort((a, b) => a - b);
     let salarySum = 0;
-    
-    // iterate through salary, skip first (min) and last (max)
-    for (let i = 1; i < salary.length - 1; i++) {
-        salarySum += salary[i];
+    let min = Infinity;
+    let max = -Infinity;
+
+    for (let num of salary) {
+        min = Math.min(min, num);
+        max = Math.max(max, num);
+        salarySum += num;
     }
 
-    const average = salarySum / (salary.length - 2); // remove two for the min and max exclusions
-
-    return average;
+    const salarySumExcludingMinMax = salarySum - min - max;
+    const salaryLengthExcludingMinMax = salary.length - 2;
+    
+    return salarySumExcludingMinMax / salaryLengthExcludingMinMax;
 }
 
 console.log( average([4000,3000,1000,2000]) ); // 2500
