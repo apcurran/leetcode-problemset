03bb97c1733f04fc44b714f01fc5381886626242
Alex Curran <alexcurran.va@gmail.com>
2023-05-06 09:35:37 -0500

    Working solution for counter.js

---
+++ js-30-challenge/counter.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {number} n 
+ * @returns {function}
+ */
+function createCounter(n) {
+    return function incrementer() {
+        return n++;
+    };
+}
