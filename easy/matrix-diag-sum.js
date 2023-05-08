597814f892b7852aa680d653a5b8821dad08b968
Alex Curran <alexcurran.va@gmail.com>
2023-05-08 10:12:13 -0500

    Alternative solution for matrix-diag-sum.js

--- easy/matrix-diag-sum.js
+++ easy/matrix-diag-sum.js
@@ -1,7 +1,33 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n)
+//  * space: O(1)
+//  * 
+//  * @param {number[][]} matrix
+//  * @return {number}
+//  */
+// function diagonalSum(matrix) {
+//     let j = matrix[0].length - 1;
+//     let totalSum = 0;
+
+//     for (let i = 0; i < matrix.length; i++, j--) {
+//         const primaryDiagonal = matrix[i][i];
+//         const secondaryDiagonal = matrix[i][j];
+//         totalSum += primaryDiagonal;
+        
+//         // skip adding numbers from the same middle diagonal position
+//         if (i !== j) {
+//             totalSum += secondaryDiagonal;
+//         }
+//     }
+
+//     return totalSum;
+// }
+
 /**
- * solution 1
+ * solution 2 -- similar alternative
  * time: O(n)
  * space: O(1)
  * 
@@ -9,21 +35,22 @@
  * @return {number}
  */
 function diagonalSum(matrix) {
-    let j = matrix[0].length - 1;
+    const n = matrix.length;
     let totalSum = 0;
 
-    for (let i = 0; i < matrix.length; i++, j--) {
-        const primaryDiagonal = matrix[i][i];
-        const secondaryDiagonal = matrix[i][j];
-        totalSum += primaryDiagonal;
-        
-        // skip adding numbers from the same middle diagonal position
-        if (i !== j) {
-            totalSum += secondaryDiagonal;
-        }
+    for (let i = 0; i < n; i++) {
+        totalSum += matrix[i][i]; // primary diagonal
+        totalSum += matrix[i][n - 1 - i]; // secondary diagonal
     }
-
-    return totalSum;
+    
+    if (n % 2 !== 0) {
+        // skip adding numbers from the same middle diagonal position
+        const matrixCenterNum = matrix[Math.floor(n / 2)][Math.floor(n / 2)];
+        
+        return totalSum - matrixCenterNum;
+    } else {
+        return totalSum;
+    }
 }
 
 const mat = [
