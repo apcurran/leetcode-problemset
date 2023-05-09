b0289112517c1b163adc9e41689a1c5140f4f2d9
Alex Curran <alexcurran.va@gmail.com>
2023-05-09 09:25:50 -0500

    Working solution for day-5-filter-elems-from-arr.js

---
+++ js-30-challenge/day-5-filter-elems-from-arr.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ * solution 1 -- push to new array
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+function filter(arr, fn) {
+    let results = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        const currentElement = arr[i];
+        const isPassingElement = fn(currentElement, i);
+
+        if (isPassingElement) {
+            results.push(currentElement);
+        }
+    }
+
+    return results;
+}
+
+console.log( filter([0,10,20,30], function greaterThan10(n) { return n > 10; }) ); // [20, 30]
+console.log( filter([1,2,3], function firstIndex(n, i) { return i === 0; }) ); // [1]
+console.log( filter([-2,-1,0,1,2], function plusOne(n) { return n + 1; }) ); // [-2,0,1,2]
