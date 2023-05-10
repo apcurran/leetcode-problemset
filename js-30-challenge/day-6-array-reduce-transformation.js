17e65df745bdad35a652ceaecf158fa878607040
Alex Curran <alexcurran.va@gmail.com>
2023-05-10 10:48:47 -0500

    For of loop solution for day-6-array-reduce-transformation.js

---
+++ js-30-challenge/day-6-array-reduce-transformation.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+function reduce(nums, fn, init) {
+    if (nums.length === 0) return init;
+
+    let accumulator = init;
+
+    for (let elem of nums) {
+        const updatedAccumulator = fn(accumulator, elem);
+        accumulator = updatedAccumulator;
+    }
+
+    return accumulator;
+}
+
+console.log( reduce([1,2,3,4], function sum(accum, curr) { return accum + curr; }, 0 ) );
+// 10 
+console.log( reduce([1,2,3,4], function sum(accum, curr) { return accum + curr * curr; }, 100 ) );
+// 130
