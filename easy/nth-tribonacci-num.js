68eb12abf7c2ca3631ae439c49d391d4e61abd64
Alex Curran <alexcurran.va@gmail.com>
2023-05-11 15:20:34 -0500

    Array buffer solution

--- easy/nth-tribonacci-num.js
+++ easy/nth-tribonacci-num.js
@@ -1,29 +1,55 @@
 "use strict";
 
+// /**
+//  * solution 1 -- dynamic programming (recursion)
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {number} n
+//  * @return {number}
+//  */
+// function tribonacci(n, cache = new Map()) {
+//     if (n === 0) return 0;
+
+//     if (n === 1 || n === 2) return 1;
+
+//     // attempt to retrieve from cache
+//     if (cache.has(n)) {
+//         return cache.get(n);
+//     }
+
+//     // not in cache, 
+//     const value = tribonacci(n - 1, cache) + tribonacci(n - 2, cache) + tribonacci(n - 3, cache);
+//     // compute and store
+//     cache.set(n, value);
+//     // then return computed answer
+//     return value;
+// }
+
 /**
- * solution 1 -- dynamic programming (recursion)
+ * solution 2 -- dynamic programming (bottom up)
  * time: O(n)
- * space: O(n)
+ * space: O(n) -- due to array buffer
  * 
  * @param {number} n
  * @return {number}
  */
-function tribonacci(n, cache = new Map()) {
+function tribonacci(n) {
     if (n === 0) return 0;
 
     if (n === 1 || n === 2) return 1;
 
-    // attempt to retrieve from cache
-    if (cache.has(n)) {
-        return cache.get(n);
+    let results = new Int32Array(n + 1);
+    // by problem definition
+    results[1] = 1;
+    results[2] = 1;
+    
+    for (let i = 3; i <= n; i++) {
+        const answer = results[i - 1] + results[i - 2] + results[i - 3];
+        results[i] = answer;
     }
 
-    // not in cache, 
-    const value = tribonacci(n - 1, cache) + tribonacci(n - 2, cache) + tribonacci(n - 3, cache);
-    // compute and store
-    cache.set(n, value);
-    // then return computed answer
-    return value;
+    return results[n];
 }
 
 console.log( tribonacci(4) ); // 4
