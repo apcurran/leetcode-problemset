ba4e6a619755b4d8d7c2966431a91764ab220c14
Alex Curran <alexcurran.va@gmail.com>
2023-05-12 08:15:39 -0500

    Re-name day 7

---
+++ js-30-challenge/day-7-function-composition.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/**
+ * solution 1
+ * n = functions length
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+const compose = function(functions) {
+	return function(x) {
+        if (functions.length === 0) return x;
+
+        let result = x;
+
+        for (let i = functions.length - 1; i >= 0; i--) {
+            const func = functions[i];
+            const updatedValue = func(result);
+            result = updatedValue;
+        }
+
+        return result;
+    };
+};
+
+
+const fn = compose( [(x) => x + 1, (x) => 2 * x] );
+console.log( fn(4) ); // 9
+
+const fn2 = compose( [(x) => x + 1, (x) => x * x, (x) => 2 * x] );
+console.log( fn2(4) ); // 65
