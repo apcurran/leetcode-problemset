ebb5b5dbcfbfb93e2d23d2ac16f39990a50ed7cc
Alex Curran <alexcurran.va@gmail.com>
2023-05-14 08:06:12 -0500

    Working solution for day-10-curry.js

---
+++ js-30-challenge/day-10-curry.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {Function} fn
+ * @return {Function}
+ */
+const curry = function (fn) {
+    return function curried(...args) {
+        if (args.length >= fn.length) {
+            return fn.apply(this, args);
+        }
+
+        return curried.bind(this, ...args);
+    };
+};
+
+function sum(a, b) { return a + b; }
+const csum = curry(sum);
+console.log( csum(1)(2) ); // 3
