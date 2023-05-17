6978efb111b4ce1effbda9fa035edf1e567314d1
Alex Curran <alexcurran.va@gmail.com>
2023-05-17 15:23:36 -0500

    Working solution for xor-operation-in-an-array.js

---
+++ easy/xor-operation-in-an-array.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number} n
+ * @param {number} start
+ * @return {number}
+ */
+function xorOperation(n, start) {
+    let xorValue = 0;
+
+    for (let i = 0; i < n; i++) {
+        xorValue ^= start + 2 * i;
+    }
+
+    return xorValue;
+}
+
+console.log(xorOperation(5, 0)); // 8
