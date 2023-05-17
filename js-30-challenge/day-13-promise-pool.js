e9006adf688770f15c469c611025be5b2e50084f
Alex Curran <alexcurran.va@gmail.com>
2023-05-17 10:53:29 -0500

    Working solution for day-13-promise-pool.js

---
+++ js-30-challenge/day-13-promise-pool.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/**
+ * @param {Function[]} functions
+ * @param {number} n
+ * @return {Function}
+ */
+const promisePool = async function (functions, n) {
+    async function evaluateNextFunc() {
+        if (functions.length === 0) return;
+
+        const func = functions.shift();
+        await func();
+        await evaluateNextFunc();
+    }
+
+    let nPromises = [];
+
+    for (let i = 0; i < n; i++) {
+        nPromises.push(evaluateNextFunc());
+    }
+
+    await Promise.all(nPromises);
+};
+
+const sleep = (t) => new Promise((res) => setTimeout(res, t));
+console.log( promisePool([() => sleep(500), () => sleep(400)], 1).then(console.log) ); // After 900ms
