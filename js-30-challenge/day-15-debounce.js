376973071d6bd5ea520768731c30f784d7f29f6a
Alex Curran <alexcurran.va@gmail.com>
2023-05-19 10:38:44 -0500

    Working solution for debounce day 15

---
+++ js-30-challenge/day-15-debounce.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/**
+ * solution 1
+ * a = args
+ * time: O(a)
+ * space: O(a)
+ * 
+ * @param {Function} fn
+ * @param {number} duration milliseconds
+ * @return {Function}
+ */
+const debounce = function(fn, duration) {
+    let timeout;
+
+    return function(...args) {
+        clearTimeout(timeout);
+
+        timeout = setTimeout(() => {
+            fn(...args);
+        }, duration);
+    };
+};
+
+const log = debounce(console.log, 100);
+log("Hello"); // cancelled
+log("Hello"); // cancelled
+log("Hello"); // Logged at t=100ms
