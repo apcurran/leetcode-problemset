d316ccc849482c1410b668cc52f1cc9b0fa1e705
Alex Curran <alexcurran.va@gmail.com>
2023-05-22 16:29:24 -0500

    Working linear time solution for number-of-senior-citizens.js

---
+++ easy/number-of-senior-citizens.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {string[]} details
+ * @return {number}
+ */
+function countSeniors(details) {
+    const SENIOR_AGE = 60;
+    let seniorsCount = 0;
+
+    for (let person of details) {
+        const personAge = Number( person.slice(11, 13) );
+        
+        if (personAge > SENIOR_AGE) {
+            seniorsCount++;
+        }
+    }
+
+    return seniorsCount;
+}
+
+console.log( countSeniors(["7868190130M7522","5303914400F9211","9273338290F4010"]) );
+// 2
