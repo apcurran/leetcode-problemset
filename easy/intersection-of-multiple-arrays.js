a3f65e6736589fff5e5f55869f7a0d72ddbc8234
Alex Curran <alexcurran.va@gmail.com>
2023-05-23 10:33:14 -0500

    Working solution for intersection-of-multiple-arrays.js

---
+++ easy/intersection-of-multiple-arrays.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/**
+ * solution 1
+ * n = nums length
+ * m = num array length
+ * time: O(m * n * m)
+ * space: O(m)
+ * 
+ * @param {number[][]} nums
+ * @return {number[]}
+ */
+function intersection(nums) {
+    const firstArr = nums[0];
+    let results = [];
+
+    firstArrLoop: for (let elem of firstArr) {
+        for (let i = 1; i < nums.length; i++) {
+            const currentArray = nums[i];
+
+            if (!currentArray.includes(elem)) continue firstArrLoop;
+        }
+
+        // elem is within all arrays
+        results.push(elem);
+    }
+
+    // sort ASC
+    return results.sort((a, b) => a - b);
+}
+
+console.log( intersection([[3,1,2,4,5], [1,2,3,4], [3,4,5,6]]) ); // [3,4]
