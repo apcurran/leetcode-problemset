89c24abbba55013cd46f2e49fc156987ac3da014
Alex Curran <alexcurran.va@gmail.com>
2023-05-25 14:56:06 -0500

    Working solution for partition-labels.js

---
+++ medium/partition-labels.js
@@ -0,0 +1,45 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1) -- limited to 26 lowercase letters
+ * 
+ * @param {string} str
+ * @return {number[]}
+ */
+function partitionLabels(str) {
+    let charsIndicesCache = new Map();
+
+    for (let i = 0; i < str.length; i++) {
+        const char = str[i];
+        charsIndicesCache.set(char, i);
+    }
+
+    let chunkSize = 0;
+    let endIndex = 0;
+    let resultSizes = [];
+    
+    for (let i = 0; i < str.length; i++) {
+        chunkSize++;
+        
+        const char = str[i];
+        const charLastIndex = charsIndicesCache.get(char);
+
+        if (charLastIndex > endIndex) {
+            // replace end index
+            endIndex = charLastIndex;
+        }
+
+        if (i === endIndex) {
+            // add chunk size to results
+            resultSizes.push(chunkSize);
+            // reset counters
+            chunkSize = 0;
+        }
+    }
+
+    return resultSizes;
+}
+
+console.log( partitionLabels("ababcbacadefegdehijhklij") ); // [9,7,8]
