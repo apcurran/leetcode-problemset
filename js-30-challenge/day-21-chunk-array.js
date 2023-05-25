d3dca9b337cec3e330a960ad350a6690513c6ee2
Alex Curran <alexcurran.va@gmail.com>
2023-05-25 09:55:17 -0500

    Working solution for day-21-chunk-array.js

---
+++ js-30-challenge/day-21-chunk-array.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/**
+ * solution 1
+ * n = array length
+ * s = size
+ * time: O(n / s * s)
+ * space: O(n)
+ * 
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array[]}
+ */
+function chunk(arr, size) {
+    if (arr.length === 0) {
+        return [];
+    }
+
+    if (size >= arr.length) {
+        return [arr];
+    }
+
+    let chunks = [];
+
+    for (let i = 0; i < arr.length; i += size) {
+        const piece = arr.slice(i, size + i);
+        chunks.push(piece);
+    }
+
+    return chunks;
+}
+
+console.log( chunk([1,2,3,4,5], 1) ); // [[1],[2],[3],[4],[5]]
+console.log( chunk([1,9,6,3,2], 3) ); // [[1,9,6],[3,2]]
