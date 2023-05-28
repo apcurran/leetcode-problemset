e6aef565a23f8c56ec2491be9d264c91be2c80ef
Alex Curran <alexcurran.va@gmail.com>
2023-05-28 08:59:27 -0500

    Working solution for day-24-group-by.js

---
+++ js-30-challenge/day-24-group-by.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {Function} fn
+ * @return {object}
+ */
+Array.prototype.groupBy = function(fn) {
+    const currentArr = this;
+    let result = {};
+
+    for (let elem of currentArr) {
+        const key = fn(elem);
+
+        if (key in result) {
+            result[key].push(elem);
+        } else {
+            result[key] = [elem];
+        }
+    }
+
+    return result;
+};
+
+
+console.log( [1,2,3].groupBy(String) ); // {"1":[1],"2":[2],"3":[3]}
