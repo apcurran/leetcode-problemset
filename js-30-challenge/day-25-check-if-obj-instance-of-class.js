1d9b0f34fb0b260146a5ada552c99ff254711875
Alex Curran <alexcurran.va@gmail.com>
2023-05-29 09:08:57 -0500

    Working solution for day-25-check-if-obj-instance-of-class.js

---
+++ js-30-challenge/day-25-check-if-obj-instance-of-class.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {any} obj
+ * @param {any} classFunction
+ * @return {boolean}
+ */
+const checkIfInstanceOf = function (obj, classFunction) {
+    if (obj === undefined ||
+        obj === null ||
+        typeof classFunction !== "function") {
+        return false;
+    }
+
+    let currentPrototype = Object.getPrototypeOf(obj);
+
+    while (currentPrototype !== null) {
+        if (currentPrototype === classFunction.prototype) {
+            return true;
+        } else {
+            // move along the chain to continue searching
+            currentPrototype = Object.getPrototypeOf(currentPrototype);
+        }
+    }
+
+    return false;
+};
+
+console.log( checkIfInstanceOf(new Date(), Date) ); // true
