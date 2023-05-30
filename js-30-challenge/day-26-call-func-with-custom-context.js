2876147385d4d2450f3b9edcc3f3e1bb7321dff5
Alex Curran <alexcurran.va@gmail.com>
2023-05-30 09:32:13 -0500

    Working solution for day-26-call-func-with-custom-context.js

---
+++ js-30-challenge/day-26-call-func-with-custom-context.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n) args array-like obj
+ * 
+ * @param {Object} context
+ * @param {any[]} args
+ * @return {any}
+ */
+Function.prototype.callPolyfill = function(context, ...args) {
+    const func = this;
+
+    return func.apply(context, args);
+};
+
+
+function increment() { this.count++; return this.count; }
+console.log( increment.callPolyfill({count: 1}) ); // 2
