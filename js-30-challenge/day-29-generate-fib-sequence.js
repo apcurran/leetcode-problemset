fe5cd4948898e97ee970fb2c5dc9d5757edd2efd
Alex Curran <alexcurran.va@gmail.com>
2023-06-02 09:17:36 -0500

    Second solution

--- js-30-challenge/day-29-generate-fib-sequence.js
+++ js-30-challenge/day-29-generate-fib-sequence.js
@@ -1,23 +1,48 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(1)
+//  * space: O(1)
+//  * 
+//  * @return {Generator<number>}
+//  */
+// function* fibGenerator() {
+//     let a = 0;
+//     let b = 1;
+
+//     while (true) {
+//         yield a;
+
+//         const temp = a;
+//         a = b;
+//         b += temp;
+//     }
+// }
+
 /**
- * solution 1
- * time: O(1)
- * space: O(1)
+ * solution 2
+ * time: O(n)
+ * space: O(n)
  * 
  * @return {Generator<number>}
  */
-function* fibGenerator() {
-    let a = 0;
-    let b = 1;
+function fibGenerator() {
+    const SEQUENCE_LENGTH = 50;
+    let fibSequence = Array(SEQUENCE_LENGTH).fill(0);
+    // first elem is 0
+    // second elem is 1
+    // in fibonacci seq
+    fibSequence[1] = 1;
 
-    while (true) {
-        yield a;
-
-        const temp = a;
-        a = b;
-        b += temp;
+    for (let i = 2; i < fibSequence.length; i++) {
+        const firstPrevious = fibSequence[i - 1];
+        const secondPrevious = fibSequence[i - 2];
+        const currentFib = firstPrevious + secondPrevious;
+        fibSequence[i] = currentFib;
     }
+
+    return fibSequence[Symbol.iterator]();
 }
 
 const gen = fibGenerator();
