f3a327fb062b9d6cca1184958528f410ec01f08a
Alex Curran <alexcurran.va@gmail.com>
2023-06-03 10:14:36 -0500

    Small optimization

--- js-30-challenge/day-30-nested-array-gen.js
+++ js-30-challenge/day-30-nested-array-gen.js
@@ -9,20 +9,16 @@
  * @return {Generator}
  */
 function* inorderTraversal(arr) {
-    const oneDimensionalArr = arr.flat(Infinity);
-
-    for (let value of oneDimensionalArr) {
-        yield value;
-    }
+    yield* arr.flat(Infinity);
 }
 
 
-// const gen = inorderTraversal([1, [2, 3]]);
-// console.log( gen.next().value ); // 1
-// console.log( gen.next().value ); // 2
-// console.log( gen.next().value ); // 3
+const gen = inorderTraversal([1, [2, 3]]);
+console.log( gen.next().value ); // 1
+console.log( gen.next().value ); // 2
+console.log( gen.next().value ); // 3
 
-const gen = inorderTraversal([[[[[]]]]]);
-console.log( gen.next().value ); // []
-console.log( gen.next().value ); // []
-console.log( gen.next().value ); // []
+const gen2 = inorderTraversal([[[[[]]]]]);
+console.log( gen2.next().value ); // []
+console.log( gen2.next().value ); // []
+console.log( gen2.next().value ); // []
