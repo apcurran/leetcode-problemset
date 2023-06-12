55ebbde5cd6baf135ad260a681d5ece1a2ec7780
Alex Curran <alexcurran.va@gmail.com>
2023-06-12 16:30:01 -0500

    Working binary search solution for single-elem-in-a-sorted-arr.js

---
+++ medium/single-elem-in-a-sorted-arr.js
@@ -0,0 +1,43 @@
+"use strict";
+
+/**
+ * solution 1 -- binary search
+ * time: O(log n)
+ * space: O(1)
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
+function singleNonDuplicate(nums) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        const middle = left + Math.floor((right - left) / 2);
+        const currentElement = nums[middle];
+        const leftNeighbor = nums[middle - 1];
+        const rightNeighbor = nums[middle + 1];
+
+        if (middle - 1 < 0 ||
+            leftNeighbor !== currentElement) {
+            if (middle + 1 === nums.length ||
+                currentElement !== rightNeighbor) {
+                return nums[middle];
+            }
+        }
+
+        // otherwise this elem is a duplicate
+        const sizeOfLeftHalf = leftNeighbor === currentElement ? middle - 1 : middle;
+
+        if (sizeOfLeftHalf % 2 === 1) {
+            // left half is odd length,
+            // odd length half always contains the non-duplicate elem,
+            // so search the left half
+            right = middle - 1;
+        } else {
+            left = middle + 1;
+        }
+    }
+}
+
+console.log(singleNonDuplicate([1, 1, 2, 3, 3, 4, 4, 8, 8])); // 2
