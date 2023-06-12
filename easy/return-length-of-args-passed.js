a5fee64decae28eafbddd99b31e6adc95959412d
Alex Curran <alexcurran.va@gmail.com>
2023-06-12 11:37:21 -0500

    Working solution for return-length-of-args-passed.js

---
+++ easy/return-length-of-args-passed.js
@@ -0,0 +1,15 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {any[]} args
+ * @return {number}
+ */
+function argumentsLength(...args) {
+    return args.length;
+}
+
+console.log(argumentsLength(1, 2, 3)); // 3
