b17f4ace77869466b64cf7a1222d9cd3710ee405
Alex Curran <alexcurran.va@gmail.com>
2023-06-13 10:01:44 -0500

    Working solution for add-two-promises.js

---
+++ easy/add-two-promises.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ * solution 1 -- Promise.all()
+ * time: O(max(n, m))
+ * space: O(1)
+ * 
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+async function addTwoPromises(promise1, promise2) {
+    try {
+        const [promise1Value, promise2Value] = await Promise.all([promise1, promise2]);
+
+        return promise1Value + promise2Value;
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+addTwoPromises(Promise.resolve(2), Promise.resolve(2)).then(console.log); // 4
