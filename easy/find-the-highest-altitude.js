43329c254e8ffd80f60391212f00b4f677099f78
Alex Curran <alexcurran.va@gmail.com>
2023-06-19 09:34:21 -0500

    Working solution with O(1) memory improvement for find-the-highest-altitude.js

--- easy/find-the-highest-altitude.js
+++ easy/find-the-highest-altitude.js
@@ -1,24 +1,44 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {number[]} gain
+//  * @return {number}
+//  */
+// function largestAltitude(gain) {
+//     let altitudesArr = [0];
+
+//     for (let i = 0; i < gain.length; i++) {
+//         const currGainNum = gain[i];
+//         const currAltNum = altitudesArr[i];
+
+//         altitudesArr.push(currAltNum + currGainNum);
+//     }
+
+//     return Math.max(...altitudesArr);
+// }
+
 /**
- * solution 1
+ * solution 2 -- prefix sum -- optimized space complexity
  * time: O(n)
- * space: O(n)
+ * space: O(1)
  * 
  * @param {number[]} gain
  * @return {number}
  */
 function largestAltitude(gain) {
-    let altitudesArr = [0];
+    let currentAltitude = 0;
+    let highestPoint = 0;
 
-    for (let i = 0; i < gain.length; i++) {
-        const currGainNum = gain[i];
-        const currAltNum = altitudesArr[i];
-
-        altitudesArr.push(currAltNum + currGainNum);
+    for (let altitudeGain of gain) {
+        currentAltitude += altitudeGain;
+        highestPoint = Math.max(highestPoint, currentAltitude);
     }
 
-    return Math.max(...altitudesArr);
+    return highestPoint;
 }
 
 console.log( largestAltitude([-5, 1, 5, 0, -7]) ); // 1
