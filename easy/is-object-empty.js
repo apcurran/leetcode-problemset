3023a18fbdd24c62f5f4307a996e057c5f974949
Alex Curran <alexcurran.va@gmail.com>
2023-06-20 10:00:50 -0500

    Working O(n) time and O(1) space solution with iteration

--- easy/is-object-empty.js
+++ easy/is-object-empty.js
@@ -1,18 +1,37 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * n = number of key/value pairs (JSON stringification)
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {Object | Array} obj
+//  * @return {boolean}
+//  */
+// function isEmpty(obj) {
+//     const strObj = JSON.stringify(obj);
+
+//     return strObj === "{}" || strObj === "[]";
+// }
+
 /**
- * solution 1
- * n = number of key/value pairs (JSON stringification)
+ * solution 2
+ * n = number of key/value pairs
  * time: O(n)
- * space: O(n)
+ * space: O(1)
  * 
  * @param {Object | Array} obj
  * @return {boolean}
  */
 function isEmpty(obj) {
-    const strObj = JSON.stringify(obj);
+    for (let property in obj) {
+        // if there is any property,
+        // then this is not an empty object
+        return false;
+    }
 
-    return strObj === "{}" || strObj === "[]";
+    return true;
 }
 
 console.log( isEmpty({}) ); // true
