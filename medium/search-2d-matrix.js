0a009c0600c8c21232d471157938e423ad2f2d1b
Alex Curran <alexcurran.va@gmail.com>
2023-06-22 11:34:32 -0500

    Working solution that passes O(log(m * n)) time reqs for search-2d-matrix.js

--- medium/search-2d-matrix.js
+++ medium/search-2d-matrix.js
@@ -15,9 +15,54 @@
 //     return flattenedArr.includes(target);
 // }
 
+// /**
+//  * solution 2 -- Passes tests but not efficient enough for problem description
+//  * time: O(m * log n)
+//  * space: O(1)
+//  * 
+//  * @param {number[][]} matrix
+//  * @param {number} target
+//  * @return {boolean}
+//  */
+// function searchMatrix(matrix, target) {
+//     for (let row of matrix) {
+//         const isTargetInRow = binSearch(row, target);
+
+//         if (isTargetInRow) return true;
+//     }
+
+//     return false;
+// }
+
+// /**
+//  * @param {number[]} arr 
+//  * @param {number} target 
+//  * @returns {boolean}
+//  */
+// function binSearch(arr, target) {
+//     let leftPointer = 0;
+//     let rightPointer = arr.length - 1;
+
+//     while (leftPointer <= rightPointer) {
+//         const middlePointer = leftPointer + Math.floor((rightPointer - leftPointer) / 2);
+//         const value = arr[middlePointer];
+
+//         if (value < target) {
+//             leftPointer = middlePointer + 1;
+//         } else if (value > target) {
+//             rightPointer = middlePointer - 1;
+//         } else {
+//             // found target in row
+//             return true;
+//         }
+//     }
+
+//     return false;
+// }
+
 /**
- * solution 2 -- Passes tests but not efficient enough for problem description
- * time: O(m * log n)
+ * solution 2 -- double binary search
+ * time: O(log m * log n)
  * space: O(1)
  * 
  * @param {number[][]} matrix
@@ -25,34 +70,38 @@
  * @return {boolean}
  */
 function searchMatrix(matrix, target) {
-    for (let row of matrix) {
-        const isTargetInRow = binSearch(row, target);
+    let low = 0;
+    let high = matrix.length - 1;
 
-        if (isTargetInRow) return true;
-    }
-
-    return false;
-}
-
-/**
- * @param {number[]} arr 
- * @param {number} target 
- * @returns {boolean}
- */
-function binSearch(arr, target) {
-    let leftPointer = 0;
-    let rightPointer = arr.length - 1;
-
-    while (leftPointer <= rightPointer) {
-        const middlePointer = leftPointer + Math.floor((rightPointer - leftPointer) / 2);
-        const value = arr[middlePointer];
+    while (low <= high) {
+        const middleRowPointer = low + Math.floor((high - low) / 2);
+        const value = matrix[middleRowPointer][0];
 
         if (value < target) {
-            leftPointer = middlePointer + 1;
+            low = middleRowPointer + 1;
         } else if (value > target) {
-            rightPointer = middlePointer - 1;
+            high = middleRowPointer - 1;
         } else {
-            // found target in row
+            return true;
+        }
+    }
+
+    let row = high;
+
+    if (row < 0) return false;
+
+    let rowLow = 0;
+    let rowHigh = matrix[row].length - 1;
+
+    while (rowLow <= rowHigh) {
+        const middlePointer = rowLow + Math.floor((rowHigh - rowLow) / 2);
+        const value = matrix[row][middlePointer];
+
+        if (value < target) {
+            rowLow = middlePointer + 1;
+        } else if (value > target) {
+            rowHigh = middlePointer - 1;
+        } else {
             return true;
         }
     }
