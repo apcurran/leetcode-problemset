115ba5d1b5c731abd12b10edb9aa9c6e11e7cab6
Alex Curran <alexcurran.va@gmail.com>
2023-06-23 15:10:51 -0500

    Working secondary iterative solution using nested loops

--- medium/daily-temps.js
+++ medium/daily-temps.js
@@ -54,4 +54,33 @@ function dailyTemperatures(temperatures) {
     return results;
 }
 
+// /**
+//  * solution 2 -- iterative
+//  * time: O(n^2) -- slow, but passes tests
+//  * space: O(n) -- results array
+//  * 
+//  * @param {number[]} temperatures
+//  * @return {number[]}
+//  */
+// function dailyTemperatures(temperatures) {
+//     let results = new Array(temperatures.length).fill(0);
+
+//     outerLoop: for (let i = 0; i < temperatures.length; i++) {
+//         const currentTemp = temperatures[i];
+
+//         for (let j = i + 1; j < temperatures.length; j++) {
+//             const futureTemp = temperatures[j];
+
+//             if (futureTemp > currentTemp) {
+//                 const daysUntilTempIncreases = j - i;
+//                 results[i] = daysUntilTempIncreases;
+
+//                 continue outerLoop;
+//             }
+//         }
+//     }
+
+//     return results;
+// }
+
 console.log( dailyTemperatures([73,74,75,71,69,72,76,73]) ); // [1,1,4,2,1,1,0,0]
