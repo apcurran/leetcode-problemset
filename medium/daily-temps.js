3e0ea15b63d502feaa70f63745720007f7ca6231
Alex Curran <alexcurran.va@gmail.com>
2023-06-23 15:05:19 -0500

    Alt stack-based solution using tuple arrays

--- medium/daily-temps.js
+++ medium/daily-temps.js
@@ -1,7 +1,34 @@
 "use strict";
 
+// /**
+//  * solution 1 -- stack
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {number[]} temperatures
+//  * @return {number[]}
+//  */
+// function dailyTemperatures(temperatures) {
+//     let resArr = new Array(temperatures.length).fill(0);
+//     let stack = [];
+
+//     for (let i = 0; i < temperatures.length; i++) {
+//         const temp = temperatures[i];
+//         // is the stack empty AND is the temp greater than the temp at the top of the stack
+//         while (stack.length > 0 && temp > temperatures[stack[stack.length - 1]]) {
+//             const stackIndex = stack.pop();
+//             // subtract days
+//             resArr[stackIndex] = i - stackIndex;
+//         }
+
+//         stack.push(i);
+//     }
+
+//     return resArr;
+// }
+
 /**
- * solution 1 -- stack
+ * solution 1 -- stack alt using tuple arrays [temp, index]
  * time: O(n)
  * space: O(n)
  * 
@@ -9,22 +36,22 @@
  * @return {number[]}
  */
 function dailyTemperatures(temperatures) {
-    let resArr = new Array(temperatures.length).fill(0);
-    let stack = []; // pair [temp, index]
+    let results = new Array(temperatures.length).fill(0);
+    let stack = []; // [temp, index]
 
     for (let i = 0; i < temperatures.length; i++) {
         const temp = temperatures[i];
-        // is the stack empty AND is the temp greater than the temp at the top of the stack
-        while (stack.length > 0 && temp > temperatures[stack[stack.length - 1]]) {
-            const stackIndex = stack.pop();
-            // subtract days
-            resArr[stackIndex] = i - stackIndex;
+
+        while (stack.length > 0 && temp > stack[stack.length - 1][0]) {
+            const topOfStackIndex = (stack.pop())[1];
+            const daysUntilTempIncreases = i - topOfStackIndex;
+            results[topOfStackIndex] = daysUntilTempIncreases;
         }
 
-        stack.push(i);
+        stack.push([temp, i]);
     }
 
-    return resArr;
+    return results;
 }
 
 console.log( dailyTemperatures([73,74,75,71,69,72,76,73]) ); // [1,1,4,2,1,1,0,0]
