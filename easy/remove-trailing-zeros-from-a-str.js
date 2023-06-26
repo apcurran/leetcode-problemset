1b91370f8b8487ddc34ad6fd35b3006ffe3e77ca
Alex Curran <alexcurran.va@gmail.com>
2023-06-26 10:02:20 -0500

    Regex replace solution

--- easy/remove-trailing-zeros-from-a-str.js
+++ easy/remove-trailing-zeros-from-a-str.js
@@ -1,7 +1,39 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {string} numStr
+//  * @return {string}
+//  */
+// function removeTrailingZeros(numStr) {
+//     let isNonZeroNumHitYet = false;
+//     let result = [];
+
+//     for (let i = numStr.length - 1; i >= 0; i--) {
+//         const value = numStr[i];
+
+//         if (!isNonZeroNumHitYet) {
+//             // check for and skip 0s
+//             if (value === "0") {
+//                 continue;
+//             } else {
+//                 isNonZeroNumHitYet = true;
+//             }
+//         }
+
+//         result.push(value);
+//     }
+
+//     return result
+//             .reverse()
+//             .join("");
+// }
+
 /**
- * solution 1
+ * solution 2 -- regex
  * time: O(n)
  * space: O(n)
  * 
@@ -9,27 +41,7 @@
  * @return {string}
  */
 function removeTrailingZeros(numStr) {
-    let isNonZeroNumHitYet = false;
-    let result = [];
-
-    for (let i = numStr.length - 1; i >= 0; i--) {
-        const value = numStr[i];
-
-        if (!isNonZeroNumHitYet) {
-            // check for and skip 0s
-            if (value === "0") {
-                continue;
-            } else {
-                isNonZeroNumHitYet = true;
-            }
-        }
-
-        result.push(value);
-    }
-
-    return result
-            .reverse()
-            .join("");
+    return numStr.replace(/0+$/, "");
 }
 
 console.log( removeTrailingZeros("51230100") ); // "512301"
