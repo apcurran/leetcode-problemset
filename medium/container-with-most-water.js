78335b6ab1cc42974c5354bde373f6cd13fd4833
Alex Curran <alexcurran.va@gmail.com>
2023-06-26 11:55:10 -0500

    Add brute-force n^2 solution and clarification refactoring to opitimized two-pointer solution

--- medium/container-with-most-water.js
+++ medium/container-with-most-water.js
@@ -1,7 +1,30 @@
 "use strict";
 
+// /**
+//  * solution 1 -- brute-force
+//  * time: O(n^2)
+//  * space: O(1)
+//  * 
+//  * @param {number[]} heightsArr
+//  * @return {number}
+//  */
+// function maxArea(heightsArr) {
+//     let largestArea = 0;
+
+//     for (let left = 0; left < heightsArr.length; left++) {
+//         for (let right = left + 1; right < heightsArr.length; right++) {
+//             const width = right - left;
+//             const height = Math.min(heightsArr[left], heightsArr[right]);
+//             const currentArea = width * height;
+//             largestArea = Math.max(largestArea, currentArea);
+//         }
+//     }
+
+//     return largestArea;
+// }
+
 /**
- * solution 1 -- two-pointer
+ * solution 2 -- two-pointer
  * time: O(n)
  * space: O(1)
  * 
@@ -10,18 +33,16 @@
  */
 function maxArea(heightsArr) {
     let maxArea = 0;
-    // start at furthest left pos
     let leftPointer = 0;
-    // start at furthest right pos
     let rightPointer = heightsArr.length - 1;
 
     while (leftPointer < rightPointer) {
         const leftHeight = heightsArr[leftPointer];
         const rightHeight = heightsArr[rightPointer];
-        const currArea = (rightPointer - leftPointer) * Math.min(leftHeight, rightHeight);
+        const currentArea = (rightPointer - leftPointer) * Math.min(leftHeight, rightHeight);
+        maxArea = Math.max(maxArea, currentArea);
 
-        maxArea = Math.max(maxArea, currArea);
-
+        // move pointer on smaller value
         if (leftHeight < rightHeight) {
             leftPointer++;
         } else {
