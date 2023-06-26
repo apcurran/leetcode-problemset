e5f7dee9743b9408f3430a02e139d6ef0bd60ef7
Alex Curran <alexcurran.va@gmail.com>
2023-06-26 09:52:54 -0500

    Working solution for remove-trailing-zeros-from-a-str.js

---
+++ easy/remove-trailing-zeros-from-a-str.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} numStr
+ * @return {string}
+ */
+function removeTrailingZeros(numStr) {
+    let isNonZeroNumHitYet = false;
+    let result = [];
+
+    for (let i = numStr.length - 1; i >= 0; i--) {
+        const value = numStr[i];
+
+        if (!isNonZeroNumHitYet) {
+            // check for and skip 0s
+            if (value === "0") {
+                continue;
+            } else {
+                isNonZeroNumHitYet = true;
+            }
+        }
+
+        result.push(value);
+    }
+
+    return result
+            .reverse()
+            .join("");
+}
+
+console.log( removeTrailingZeros("51230100") ); // "512301"
