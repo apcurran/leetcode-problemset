952be738a3a77d97110219c54fddda6a167f534f
Alex Curran <alexcurran.va@gmail.com>
2023-06-28 12:09:30 -0500

    Working iterative DFS solution

--- easy/max-depth-of-binary-tree.js
+++ easy/max-depth-of-binary-tree.js
@@ -19,9 +19,44 @@
 //     return countOfSelfNode + maxCountofChildSubtrees;
 // }
 
+// /**
+//  * solution 2 -- BFS
+//  * time: O(n^2)
+//  * space: O(n)
+//  * 
+//  * @param {TreeNode} root
+//  * @return {number}
+//  */
+// function maxDepth(root) {
+//     if (root === null) return 0;
+
+//     let levelsCount = 0;
+//     let queue = [root];
+
+//     while (queue.length > 0) {
+//         const queueCurrentLength = queue.length;
+
+//         for (let i = 0; i < queueCurrentLength; i++) {
+//             const currentNode = queue.shift();
+
+//             if (currentNode.left !== null) {
+//                 queue.push(currentNode.left);
+//             }
+
+//             if (currentNode.right !== null) {
+//                 queue.push(currentNode.right);
+//             }
+//         }
+
+//         levelsCount++;
+//     }
+
+//     return levelsCount;
+// }
+
 /**
- * solution 2 -- BFS
- * time: O(n^2)
+ * solution 3 -- interative DFS
+ * time: O(n)
  * space: O(n)
  * 
  * @param {TreeNode} root
@@ -30,26 +65,18 @@
 function maxDepth(root) {
     if (root === null) return 0;
 
-    let levelsCount = 0;
-    let queue = [root];
+    let stack = [[root, 1]]; // [node, depth]
+    let maxDepthCount = 1;
 
-    while (queue.length > 0) {
-        const queueCurrentLength = queue.length;
+    while (stack.length > 0) {
+        const [node, nodeDepth] = stack.pop();
 
-        for (let i = 0; i < queueCurrentLength; i++) {
-            const currentNode = queue.shift();
+        // ignore any null values added to the stack
+        if (node === null) continue;
 
-            if (currentNode.left !== null) {
-                queue.push(currentNode.left);
-            }
-
-            if (currentNode.right !== null) {
-                queue.push(currentNode.right);
-            }
-        }
-
-        levelsCount++;
+        maxDepthCount = Math.max(maxDepthCount, nodeDepth);
+        stack.push([node.left, nodeDepth + 1], [node.right, nodeDepth + 1]);
     }
 
-    return levelsCount;
+    return maxDepthCount;
 }
