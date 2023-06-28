81ddf915ebd84fa1c0aac36f855830b5acdd22fe
Alex Curran <alexcurran.va@gmail.com>
2023-06-28 13:33:06 -0500

    Alternative style BFS with updated time/space complexity

--- medium/bin-tree-level-order-traversal.js
+++ medium/bin-tree-level-order-traversal.js
@@ -9,11 +9,46 @@
  * }
  */
 
+// /**
+//  * solution 1 -- iterative bfs (queue)
+//  * n = num of nodes
+//  * m = num of nodes in each level
+//  * time: O(n * m^2) -- m^2 due to use of array.shift() which has O(m) time complexity
+//  * space: O(n)
+//  * 
+//  * @param {TreeNode} root
+//  * @return {number[][]}
+//  */
+// function levelOrder(root) {
+//     if (root === null) return [];
+
+//     let queue = [root];
+//     let resArr = [];
+
+//     while (queue.length > 0) {
+//         // cache curr queue length
+//         const queueLen = queue.length;
+//         let currLevelArr = [];
+//         // iterate through curr level
+//         for (let i = 0; i < queueLen; i++) {
+//             const currNode = queue.shift();
+    
+//             if (currNode.left) queue.push(currNode.left);
+    
+//             if (currNode.right) queue.push(currNode.right);
+    
+//             currLevelArr.push(currNode.val);
+//         }
+
+//         resArr.push(currLevelArr);
+//     }
+
+//     return resArr;
+// }
+
 /**
- * solution 1 -- iterative bfs (queue)
- * n = num of nodes
- * m = num of nodes in each level
- * time: O(n * m^2) -- m^2 due to use of array.shift() which has O(m) time complexity
+ * solution 2 -- BFS
+ * time: O(n^2)
  * space: O(n)
  * 
  * @param {TreeNode} root
@@ -23,25 +58,27 @@ function levelOrder(root) {
     if (root === null) return [];
 
     let queue = [root];
-    let resArr = [];
+    let results = [];
 
     while (queue.length > 0) {
-        // cache curr queue length
-        const queueLen = queue.length;
-        let currLevelArr = [];
-        // iterate through curr level
-        for (let i = 0; i < queueLen; i++) {
-            const currNode = queue.shift();
-    
-            if (currNode.left) queue.push(currNode.left);
-    
-            if (currNode.right) queue.push(currNode.right);
-    
-            currLevelArr.push(currNode.val);
+        const currentQueueLength = queue.length;
+        let levelNodeValues = [];
+
+        for (let i = 0; i < currentQueueLength; i++) {
+            const node = queue.shift();
+            levelNodeValues.push(node.val);
+
+            if (node.left !== null) {
+                queue.push(node?.left);
+            }
+
+            if (node.right !== null) {
+                queue.push(node?.right);
+            }
         }
 
-        resArr.push(currLevelArr);
+        results.push(levelNodeValues);
     }
 
-    return resArr;
+    return results;
 }
