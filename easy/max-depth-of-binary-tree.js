90c7c8e4831081b0a8531dbde9e11e47d9624fb5
Alex Curran <alexcurran.va@gmail.com>
2023-06-28 11:37:00 -0500

    BFS solution

--- easy/max-depth-of-binary-tree.js
+++ easy/max-depth-of-binary-tree.js
@@ -1,9 +1,28 @@
 "use strict";
 
+// /**
+//  * solution 1 -- recursive DFS
+//  * time: O(n)
+//  * space: O(n) -- callstack
+//  * 
+//  * @param {TreeNode} root
+//  * @return {number}
+//  */
+// function maxDepth(root) {
+//     if (root === null) return 0;
+
+//     const leftSubtreeCount = maxDepth(root.left);
+//     const rightSubtreeCount = maxDepth(root.right);
+//     const maxCountofChildSubtrees = Math.max(leftSubtreeCount, rightSubtreeCount);
+//     const countOfSelfNode = 1;
+
+//     return countOfSelfNode + maxCountofChildSubtrees;
+// }
+
 /**
- * solution 1 -- recursive DFS
- * time: O(n)
- * space: O(n) -- callstack
+ * solution 2 -- BFS
+ * time: O(n^2)
+ * space: O(n)
  * 
  * @param {TreeNode} root
  * @return {number}
@@ -11,10 +30,26 @@
 function maxDepth(root) {
     if (root === null) return 0;
 
-    const leftSubtreeCount = maxDepth(root.left);
-    const rightSubtreeCount = maxDepth(root.right);
-    const maxCountofChildSubtrees = Math.max(leftSubtreeCount, rightSubtreeCount);
-    const countOfSelfNode = 1;
+    let levelsCount = 0;
+    let queue = [root];
 
-    return countOfSelfNode + maxCountofChildSubtrees;
+    while (queue.length > 0) {
+        const queueCurrentLength = queue.length;
+
+        for (let i = 0; i < queueCurrentLength; i++) {
+            const currentNode = queue.shift();
+
+            if (currentNode.left !== null) {
+                queue.push(currentNode.left);
+            }
+
+            if (currentNode.right !== null) {
+                queue.push(currentNode.right);
+            }
+        }
+
+        levelsCount++;
+    }
+
+    return levelsCount;
 }
