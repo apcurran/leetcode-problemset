83d2908ce5da741fde28afbe7544b1293f3eac63
Alex Curran <alexcurran.va@gmail.com>
2023-06-29 15:18:23 -0500

    Optimize valid-anagrams.js with a hashmap solution

--- easy/valid-anagram.js
+++ easy/valid-anagram.js
@@ -1,8 +1,30 @@
 "use strict";
 
+// /**
+//  * solution 1 -- sort and compare
+//  * time: O((s * log s) + (t * log t))
+//  * space: O(s + t)
+//  * 
+//  * @param {string} s
+//  * @param {string} t
+//  * @return {boolean}
+//  */
+// function isAnagram(s, t) {
+//     const sSorted = s
+//                         .split("")
+//                         .sort()
+//                         .join("");
+//     const tSorted = t
+//                         .split("")
+//                         .sort()
+//                         .join("");
+
+//     return sSorted === tSorted;
+// }
+
 /**
- * solution 1 -- sort and compare
- * time: O((s * log s) + (t * log t))
+ * solution 1 -- hashmaps
+ * time: O(s + t)
  * space: O(s + t)
  * 
  * @param {string} s
@@ -10,16 +32,32 @@
  * @return {boolean}
  */
 function isAnagram(s, t) {
-    const sSorted = s
-                        .split("")
-                        .sort()
-                        .join("");
-    const tSorted = t
-                        .split("")
-                        .sort()
-                        .join("");
+    let sLetters = new Map();
 
-    return sSorted === tSorted;
+    for (let char of s) {
+        const previousCount = sLetters.get(char) || 0;
+        sLetters.set(char, previousCount + 1);
+    }
+
+    let tLetters = new Map();
+
+    for (let char of t) {
+        const previousCount = tLetters.get(char) || 0;
+        tLetters.set(char, previousCount + 1);
+    }
+
+    // now compare maps
+    const largestLettersCache = sLetters.size >= tLetters.size ? sLetters : tLetters;
+
+    for (let tuple of largestLettersCache) {
+        const char = tuple[0];
+        const sCharCount = sLetters.get(char);
+        const tCharCount = tLetters.get(char);
+
+        if (sCharCount !== tCharCount) return false;
+    }
+
+    return true;
 }
 
 console.log( isAnagram("anagram", "nagaram") ); // true
