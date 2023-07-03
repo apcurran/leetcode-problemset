ba45be5fe861788a696b7e31db84ec4158b0c42d
Alex Curran <alexcurran.va@gmail.com>
2023-07-03 14:47:41 -0500

    Stack-based solution for gen-parens.js

--- medium/gen-parens.js
+++ medium/gen-parens.js
@@ -1,7 +1,42 @@
 "use strict";
 
+// /**
+//  * solution 1 -- recursion
+//  * time: O(2^n)
+//  * space: O(n)
+//  * 
+//  * @param {number} n
+//  * @return {string[]}
+//  */
+// function generateParenthesis(n) {
+//     let results = [];
+
+//     function recurse(openN, closedN, parenthesisStr) {
+//         // only add if openN is greater -- this prevents invalid paren combos
+//         if (openN > closedN) return;
+
+//         if (openN === 0 && closedN === 0) {
+//             results.push(parenthesisStr);
+
+//             return;
+//         }
+
+//         if (openN > 0) {
+//             recurse(openN - 1, closedN, parenthesisStr + "(");
+//         }
+
+//         if (closedN > 0) {
+//             recurse(openN, closedN - 1, parenthesisStr + ")");
+//         }
+//     }
+
+//     recurse(n, n, "");
+
+//     return results;
+// }
+
 /**
- * solution 1 -- recursion
+ * solution 2 -- recursion (stack)
  * time: O(2^n)
  * space: O(n)
  * 
@@ -10,27 +45,33 @@
  */
 function generateParenthesis(n) {
     let results = [];
+    let stack = [];
 
-    function recurse(openN, closedN, parenthesisStr) {
-        // only add if openN is greater -- this prevents invalid paren combos
-        if (openN > closedN) return;
-
-        if (openN === 0 && closedN === 0) {
-            results.push(parenthesisStr);
-
-            return;
+    /**
+     * @param {number} openN 
+     * @param {number} closedN 
+     * @returns {void}
+     */
+    function recurse(openN, closedN) {
+        if (openN === closedN && closedN === n) {
+            const completedParensCombination = stack.join("");
+            results.push(completedParensCombination);
         }
 
-        if (openN > 0) {
-            recurse(openN - 1, closedN, parenthesisStr + "(");
+        if (openN < n) {
+            stack.push("(");
+            recurse(openN + 1, closedN);
+            stack.pop();
         }
 
-        if (closedN > 0) {
-            recurse(openN, closedN - 1, parenthesisStr + ")");
+        if (closedN < openN) {
+            stack.push(")");
+            recurse(openN, closedN + 1);
+            stack.pop();
         }
     }
 
-    recurse(n, n, "");
+    recurse(0, 0);
 
     return results;
 }
