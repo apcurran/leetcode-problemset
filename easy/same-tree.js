70b09c22da2734a4564f9710d9cacc23ac8c0419
Alex Curran <alexcurran.va@gmail.com>
2023-07-06 14:40:49 -0500

    Uncomment constructor func

--- easy/same-tree.js
+++ easy/same-tree.js
@@ -1,13 +1,11 @@
 "use strict";
 
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
+// Definition for a binary tree node.
+function TreeNode(val, left, right) {
+    this.val = (val===undefined ? 0 : val);
+    this.left = (left===undefined ? null : left);
+    this.right = (right===undefined ? null : right);
+}
 
 /**
  * n = num of nodes in tree
