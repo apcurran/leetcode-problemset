a5f90a53786c572194fc35f79b19733cf6f2bba5
Alex Curran <alexcurran.va@gmail.com>
2023-07-10 15:38:45 -0500

    Working solution for insert-interval.js

---
+++ medium/insert-interval.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number[][]} intervals
+ * @param {number[]} newInterval
+ * @return {number[][]}
+ */
+function insert(intervals, newInterval) {
+    let [newIntervalStart, newIntervalEnd] = newInterval;
+    let left = [];
+    let right = [];
+
+    for (let interval of intervals) {
+        const [currentIntervalStart, currentIntervalEnd] = interval;
+
+        if (currentIntervalEnd < newIntervalStart) {
+            left.push(interval);
+        } else if (currentIntervalStart > newIntervalEnd) {
+            right.push(interval);
+        } else {
+            // merge overlaps
+            newIntervalStart = Math.min(newIntervalStart, currentIntervalStart);
+            newIntervalEnd = Math.max(newIntervalEnd, currentIntervalEnd);
+        }
+    }
+
+    return [...left, [newIntervalStart, newIntervalEnd], ...right];
+}
+
+console.log( insert([[1,3],[6,9]], [2,5]) ); // [[1,5],[6,9]]
