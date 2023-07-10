0a7a2eb27a9a77f2feb170fe88bbef867cac1edd
Alex Curran <alexcurran.va@gmail.com>
2023-07-10 09:36:05 -0500

    Working recursive solution for min-depth-of-bin-tree.js

--- easy/min-depth-of-bin-tree.js
+++ easy/min-depth-of-bin-tree.js
@@ -24,9 +24,52 @@ c.right = e;
 //            /  \
 //           15   7
 
+// /**
+//  * solution 1 -- BFS
+//  * time: O(n^2)
+//  * space: O(n)
+//  * 
+//  * @param {TreeNode} root
+//  * @return {number}
+//  */
+// function minDepth(root) {
+//     if (root === null) return 0;
+
+//     let queue = [root];
+//     let currentLevel = 1; // start with the count of the first root node
+
+//     while (queue.length > 0) {
+//         // search this tree level
+//         let queueLen = queue.length;
+
+//         for (let i = 0; i < queueLen; i++) {
+//             const node = queue.shift();
+
+//             if (node.left === null && node.right === null) {
+//                 // leaf node -> end the func
+//                 return currentLevel;
+//             }
+            
+//             if (node.left !== null) {
+//                 // add child
+//                 queue.push(node?.left);
+//             }
+
+//             if (node.right !== null) {
+//                 // add child
+//                 queue.push(node?.right);
+//             }
+//         }
+
+//         currentLevel++;
+//     }
+
+//     return 0; // this should not ever be reached
+// }
+
 /**
- * solution 1 -- BFS
- * time: O(n^2)
+ * solution 2 -- recursive DFS
+ * time: O(n)
  * space: O(n)
  * 
  * @param {TreeNode} root
@@ -35,36 +78,17 @@ c.right = e;
 function minDepth(root) {
     if (root === null) return 0;
 
-    let queue = [root];
-    let currentLevel = 1; // start with the count of the first root node
+    const currentNodeCount = 1;
 
-    while (queue.length > 0) {
-        // search this tree level
-        let queueLen = queue.length;
-
-        for (let i = 0; i < queueLen; i++) {
-            const node = queue.shift();
-
-            if (node.left === null && node.right === null) {
-                // leaf node -> end the func
-                return currentLevel;
-            }
-            
-            if (node.left !== null) {
-                // add child
-                queue.push(node?.left);
-            }
-
-            if (node.right !== null) {
-                // add child
-                queue.push(node?.right);
-            }
-        }
-
-        currentLevel++;
+    if (root.left === null) {
+        return currentNodeCount + minDepth(root.right);
     }
 
-    return 0; // this should not ever be reached
+    if (root.right === null) {
+        return currentNodeCount + minDepth(root.left);
+    }
+
+    return currentNodeCount + Math.min(minDepth(root.left), minDepth(root.right));
 }
 
 console.log( minDepth(a) ); // 2
