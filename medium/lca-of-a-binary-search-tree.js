b4c1b5ddeb98a64d1f83b797d882ba741505714f
Alex Curran <alexcurran.va@gmail.com>
2023-07-11 15:04:39 -0500

    Recursive solution for lca-of-a-binary-search-tree.js

--- medium/lca-of-a-binary-search-tree.js
+++ medium/lca-of-a-binary-search-tree.js
@@ -6,10 +6,37 @@ function TreeNode(val) {
     this.left = this.right = null;
 }
 
+// /**
+//  * solution 1 -- DFS iterative
+//  * time: O(log n) -- halving each time until LCA node is found
+//  * space: O(1)
+//  * 
+//  * @param {TreeNode} root
+//  * @param {TreeNode} p
+//  * @param {TreeNode} q
+//  * @return {TreeNode}
+//  */
+// function lowestCommonAncestor(root, p, q) {
+//     let currentNode = root;
+
+//     while (currentNode !== null) {
+//         if (p.val < currentNode.val && q.val < currentNode.val) {
+//             // move left
+//             currentNode = currentNode.left;
+//         } else if (p.val > currentNode.val && q.val > currentNode.val) {
+//             // move right
+//             currentNode = currentNode.right;
+//         } else {
+//             // found LCA node
+//             return currentNode;
+//         }
+//     }
+// }
+
 /**
- * solution 1 -- DFS iterative
+ * solution 1 -- DFS recursion
  * time: O(log n) -- halving each time until LCA node is found
- * space: O(1)
+ * space: O(log n)
  * 
  * @param {TreeNode} root
  * @param {TreeNode} p
@@ -17,19 +44,15 @@ function TreeNode(val) {
  * @return {TreeNode}
  */
 function lowestCommonAncestor(root, p, q) {
-    let currentNode = root;
-
-    while (currentNode !== null) {
-        if (p.val < currentNode.val && q.val < currentNode.val) {
-            // move left
-            currentNode = currentNode.left;
-        } else if (p.val > currentNode.val && q.val > currentNode.val) {
-            // move right
-            currentNode = currentNode.right;
-        } else {
-            // found LCA node
-            return currentNode;
-        }
+    if (p.val < root.val && q.val < root.val) {
+        // move left
+        return lowestCommonAncestor(root.left, p, q);
+    } else if (p.val > root.val && q.val > root.val) {
+        // move right
+        return lowestCommonAncestor(root.right, p, q);
+    } else {
+        // found LCA node
+        return root;
     }
 }
 
