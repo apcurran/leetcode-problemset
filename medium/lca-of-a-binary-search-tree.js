fa46a92d39d0baccea1453ea2db9996eade42199
Alex Curran <alexcurran.va@gmail.com>
2023-07-11 15:01:35 -0500

    Working iterative solution for lca-of-a-binary-search-tree.js

---
+++ medium/lca-of-a-binary-search-tree.js
@@ -0,0 +1,55 @@
+"use strict";
+
+// Definition for a binary tree node.
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+
+/**
+ * solution 1 -- DFS iterative
+ * time: O(log n) -- halving each time until LCA node is found
+ * space: O(1)
+ * 
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+function lowestCommonAncestor(root, p, q) {
+    let currentNode = root;
+
+    while (currentNode !== null) {
+        if (p.val < currentNode.val && q.val < currentNode.val) {
+            // move left
+            currentNode = currentNode.left;
+        } else if (p.val > currentNode.val && q.val > currentNode.val) {
+            // move right
+            currentNode = currentNode.right;
+        } else {
+            // found LCA node
+            return currentNode;
+        }
+    }
+}
+
+const a = new TreeNode(6);
+const b = new TreeNode(2); 
+const c = new TreeNode(8); 
+const d = new TreeNode(0); 
+const e = new TreeNode(4); 
+const f = new TreeNode(3); 
+const g = new TreeNode(5); 
+const h = new TreeNode(7); 
+const i = new TreeNode(9); 
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+e.left = f;
+e.right = g;
+c.left = h;
+c.right = i;
+
+console.log( lowestCommonAncestor(a, b, c) ); // node -> val === 6
