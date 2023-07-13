f710d753954bab7f01c91b17cc9153cf03c35add
Alex Curran <alexcurran.va@gmail.com>
2023-07-13 10:36:23 -0500

    Optimize solution 2 for lexicographically-smallest-palindrome.js, avoids unnecessary work

--- easy/lexicographically-smallest-palindrome.js
+++ easy/lexicographically-smallest-palindrome.js
@@ -1,7 +1,50 @@
 "use strict";
 
+// /**
+//  * solution 1 -- two pointers
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {string} str
+//  * @return {string}
+//  */
+// function makeSmallestPalindrome(str) {
+//     const middle = Math.floor(str.length / 2);
+//     let palindromeLettersLeft = [];
+//     let left = 0;
+//     let right = str.length - 1;
+    
+//     while (left < right) {
+//         const leftChar = str[left];
+//         const rightChar = str[right];
+
+//         if (leftChar !== rightChar) {
+//             // get smallest letter
+//             const leftCharCode = leftChar.charCodeAt(0);
+//             const rightCharCode = rightChar.charCodeAt(0);
+//             const smallerChar = leftCharCode < rightCharCode ? leftChar : rightChar;
+//             palindromeLettersLeft.push(smallerChar);
+//         } else {
+//             palindromeLettersLeft.push(leftChar);
+//         }
+
+//         left++;
+//         right--;
+//     }
+
+//     const leftHalf = palindromeLettersLeft.join("");
+//     const middleLetter = str[middle];
+//     const rightHalf = palindromeLettersLeft.reverse().join("");
+
+//     if (str.length % 2 === 0) {
+//         return leftHalf + rightHalf;
+//     } else {
+//         return leftHalf + middleLetter + rightHalf;
+//     }
+// }
+
 /**
- * solution 1 -- two pointers
+ * solution 2 -- optimized
  * time: O(n)
  * space: O(n)
  * 
@@ -9,38 +52,18 @@
  * @return {string}
  */
 function makeSmallestPalindrome(str) {
-    const middle = Math.floor(str.length / 2);
-    let palindromeLettersLeft = [];
-    let left = 0;
-    let right = str.length - 1;
-    
-    while (left < right) {
-        const leftChar = str[left];
-        const rightChar = str[right];
+    const n = str.length;
+    let result = "";
 
-        if (leftChar !== rightChar) {
-            // get smallest letter
-            const leftCharCode = leftChar.charCodeAt(0);
-            const rightCharCode = rightChar.charCodeAt(0);
-            const smallerChar = leftCharCode < rightCharCode ? leftChar : rightChar;
-            palindromeLettersLeft.push(smallerChar);
-        } else {
-            palindromeLettersLeft.push(leftChar);
-        }
-
-        left++;
-        right--;
+    for (let i = 0; i < n; i++) {
+        const currentCharCode = (str[i]).charCodeAt(0);
+        const mirroredCharCode = (str[n - i - 1]).charCodeAt(0);
+        const smallerCharCode = Math.min(currentCharCode, mirroredCharCode);
+        const smallerChar = String.fromCharCode(smallerCharCode);
+        result += smallerChar;
     }
 
-    const leftHalf = palindromeLettersLeft.join("");
-    const middleLetter = str[middle];
-    const rightHalf = palindromeLettersLeft.reverse().join("");
-
-    if (str.length % 2 === 0) {
-        return leftHalf + rightHalf;
-    } else {
-        return leftHalf + middleLetter + rightHalf;
-    }
+    return result;
 }
 
 console.log( makeSmallestPalindrome("egcfe") ); // "efcfe"
