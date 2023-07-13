33e0044073b46bfc2eb1b09f9a4feca8d1f6cd79
Alex Curran <alexcurran.va@gmail.com>
2023-07-13 11:22:00 -0500

    Optimal solution with prefix and postfix sums for find-the-middle-index-in-array.js

--- easy/find-the-middle-index-in-array.js
+++ easy/find-the-middle-index-in-array.js
@@ -1,21 +1,46 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n^2)
+//  * space: O(n)
+//  * 
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// function findMiddleIndex(nums) {
+//     for (let i = 0; i < nums.length; i++) {
+//         const leftNums = nums.slice(0, i);
+//         const rightNums = nums.slice(i + 1);
+//         const leftNumsSum = leftNums.reduce((sum, currentNum) => sum + currentNum, 0);
+//         const rightNumsSum = rightNums.reduce((sum, currentNum) => sum + currentNum, 0);
+
+//         if (leftNumsSum === rightNumsSum) return i;
+//     }
+
+//     // default if no index is found
+//     return -1;
+// }
+
 /**
- * solution 1
- * time: O(n^2)
- * space: O(n)
+ * solution 2 -- optimized (prefix and postfix values)
+ * time: O(n)
+ * space: O(1)
  * 
  * @param {number[]} nums
  * @return {number}
  */
 function findMiddleIndex(nums) {
-    for (let i = 0; i < nums.length; i++) {
-        const leftNums = nums.slice(0, i);
-        const rightNums = nums.slice(i + 1);
-        const leftNumsSum = leftNums.reduce((sum, currentNum) => sum + currentNum, 0);
-        const rightNumsSum = rightNums.reduce((sum, currentNum) => sum + currentNum, 0);
+    const numsSum = nums.reduce((sum, currentNum) => sum + currentNum, 0);
+    let leftSum = 0;
 
-        if (leftNumsSum === rightNumsSum) return i;
+    for (let i = 0; i < nums.length; i++) {
+        const currentNum = nums[i];
+        const rightSumWithoutCurrentNum = numsSum - leftSum - currentNum;
+
+        if (leftSum === rightSumWithoutCurrentNum) return i;
+
+        leftSum += currentNum;
     }
 
     // default if no index is found
