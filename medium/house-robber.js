a94c0319d785d979df789b1cd0ea6c15c70a6a55
Alex Curran <alexcurran.va@gmail.com>
2023-07-13 15:26:19 -0500

    TLE, but correct, recursive solution for house-robber.js

--- medium/house-robber.js
+++ medium/house-robber.js
@@ -1,25 +1,51 @@
 "use strict";
 
 /**
- * solution 1 -- DP
- * time: O(n)
- * space: O(1)
+ * solution 1 -- recursion top-down (TLE)
+ * time: O(2^n)
+ * space: O(n)
  * 
  * @param {number[]} nums
  * @return {number}
  */
 function rob(nums) {
-    let rob1 = 0;
-    let rob2 = 0;
-
-    // [rob1, rob2, n, n + 1, ...]
-    for (let num of nums) {
-        const temp = Math.max(num + rob1, rob2);
-        rob1 = rob2;
-        rob2 = temp;
-    }
-
-    return rob2;
+    return getRobberyTotal(nums, nums.length - 1);    
 }
+
+/**
+ * @param {number[]} nums 
+ * @param {number} i
+ * @returns {number}
+ */
+function getRobberyTotal(nums, i) {
+    if (i < 0) return 0;
+
+    const totalOfCurrentPlusHouseBeforeLastLoot = nums[i] + getRobberyTotal(nums, i - 2);
+    const totalOfPreviousHouseLoot = getRobberyTotal(nums, i - 1);
+
+    return Math.max(totalOfCurrentPlusHouseBeforeLastLoot, totalOfPreviousHouseLoot); 
+}
+
+// /**
+//  * solution 3 -- DP
+//  * time: O(n)
+//  * space: O(1)
+//  * 
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// function rob(nums) {
+//     let rob1 = 0;
+//     let rob2 = 0;
+
+//     // [rob1, rob2, n, n + 1, ...]
+//     for (let num of nums) {
+//         const temp = Math.max(num + rob1, rob2);
+//         rob1 = rob2;
+//         rob2 = temp;
+//     }
+
+//     return rob2;
+// }
 
 console.log( rob([1, 2, 3, 1]) ); // 4
