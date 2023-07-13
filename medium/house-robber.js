3c2c806fcfc5b1d2094081844ae243f49be7f563
Alex Curran <alexcurran.va@gmail.com>
2023-07-13 16:22:50 -0500

    Bottom-up with cache approach

--- medium/house-robber.js
+++ medium/house-robber.js
@@ -26,39 +26,39 @@
 //     return Math.max(totalOfCurrentPlusHouseBeforeLastLoot, totalOfPreviousHouseLoot); 
 // }
 
-/**
- * solution 2 -- recursion top-down with caching
- * time: O(n)
- * space: O(n)
- * 
- * @param {number[]} nums
- * @return {number}
- */
-function rob(nums) {
-    return getRobberyTotal(nums, nums.length - 1);    
-}
+// /**
+//  * solution 2 -- recursion top-down with caching
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// function rob(nums) {
+//     return getRobberyTotal(nums, nums.length - 1);    
+// }
 
-/**
- * @param {number[]} nums 
- * @param {number} i
- * @returns {number}
- */
-function getRobberyTotal(nums, i, cache = new Map()) {
-    if (i < 0) return 0;
+// /**
+//  * @param {number[]} nums 
+//  * @param {number} i
+//  * @returns {number}
+//  */
+// function getRobberyTotal(nums, i, cache = new Map()) {
+//     if (i < 0) return 0;
 
-    // check cache for pre-computed value
-    if (cache.has(i)) {
-        return cache.get(i);
-    }
+//     // check cache for pre-computed value
+//     if (cache.has(i)) {
+//         return cache.get(i);
+//     }
 
-    const totalOfCurrentPlusHouseBeforeLastLoot = nums[i] + getRobberyTotal(nums, i - 2, cache);
-    const totalOfPreviousHouseLoot = getRobberyTotal(nums, i - 1, cache);
-    const maxOfBothRobberyOptions = Math.max(totalOfCurrentPlusHouseBeforeLastLoot, totalOfPreviousHouseLoot);
-    // update cache
-    cache.set(i, maxOfBothRobberyOptions);
+//     const totalOfCurrentPlusHouseBeforeLastLoot = nums[i] + getRobberyTotal(nums, i - 2, cache);
+//     const totalOfPreviousHouseLoot = getRobberyTotal(nums, i - 1, cache);
+//     const maxOfBothRobberyOptions = Math.max(totalOfCurrentPlusHouseBeforeLastLoot, totalOfPreviousHouseLoot);
+//     // update cache
+//     cache.set(i, maxOfBothRobberyOptions);
 
-    return maxOfBothRobberyOptions;
-}
+//     return maxOfBothRobberyOptions;
+// }
 
 // /**
 //  * solution 3 -- DP
@@ -69,17 +69,41 @@ function getRobberyTotal(nums, i, cache = new Map()) {
 //  * @return {number}
 //  */
 // function rob(nums) {
-//     let rob1 = 0;
-//     let rob2 = 0;
+//     if (nums.length === 0) return 0;
 
-//     // [rob1, rob2, n, n + 1, ...]
-//     for (let num of nums) {
-//         const temp = Math.max(num + rob1, rob2);
-//         rob1 = rob2;
-//         rob2 = temp;
+//     let memo = new Array(nums.length + 1);
+//     memo[0] = 0;
+//     memo[1] = nums[0];
+
+//     for (let i = 1; i < nums.length; i++) {
+//         const currentValue = nums[i];
+//         const maxAmount = Math.max(memo[i], currentValue + memo[i - 1]);
+//         memo[i + 1] = maxAmount;
 //     }
 
-//     return rob2;
+//     return memo[nums.length];
 // }
+
+/**
+ * solution 3 -- DP
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
+function rob(nums) {
+    let rob1 = 0;
+    let rob2 = 0;
+
+    // [rob1, rob2, n, n + 1, ...]
+    for (let num of nums) {
+        const temp = Math.max(num + rob1, rob2);
+        rob1 = rob2;
+        rob2 = temp;
+    }
+
+    return rob2;
+}
 
 console.log( rob([1, 2, 3, 1]) ); // 4
