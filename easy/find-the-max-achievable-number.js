1ba33b71ec198ec38a45cbde67f283da064efde8
Alex Curran <alexcurran.va@gmail.com>
2023-07-14 14:18:19 -0500

    Working solution for find-the-max-achievable-number.js

---
+++ easy/find-the-max-achievable-number.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(1)
+ * space: O(1)
+ * 
+ * @param {number} num
+ * @param {number} t
+ * @return {number}
+ */
+function theMaximumAchievableX(num, t) {
+    return num + t + t;
+}
+
+console.log( theMaximumAchievableX(4, 1) ); // 6
