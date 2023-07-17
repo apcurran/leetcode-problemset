e9f2210aff84c012034f673fe13cd0d8d4303f45
Alex Curran <alexcurran.va@gmail.com>
2023-07-17 11:53:30 -0500

    Bottom-up tabulation dp solution for coin-change.js

--- medium/coin-change.js
+++ medium/coin-change.js
@@ -37,47 +37,79 @@
 //     return minTotalCoins;
 // }
 
+// /**
+//  * solution 2 -- memoized DP top-down
+//  * n = number of coins in array
+//  * a = amount
+//  * time: O(n * a)
+//  * space: O(n)
+//  * 
+//  * @param {number[]} coins
+//  * @param {number} amount
+//  * @return {number}
+//  */
+// function coinChange(coins, amount, cache = new Map()) {
+//     // base case
+//     if (amount === 0) return 0;
+
+//     if (amount < 0) return -1;
+
+//     if (cache.has(amount)) {
+//         return cache.get(amount);
+//     }
+
+//     let minTotalCoins = -1;
+
+//     // recursive case
+//     for (let coin of coins) {
+//         const updatedAmount = amount - coin;
+//         const coinCount = coinChange(coins, updatedAmount, cache);
+        
+//         if (coinCount >= 0) {
+//             // coinCount + 1 to include this current coin for total coins
+//             if (minTotalCoins < 0) {
+//                 minTotalCoins = coinCount + 1;
+//             } else {
+//                 minTotalCoins = Math.min(minTotalCoins, coinCount + 1);
+//             }
+//         }
+//     }
+
+//     cache.set(amount, minTotalCoins);
+    
+//     return minTotalCoins;
+// }
+
 /**
- * solution 2 -- memoized DP top-down
+ * solution 3 -- bottom-up tabulation
  * n = number of coins in array
  * a = amount
- * time: O(n * a)
- * space: O(n)
+ * time: O(m * c)
+ * space: O(m)
  * 
  * @param {number[]} coins
  * @param {number} amount
  * @return {number}
  */
-function coinChange(coins, amount, cache = new Map()) {
-    // base case
-    if (amount === 0) return 0;
+function coinChange(coins, amount) {
+    let memo = new Array(amount + 1).fill(Infinity);
+    memo[0] = 0;
 
-    if (amount < 0) return -1;
-
-    if (cache.has(amount)) {
-        return cache.get(amount);
-    }
-
-    let minTotalCoins = -1;
-
-    // recursive case
-    for (let coin of coins) {
-        const updatedAmount = amount - coin;
-        const coinCount = coinChange(coins, updatedAmount, cache);
-        
-        if (coinCount >= 0) {
-            // coinCount + 1 to include this current coin for total coins
-            if (minTotalCoins < 0) {
-                minTotalCoins = coinCount + 1;
-            } else {
-                minTotalCoins = Math.min(minTotalCoins, coinCount + 1);
+    for (let i = 1; i < memo.length; i++) {
+        for (let coin of coins) {
+            if (i - coin >= 0) {
+                memo[i] = Math.min(memo[i], memo[i - coin] + 1);
             }
         }
     }
 
-    cache.set(amount, minTotalCoins);
+    const result = memo[amount];
     
-    return minTotalCoins;
+    if (result === Infinity) {
+        return -1;
+    } else {
+        return result;
+    }
 }
 
 console.log( coinChange([1], 0) ); // 0
