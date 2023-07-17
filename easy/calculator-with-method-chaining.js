7a1dd18a54d0e15ba5871e3e1075514c8e957bf1
Alex Curran <alexcurran.va@gmail.com>
2023-07-17 10:30:26 -0500

    Working solution for calculator-with-method-chaining.js

---
+++ easy/calculator-with-method-chaining.js
@@ -0,0 +1,76 @@
+"use strict";
+
+class Calculator {
+    /** 
+     * @param {number} value
+     */
+    constructor(value) {
+        this.current = value;
+    }
+
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    add(value) {
+        this.current += value;
+
+        return this;
+    }
+
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    subtract(value) {
+        this.current -= value;
+
+        return this;
+    }
+
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    multiply(value) {
+        this.current *= value;
+
+        return this;
+    }
+
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    divide(value) {
+        if (value === 0) {
+            throw new Error("Division by zero is not allowed");
+        }
+
+        this.current /= value;
+
+        return this;
+    }
+
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    power(value) {
+        const updatedCurrent = this.current ** value;
+        this.current = updatedCurrent;
+
+        return this;
+    }
+
+    /** 
+     * @return {number}
+     */
+    getResult() {
+        return this.current;
+    }
+}
+
+new Calculator(10)
+        .power(5)
+        .getResult(); // 10 + 5 - 7 = 8
