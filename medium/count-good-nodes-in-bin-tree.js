fccca626191f97637ac5f421b13efcacaece77c9
Alex Curran <alexcurran.va@gmail.com>
2023-07-21 14:48:07 -0500

    Working iterative solution for count-good-nodes-in-bin-tree.js

--- medium/count-good-nodes-in-bin-tree.js
+++ medium/count-good-nodes-in-bin-tree.js
@@ -7,8 +7,41 @@ function TreeNode(val, left, right) {
     this.right = (right===undefined ? null : right);
 }
 
+// /**
+//  * solution 1 -- recursion
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {TreeNode} root
+//  * @return {number}
+//  */
+// function goodNodes(root) {
+//     let goodNodesCount = 0;
+
+//     /**
+//      * @param {TreeNode} root 
+//      * @param {number} maxValue 
+//      * @returns {void}
+//      */
+//     function dfs(root, maxValue) {
+//         if (root === null) return;
+    
+//         if (root.val >= maxValue) {
+//             maxValue = root.val;
+//             goodNodesCount++;
+//         }
+
+//         dfs(root.left, maxValue);
+//         dfs(root.right, maxValue);
+//     }
+
+//     dfs(root, root.val);
+
+//     return goodNodesCount;
+// }
+
 /**
- * solution 1 -- recursion
+ * solution 2 -- iterative
  * time: O(n)
  * space: O(n)
  * 
@@ -17,26 +50,25 @@ function TreeNode(val, left, right) {
  */
 function goodNodes(root) {
     let goodNodesCount = 0;
+    let stack = [[root, root.val]];
 
-    /**
-     * @param {TreeNode} root 
-     * @param {number} maxValue 
-     * @returns {void}
-     */
-    function dfs(root, maxValue) {
-        if (root === null) return;
-    
-        if (root.val >= maxValue) {
-            maxValue = root.val;
+    while (stack.length > 0) {
+        let [node, maxValueSoFar] = stack.pop();
+        
+        if (node.val >= maxValueSoFar) {
             goodNodesCount++;
+            maxValueSoFar = node.val;
         }
 
-        dfs(root.left, maxValue);
-        dfs(root.right, maxValue);
+        if (node.right) {
+            stack.push([node.right, maxValueSoFar]);
+        }
+
+        if (node.left) {
+            stack.push([node.left, maxValueSoFar]);
+        }
     }
     
-    dfs(root, root.val);
-
     return goodNodesCount;
 }
 
