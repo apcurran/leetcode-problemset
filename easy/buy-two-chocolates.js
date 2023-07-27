1ff9e9192a93f70b451e8e7dd76c54e6d9ab1bc4
Alex Curran <alexcurran.va@gmail.com>
2023-07-27 11:09:26 -0500

    Working brute force solution for buy-two-chocolates.js

---
+++ easy/buy-two-chocolates.js
@@ -0,0 +1,40 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n^2)
+ * space: O(1)
+ * 
+ * @param {number[]} prices
+ * @param {number} money
+ * @return {number}
+ */
+function buyChoco(prices, money) {
+    let lowestChocoTotal = prices[0] + prices[1];
+
+    for (let i = 0; i < prices.length; i++) {
+        const chocoOne = prices[i];
+
+        for (let j = i + 1; j < prices.length; j++) {
+            const chocoTwo = prices[j];
+            const chocoTotal = chocoOne + chocoTwo;
+
+            if (chocoTotal <= money) {
+                lowestChocoTotal = Math.min(lowestChocoTotal, chocoTotal);
+            }
+        }
+    }
+
+    const leftOverMoney = money - lowestChocoTotal;
+
+    if (leftOverMoney >= 0) {
+        return leftOverMoney;
+    } else {
+        return money;
+    }
+}
+
+console.log(buyChoco([1, 2, 2], 3)); // 0
+console.log(buyChoco([3,2,3], 3)); // 3
+console.log(buyChoco([98,54,6,34,66,63,52,39], 62)); // 22
+console.log(buyChoco([95,88,91,60,98,6,73,16,53,50], 22)); // 0
