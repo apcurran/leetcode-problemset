59df21c19cc7f058b974d5c413ed2d17202308d1
Alex Curran <alexcurran.va@gmail.com>
2023-08-02 10:02:14 -0500

    Second solution without swap for permutations.js

--- medium/permutations.js
+++ medium/permutations.js
@@ -1,44 +1,107 @@
 "use strict";
 
+// /**
+//  * Solution 1
+//  * time: O(n * n!)
+//  * space: O(n)
+//  * 
+//  * @param {number[]} nums
+//  * @return {number[][]}
+//  */
+// function permute(nums) {
+//     let resArr = [];
+    
+//     /**
+//      * @param {number} n 
+//      * @param {number[]} heapArr 
+//      * @returns {void}
+//      */
+//     function generate(n, heapArr) {
+//         if (n === 1) {
+//             resArr.push([...heapArr]);
+    
+//             return;
+//         }
+    
+//         generate(n - 1, heapArr);
+    
+//         for (let i = 0; i < n - 1; i++) {
+//             if (n % 2 === 0) {
+//                 swapInPlace(heapArr, i, n - 1);
+//             } else {
+//                 swapInPlace(heapArr, 0, n - 1);
+//             }
+
+//             generate(n - 1, heapArr);
+//         }
+//     }
+
+//     generate(nums.length, [...nums]);
+
+//     return resArr;
+// }
+
+// console.log( permute([1, 2, 3]) );
+// // [
+// //     [1,2,3],
+// //     [1,3,2],
+// //     [2,1,3],
+// //     [2,3,1],
+// //     [3,1,2],
+// //     [3,2,1]
+// // ]
+
+// /**
+//  * @param {number[]} arrToSwap 
+//  * @param {number} indexA 
+//  * @param {number} indexB
+//  * @returns {void}
+//  */
+// function swapInPlace(arrToSwap, indexA, indexB) {
+//     [arrToSwap[indexA], arrToSwap[indexB]] = [arrToSwap[indexB], arrToSwap[indexA]];
+// }
+
 /**
- * Solution 1
- * time: O(n!)
- * space: O(n!)
+ * Solution 2
+ * time: O(n * n!)
+ * space: O(n)
  * 
  * @param {number[]} nums
  * @return {number[][]}
  */
 function permute(nums) {
-    let resArr = [];
-    
-    /**
-     * @param {number} n 
-     * @param {number[]} heapArr 
-     * @returns {void}
-     */
-    function generate(n, heapArr) {
-        if (n === 1) {
-            resArr.push([...heapArr]);
-    
-            return;
-        }
-    
-        generate(n - 1, heapArr);
-    
-        for (let i = 0; i < n - 1; i++) {
-            if (n % 2 === 0) {
-                swapInPlace(heapArr, i, n - 1);
-            } else {
-                swapInPlace(heapArr, 0, n - 1);
-            }
+    let results = [];
+    dfs(nums, [], new Array(nums.length).fill(false), results);
 
-            generate(n - 1, heapArr);
-        }
+    return results;
+}
+
+/**
+ * @param {number[]} nums 
+ * @param {number[]} path 
+ * @param {boolean[]} used 
+ * @param {number[][]} results 
+ */
+function dfs(nums, path, used, results) {
+    if (path.length === nums.length) {
+        const listCopy = [...path];
+        results.push(listCopy);
+
+        return;
     }
 
-    generate(nums.length, [...nums]);
+    for (let i = 0; i < nums.length; i++) {
+        if (used[i]) continue;
 
-    return resArr;
+        // add to permutation, mark as used
+        path.push(nums[i]);
+        used[i] = true;
+
+        dfs(nums, path, used, results);
+        // remove from permutation mark as unused
+        path.pop();
+        used[i] = false;
+    }
 }
 
 console.log( permute([1, 2, 3]) );
@@ -50,13 +113,3 @@ console.log( permute([1, 2, 3]) );
 //     [3,1,2],
 //     [3,2,1]
 // ]
-
-/**
- * @param {number[]} arrToSwap 
- * @param {number} indexA 
- * @param {number} indexB
- * @returns {void}
- */
-function swapInPlace(arrToSwap, indexA, indexB) {
-    [arrToSwap[indexA], arrToSwap[indexB]] = [arrToSwap[indexB], arrToSwap[indexA]];
-}
