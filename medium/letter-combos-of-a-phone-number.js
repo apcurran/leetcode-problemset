13bfa2f2d0b353646e27c78f18c24be7c5e5533f
Alex Curran <alexcurran.va@gmail.com>
2023-08-03 09:55:26 -0500

    Backtracking solution for letter-combos-of-a-phone-number.js

---
+++ medium/letter-combos-of-a-phone-number.js
@@ -0,0 +1,50 @@
+"use strict";
+
+/**
+ * solution 1 -- backtracking
+ * time: O(n * 4^n)
+ * space: O(n)
+ * 
+ * @param {string} digits
+ * @return {string[]}
+ */
+function letterCombinations(digits) {
+    let results = [];
+
+    if (digits === "") return results;
+
+    const digitToChars = new Map([
+        ["2", "abc"],
+        ["3", "def"],
+        ["4", "ghi"],
+        ["5", "jkl"],
+        ["6", "mno"],
+        ["7", "qprs"],
+        ["8", "tuv"],
+        ["9", "wxyz"],
+    ]);
+
+    /**
+     * @param {number} i 
+     * @param {string} currentStr 
+     * @returns {void} -- side-effect with results from letterCombinations func
+     */
+    function backtrack(i, currentStr) {
+        if (currentStr.length === digits.length) {
+            results.push(currentStr);
+
+            return;
+        }
+
+        for (let char of digitToChars.get(digits[i])) {
+            backtrack(i + 1, currentStr + char);
+        }
+    }
+
+    backtrack(0, "");
+
+    return results;
+}
+
+console.log(letterCombinations("23")); // ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+console.log(letterCombinations("")); // []
