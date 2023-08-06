9e8110b763b008adab53f092b464ba0ddba541f8
Alex Curran <alexcurran.va@gmail.com>
2023-08-06 15:07:04 -0500

    Working solution for number-of-employees-who-met-the-target.js

---
+++ easy/number-of-employees-who-met-the-target.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number[]} hours
+ * @param {number} target
+ * @return {number}
+ */
+function numberOfEmployeesWhoMetTarget(hours, target) {
+    let goodEmployeesCount = 0;
+
+    for (let hour of hours) {
+        if (hour >= target) goodEmployeesCount++;
+    }
+
+    return goodEmployeesCount;
+}
+
+console.log(numberOfEmployeesWhoMetTarget([0, 1, 2, 3, 4], 2)); // 3
