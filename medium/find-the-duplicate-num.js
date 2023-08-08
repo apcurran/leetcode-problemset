3270cd457f3f5dde6294b77dd1f734feb7328d6f
Alex Curran <alexcurran.va@gmail.com>
2023-08-08 15:22:37 -0500

    Correct solution for find-the-duplicate-num.js

--- medium/find-the-duplicate-num.js
+++ medium/find-the-duplicate-num.js
@@ -51,22 +51,37 @@
 // }
 
 /**
- * solution 2 -- arr as hashmap
- * Time: O(n)
+ * solution 2 -- binary search
+ * Time: O(n * log n)
  * Space: O(1)
  * 
  * @param {number[]} nums
  * @return {number}
  */
 function findDuplicate(nums) {
-    while (nums[0] !== nums[nums[0]]) {
-        const next = nums[nums[0]];
-        // swap
-        nums[nums[0]] = nums[0];
-        nums[0] = next;
+    let left = 1;
+    let right = nums.length - 1;
+    let duplicate = -1;
+
+    while (left <= right) {
+        const current = left + Math.floor(((right - left) / 2));
+
+        // count how many nums in array are <= current
+        let count = 0;
+
+        for (let num of nums) {
+            if (num <= current) count++;
+        }
+
+        if (count > current) {
+            duplicate = current;
+            right = current - 1;
+        } else {
+            left = current + 1;
+        }
     }
 
-    return nums[0];
+    return duplicate;
 }
 
 console.log( findDuplicate([1, 3, 4, 2, 2], ) ); // 2
