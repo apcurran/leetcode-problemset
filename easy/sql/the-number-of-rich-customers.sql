44f337ff1dec6bbc64da592645028b1e3ead71c6
Alex Curran <alexcurran.va@gmail.com>
2023-08-09 14:47:59 -0500

    Simplified solution 2

--- easy/sql/the-number-of-rich-customers.sql
+++ easy/sql/the-number-of-rich-customers.sql
@@ -1,6 +1,12 @@
-SELECT COUNT(*) AS rich_count
-FROM (
-    SELECT DISTINCT customer_id
-    FROM Store
-    WHERE amount > 500
-) AS rich_count;
+-- -- solution 1
+-- SELECT COUNT(*) AS rich_count
+-- FROM (
+--     SELECT DISTINCT customer_id
+--     FROM Store
+--     WHERE amount > 500
+-- ) AS rich_count;
+
+-- solution 2
+SELECT COUNT(DISTINCT customer_id) AS rich_count
+FROM Store
+WHERE amount > 500;
