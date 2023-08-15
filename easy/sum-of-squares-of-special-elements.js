0adbd3b5b8b05efcbf3c71365aaf0314d3419bdb
Alex Curran <alexcurran.va@gmail.com>
2023-08-15 10:13:15 -0500

    Working linear time solution for sum-of-squares-of-special-elements.js

---
+++ easy/sum-of-squares-of-special-elements.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
+function sumOfSquares(nums) {
+    let sum = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        const isSpecial = nums.length % (i + 1) === 0;
+
+        if (!isSpecial) continue;
+
+        const squaredSpecialNum = nums[i] * nums[i];
+        sum += squaredSpecialNum;
+    }
+
+    return sum;
+}
+
+console.log(sumOfSquares([1,2,3,4])); // 21
