d6d8da71432a0c097ca1e9da6e96d2fafb7207a9
Alex Curran <alexcurran.va@gmail.com>
2023-08-23 14:24:16 -0500

    Working solution for check-if-a-string-is-an-acronym-of-words.js

---
+++ easy/check-if-a-string-is-an-acronym-of-words.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * solution 1
+ * n = words.length
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string[]} words
+ * @param {string} s
+ * @return {boolean}
+ */
+function isAcronym(words, s) {
+    let firstLetters = "";
+
+    for (let word of words) {
+        const firstLetter = word[0];
+        firstLetters += firstLetter;
+    }
+
+    return s === firstLetters;
+}
+
+console.log(isAcronym(["alice","bob","charlie"], "abc")); // true
+console.log(isAcronym(["an","apple"], "a")); // false
