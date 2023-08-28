be6dc8535dcd71564af8bd2cc669f2eaf94496fb
Alex Curran <alexcurran.va@gmail.com>
2023-08-28 09:39:14 -0500

    Working solution for encode-and-decode-tinyurl.js

---
+++ medium/encode-and-decode-tinyurl.js
@@ -0,0 +1,33 @@
+"use strict";
+
+let hashMap = new Map();
+
+/**
+ * Encodes a URL to a shortened URL.
+ * 
+ * time: O(1)
+ * space: O(1)
+ *
+ * @param {string} longUrl
+ * @return {string}
+ */
+function encode(longUrl) {
+    const code = Math.random().toString(32).slice(5);
+    const tinyURL = `https://tinyurl.com/${code}`;
+    hashMap.set(tinyURL, longUrl);
+
+    return tinyURL;
+}
+
+/**
+ * Decodes a shortened URL to its original URL.
+ * 
+ * time: O(1)
+ * space: O(1)
+ *
+ * @param {string} shortUrl
+ * @return {string}
+ */
+function decode(shortUrl) {
+    return hashMap.get(shortUrl);
+}
