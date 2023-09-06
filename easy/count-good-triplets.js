bed89171bf5cae7a780527df78437890d52de453
Alex Curran <alexcurran.va@gmail.com>
2023-09-06 13:50:38 -0500

    Optimal brute force solution

--- easy/count-good-triplets.js
+++ easy/count-good-triplets.js
@@ -1,7 +1,38 @@
 "use strict";
 
+// /**
+//  * solution 1 -- brute force
+//  * time: O(n^3)
+//  * space: O(1)
+//  * 
+//  * @param {number[]} arr
+//  * @param {number} a
+//  * @param {number} b
+//  * @param {number} c
+//  * @return {number}
+//  */
+// function countGoodTriplets(arr, a, b, c) {
+//     let goodTripletCount = 0;
+
+//     for (let i = 0; i < arr.length; i++) {
+
+//         for (let j = i + 1; j < arr.length; j++) {
+
+//             for (let k = j + 1; k < arr.length; k++) {
+//                 if (Math.abs(arr[i] - arr[j]) <= a &&
+//                     Math.abs(arr[j] - arr[k]) <= b &&
+//                     Math.abs(arr[i] - arr[k]) <= c) {
+//                     goodTripletCount++;
+//                 }
+//             }
+//         }
+//     }
+
+//     return goodTripletCount;
+// }
+
 /**
- * solution 1 -- brute force
+ * solution 2 -- brute force (optimized)
  * time: O(n^3)
  * space: O(1)
  * 
@@ -15,13 +46,13 @@ function countGoodTriplets(arr, a, b, c) {
     let goodTripletCount = 0;
 
     for (let i = 0; i < arr.length; i++) {
-
         for (let j = i + 1; j < arr.length; j++) {
+            if (Math.abs(arr[i] - arr[j]) > a) continue;
 
             for (let k = j + 1; k < arr.length; k++) {
-                if (Math.abs(arr[i] - arr[j]) <= a &&
-                    Math.abs(arr[j] - arr[k]) <= b &&
-                    Math.abs(arr[i] - arr[k]) <= c) {
+                if (Math.abs(arr[j] - arr[k]) > b) continue;
+                
+                if (Math.abs(arr[i] - arr[k]) <= c) {
                     goodTripletCount++;
                 }
             }
