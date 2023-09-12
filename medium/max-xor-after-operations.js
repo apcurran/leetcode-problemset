9628e38642eb542d106fff9653f860a41bc5d773
Alex Curran <alexcurran.va@gmail.com>
2023-09-12 11:27:46 -0500

    Bitwise OR solution for max-xor-after-operations.js

---
+++ medium/max-xor-after-operations.js
@@ -0,0 +1,21 @@
+"use strict";
+
+/**
+ * solution 1 -- bitwise OR
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
+function maximumXOR(nums) {
+    let result = 0;
+
+    for (let num of nums) {
+        result |= num;
+    }
+
+    return result;
+}
+
+console.log(maximumXOR([3,2,4,6])); // 7
