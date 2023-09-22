6fd987283d18be5dbb26643adc44f52d73d81700
Alex Curran <alexcurran.va@gmail.com>
2023-09-22 11:41:02 -0500

    Working solution for generate-a-string-with-chars-that-have-odd-counts.js

---
+++ easy/generate-a-string-with-chars-that-have-odd-counts.js
@@ -0,0 +1,20 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number} n
+ * @return {string}
+ */
+function generateTheString(n) {
+    if (n % 2 === 0) {
+        // even n
+        return "a".repeat(n - 1) + "b";
+    } else {
+        return "a".repeat(n);
+    }
+}
+
+console.log(generateTheString(4)); // "aaab"
