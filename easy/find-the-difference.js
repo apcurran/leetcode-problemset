1c099dfa4a22afadf729720ab27fbc3b2603f94c
Alex Curran <alexcurran.va@gmail.com>
2023-09-26 10:10:19 -0500

    Third optimal time O(n) solution

--- easy/find-the-difference.js
+++ easy/find-the-difference.js
@@ -30,39 +30,70 @@
 //     }
 // }
 
+// /**
+//  * Solution 2 -- hashmap
+//  * n = t length
+//  * Time: O(n) -- t is the same s length + 1
+//  * Space: O(1) -- technically O(26) since there are only 26 alpha chars
+//  * 
+//  * @param {string} s
+//  * @param {string} t
+//  * @return {string}
+//  */
+// function findTheDifference(s, t) {
+//     let sMap = new Map();
+//     let tMap = new Map();
+
+//     for (let char of s) {
+//         const previousCount = sMap.get(char) || 0;
+//         sMap.set(char, previousCount + 1);
+//     }
+
+//     for (let char of t) {
+//         if (!sMap.has(char)) return char;
+
+//         const previousTCharCount = tMap.get(char) || 0;
+//         const updatedTCharCount = previousTCharCount + 1;
+
+//         const sCharCount = sMap.get(char);
+
+//         if (sCharCount < updatedTCharCount) return char;
+
+//         tMap.set(char, updatedTCharCount);
+//     }
+
+//     return "Not found";
+// }
+
 /**
- * Solution 2 -- hashmap
+ * Solution 3 -- sum char codes and get diff
  * n = t length
- * Time: O(n) -- t is the same s length + 1
- * Space: O(1) -- technically O(26) since there are only 26 alpha chars
+ * Time: O(n)
+ * Space: O(1)
  * 
  * @param {string} s
  * @param {string} t
  * @return {string}
  */
 function findTheDifference(s, t) {
-    let sMap = new Map();
-    let tMap = new Map();
+    let sCharCodeSum = 0;
+    let tCharCodeSum = 0;
 
+    // add sChar codes
     for (let char of s) {
-        const previousCount = sMap.get(char) || 0;
-        sMap.set(char, previousCount + 1);
+        const sCharCode = char.charCodeAt(0);
+        sCharCodeSum += sCharCode;
     }
-
+    // add tChar codes
     for (let char of t) {
-        if (!sMap.has(char)) return char;
-
-        const previousTCharCount = tMap.get(char) || 0;
-        const updatedTCharCount = previousTCharCount + 1;
-
-        const sCharCount = sMap.get(char);
-
-        if (sCharCount < updatedTCharCount) return char;
-
-        tMap.set(char, updatedTCharCount);
+        const tCharCode = char.charCodeAt(0);
+        tCharCodeSum += tCharCode;
     }
 
-    return "Not found";
+    // get the diff
+    const charCodesDiff = tCharCodeSum - sCharCodeSum;
+
+    return String.fromCharCode(charCodesDiff);
 }
 
 console.log( findTheDifference("abcd", "abcde") ); // "e"
