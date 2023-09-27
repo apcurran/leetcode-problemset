46f5dcd8e3d2bb72ad005be6f521407cd4801694
Alex Curran <alexcurran.va@gmail.com>
2023-09-27 11:04:34 -0500

    Working solution for split-with-min-sum.js

---
+++ easy/split-with-min-sum.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n * log n)
+ * space: O(n)
+ * 
+ * @param {number} num
+ * @return {number}
+ */
+function splitNum(num) {
+    const digits = String(num)
+                    .split("")
+                    .map(Number)
+                    .sort((digitA, digitB) => digitA - digitB);
+
+    let num1Str = "";
+    let num2Str = "";
+
+    for (let i = 0; i < digits.length; i++) {
+        if (i % 2 === 0) {
+            // even index
+            num1Str += digits[i];
+        } else {
+            // odd index
+            num2Str += digits[i];
+        }
+    }
+
+    return Number(num1Str) + Number(num2Str);
+}
+
+console.log(splitNum(4325)); // 59
