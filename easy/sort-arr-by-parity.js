651300d7e481ca57020ebdb3173987f8d9afec00
Alex Curran <alexcurran.va@gmail.com>
2023-09-28 11:26:06 -0500

    Refactor solution 1

--- easy/sort-arr-by-parity.js
+++ easy/sort-arr-by-parity.js
@@ -1,5 +1,29 @@
 "use strict";
 
+// /**
+//  * Given an integer array nums,
+//  * move all the even integers at the beginning of the array followed by all the odd integers.
+//  * 
+//  * solution 1
+//  * time: O(n log n)
+//  * space: O(n)
+//  * 
+//  * @param {number[]} nums
+//  * @return {number[]}
+//  */
+// function sortArrayByParity(nums) {
+//     return [...nums].sort(compareNums);
+// }
+
+// function compareNums(a, b) {
+//     // If num is even, place at the front of arr
+//     if (a % 2 === 0 && b % 2 !== 0) {
+//         return -1;
+//     }
+
+//     return 1;
+// }
+
 /**
  * Given an integer array nums,
  * move all the even integers at the beginning of the array followed by all the odd integers.
@@ -12,15 +36,20 @@
  * @return {number[]}
  */
 function sortArrayByParity(nums) {
-    return [...nums].sort(compareNums);
+    return nums.sort(compareNums);
 }
 
-function compareNums(a, b) {
+/**
+ * @param {number} a 
+ * @returns {-1|1}
+ */
+function compareNums(a) {
     // If num is even, place at the front of arr
-    if (a % 2 === 0 && b % 2 !== 0) {
+    if (a % 2 === 0) {
         return -1;
+    } else {
+        return 1;
     }
-
-    return 1;
 }
+
 console.log( sortArrayByParity([3,1,2,4]) ); // [2, 4, 3, 1]
