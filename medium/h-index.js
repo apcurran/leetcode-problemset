2d6265d41c7859738baf84205c84e15268965ff5
Alex Curran <alexcurran.va@gmail.com>
2023-10-09 12:49:05 -0500

    Add comments for clarity

--- medium/h-index.js
+++ medium/h-index.js
@@ -3,12 +3,13 @@
 /**
  * solution 1
  * time: O(n)
- * space: O(n)
+ * space: O(n) -- built-in JS sort uses O(n)
  * 
  * @param {number[]} citations
  * @return {number}
  */
 function hIndex(citations) {
+    // reverse sort
     citations.sort((a, b) => b - a);
 
     for (let i = 0; i < citations.length; i++) {
