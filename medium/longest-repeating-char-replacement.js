927fbda8f53800705813e2bb7b988be54fb8d5e3
Alex Curran <alexcurran.va@gmail.com>
2023-10-12 17:05:53 -0500

    Working solution 2 alternative

--- medium/longest-repeating-char-replacement.js
+++ medium/longest-repeating-char-replacement.js
@@ -1,7 +1,48 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {string} str
+//  * @param {number} k
+//  * @return {number}
+//  */
+// function characterReplacement(str, k) {
+//     let left = 0;
+//     let right = 0;
+//     let maxCharCount = 0;
+//     let charsCache = new Map();
+
+//     while (right < str.length) {
+//         const currentRightChar = str[right];
+//         const previousCharCount = charsCache.get(currentRightChar) || 0;
+//         const updatedCharCount = previousCharCount + 1;
+//         charsCache.set(currentRightChar, updatedCharCount);
+
+//         if (updatedCharCount > maxCharCount) {
+//             maxCharCount = updatedCharCount;
+//         }
+
+//         const windowLength = right - left + 1;
+
+//         if (windowLength - maxCharCount > k) {
+//             // shrink window by moving left pointer
+//             const currentLeftChar = str[left];
+//             const currentLeftCharCount = charsCache.get(currentLeftChar);
+//             charsCache.set(currentLeftChar, currentLeftCharCount - 1);
+//             left++;
+//         }
+
+//         right++;
+//     }
+
+//     return right - left;
+// }
+
 /**
- * solution 1
+ * solution 2
  * time: O(n)
  * space: O(n)
  * 
@@ -10,35 +51,50 @@
  * @return {number}
  */
 function characterReplacement(str, k) {
+    let charCounts = new Map();
     let left = 0;
-    let right = 0;
-    let maxCharCount = 0;
-    let charsCache = new Map();
+    let result = 0;
 
-    while (right < str.length) {
-        const currentRightChar = str[right];
-        const previousCharCount = charsCache.get(currentRightChar) || 0;
-        const updatedCharCount = previousCharCount + 1;
-        charsCache.set(currentRightChar, updatedCharCount);
+    for (let right = 0; right < str.length; right++) {
+        const currentChar = str[right];
+        const charCount = charCounts.get(currentChar) || 0;
+        charCounts.set(currentChar, charCount + 1);
 
-        if (updatedCharCount > maxCharCount) {
-            maxCharCount = updatedCharCount;
-        }
-
-        const windowLength = right - left + 1;
-
-        if (windowLength - maxCharCount > k) {
-            // shrink window by moving left pointer
-            const currentLeftChar = str[left];
-            const currentLeftCharCount = charsCache.get(currentLeftChar);
-            charsCache.set(currentLeftChar, currentLeftCharCount - 1);
+        while (getWindowLength(left, right) - getMax(charCounts.values()) > k) {
+            const leftChar = str[left];
+            const leftCharCount = charCounts.get(leftChar);
+            charCounts.set(leftChar, leftCharCount - 1);
             left++;
         }
 
-        right++;
+        result = Math.max(result, getWindowLength(left, right));
     }
 
-    return right - left;
+    return result;
+}
+
+/**
+ * @param {number} left 
+ * @param {number} right 
+ * @returns {number}
+ */
+function getWindowLength(left, right) {
+    return right - left + 1;
+}
+
+/**
+ * @param {Iterable} iterable 
+ * @returns {number}
+ */
+function getMax(iterable) {
+    const firstValue = iterable.next().value;
+    let max = firstValue;
+
+    for (let num of iterable) {
+        max = Math.max(max, num);
+    }
+
+    return max;
 }
 
 console.log( characterReplacement("ABAB", 2) ); // 4
