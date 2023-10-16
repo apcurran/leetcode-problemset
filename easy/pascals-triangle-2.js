d799b67803532a696967a9e309038c2539dcf056
Alex Curran <alexcurran.va@gmail.com>
2023-10-16 09:36:32 -0500

    Working solution for pascals-triangle-2.js

---
+++ easy/pascals-triangle-2.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number} rowIndex
+ * @return {number[]}
+ */
+function getRow(rowIndex) {
+    // 0th row
+    let result = [1];
+
+    for (let i = 0; i < rowIndex; i++) {
+        let nextRow = new Array(result.length + 1).fill(0);
+
+        for (let j = 0; j < result.length; j++) {
+            nextRow[j] += result[j];
+            nextRow[j + 1] += result[j];
+        }
+
+        // update result
+        result = nextRow;
+    }
+
+    return result;
+}
