e8776862ef116b36f19e58e3f9fefac501bbd252
Alex Curran <alexcurran.va@gmail.com>
2023-10-25 11:02:44 -0500

    Working O(m * n) time and O(m + n) space solution for set-matrix-zeros.js using Sets

---
+++ medium/set-matrix-zeros.js
@@ -0,0 +1,36 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(m * n)
+ * space: O(m + n)
+ * 
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+function setZeroes(matrix) {
+    let rowSet = new Set();
+    let colSet = new Set();
+
+    // mark rows and columns if they require zeroing out
+    for (let row = 0; row < matrix.length; row++) {
+        for (let col = 0; col < matrix[row].length; col++) {
+            if (matrix[row][col] === 0) {
+                rowSet.add(row);
+                colSet.add(col);
+            }
+        }
+    }
+
+    for (let row = 0; row < matrix.length; row++) {
+        for (let col = 0; col < matrix[row].length; col++) {
+            if (rowSet.has(row) || colSet.has(col)) {
+                // zero out specified rows and columns
+                matrix[row][col] = 0;
+            }
+        }
+    }
+}
+
+console.log(setZeroes([[1,1,1],[1,0,1],[1,1,1]]));
+// [[1,0,1], [0,0,0], [1,0,1]]
