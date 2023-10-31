c1a1b7267b11e1fe7bbaf7662d7db2d5a201f65e
Alex Curran <alexcurran.va@gmail.com>
2023-10-31 09:35:53 -0500

    Working linear time/space solution for find-the-original-arr-of-prefix-xor.js

---
+++ medium/find-the-original-arr-of-prefix-xor.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number[]} pref
+ * @return {number[]}
+ */
+function findArray(pref) {
+    let results = new Array(pref.length);
+    results[0] = pref[0];
+
+    for (let i = 1; i < pref.length; i++) {
+        results[i] = pref[i] ^ pref[i - 1];
+    }
+
+    return results;
+}
+
+console.log(findArray([5,2,0,3,1])); // [5, 7, 2, 3, 2]
