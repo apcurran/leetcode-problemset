b48e1292d8ec0e48806154b2b43fcf2cf42e386e
Alex Curran <alexcurran.va@gmail.com>
2023-11-14 10:43:25 -0600

    Hashset solution for unique-len-3-palin-subseq.js

--- medium/unique-len-3-palin-subseq.js
+++ medium/unique-len-3-palin-subseq.js
@@ -1,47 +1,30 @@
 "use strict";
 
 /**
- * solution 1 -- correct, but TLE
- * n = str len
- * m = total num of three letter palindromes in str
- * time: O(n^3)
- * space: O(m)
+ * solution 1 -- hashset
+ * time: O(n)
+ * space: O(26) -> O(1)
  * 
  * @param {string} s
  * @return {number}
  */
 function countPalindromicSubsequence(s) {
-    let palinSet = new Set();
+    const letters = new Set(s);
+    let result = 0;
 
-    for (let i = 0; i < s.length; i++) {
-        const firstChar = s[i];
+    for (let letter of letters) {
+        let i = s.indexOf(letter);
+        let j = s.lastIndexOf(letter);
+        let lettersBetween = new Set();
 
-        for (let j = i + 1; j < s.length; j++) {
-            const secondChar = s[j];
-
-            for (let k = j + 1; k < s.length; k++) {
-                const thirdChar = s[k];
-                const possiblePalinStr = firstChar + secondChar + thirdChar;
-
-                if (isThreeLenStrPalindrome(possiblePalinStr)) {
-                    palinSet.add(possiblePalinStr);
-                }
-            }
+        for (let k = i + 1; k < j; k++) {
+            lettersBetween.add(s[k]);
         }
+
+        result += lettersBetween.size;
     }
 
-    return palinSet.size;
-}
-
-/**
- * time: O(1)
- * space: O(1)
- * 
- * @param {string} str 
- * @returns {boolean}
- */
-function isThreeLenStrPalindrome(str) {
-    return str[0] === str[2];
+    return result;
 }
 
 console.log( countPalindromicSubsequence("aabca") ); // 3
