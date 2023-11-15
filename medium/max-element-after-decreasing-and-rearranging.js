759f488328531811c3a3d57e12f8101421028a3c
Alex Curran <alexcurran.va@gmail.com>
2023-11-15 12:15:08 -0600

    Working solution for max-element-after-decreasing-and-rearranging.js

---
+++ medium/max-element-after-decreasing-and-rearranging.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n * log n)
+ * space: O(n)
+ * 
+ * @param {number[]} arr
+ * @return {number}
+ */
+function maximumElementAfterDecrementingAndRearranging(arr) {
+    arr.sort((a, b) => a - b);
+
+    let previous = 0;
+
+    for (let num of arr) {
+        previous = Math.min(previous + 1, num);
+    }
+
+    return previous;
+}
+
+console.log(maximumElementAfterDecrementingAndRearranging([2,2,1,2,1])); // 2
