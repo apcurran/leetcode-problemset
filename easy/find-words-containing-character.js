26d4623275828405aa60ec4fb80d48b099f74541
Alex Curran <alexcurran.va@gmail.com>
2023-11-28 13:56:03 -0600

    Working solution for find-words-containing-character.js

---
+++ easy/find-words-containing-character.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n * m)
+ * space: O(n) -- (if return value is counted)
+ * 
+ * @param {string[]} words
+ * @param {character} x
+ * @return {number[]}
+ */
+function findWordsContaining(words, x) {
+    let results = [];
+
+    for (let i = 0; i < words.length; i++) {
+        const currentWord = words[i];
+
+        if (currentWord.includes(x)) {
+            results.push(i);
+        }
+    }
+
+    return results;
+}
+
+console.log(findWordsContaining(["leet","code"], "e")); // [0, 1]
