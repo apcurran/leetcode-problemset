c952cafdfac91a4a494a6798f21d117e26bba9b0
Alex Curran <alexcurran.va@gmail.com>
2023-12-04 13:30:12 -0600

    Working linear time solution for largest-3-same-digit-num-in-str.js

---
+++ easy/largest-3-same-digit-num-in-str.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {string} num
+ * @return {string} max good integer (substr int of len === 3)
+ */
+function largestGoodInteger(num) {
+    let maxGoodInt = "";
+
+    for (let i = 0; i < num.length; i++) {
+        if (num[i - 1] === num[i] && num[i] === num[i + 1]) {
+            // three consecutive digits
+            maxGoodInt = Math.max(maxGoodInt, Number(num[i]));
+        }
+    }
+
+    return `${maxGoodInt}${maxGoodInt}${maxGoodInt}`;
+}
+
+console.log(largestGoodInteger("6777133339")); // "777"
+console.log(largestGoodInteger("2300019")); // "000"
+console.log(largestGoodInteger("42352338")); // ""
