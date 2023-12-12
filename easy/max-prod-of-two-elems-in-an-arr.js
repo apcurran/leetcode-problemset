7a5149bd5b8a95e73f77359cf2d2a34bef8e93fe
Alex Curran <alexcurran.va@gmail.com>
2023-12-12 10:58:17 -0600

    Brute force solution 0

--- easy/max-prod-of-two-elems-in-an-arr.js
+++ easy/max-prod-of-two-elems-in-an-arr.js
@@ -1,6 +1,27 @@
 "use strict";
 
 // /**
+//  * solution 0 -- brute force
+//  * time: O(n^2)
+//  * space: O(1)
+//  * 
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// function maxProduct(nums) {
+//     let max = 0;
+
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             const currentProduct = (nums[i] - 1) * (nums[j] - 1);
+//             max = Math.max(max, currentProduct);
+//         }
+//     }
+
+//     return max;
+// }
+
+// /**
 //  * solution 1
 //  * time: O(n * log n) -- due to sorting first
 //  * space: O(n) -- built-in uses timsort
