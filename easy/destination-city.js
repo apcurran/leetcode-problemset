c330736b320a917f9eb98f0ee0295e879bb0e33e
Alex Curran <alexcurran.va@gmail.com>
2023-12-15 09:57:43 -0600

    Hashset algorithm with linear time/space

--- easy/destination-city.js
+++ easy/destination-city.js
@@ -1,20 +1,44 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n^2)
+//  * space: O(n)
+//  * 
+//  * @param {string[][]} paths
+//  * @return {string}
+//  */
+// function destCity(paths) {
+//     const startingCities = paths.map((city) => city[0]);
+//     const destinationCities = paths.map((city) => city[1]);
+
+//     return destinationCities
+//             .filter((destination) => !startingCities.includes(destination))
+//             .join("");
+// }
+
 /**
  * solution 1
- * time: O(n^2)
+ * time: O(n)
  * space: O(n)
  * 
  * @param {string[][]} paths
  * @return {string}
  */
 function destCity(paths) {
-    const startingCities = paths.map((city) => city[0]);
-    const destinationCities = paths.map((city) => city[1]);
+    let hashSet = new Set();
 
-    return destinationCities
-            .filter((destination) => !startingCities.includes(destination))
-            .join("");
+    for (let pair of paths) {
+        hashSet.add(pair[0]);
+    }
+
+    for (let pair of paths) {
+        const destinationCity = pair[1];
+
+        if (!hashSet.has(destinationCity)) {
+            return destinationCity;
+        }
+    }
 }
 
 console.log( destCity([["London","New York"], ["New York","Lima"], ["Lima","Sao Paulo"]]) ); // "Sao Paulo"
