295cf9876ba0684848fe5b3dcbc68b86d5e36988
Alex Curran <alexcurran.va@gmail.com>
2023-12-20 16:01:05 -0600

    Working solution for number-of-laser-beams-in-a-bank.js

---
+++ medium/number-of-laser-beams-in-a-bank.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n * m)
+ * space: O(1)
+ * 
+ * @param {string[]} bank
+ * @return {number}
+ */
+function numberOfBeams(bank) {
+    let previous = 0;
+    let result = 0;
+
+    for (let row of bank) {
+        let devicesCounter = 0;
+
+        for (let char of row) {
+            if (char === "1") devicesCounter++;
+        }
+
+        if (devicesCounter > 0) {
+            result += (previous * devicesCounter);
+            previous = devicesCounter;
+        }
+    }
+
+    return result;
+}
+
+console.log(numberOfBeams(["011001","000000","010100","001000"])); // 8
