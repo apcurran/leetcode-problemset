4349230dd588af360797a8f7cd2d31b7fe973e7f
Alex Curran <alexcurran.va@gmail.com>
2023-12-22 11:10:26 -0600

    Memory optimize to constant space

--- easy/max-score-after-splitting-a-str.js
+++ easy/max-score-after-splitting-a-str.js
@@ -1,9 +1,43 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n^2)
+//  * space: O(n)
+//  * 
+//  * @param {string} str
+//  * @return {number}
+//  */
+// function maxScore(str) {
+//     let maxSumScore = 0;
+
+//     for (let i = 0; i < str.length - 1; i++) {
+//         const leftSubStr = str.slice(0, i + 1);
+//         const rightSubStr = str.slice(i + 1);
+
+//         let leftSubStrZerosCount = 0;
+
+//         for (let numStr of leftSubStr) {
+//             if (numStr === "0") leftSubStrZerosCount++;
+//         }
+
+//         let rightSubStrOnesCount = 0;
+
+//         for (let numStr of rightSubStr) {
+//             if (numStr === "1") rightSubStrOnesCount++;
+//         }
+        
+//         const sumScore = leftSubStrZerosCount + rightSubStrOnesCount;
+//         maxSumScore = Math.max(maxSumScore, sumScore);
+//     }
+
+//     return maxSumScore;
+// }
+
 /**
- * solution 1
+ * solution 2 -- memory optimization
  * time: O(n^2)
- * space: O(n)
+ * space: O(1)
  * 
  * @param {string} str
  * @return {number}
@@ -12,19 +46,16 @@ function maxScore(str) {
     let maxSumScore = 0;
 
     for (let i = 0; i < str.length - 1; i++) {
-        const leftSubStr = str.slice(0, i + 1);
-        const rightSubStr = str.slice(i + 1);
-
         let leftSubStrZerosCount = 0;
 
-        for (let numStr of leftSubStr) {
-            if (numStr === "0") leftSubStrZerosCount++;
+        for (let j = 0; j <= i; j++) {
+            if (str[j] === "0") leftSubStrZerosCount++;
         }
 
         let rightSubStrOnesCount = 0;
 
-        for (let numStr of rightSubStr) {
-            if (numStr === "1") rightSubStrOnesCount++;
+        for (let j = i + 1; j < str.length; j++) {
+            if (str[j] === "1") rightSubStrOnesCount++;
         }
         
         const sumScore = leftSubStrZerosCount + rightSubStrOnesCount;
