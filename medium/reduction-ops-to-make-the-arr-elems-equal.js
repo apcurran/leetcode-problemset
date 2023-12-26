adcaeb503b17003bd9b972788624e7636bdc2827
Alex Curran <alexcurran.va@gmail.com>
2023-12-26 16:38:10 -0600

    Clarifying comment and update var naming

--- medium/reduction-ops-to-make-the-arr-elems-equal.js
+++ medium/reduction-ops-to-make-the-arr-elems-equal.js
@@ -12,19 +12,21 @@ function reductionOperations(nums) {
     // sort ASC
     nums.sort((a, b) => a - b);
     let upSteps = 0;
-    let result = 0;
+    let totalReductions = 0;
 
+    // start at i = 1,
+    // since the 0th elem is already at the minimum
     for (let i = 1; i < nums.length; i++) {
         // compare current with previous value
         if (nums[i] !== nums[i - 1]) {
             upSteps++;
         }
 
-        result += upSteps;
+        totalReductions += upSteps;
     }
 
-    return result;
+    return totalReductions;
 }
 
-console.log(reductionOperations([5,1,3])); // 3
+// console.log(reductionOperations([5,1,3])); // 3
 console.log(reductionOperations([1,1,2,2,3])); // 4
