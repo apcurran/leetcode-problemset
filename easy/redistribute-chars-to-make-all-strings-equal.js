ff28ecb110f237999622dc87a56e1f3015203ea0
Alex Curran <alexcurran.va@gmail.com>
2024-01-02 11:43:32 -0600

    Working hashmap solution for redistribute-chars-to-make-all-strings-equal.js

---
+++ easy/redistribute-chars-to-make-all-strings-equal.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/**
+ * solution 1 -- hashmap
+ * time: O(n * m)
+ * space: O(1)
+ * 
+ * @param {string[]} words
+ * @return {boolean}
+ */
+function makeEqual(words) {
+    let charsCount = new Map();
+
+    for (let word of words) {
+        for (let char of word) {
+            const previousCharCount = charsCount.get(char) || 0;
+            charsCount.set(char, previousCharCount + 1);
+        }
+    }
+
+    // return early if not possible to make all equal strings
+    for (let charTuple of charsCount) {
+        const charCount = charTuple[1];
+
+        // if char count is not divisible by the total words,
+        // then it is not possible to distribute this char evenly amongst all for equal strings
+        if (charCount % words.length !== 0) return false;
+    }
+
+    return true;
+}
+
+console.log(makeEqual(["abc","aabc","bc"])); // true
+console.log(makeEqual(["ab","a"])); // false
