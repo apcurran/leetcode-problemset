d284bb6dd8a7db1bf4465866c3111cf3985ffd61
Alex Curran <alexcurran.va@gmail.com>
2024-01-03 12:12:59 -0600

    Small refactor of vars and process for clarity

--- medium/gen-parens.js
+++ medium/gen-parens.js
@@ -1,42 +1,7 @@
 "use strict";
 
-// /**
-//  * solution 1 -- recursion
-//  * time: O(2^n)
-//  * space: O(n)
-//  * 
-//  * @param {number} n
-//  * @return {string[]}
-//  */
-// function generateParenthesis(n) {
-//     let results = [];
-
-//     function recurse(openN, closedN, parenthesisStr) {
-//         // only add if openN is greater -- this prevents invalid paren combos
-//         if (openN > closedN) return;
-
-//         if (openN === 0 && closedN === 0) {
-//             results.push(parenthesisStr);
-
-//             return;
-//         }
-
-//         if (openN > 0) {
-//             recurse(openN - 1, closedN, parenthesisStr + "(");
-//         }
-
-//         if (closedN > 0) {
-//             recurse(openN, closedN - 1, parenthesisStr + ")");
-//         }
-//     }
-
-//     recurse(n, n, "");
-
-//     return results;
-// }
-
 /**
- * solution 2 -- recursion (stack)
+ * solution 1 -- recursion
  * time: O(2^n)
  * space: O(n)
  * 
@@ -45,35 +10,68 @@
  */
 function generateParenthesis(n) {
     let results = [];
-    let stack = [];
 
-    /**
-     * @param {number} openN 
-     * @param {number} closedN 
-     * @returns {void}
-     */
-    function recurse(openN, closedN) {
-        if (openN === closedN && closedN === n) {
-            const completedParensCombination = stack.join("");
-            results.push(completedParensCombination);
+    function backtrack(openN, closedN, parenthesisStr) {
+        if (openN === n && closedN === n) {
+            // add valid, finished parentheses string into results
+            results.push(parenthesisStr);
+
+            return;
         }
 
         if (openN < n) {
-            stack.push("(");
-            recurse(openN + 1, closedN);
-            stack.pop();
+            backtrack(openN + 1, closedN, parenthesisStr + "(");
         }
 
         if (closedN < openN) {
-            stack.push(")");
-            recurse(openN, closedN + 1);
-            stack.pop();
+            backtrack(openN, closedN + 1, parenthesisStr + ")");
         }
     }
 
-    recurse(0, 0);
+    backtrack(0, 0, "");
 
     return results;
 }
+
+// /**
+//  * solution 2 -- recursion (stack)
+//  * time: O(2^n)
+//  * space: O(n)
+//  * 
+//  * @param {number} n
+//  * @return {string[]}
+//  */
+// function generateParenthesis(n) {
+//     let results = [];
+//     let stack = [];
+
+//     /**
+//      * @param {number} openN 
+//      * @param {number} closedN 
+//      * @returns {void}
+//      */
+//     function recurse(openN, closedN) {
+//         if (openN === closedN && closedN === n) {
+//             const completedParensCombination = stack.join("");
+//             results.push(completedParensCombination);
+//         }
+
+//         if (openN < n) {
+//             stack.push("(");
+//             recurse(openN + 1, closedN);
+//             stack.pop();
+//         }
+
+//         if (closedN < openN) {
+//             stack.push(")");
+//             recurse(openN, closedN + 1);
+//             stack.pop();
+//         }
+//     }
+
+//     recurse(0, 0);
+
+//     return results;
+// }
 
 console.log( generateParenthesis(3) ); // ["((()))","(()())","(())()","()(())","()()()"]
