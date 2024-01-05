b49d3f10193ee17250a3ee34cb7401e5d73d7dc3
Alex Curran <alexcurran.va@gmail.com>
2024-01-05 14:35:42 -0600

    Sort solution O(n * log n)

--- easy/min-num-game.js
+++ easy/min-num-game.js
@@ -1,50 +1,72 @@
 "use strict";
 
+// /**
+//  * solution 1 -- brute force
+//  * time: O(n^2)
+//  * space: O(n)
+//  * 
+//  * @param {number[]} nums
+//  * @return {number[]}
+//  */
+// function numberGame(nums) {
+//     let results = [];
+//     // iterate nums
+//     while (nums.length > 0) {
+//         // alice
+//         const [aliceMinValue, aliceMinIndex] = getMinValueAndIndex(nums);
+//         // modify arr
+//         nums.splice(aliceMinIndex, 1);
+//         // bob
+//         const [bobMinValue, bobMinIndex] = getMinValueAndIndex(nums);
+//         // modify arr
+//         nums.splice(bobMinIndex, 1);
+//         // update results
+//         results.push(bobMinValue, aliceMinValue);
+//     }
+
+//     return results;
+// }
+
+// /**
+//  * @param {number[]} arr 
+//  * @returns {[number, number]} [minValue, minIndex]
+//  */
+// function getMinValueAndIndex(arr) {
+//     let min = arr[0];
+//     let minIndex = 0;
+
+//     for (let i = 1; i < arr.length; i++) {
+//         if (arr[i] < min) {
+//             // update min num
+//             min = arr[i];
+//             // update min index
+//             minIndex = i;
+//         }
+//     }
+
+//     return [min, minIndex];
+// }
+
 /**
- * solution 1
- * time: O(n^2)
+ * solution 2 -- sorting
+ * time: O(n * log n)
  * space: O(n)
  * 
  * @param {number[]} nums
  * @return {number[]}
  */
 function numberGame(nums) {
-    let results = [];
-    // iterate nums
-    while (nums.length > 0) {
-        // alice
-        const [aliceMinValue, aliceMinIndex] = getMinValueAndIndex(nums);
-        // modify arr
-        nums.splice(aliceMinIndex, 1);
-        // bob
-        const [bobMinValue, bobMinIndex] = getMinValueAndIndex(nums);
-        // modify arr
-        nums.splice(bobMinIndex, 1);
-        // update results
-        results.push(bobMinValue, aliceMinValue);
+    nums.sort(function sortAsc(a, b) {
+        return a - b;
+    });
+
+    // iterate by pairs
+    for (let i = 0; i < nums.length - 1; i += 2) {
+        // swap places
+        [nums[i], nums[i + 1]] = [nums[i + 1], nums[i]];
     }
 
-    return results;
-}
-
-/**
- * @param {number[]} arr 
- * @returns {[number, number]} [minValue, minIndex]
- */
-function getMinValueAndIndex(arr) {
-    let min = arr[0];
-    let minIndex = 0;
-
-    for (let i = 1; i < arr.length; i++) {
-        if (arr[i] < min) {
-            // update min num
-            min = arr[i];
-            // update min index
-            minIndex = i;
-        }
-    }
-
-    return [min, minIndex];
+    return nums;
 }
 
 console.log(numberGame([5,4,2,3])); // [3,2,5,4]
