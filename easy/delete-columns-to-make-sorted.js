dda7f9248fe9d809fb73b4a80de8da86ad4a774a
Alex Curran <alexcurran.va@gmail.com>
2024-01-10 13:38:32 -0600

    Working solution for delete-columns-to-make-sorted.js

---
+++ easy/delete-columns-to-make-sorted.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n * m)
+ * space: O(1)
+ * 
+ * @param {string[]} strs
+ * @return {number}
+ */
+function minDeletionSize(strs) {
+    let deleteCount = 0;
+
+    for (let col = 0; col < strs[0].length; col++) {
+        for (let row = 0; row < strs.length - 1; row++) {
+            if (strs[row][col] > strs[row + 1][col]) {
+                deleteCount++;
+
+                break;
+            }
+        }
+    }
+
+    return deleteCount;
+}
+
+console.log(minDeletionSize(["cba","daf","ghi"])); // 1
+// c b a
+// d a f
+// g h i
+console.log(minDeletionSize(["zyx","wvu","tsr"])); // 3
