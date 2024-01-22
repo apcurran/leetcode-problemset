b75f281315311379b2e6985b58bee24693b21b2d
Alex Curran <alexcurran.va@gmail.com>
2024-01-22 12:09:21 -0600

    Working hashmap solution for set-mismatch.js

---
+++ easy/set-mismatch.js
@@ -0,0 +1,39 @@
+"use strict";
+
+/**
+ * solution 1 -- hashmap
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number[]} actualNums
+ * @return {number[]}
+ */
+function findErrorNums(actualNums) {
+    let numsMap = new Map();
+
+    for (let i = 1; i <= actualNums.length; i++) {
+        numsMap.set(i, 1);
+    }
+
+    for (let num of actualNums) {
+        const previousNumCount = numsMap.get(num);
+        numsMap.set(num, previousNumCount - 1);
+    }
+
+    let duplicateNum = 0;
+    let missingNum = 0;
+
+    for (let [num, numCount] of numsMap) {
+        if (numCount === -1) {
+            duplicateNum = num;
+        }
+
+        if (numCount === 1) {
+            missingNum = num;
+        }
+    }
+
+    return [duplicateNum, missingNum];
+}
+
+console.log(findErrorNums([1,2,2,4])); // [2, 3]
