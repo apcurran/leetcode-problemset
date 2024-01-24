8f752553800f28dc8a6510d75b907cdb3cc54e1b
Alex Curran <alexcurran.va@gmail.com>
2024-01-24 12:02:12 -0600

    Working solution for count-elements-with-max-freq.js

---
+++ easy/count-elements-with-max-freq.js
@@ -0,0 +1,30 @@
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number[]} nums 
+ * @returns {number}
+ */
+function maxFrequencyElements(nums) {
+    let numsFrequencies = new Map();
+
+    for (let num of nums) {
+        const previousCount = numsFrequencies.get(num) || 0;
+        numsFrequencies.set(num, previousCount + 1);
+    }
+
+    const maxFequency = Math.max(...numsFrequencies.values());
+    let sum = 0;
+    
+    for (let [, numFreq] of numsFrequencies) {
+        if (numFreq === maxFequency) {
+            sum += numFreq;
+        }
+    }
+    
+    return sum;
+}
+
+console.log(maxFrequencyElements([1,2,2,3,1,4])); // 4
