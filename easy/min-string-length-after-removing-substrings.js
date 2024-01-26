ffdf0f322ab92d79744420a54945d81da37bfa54
Alex Curran <alexcurran.va@gmail.com>
2024-01-26 15:45:59 -0600

    Stack-based linear time/space solution for min-string-length-after-removing-substrings.js

---
+++ easy/min-string-length-after-removing-substrings.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/**
+ * solution 1 -- stack
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} s
+ * @return {number}
+ */
+function minLength(s) {
+    let stack = [];
+
+    for (let char of s) {
+        if (stack.length > 0 && char === "B" && stack.at(-1) === "A") {
+            // if non-empty stack, and top of stack is "A" with current char "B" (adjacent "AB", remove)
+            stack.pop();
+        } else if (stack.length > 0 && char === "D" && stack.at(-1) === "C") {
+            // if non-empty stack, and top of stack is "D" with current char "C" (adjacent "CD", remove)
+            stack.pop();
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.length;
+}
+
+console.log(minLength("ABFCACDB")); // 2
+console.log(minLength("ACBBD")); // 5
