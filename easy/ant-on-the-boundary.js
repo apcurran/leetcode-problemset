bc3da3404f1f19f3bf476e786ababb16089bdae8
Alex Curran <alexcurran.va@gmail.com>
2024-02-05 11:29:32 -0600

    Linear time solution for ant-on-the-boundary.js

---
+++ easy/ant-on-the-boundary.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
+function returnToBoundaryCount(nums) {
+    let steps = nums[0];
+    let boundaryHitsCount = 0;
+
+    for (let i = 1; i < nums.length; i++) {
+        steps += nums[i];
+
+        if (steps === 0) boundaryHitsCount++;
+    }
+
+    return boundaryHitsCount;
+}
+
+console.log(returnToBoundaryCount([2,3,-5])); // 1
+console.log(returnToBoundaryCount([3,2,-3,-4])); // 0
