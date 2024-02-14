0d1a03fa04764bab988b6da5d5a183869936c58f
Alex Curran <alexcurran.va@gmail.com>
2024-02-14 11:47:27 -0600

    Working hashset solution for keys-and-rooms.js

---
+++ medium/keys-and-rooms.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/**
+ * solution 1 -- hashset
+ * n = rooms length
+ * m = keys length
+ * time: O(n * m)
+ * space: O(n)
+ * 
+ * @param {number[][]} rooms
+ * @return {boolean}
+ */
+function canVisitAllRooms(rooms) {
+    let accessibleRooms = new Set([0]);
+
+    for (let room of accessibleRooms) {
+        const keys = rooms[room];
+
+        for (let key of keys) {
+            accessibleRooms.add(key);
+        }
+    }
+
+    return accessibleRooms.size === rooms.length;
+}
+
+console.log(canVisitAllRooms([[1,3],[3,0,1],[2],[0]])); // false
