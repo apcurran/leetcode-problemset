c205c644ef68a068609907ef698444294fe33aae
Alex Curran <alexcurran.va@gmail.com>
2024-02-14 09:33:24 -0600

    Alternate linear time/space solution for rearrange-arr-elems-by-sign.js

--- medium/rearrange-arr-elems-by-sign.js
+++ medium/rearrange-arr-elems-by-sign.js
@@ -1,7 +1,29 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {number[]} nums
+//  * @return {number[]}
+//  */
+// function rearrangeArray(nums) {
+//     const posElems = nums.filter((num) => num > 0);
+//     const negElems = nums.filter((num) => num <= 0);
+//     let resArr = [];
+
+//     for (let i = 0; i < posElems.length; i++) {
+//         const posElem = posElems[i];
+//         const negElem = negElems[i];
+//         resArr.push(posElem, negElem);
+//     }
+
+//     return resArr;
+// }
+
 /**
- * solution 1
+ * solution 2 -- pre-filled array
  * time: O(n)
  * space: O(n)
  * 
@@ -9,17 +31,25 @@
  * @return {number[]}
  */
 function rearrangeArray(nums) {
-    const posElems = nums.filter((num) => num > 0);
-    const negElems = nums.filter((num) => num <= 0);
-    let resArr = [];
+    let results = new Array(nums.length).fill(0);
+    let positiveValuesIndex = 0;
+    let negativeValuesIndex = 1;
 
-    for (let i = 0; i < posElems.length; i++) {
-        const posElem = posElems[i];
-        const negElem = negElems[i];
-        resArr.push(posElem, negElem);
+    for (let i = 0; i < nums.length; i++) {
+        const current = nums[i];
+
+        if (current > 0) {
+            // positive
+            results[positiveValuesIndex] = current;
+            positiveValuesIndex += 2;
+        } else {
+            // negative
+            results[negativeValuesIndex] = current;
+            negativeValuesIndex += 2;
+        }
     }
 
-    return resArr;
+    return results;
 }
 
 console.log( rearrangeArray([3, 1, -2, -5, 2, -4]) ); // [3, -2, 1, -5, 2, -4]
