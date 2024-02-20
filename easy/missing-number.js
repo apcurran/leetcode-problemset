92730d574255273a7c1ab73a82b6df5ec860789b
Alex Curran <alexcurran.va@gmail.com>
2024-02-20 13:40:45 -0600

    Working optimized solution using Gauss summation formula

--- easy/missing-number.js
+++ easy/missing-number.js
@@ -1,19 +1,58 @@
 "use strict";
 
+// /**
+//  * solution 1 -- iterative
+//  * time: O(n^2) -- array.includes() within loop
+//  * space: O(1)
+//  * 
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// function missingNumber(nums) {
+//     for (let i = 0; i <= nums.length; i++) {
+//         if (!nums.includes(i)) return i;
+//     }
+
+//     return -1;
+// }
+
+// /**
+//  * solution 2 -- sort
+//  * time: O(n * log n)
+//  * space: O(n) -- sorting uses linear space
+//  * 
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// function missingNumber(nums) {
+//     nums.sort(function sortAsc(a, b) {
+//         return a - b;
+//     });
+
+//     if (nums[0] !== 0) return 0;
+
+//     if (nums[nums.length - 1] !== nums.length) return nums.length;
+
+//     for (let i = 1; i < nums.length; i++) {
+//         if (nums[i] !== i) return i;
+//     }
+
+//     return -1;
+// }
+
 /**
- * solution 1 -- iterative
- * time: O(n^2) -- array.includes() within loop
+ * solution 3 -- math (Gauss summation formula)
+ * time: O(n)
  * space: O(1)
  * 
  * @param {number[]} nums
  * @return {number}
  */
 function missingNumber(nums) {
-    for (let i = 0; i <= nums.length; i++) {
-        if (!nums.includes(i)) return i;
-    }
-
-    return -1;
+    const allNumsSum = (nums.length * (nums.length + 1)) / 2;
+    const actualNumsSum = nums.reduce((sum, current) => sum + current, 0);
+    
+    return allNumsSum - actualNumsSum;
 }
 
 console.log(missingNumber([3,0,1])); // 2
