f2022e45b83bbfb84c40f549808b3858eef79e02
Alex Curran <alexcurran.va@gmail.com>
2024-03-01 13:49:13 -0600

    Working linear time/space solution for max-odd-binary-number.js

---
+++ easy/max-odd-binary-number.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * solution 1 -- counting 1 bits
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} s
+ * @return {string}
+ */
+function maximumOddBinaryNumber(s) {
+    let onesCount = 0;
+
+    for (let bit of s) {
+        if (bit === "1") onesCount++;
+    }
+
+    const onesBits = "1".repeat(onesCount - 1);
+    const zerosBits = "0".repeat(s.length - onesCount);
+    const finalOneBit = "1";
+
+    return onesBits + zerosBits + finalOneBit;
+}
+
+console.log(maximumOddBinaryNumber("0101")); // "1001"
