40a36015ac5fc79f1380ccaa48d94eec1bff1eff
Alex Curran <alexcurran.va@gmail.com>
2024-03-04 11:20:07 -0600

    Linear time solution for squares-of-a-sorted-arr.js

--- easy/squares-of-a-sorted-arr.js
+++ easy/squares-of-a-sorted-arr.js
@@ -1,17 +1,51 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n * log n)
+//  * space: O(n)
+//  * 
+//  * @param {number[]} nums
+//  * @return {number[]}
+//  */
+// function sortedSquares(nums) {
+//     return nums
+//             .map((num) => num ** 2)
+//             .sort((a, b) => a - b);
+// }
+
 /**
- * solution 1
- * time: O(n * log n)
- * space: O(n)
+ * solution 2
+ * time: O(n)
+ * space: O(1) -- not including results array
  * 
  * @param {number[]} nums
  * @return {number[]}
  */
 function sortedSquares(nums) {
-    return nums
-            .map((num) => num ** 2)
-            .sort((a, b) => a - b);
+    let results = [];
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        const leftValueSquared = nums[left] ** 2;
+        const rightValueSquared = nums[right] ** 2;
+
+        if (leftValueSquared > rightValueSquared) {
+            // add left squared value
+            results.push(leftValueSquared);
+            // move left pointer
+            left++;
+        } else {
+            // add right squared value
+            results.push(rightValueSquared);
+            // move right pointer
+            right--;
+        }
+    }
+
+
+    return results.reverse();
 }
 
 console.log( sortedSquares([-4, -1, 0, 3, 10]) ); // [0,1,9,16,100]
