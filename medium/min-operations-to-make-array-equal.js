2aa976ef79f02500161bfb71b16e446ff3bcc398
Alex Curran <alexcurran.va@gmail.com>
2024-03-08 11:39:10 -0600

    Working linear time solution for min-operations-to-make-array-equal.js

---
+++ medium/min-operations-to-make-array-equal.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number} n
+ * @return {number}
+ */
+function minOperations(n) {
+    let result = 0;
+
+    for (let i = 0; i < n / 2; i++) {
+        const count = n - ((2 * i) + 1);
+        result += count;
+    }
+
+    return result;
+}
+
+console.log(minOperations(3)); // 2
+console.log(minOperations(6)); // 9
