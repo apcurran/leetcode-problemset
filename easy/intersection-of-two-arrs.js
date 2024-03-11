370d80a4ad0b0dd8f904663c6e2e13bffb6761d3
Alex Curran <alexcurran.va@gmail.com>
2024-03-11 09:52:25 -0500

    Two hashsets solution for intersection-of-two-arrs.js

--- easy/intersection-of-two-arrs.js
+++ easy/intersection-of-two-arrs.js
@@ -1,28 +1,51 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n * m)
+//  * space: O(n) || O(m) -- smaller of two arrs
+//  * 
+//  * @param {number[]} nums1
+//  * @param {number[]} nums2
+//  * @return {number[]}
+//  */
+// function intersection(nums1, nums2) {
+//     // create an empty result shared set
+//     let resSet = new Set();
+//     // iterate nums1 arr
+//     for (let num of nums1) {
+//         // check if nums2 arr includes curr num
+//         if (nums2.includes(num)) {
+//             // if yes, add num to set
+//             resSet.add(num);
+//         }
+//     }
+
+//     // return set converted to arr for res
+//     return [...resSet];
+// }
+
 /**
- * solution 1
- * time: O(n * m)
- * space: O(n) || O(m) -- smaller of two arrs
+ * solution 2 -- hashsets
+ * time: O(n + m)
+ * space: O(n + m)
  * 
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number[]}
  */
 function intersection(nums1, nums2) {
-    // create an empty result shared set
-    let resSet = new Set();
-    // iterate nums1 arr
-    for (let num of nums1) {
-        // check if nums2 arr includes curr num
-        if (nums2.includes(num)) {
-            // if yes, add num to set
-            resSet.add(num);
+    const nums1Set = new Set(nums1);
+    const nums2Set = new Set(nums2);
+    let sharedValues = [];
+
+    for (let num1Value of nums1Set) {
+        if (nums2Set.has(num1Value)) {
+            sharedValues.push(num1Value);
         }
     }
 
-    // return set converted to arr for res
-    return [...resSet];
+    return sharedValues;
 }
 
 console.log( intersection([1, 2, 2, 1], [2, 2]) ); // [2]
