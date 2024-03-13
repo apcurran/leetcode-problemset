0095da23ab3899e14c5efde8c15c79c8474b271a
Alex Curran <alexcurran.va@gmail.com>
2024-03-13 13:59:50 -0500

    Brute force solution 2

--- easy/find-the-pivot-int.js
+++ easy/find-the-pivot-int.js
@@ -30,4 +30,34 @@ function getSequenceSum(a, b) {
     return ((a + b) * (b - a + 1)) / 2;
 }
 
+// /**
+//  * solution 2 -- brute force
+//  * time: O(n^2)
+//  * space: O(1)
+//  * 
+//  * @param {number} n
+//  * @return {number}
+//  */
+// function pivotInteger(n) {
+//     for (let i = 1; i <= n; i++) {
+//         let sumLeft = 0;
+//         let sumRight = 0;
+
+//         for (let j = 1; j <= i; j++) {
+//             sumLeft += j;
+//         }
+
+//         for (let j = i; j <= n; j++) {
+//             sumRight += j;
+//         }
+
+//         if (sumLeft === sumRight) {
+//             return i; // pivot
+//         }
+//     }
+
+//     // no pivot found
+//     return -1;
+// }
+
 console.log( pivotInteger(8) ); // 6
