240e914707ff4917a3e1aceebdfe2d7abdc64d6e
Alex Curran <alexcurran.va@gmail.com>
2024-03-13 14:11:06 -0500

    Two pointer solution

--- easy/find-the-pivot-int.js
+++ easy/find-the-pivot-int.js
@@ -1,34 +1,34 @@
 "use strict";
 
-/**
- * solution 1 -- summation formula
- * time: O(n)
- * space: O(1)
- * 
- * @param {number} n
- * @return {number}
- */
-function pivotInteger(n) {
-    for (let pivot = 1; pivot <= n; pivot++) {
-        const sequenceSumUpToPivot = getSequenceSum(1, pivot);
-        const sequenceSumAfterPivot = getSequenceSum(pivot, n);
+// /**
+//  * solution 1 -- summation formula
+//  * time: O(n)
+//  * space: O(1)
+//  * 
+//  * @param {number} n
+//  * @return {number}
+//  */
+// function pivotInteger(n) {
+//     for (let pivot = 1; pivot <= n; pivot++) {
+//         const sequenceSumUpToPivot = getSequenceSum(1, pivot);
+//         const sequenceSumAfterPivot = getSequenceSum(pivot, n);
 
-        if (sequenceSumUpToPivot === sequenceSumAfterPivot) return pivot;
-    }
+//         if (sequenceSumUpToPivot === sequenceSumAfterPivot) return pivot;
+//     }
 
-    // if none work
-    return -1;
-}
+//     // if none work
+//     return -1;
+// }
 
-/**
- * 
- * @param {number} a 
- * @param {number} b 
- * @returns {number}
- */
-function getSequenceSum(a, b) {
-    return ((a + b) * (b - a + 1)) / 2;
-}
+// /**
+//  * 
+//  * @param {number} a 
+//  * @param {number} b 
+//  * @returns {number}
+//  */
+// function getSequenceSum(a, b) {
+//     return ((a + b) * (b - a + 1)) / 2;
+// }
 
 // /**
 //  * solution 2 -- brute force
@@ -59,5 +59,42 @@ function getSequenceSum(a, b) {
 //     // no pivot found
 //     return -1;
 // }
+
+/**
+ * solution 3 -- two pointers
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number} n
+ * @return {number}
+ */
+function pivotInteger(n) {
+    if (n === 1) return n;
+
+    let leftValue = 1;
+    let rightValue = n;
+    let sumLeft = leftValue;
+    let sumRight = rightValue;
+
+    while (leftValue < rightValue) {
+        if (sumLeft < sumRight) {
+            // move left pointer
+            leftValue++;
+            sumLeft += leftValue;
+        } else {
+            // move right pointer
+            rightValue--;
+            sumRight += rightValue;
+        }
+
+        // pivot condition
+        if (sumLeft === sumRight && leftValue + 1 === rightValue - 1) {
+            return leftValue + 1;
+        }
+    }
+
+    // no pivot found
+    return -1;
+}
 
 console.log( pivotInteger(8) ); // 6
