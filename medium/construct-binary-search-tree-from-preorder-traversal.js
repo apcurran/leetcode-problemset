79106c33457e484abe1d7663e9f3ca64ee2f505a
Alex Curran <alexcurran.va@gmail.com>
2024-03-15 16:03:24 -0500

    Recursive DFS for construct-binary-search-tree-from-preorder-traversal.js

---
+++ medium/construct-binary-search-tree-from-preorder-traversal.js
@@ -0,0 +1,49 @@
+"use strict";
+
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val);
+    this.left = (left === undefined ? null : left);
+    this.right = (right === undefined ? null : right);
+}
+
+/**
+ * solution 1 -- DFS recursion
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number[]} preorder
+ * @return {TreeNode}
+ */
+function bstFromPreorder(preorder) {
+    if (preorder.length === 0) return null;
+
+    let root = new TreeNode(preorder[0]);
+
+    for (let i = 1; i < preorder.length; i++) {
+        root = insert(root, preorder[i]);
+    }
+
+    return root;
+}
+
+/**
+ * 
+ * @param {TreeNode} root 
+ * @param {number} currentValue 
+ * @returns {TreeNode} altered node
+ */
+function insert(root, currentValue) {
+    if (root === null) {
+        return new TreeNode(currentValue);
+    }
+
+    // compare current val and root val
+    if (currentValue < root.val) {
+        root.left = insert(root.left, currentValue);
+    } else {
+        root.right = insert(root.right, currentValue);
+    }
+
+    // root with value inserted
+    return root;
+}
