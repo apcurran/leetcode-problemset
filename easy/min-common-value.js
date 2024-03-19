59dcc3aaf48604421b50b184edb3ec66adf08787
Alex Curran <alexcurran.va@gmail.com>
2024-03-19 11:58:47 -0500

    Two pointer solution

--- easy/min-common-value.js
+++ easy/min-common-value.js
@@ -1,21 +1,51 @@
 "use strict";
 
+// /**
+//  * solution 1 -- hashset
+//  * n = nums1 length
+//  * m = nums2 length
+//  * time: O(n + m)
+//  * space: O(n)
+//  * 
+//  * @param {number[]} nums1
+//  * @param {number[]} nums2
+//  * @return {number}
+//  */
+// function getCommon(nums1, nums2) {
+//     const nums1Set = new Set(nums1);
+
+//     for (let num of nums2) {
+//         if (nums1Set.has(num)) return num;
+//     }
+
+//     return -1;
+// }
+
 /**
- * solution 1 -- hashset
+ * solution 2 -- two pointers
  * n = nums1 length
  * m = nums2 length
  * time: O(n + m)
- * space: O(n)
+ * space: O(1)
  * 
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number}
  */
 function getCommon(nums1, nums2) {
-    const nums1Set = new Set(nums1);
+    let nums1Pointer = 0;
+    let nums2Pointer = 0;
 
-    for (let num of nums2) {
-        if (nums1Set.has(num)) return num;
+    while (nums1Pointer < nums1.length && nums2Pointer < nums2.length) {
+        if (nums1[nums1Pointer] < nums2[nums2Pointer]) {
+            // move nums1Pointer forwards
+            nums1Pointer++;
+        } else if (nums1[nums1Pointer] > nums2[nums2Pointer]) {
+            nums2Pointer++;
+        } else {
+            // equal values
+            return nums1[nums1Pointer];
+        }
     }
 
     return -1;
