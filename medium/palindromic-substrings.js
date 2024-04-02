1bf1a12a2ff805e94c54b3c211d6bab92df45368
Alex Curran <alexcurran.va@gmail.com>
2024-04-02 17:04:34 -0500

    Working solution for palindromic-substrings.js

---
+++ medium/palindromic-substrings.js
@@ -0,0 +1,40 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n^2)
+ * space: O(1)
+ * 
+ * @param {string} s
+ * @return {number}
+ */
+function countSubstrings(s) {
+    let totalPalindromes = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        // get odd-length palindromes
+        let left = i;
+        let right = i;
+
+        while (left >= 0 && right < s.length && s[left] === s[right]) {
+            totalPalindromes++;
+            left--;
+            right++;
+        }        
+
+        // get even-length palindromes
+        left = i;
+        right = i + 1;
+
+        while (left >= 0 && right < s.length && s[left] === s[right]) {
+            totalPalindromes++;
+            left--;
+            right++;
+        }        
+    }
+
+    return totalPalindromes;
+}
+
+console.log(countSubstrings("abc")); // 3
+console.log(countSubstrings("aaa")); // 6
