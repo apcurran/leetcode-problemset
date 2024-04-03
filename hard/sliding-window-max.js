1df93c3f7d184e5ed1275934a99661cbaa949566
Alex Curran <alexcurran.va@gmail.com>
2024-04-03 11:48:19 -0500

    Correct, but TLE solution for sliding-window-max.js

---
+++ hard/sliding-window-max.js
@@ -0,0 +1,40 @@
+"use strict";
+
+/**
+ * solution 1 -- correct, but TLE
+ * time: O(n * k)
+ * space: O(n + k)
+ * 
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+function maxSlidingWindow(nums, k) {
+    let maxValues = [];
+
+    // iterate nums by 1 each time
+    for (let i = 0; i <= nums.length - k; i++) {
+        // check current window of length k
+        const chunk = nums.slice(i, i + k);
+        const chunkMax = getMax(chunk);
+        maxValues.push(chunkMax);
+    }
+
+    return maxValues;
+}
+
+
+/**
+ * @param {number[]} nums 
+ */
+function getMax(nums) {
+    let max = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        max = Math.max(max, nums[i]);
+    }
+
+    return max;
+}
+
+console.log(maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3)); // [3,3,5,5,6,7]
