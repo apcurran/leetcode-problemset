451f6e57629dec73fde6653b28dc5a300ff9bb40
Alex Curran <alexcurran.va@gmail.com>
2024-04-09 11:23:18 -0500

    Linear time and constant space solution for time-needed-to-buy-tickets.js

--- easy/time-needed-to-buy-tickets.js
+++ easy/time-needed-to-buy-tickets.js
@@ -1,8 +1,39 @@
 "use strict";
 
+// /**
+//  * solution 1 -- simulation
+//  * time: O(n^2)
+//  * space: O(1)
+//  * 
+//  * @param {number[]} tickets
+//  * @param {number} k
+//  * @return {number}
+//  */
+// function timeRequiredToBuy(tickets, k) {
+//     let totalTimeInSeconds = 0;
+
+//     outerLoop: while (tickets[k] > 0) {
+//         for (let i = 0; i < tickets.length; i++) {
+//             // decrease each value by 1 that is over 0
+//             if (tickets[i] <= 0) continue;
+
+//             tickets[i] -= 1;
+//             // for each value decreased, add 1 to total time
+//             totalTimeInSeconds += 1;
+
+//             // check current position is kth and at 0
+//             if (i === k && tickets[i] === 0) {
+//                 break outerLoop;
+//             }
+//         }
+//     }
+
+//     return totalTimeInSeconds;
+// }
+
 /**
- * solution 1 -- simulation
- * time: O(n^2)
+ * solution 2
+ * time: O(n)
  * space: O(1)
  * 
  * @param {number[]} tickets
@@ -12,19 +43,11 @@
 function timeRequiredToBuy(tickets, k) {
     let totalTimeInSeconds = 0;
 
-    outerLoop: while (tickets[k] > 0) {
-        for (let i = 0; i < tickets.length; i++) {
-            // decrease each value by 1 that is over 0
-            if (tickets[i] <= 0) continue;
-
-            tickets[i] -= 1;
-            // for each value decreased, add 1 to total time
-            totalTimeInSeconds += 1;
-
-            // check current position is kth and at 0
-            if (i === k && tickets[i] === 0) {
-                break outerLoop;
-            }
+    for (let i = 0; i < tickets.length; i++) {
+        if (i <= k) {
+            totalTimeInSeconds += Math.min(tickets[i], tickets[k]);
+        } else {
+            totalTimeInSeconds += Math.min(tickets[i], tickets[k] - 1);
         }
     }
 
