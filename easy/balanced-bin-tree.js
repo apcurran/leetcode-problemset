703dbbdf411924974ecdd3c7a74d4f15243a7392
Alex Curran <alexcurran.va@gmail.com>
2024-04-29 13:45:24 -0500

    Add comments explaining reasoning for subtree balance checks and height diff

--- easy/balanced-bin-tree.js
+++ easy/balanced-bin-tree.js
@@ -1,6 +1,5 @@
 "use strict";
 
-// Definition for a binary tree node.
 function TreeNode(val, left, right) {
     this.val = (val === undefined ? 0 : val);
     this.left = (left === undefined ? null : left);
@@ -52,6 +51,7 @@ function isBalanced(root) {
      */
     function dfs(root) {
         if (root === null) {
+            // a null node is a zero-height, balanced tree since height 0 is technically balanced
             return [true, 0];
         }
 
@@ -59,6 +59,8 @@ function isBalanced(root) {
         const rightSubtree = dfs(root.right);
         const heightDiff = Math.abs(leftSubtree[1] - rightSubtree[1]);
         const areSubtreesBalanced = leftSubtree[0] && rightSubtree[0];
+        // subtrees (left and right) must be balanced first
+        // also, the difference between left and right heights must differ by no more than 1 (definition of height-balanced trees)
         const isCurrentBalanced = areSubtreesBalanced && heightDiff <= 1;
         const currentNodeHeight = 1;
         const currentHeight = currentNodeHeight + Math.max(leftSubtree[1], rightSubtree[1]);
