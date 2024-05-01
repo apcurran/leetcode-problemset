c695195fb4508f24cb5d510cfcd684392831ce49
Alex Curran <alexcurran.va@gmail.com>
2024-05-01 10:17:01 -0500

    Difference between smallest ints yields solution 1 for find-the-int-added-to-array-1.js

---
+++ easy/find-the-int-added-to-array-1.js
@@ -0,0 +1,33 @@
+"use strict";
+
+/**
+ * solution 1 -- diff between smallest ints
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+function addedInteger(nums1, nums2) {
+    const nums1Min = getMin(nums1);
+    const nums2Min = getMin(nums2);
+
+    return nums2Min - nums1Min;
+}
+
+/**
+ * @param {number[]} nums 
+ * @returns {number}
+ */
+function getMin(nums) {
+    let min = nums[0];
+
+    for (let i = 1; i < nums.length; i++) {
+        min = Math.min(min, nums[i]);
+    }
+
+    return min;
+}
+
+console.log(addedInteger([2,6,4], [9,7,5])); // 3
