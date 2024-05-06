eebb269083ca35ea3f5c9511a9b6b9436a759c67
Alex Curran <alexcurran.va@gmail.com>
2024-05-06 14:21:27 -0500

    Recursive solution for remove-nodes-from-linked-list.js

--- medium/remove-nodes-from-linked-list.js
+++ medium/remove-nodes-from-linked-list.js
@@ -5,8 +5,46 @@ function ListNode(val, next) {
     this.next = (next === undefined ? null : next);
 }
 
+// /**
+//  * solution 1 -- iterative
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {ListNode} head
+//  * @return {ListNode}
+//  */
+// function removeNodes(head) {
+//     let stack = [];
+//     let current = head;
+
+//     while (current !== null) {
+//         stack.push(current);
+//         current = current.next;
+//     }
+
+//     // check stack with nodes
+//     current = stack.pop();
+//     let maxValue = current.val;
+//     let resultList = new ListNode(maxValue);
+
+//     while (stack.length > 0) {
+//         current = stack.pop();
+
+//         if (current.val < maxValue) {
+//             continue;
+//         } else {
+//             const newNode = new ListNode(current.val);
+//             newNode.next = resultList;
+//             resultList = newNode;
+//             maxValue = current.val;
+//         }
+//     }
+
+//     return resultList;
+// }
+
 /**
- * solution 1 -- iterative
+ * solution 1 -- recursive
  * time: O(n)
  * space: O(n)
  * 
@@ -14,31 +52,21 @@ function ListNode(val, next) {
  * @return {ListNode}
  */
 function removeNodes(head) {
-    let stack = [];
-    let current = head;
-
-    while (current !== null) {
-        stack.push(current);
-        current = current.next;
+    // reached the end of the linked list
+    if (head === null || head.next === null) {
+        return head;
     }
 
-    // check stack with nodes
-    current = stack.pop();
-    let maxValue = current.val;
-    let resultList = new ListNode(maxValue);
+    const nextNode = removeNodes(head.next);
 
-    while (stack.length > 0) {
-        current = stack.pop();
+    if (head.val < nextNode.val) {
+        // if current node's val is less than next node's val,
+        // delete current node by returning nextNode
+        return nextNode;
+    } else {
+        // otherwise, keep current node
+        head.next = nextNode;
 
-        if (current.val < maxValue) {
-            continue;
-        } else {
-            const newNode = new ListNode(current.val);
-            newNode.next = resultList;
-            resultList = newNode;
-            maxValue = current.val;
-        }
+        return head;
     }
-
-    return resultList;
 }
