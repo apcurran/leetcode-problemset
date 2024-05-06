d2c83f7577b83164a04df4ffc4eb1814093c6317
Alex Curran <alexcurran.va@gmail.com>
2024-05-06 10:14:27 -0500

    Indent ON keyword from LEFT JOIN

--- easy/sql/average-time-of-process-per-machine.sql
+++ easy/sql/average-time-of-process-per-machine.sql
@@ -4,5 +4,5 @@ SELECT
     ROUND(AVG(activity_end.timestamp - activity_start.timestamp), 3) AS processing_time
 FROM (SELECT * FROM Activity WHERE activity_type = 'end') AS activity_end
 LEFT JOIN (SELECT * FROM Activity WHERE activity_type = 'start') AS activity_start
-ON activity_start.machine_id = activity_end.machine_id
+    ON activity_start.machine_id = activity_end.machine_id
 GROUP BY activity_start.machine_id;
