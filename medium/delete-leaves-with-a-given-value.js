272c2a374b668efefba63d43420c523da4f7ee78
Alex Curran <alexcurran.va@gmail.com>
2024-05-17 11:52:46 -0500

    Add explanation for postorder DFS

--- medium/delete-leaves-with-a-given-value.js
+++ medium/delete-leaves-with-a-given-value.js
@@ -1,6 +1,5 @@
 "use strict";
 
-// Definition for a binary tree node.
 function TreeNode(val, left, right) {
     this.val = (val===undefined ? 0 : val);
     this.left = (left===undefined ? null : left);
@@ -8,10 +7,11 @@ function TreeNode(val, left, right) {
 }
 
 /**
- * solution 1 -- recursion
+ * solution 1 -- recursion (postorder DFS)
  * time: O(n)
  * space: O(n) -- callstack
  * 
+ * delete all leaf nodes with target value
  * @param {TreeNode} root
  * @param {number} target
  * @return {TreeNode|null}
@@ -19,15 +19,18 @@ function TreeNode(val, left, right) {
 function removeLeafNodes(root, target) {
     if (root === null) return null;
 
+    // postorder -- check children first
+    // set node children left or right to the result (either the node itself or null when deleting a node)
     root.left = removeLeafNodes(root.left, target);
     root.right = removeLeafNodes(root.right, target);
 
+    // now check current node
     if (root.left === null && root.right === null && root.val === target) {
         // leaf node identified
         // leaf node has a value equivalent to target
         // remove leaf node
         return null;
+    } else {
+        return root;
     }
-
-    return root;
 }
