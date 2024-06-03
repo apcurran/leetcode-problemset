7ca013c32a29edd8d1d2c6ad56726463cc8534db
Alex Curran <alexcurran.va@gmail.com>
2024-06-03 14:41:03 -0500

    Working solution for mean-of-array-after-removing-some-elements.js

---
+++ easy/mean-of-array-after-removing-some-elements.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n * log n)
+ * space: O(n) -- due to array.sort() timsort
+ * 
+ * @param {number[]} arr
+ * @return {number}
+ */
+function trimMean(arr) {
+    arr.sort(function sortAsc(a, b) {
+        return a - b;
+    });
+    const fivePercentLength = arr.length * .05;
+    let remainingNumsSum = 0;
+    
+    for (let i = fivePercentLength; i < arr.length - fivePercentLength; i++) {
+        remainingNumsSum += arr[i];
+    }
+
+    const remainingNumsLength = arr.length - fivePercentLength * 2;
+
+    return remainingNumsSum / remainingNumsLength;
+}
+
+console.log(trimMean([1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3])); // 2
