05c5527fe1cb922721c7e597a65fdf7eab2c89d8
Alex Curran <alexcurran.va@gmail.com>
2024-06-03 12:34:04 -0500

    Built-in method solution 2

--- easy/rev-str.js
+++ easy/rev-str.js
@@ -1,7 +1,36 @@
 "use strict";
 
+// /**
+//  * solution 1 -- two-pointer technique
+//  * time: O(n)
+//  * space: O(1)
+//  * 
+//  * @param {string[]} s
+//  * @return {void} Do not return anything, modify s in-place instead.
+//  */
+// function reverseString(s) {
+//     let leftPointer = 0;
+//     let rightPointer = s.length - 1;
+//     // 0 -> 4
+//     // 1 -> 3
+//     // 2 -> 2
+//     // 3 -> 1
+//     // 4 -> 0
+//     while (leftPointer < rightPointer) {
+//         const temp = s[leftPointer];
+//         // left char
+//         s[leftPointer] = s[rightPointer];
+//         // right char
+//         s[rightPointer] = temp;
+
+//         // move pointers
+//         leftPointer++;
+//         rightPointer--;
+//     }
+// }
+
 /**
- * solution 1 -- two-pointer technique
+ * solution 1 -- array.reverse() method
  * time: O(n)
  * space: O(1)
  * 
@@ -9,24 +38,7 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 function reverseString(s) {
-    let leftPointer = 0;
-    let rightPointer = s.length - 1;
-    // 0 -> 4
-    // 1 -> 3
-    // 2 -> 2
-    // 3 -> 1
-    // 4 -> 0
-    while (leftPointer < rightPointer) {
-        const temp = s[leftPointer];
-        // left char
-        s[leftPointer] = s[rightPointer];
-        // right char
-        s[rightPointer] = temp;
-
-        // move pointers
-        leftPointer++;
-        rightPointer--;
-    }
+    s.reverse();
 }
 
 console.log( reverseString(["h","e","l","l","o"]) ); // ["o","l","l","e","h"]
