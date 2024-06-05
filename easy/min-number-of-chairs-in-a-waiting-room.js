309dd827f32a56eccafcd73455d5568a4f7276d4
Alex Curran <alexcurran.va@gmail.com>
2024-06-05 10:53:43 -0500

    Linear time, constant space solution for min-number-of-chairs-in-a-waiting-room.js

---
+++ easy/min-number-of-chairs-in-a-waiting-room.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {string} s
+ * @return {number}
+ */
+function minimumChairs(s) {
+    let enterCount = 0;
+    let maxEnterCount = 0;
+
+    for (let char of s) {
+        if (char === "E") {
+            enterCount++;
+            maxEnterCount = Math.max(maxEnterCount, enterCount);
+        } else {
+            enterCount--;
+        }
+    }
+
+    return maxEnterCount;
+}
+
+console.log(minimumChairs("EEEEEEE")); // 7
+console.log(minimumChairs("ELELEEL")); // 2
+console.log(minimumChairs("ELEELEELLL")); // 3
