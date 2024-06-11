47dbfb1945f72f3692fd82879b0a0b53abfe078b
Alex Curran <alexcurran.va@gmail.com>
2024-06-11 09:31:20 -0500

    Replace array.sort() with array.toSorted() to avoid having to make an array copy first before sorting

--- easy/height-checker.js
+++ easy/height-checker.js
@@ -8,20 +8,22 @@
  * @param {number[]} actualHeights
  * @return {number}
  */
-function heightChecker(actualHeights) {
-    const expectedHeights = [...actualHeights].sort((a, b) => a - b);
-    let differCounter = 0;
+function heightChecker(heights) {
+    const sortedHeights = heights.toSorted(function sortAsc(a, b) {
+        return a - b;
+    });
+    let heightDiscrepancyCounter = 0;
+    
+    for (let i = 0; i < heights.length; i++) {
+        const originalHeight = heights[i];
+        const expectedHeight = sortedHeights[i];
 
-    for (let i = 0; i < actualHeights.length; i++) {
-        const currentActualHeight = actualHeights[i];
-        const currentExpectedHeight = expectedHeights[i];
-
-        if (currentActualHeight !== currentExpectedHeight) {
-            differCounter++;
+        if (expectedHeight !== originalHeight) {
+            heightDiscrepancyCounter++;
         }
     }
 
-    return differCounter;
+    return heightDiscrepancyCounter;
 }
 
 console.log( heightChecker([1,1,4,2,1,3]) ); // 3
