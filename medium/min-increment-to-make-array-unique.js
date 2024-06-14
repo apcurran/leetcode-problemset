456a4c7dc4bc72e790b6ba8d25a6042e384c82a9
Alex Curran <alexcurran.va@gmail.com>
2024-06-14 09:53:39 -0500

    Sort-based solution for min-increment-to-make-array-unique.js

---
+++ medium/min-increment-to-make-array-unique.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/**
+ * solution 1 -- sorting
+ * time: O(n * log n)
+ * space: O(n)
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
+function minIncrementForUnique(nums) {
+    nums.sort(function sortAsc(a, b) {
+        return a - b;
+    });
+
+    let incrementCount = 0;
+
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i - 1] >= nums[i]) {
+            // update counter
+            incrementCount += (1 + nums[i - 1] - nums[i]);
+            // update current num
+            nums[i] = nums[i - 1] + 1;
+        }
+    }
+
+    return incrementCount;
+}
+
+console.log(minIncrementForUnique([1,2,2])); // 1
