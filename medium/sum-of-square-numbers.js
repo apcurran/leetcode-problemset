3b5f749d76f77e38c178ec1e12a2bb9ae474eddd
Alex Curran <alexcurran.va@gmail.com>
2024-06-17 12:23:42 -0500

    Hashset sqrt solution for sum-of-square-numbers.js

---
+++ medium/sum-of-square-numbers.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/**
+ * solution 1 -- hashset
+ * time: O(sqrt(c))
+ * space: O(sqrt(c));
+ * 
+ * @param {number} c
+ * @return {boolean}
+ */
+function judgeSquareSum(c) {
+    let squareRoots = new Set();
+
+    for (let i = 0; i <= Math.sqrt(c); i++) {
+        const square = i * i;
+        squareRoots.add(square);
+    }
+
+    let a = 0;
+
+    while (a * a <= c) {
+        const target = c - (a * a);
+
+        if (squareRoots.has(target)) {
+            return true;
+        }
+
+        a++;
+    }
+
+    return false;
+}
+
+console.log(judgeSquareSum(5)); // true
+console.log(judgeSquareSum(3)); // false
