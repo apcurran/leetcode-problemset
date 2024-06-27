0b591b8358048275a3225f4ed397260a82776fd1
Alex Curran <alexcurran.va@gmail.com>
2024-06-27 10:51:30 -0500

    Constant time/space solution

--- easy/find-center-of-star-graph.js
+++ easy/find-center-of-star-graph.js
@@ -1,9 +1,30 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * m = len of first nested arr
+//  * time: O(m^2)
+//  * space: O(1)
+//  * 
+//  * @param {number[][]} edges
+//  * @return {number}
+//  */
+// function findCenter(edges) {
+//     const firstEdge = edges[0];
+//     const secondEdge = edges[1];
+    
+//     for (let i = 0; i < firstEdge.length; i++) {
+//         const currFirstArrNum = firstEdge[i];
+
+//         if (secondEdge.includes(currFirstArrNum)) return currFirstArrNum;
+//     }
+    
+//     return -1;
+// }
+
 /**
- * solution 1
- * m = len of first nested arr
- * time: O(m^2)
+ * solution 2
+ * time: O(1)
  * space: O(1)
  * 
  * @param {number[][]} edges
@@ -13,13 +34,11 @@ function findCenter(edges) {
     const firstEdge = edges[0];
     const secondEdge = edges[1];
     
-    for (let i = 0; i < firstEdge.length; i++) {
-        const currFirstArrNum = firstEdge[i];
-
-        if (secondEdge.includes(currFirstArrNum)) return currFirstArrNum;
+    if (secondEdge.includes(firstEdge[0])) {
+        return firstEdge[0];
+    } else {
+        return firstEdge[1];
     }
-    
-    return -1;
 }
 
 console.log( findCenter([[1, 2], [2, 3], [4, 2]]) ); // 2
