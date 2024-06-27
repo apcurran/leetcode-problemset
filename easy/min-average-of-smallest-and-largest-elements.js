233b7021e2b402dbcf2e36f6f3b4d934867f0525
Alex Curran <alexcurran.va@gmail.com>
2024-06-27 11:56:25 -0500

    Improve time complexity to O(n * log n)

--- easy/min-average-of-smallest-and-largest-elements.js
+++ easy/min-average-of-smallest-and-largest-elements.js
@@ -1,8 +1,37 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n^2)
+//  * space: O(n)
+//  * 
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// function minimumAverage(nums) {
+//     nums.sort(function sortAsc(a, b) {
+//         return a - b;
+//     });
+//     const totalIterations = nums.length / 2;
+//     let globalMin = Infinity;
+
+//     // repeat n / 2 times
+//     for (let i = 0; i < totalIterations; i++) {
+//         const min = nums[0];
+//         const max = nums[nums.length - 1];
+//         const avg = (min + max) / 2;
+//         globalMin = Math.min(globalMin, avg);
+//         // remove min and max from arr
+//         const numsWithMinAndMaxRemoved = nums.slice(1, nums.length - 1);
+//         nums = numsWithMinAndMaxRemoved;
+//     }
+
+//     return globalMin;
+// }
+
 /**
  * solution 1
- * time: O(n^2)
+ * time: O(n * log n) -- array.sort()
  * space: O(n)
  * 
  * @param {number[]} nums
@@ -12,18 +41,13 @@ function minimumAverage(nums) {
     nums.sort(function sortAsc(a, b) {
         return a - b;
     });
-    const totalIterations = nums.length / 2;
     let globalMin = Infinity;
 
-    // repeat n / 2 times
-    for (let i = 0; i < totalIterations; i++) {
-        const min = nums[0];
-        const max = nums[nums.length - 1];
-        const avg = (min + max) / 2;
-        globalMin = Math.min(globalMin, avg);
-        // remove min and max from arr
-        const numsWithMinAndMaxRemoved = nums.slice(1, nums.length - 1);
-        nums = numsWithMinAndMaxRemoved;
+    for (let i = 0; i < nums.length / 2; i++) {
+        const min = nums[i];
+        const max = nums[nums.length - 1 - i];
+        const average = (min + max) / 2;
+        globalMin = Math.min(globalMin, average);
     }
 
     return globalMin;
