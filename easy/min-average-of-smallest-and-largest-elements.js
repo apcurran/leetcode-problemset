f7eb79cc442351e89f4a964e4480d7074741de62
Alex Curran <alexcurran.va@gmail.com>
2024-06-27 11:49:56 -0500

    Working solution for min-average-of-smallest-and-largest-elements.js

---
+++ easy/min-average-of-smallest-and-largest-elements.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n^2)
+ * space: O(n)
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
+function minimumAverage(nums) {
+    nums.sort(function sortAsc(a, b) {
+        return a - b;
+    });
+    const totalIterations = nums.length / 2;
+    let globalMin = Infinity;
+
+    // repeat n / 2 times
+    for (let i = 0; i < totalIterations; i++) {
+        const min = nums[0];
+        const max = nums[nums.length - 1];
+        const avg = (min + max) / 2;
+        globalMin = Math.min(globalMin, avg);
+        // remove min and max from arr
+        const numsWithMinAndMaxRemoved = nums.slice(1, nums.length - 1);
+        nums = numsWithMinAndMaxRemoved;
+    }
+
+    return globalMin;
+}
+
+console.log(minimumAverage([7,8,3,4,15,13,4,1])); // 5.5
