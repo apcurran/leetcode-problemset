087f2c9457420a41da37e03e8eaac4152db2d21a
Alex Curran <alexcurran.va@gmail.com>
2024-07-03 13:57:45 -0500

    Two pointer solution for min-difference-between-largest-and-smallest-value-in-three-moves.js

---
+++ medium/min-difference-between-largest-and-smallest-value-in-three-moves.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n * log n)
+ * space: O(n)
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
+function minDifference(nums) {
+    if (nums.length <= 4) {
+        return 0;
+    }
+
+    nums.sort(function sortAsc(numA, numB) {
+        return numA - numB;
+    });
+    let result = Infinity;
+
+    for (let left = 0; left < 4; left++) {
+        const right = nums.length - 4 + left;
+        const difference = nums[right] - nums[left];
+        result = Math.min(result, difference);
+    }
+
+    return result;
+}
+
+console.log(minDifference([5,3,2,4])); // 0
+console.log(minDifference([1,5,0,10,14])); // 1
