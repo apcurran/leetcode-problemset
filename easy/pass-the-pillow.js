9b5dbdbb3d6cd5459b490b58e3912d3649adbd81
Alex Curran <alexcurran.va@gmail.com>
2024-07-08 11:29:00 -0500

    Working iterative solution for pass-the-pillow.js

---
+++ easy/pass-the-pillow.js
@@ -0,0 +1,32 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number} n
+ * @param {number} time
+ * @return {number}
+ */
+function passThePillow(n, time) {
+    let currentPillowPosition = 1;
+    let currentTime = 0;
+    let currentDirection = 1;
+
+    while (currentTime < time) {
+        if (currentPillowPosition + currentDirection > 0 &&
+            currentPillowPosition + currentDirection <= n) {
+
+            currentPillowPosition += currentDirection;
+            currentTime++;
+        } else {
+            currentDirection *= -1;
+        }
+    }
+
+    return currentPillowPosition;
+}
+
+console.log(passThePillow(4, 5)); // 2
+console.log(passThePillow(3, 2)); // 3
