10a870a394eb1c898ba7ac3557476aa93d1e3bb3
Alex Curran <alexcurran.va@gmail.com>
2024-07-09 11:05:53 -0500

    Simulation-based solution for average-waiting-time.js

---
+++ medium/average-waiting-time.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ * solution 1 -- simulation
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number[][]} customers
+ * @return {number}
+ */
+function averageWaitingTime(customers) {
+    let nextIdleTime = 0;
+    let totalWaitTime = 0;
+
+    for (let i = 0; i < customers.length; i++) {
+        const [arrivalTime, orderTime] = customers[i];
+        nextIdleTime = Math.max(arrivalTime, nextIdleTime) + orderTime;
+        
+        const customerWaitTime = nextIdleTime - arrivalTime;
+        totalWaitTime += customerWaitTime;
+    }
+
+    const average = totalWaitTime / customers.length;
+
+    return average;
+}
+
+console.log(averageWaitingTime([[1,2],[2,5],[4,3]])); // 5
+console.log(averageWaitingTime([[5,2],[5,4],[10,3],[20,1]])); // 3.25
