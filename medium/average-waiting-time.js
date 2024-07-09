8c023319e93c823b7b01f8fb12fd48be098cd6e9
Alex Curran <alexcurran.va@gmail.com>
2024-07-09 13:40:56 -0500

    Alternative approach to simulation solution with conditional logic

--- medium/average-waiting-time.js
+++ medium/average-waiting-time.js
@@ -1,7 +1,32 @@
 "use strict";
 
+// /**
+//  * solution 1 -- simulation
+//  * time: O(n)
+//  * space: O(1)
+//  * 
+//  * @param {number[][]} customers
+//  * @return {number}
+//  */
+// function averageWaitingTime(customers) {
+//     let nextIdleTime = 0;
+//     let totalWaitTime = 0;
+
+//     for (let i = 0; i < customers.length; i++) {
+//         const [arrivalTime, orderTime] = customers[i];
+//         nextIdleTime = Math.max(arrivalTime, nextIdleTime) + orderTime;
+        
+//         const customerWaitTime = nextIdleTime - arrivalTime;
+//         totalWaitTime += customerWaitTime;
+//     }
+
+//     const average = totalWaitTime / customers.length;
+
+//     return average;
+// }
+
 /**
- * solution 1 -- simulation
+ * solution 2 -- simulation
  * time: O(n)
  * space: O(1)
  * 
@@ -10,17 +35,21 @@
  */
 function averageWaitingTime(customers) {
     let nextIdleTime = 0;
-    let totalWaitTime = 0;
+    let sumOfWaitTimes = 0;
 
-    for (let i = 0; i < customers.length; i++) {
-        const [arrivalTime, orderTime] = customers[i];
-        nextIdleTime = Math.max(arrivalTime, nextIdleTime) + orderTime;
-        
-        const customerWaitTime = nextIdleTime - arrivalTime;
-        totalWaitTime += customerWaitTime;
+    for (const [arrivalTime, orderTime] of customers) {
+        if (nextIdleTime > arrivalTime) {
+            sumOfWaitTimes += nextIdleTime - arrivalTime;
+        } else {
+            // fast-forward in time
+            nextIdleTime = arrivalTime;
+        }
+
+        sumOfWaitTimes += orderTime;
+        nextIdleTime += orderTime;
     }
 
-    const average = totalWaitTime / customers.length;
+    const average = sumOfWaitTimes / customers.length;
 
     return average;
 }
