cdd10bcba646484c7c6ae038e2b0077b92a36869
Alex Curran <alexcurran.va@gmail.com>
2024-07-10 14:34:08 -0500

    Stack-based solution

--- easy/crawler-log-folder.js
+++ easy/crawler-log-folder.js
@@ -1,29 +1,54 @@
 "use strict";
 
+// /**
+//  * solution 1 -- counter simulation
+//  * time: O(n)
+//  * space: O(1)
+//  * 
+//  * @param {string[]} logs
+//  * @return {number}
+//  */
+// function minOperations(logs) {
+//     let operationsCount = 0;
+
+//     for (let log of logs) {
+//         if (log === "../") {
+//             // never go back further than main dir -- 0 in this case
+//             operationsCount = Math.max(0, operationsCount - 1);
+//         } else if (!log.includes("./")) {
+//             // increase counter by 1
+//             operationsCount++;
+//         }
+//         // ignore "./" operations,
+//         // these stay in the current directory
+//     }
+
+//     return operationsCount;
+// }
+
 /**
- * solution 1 -- counter simulation
+ * solution 2 -- stack simulation
  * time: O(n)
- * space: O(1)
+ * space: O(n)
  * 
  * @param {string[]} logs
  * @return {number}
  */
 function minOperations(logs) {
-    let operationsCount = 0;
+    let stack = [];
 
     for (let log of logs) {
         if (log === "../") {
-            // never go back further than main dir -- 0 in this case
-            operationsCount = Math.max(0, operationsCount - 1);
+            // remove from stack if not empty
+            if (stack.length !== 0) {
+                stack.pop();
+            }
         } else if (!log.includes("./")) {
-            // increase counter by 1
-            operationsCount++;
+            stack.push(log);
         }
-        // ignore "./" operations,
-        // these stay in the current directory
     }
 
-    return operationsCount;
+    return stack.length;
 }
 
 console.log(minOperations(["d1/","d2/","../","d21/","./"])); // 2
