262d6e6b50a78a00df106c4bfd31ff6dbcb9229c
Alex Curran <alexcurran.va@gmail.com>
2024-07-15 14:12:38 -0500

    Stack-based solution for reverse-substrings-between-each-pair-of-parentheses.js

---
+++ medium/reverse-substrings-between-each-pair-of-parentheses.js
@@ -0,0 +1,37 @@
+"use strict";
+
+/**
+ * solution 1 -- stack
+ * n = stack length
+ * m = chunk length
+ * time: O(n * m)
+ * space: O(n * m)
+ * 
+ * @param {string} str
+ * @return {string}
+ */
+function reverseParentheses(str) {
+    let stack = [];
+
+    for (let char of str) {
+        if (char === ")") {
+            let chunk = [];
+
+            while (stack.at(-1) !== "(") {
+                const removedElement = stack.pop();
+                chunk.push(removedElement);
+            }
+
+            // remove open paren from stack
+            stack.pop();
+            // add reversed chunk back on to the stack
+            stack.push(...chunk);
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.join("");
+}
+
+console.log(reverseParentheses("(u(love)i)")); // "iloveu"
