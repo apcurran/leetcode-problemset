fdd46c0f9702c40fed93f4bbae77915fc9d42c81
Alex Curran <alexcurran.va@gmail.com>
2024-07-23 13:39:41 -0500

    Refactor

--- easy/sort-arr-by-incr-freq.js
+++ easy/sort-arr-by-incr-freq.js
@@ -1,36 +1,35 @@
 "use strict";
 
 /**
- * solution 1 -- cache
- * time: O(n * log n)
- * space: O(n)
+ * solution 1
+ * time: O(n * log n) -- array.sort() call
+ * space: O(n) -- map and built-in sort use linear space
  * 
  * @param {number[]} nums
  * @return {number[]}
  */
 function frequencySort(nums) {
-    // cache num frequencies
-    let numFreqs = new Map();
+    // count freq of each num
+    let numberFrequencies = new Map();
 
     for (let num of nums) {
-        const prevFreq = numFreqs.get(num) || 0;
-        numFreqs.set(num, prevFreq + 1);
+        const previousNumFreq = numberFrequencies.get(num) || 0;
+        numberFrequencies.set(num, previousNumFreq + 1);
     }
 
-    return nums.sort((a, b) => {
-        // check for num freq and compare with prev num freq
-        const aFreq = numFreqs.get(a);
-        const bFreq = numFreqs.get(b);
-        const freqsDiff = aFreq - bFreq;
-        
-        // sort in incr freq
-        // only if freqs diff is not falsy
-        if (freqsDiff !== 0) return freqsDiff;
-        
-        // if mult vals have same freq sort in decr order
-        return b - a;
+    // sort func with comparison callback checking num freq from map,
+    return nums.sort(function sortByIncreasingFreq(numA, numB) {
+        const numAFreq = numberFrequencies.get(numA);
+        const numBFreq = numberFrequencies.get(numB);
+        const frequenciesDiff = numAFreq - numBFreq;
+
+        if (numAFreq === numBFreq) {
+            return numB - numA;
+        } else {
+            return frequenciesDiff;
+        }
     });
 }
 
-// console.log( frequencySort([1, 1, 2, 2, 2, 3]) ); // [3,1,1,2,2,2]
-console.log( frequencySort([2, 3, 1, 3, 2]) ); // [1,3,3,2,2]
+console.log(frequencySort([1,1,2,2,2,3])); // [3,1,1,2,2,2]
+console.log(frequencySort([2,3,1,3,2])); // [1,3,3,2,2]
