e972ea14b9f88890bf479618f1448ae8cdeedf11
Alex Curran <alexcurran.va@gmail.com>
2024-07-29 12:36:18 -0500

    Working solution for find-if-digit-game-can-be-won.js

---
+++ easy/find-if-digit-game-can-be-won.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+function canAliceWin(nums) {
+    // sum all single-digit nums
+    let sumSingleDigitNums = 0;
+    let sumDoubleDigitNums = 0;
+
+    for (let num of nums) {
+        if (num >= -9 && num <= 9) {
+            sumSingleDigitNums += num;
+        } else {
+            sumDoubleDigitNums += num;
+        }
+    }
+
+    // compare sums
+    if (sumDoubleDigitNums === sumSingleDigitNums) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+console.log(canAliceWin([1,2,3,4,10])); // false 10 sum === 10 sum
+console.log(canAliceWin([1,2,3,4,5,14])); // true
+console.log(canAliceWin([5,5,5,25])); // true
