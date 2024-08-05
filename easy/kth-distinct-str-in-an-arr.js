08878d3346982a9f300c0ad786dc516e8fd72e7b
Alex Curran <alexcurran.va@gmail.com>
2024-08-05 12:13:58 -0500

    Brute-force constant space solution for kth-distinct-str-in-an-arr.js

--- easy/kth-distinct-str-in-an-arr.js
+++ easy/kth-distinct-str-in-an-arr.js
@@ -1,34 +1,70 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {string[]} arr
+//  * @param {number} k
+//  * @return {string}
+//  */
+// function kthDistinct(arr, k) {
+//     // cache strs and counts in map
+//     let strsMap = new Map();
+
+//     for (let str of arr) {
+//         const prevCount = strsMap.get(str) || 0;
+//         strsMap.set(str, prevCount + 1);
+//     }
+
+//     let kthCount = 1;
+//     // iterate map looking for distinct (1 count) str that is the kth distinct str in arr
+//     for (let [str, count] of strsMap) {
+//         if (count !== 1) continue;
+
+//         if (kthCount === k) return str;
+
+//         kthCount++;
+//     }
+
+//     // no match present in arr
+//     return "";
+// }
+
 /**
- * solution 1
- * time: O(n)
- * space: O(n)
+ * solution 2 -- brute force
+ * time: O(n^2)
+ * space: O(1)
  * 
  * @param {string[]} arr
  * @param {number} k
  * @return {string}
  */
 function kthDistinct(arr, k) {
-    // cache strs and counts in map
-    let strsMap = new Map();
+    for (let i = 0; i < arr.length; i++) {
+        let distinctFlag = true;
+        
+        for (let j = 0; j < arr.length; j++) {
+            if (i === j) continue;
 
-    for (let str of arr) {
-        const prevCount = strsMap.get(str) || 0;
-        strsMap.set(str, prevCount + 1);
+            if (arr[i] === arr[j]) {
+                distinctFlag = false;
+
+                break;
+            }
+        }
+
+        if (distinctFlag) {
+            k--;
+
+            if (k === 0) {
+                return arr[i];
+            }
+        }
     }
 
-    let kthCount = 1;
-    // iterate map looking for distinct (1 count) str that is the kth distinct str in arr
-    for (let [str, count] of strsMap) {
-        if (count !== 1) continue;
-
-        if (kthCount === k) return str;
-
-        kthCount++;
-    }
-
-    // no match present in arr
+    // no match from array
     return "";
 }
 
