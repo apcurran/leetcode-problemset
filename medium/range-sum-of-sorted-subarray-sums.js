ba7566704f8f7aad2298e53ae64e125081b41cef
Alex Curran <alexcurran.va@gmail.com>
2024-08-05 14:58:54 -0500

    Brute force subarray sums with sort solution for range-sum-of-sorted-subarray-sums.js

---
+++ medium/range-sum-of-sorted-subarray-sums.js
@@ -0,0 +1,42 @@
+"use strict";
+
+/**
+ * solution 1 -- brute force
+ * time: O(n^2 * log n)
+ * space: O(n^2)
+ * 
+ * @param {number[]} nums
+ * @param {number} n
+ * @param {number} left
+ * @param {number} right
+ * @return {number}
+ */
+function rangeSum(nums, n, left, right) {
+    // build subarray sum list
+    const MOD_AMOUNT = (10 ** 9) + 7;
+    let subarraySums = [];
+
+    for (let i = 0; i < n; i++) {
+        let currentSubarraySum = 0;
+
+        for (let j = i; j < n; j++) {
+            const num = nums[j];
+            currentSubarraySum += num;
+            subarraySums.push(currentSubarraySum);
+        }
+    }
+
+    subarraySums.sort(function sortAsc(subarraySumA, subarraySumB) {
+        return subarraySumA - subarraySumB;
+    });
+
+    let sumFromLeftToRight = 0;
+
+    for (let i = left - 1; i <= right - 1; i++) {
+        sumFromLeftToRight += subarraySums[i];
+    }
+
+    return sumFromLeftToRight % MOD_AMOUNT;
+}
+
+console.log(rangeSum([1,2,3,4], 4, 1, 5)); // 13
