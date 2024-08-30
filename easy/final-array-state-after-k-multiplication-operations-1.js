4c579946dd303780ac423b524c8aaa467dc79e38
Alex Curran <alexcurran.va@gmail.com>
2024-08-30 11:09:55 -0500

    Iterative solution and update time/space complexities for solutions 1 & 2

--- easy/final-array-state-after-k-multiplication-operations-1.js
+++ easy/final-array-state-after-k-multiplication-operations-1.js
@@ -1,9 +1,30 @@
 "use strict";
 
+// /**
+//  * solution 1 -- recursion
+//  * time: O(n^2)
+//  * space: O(n)
+//  * 
+//  * @param {number[]} nums
+//  * @param {number} k
+//  * @param {number} multiplier
+//  * @return {number[]}
+//  */
+// function getFinalState(nums, k, multiplier) {
+//     if (k === 0) {
+//         return nums;
+//     }
+
+//     const minIndex = getMinIndex(nums);
+//     nums[minIndex] *= multiplier;
+
+//     return getFinalState(nums, k - 1, multiplier);
+// }
+
 /**
- * solution 1 -- recursion
+ * solution 2 -- iterative
  * time: O(n^2)
- * space: O(n)
+ * space: O(1)
  * 
  * @param {number[]} nums
  * @param {number} k
@@ -11,14 +32,12 @@
  * @return {number[]}
  */
 function getFinalState(nums, k, multiplier) {
-    if (k === 0) {
-        return nums;
+    for (let i = 0; i < k; i++) {
+        const minIndex = getMinIndex(nums);
+        nums[minIndex] *= multiplier;
     }
 
-    const minIndex = getMinIndex(nums);
-    nums[minIndex] *= multiplier;
-
-    return getFinalState(nums, k - 1, multiplier);
+    return nums;
 }
 
 console.log(getFinalState([2,1,3,5,6], 5, 2)); // [8,4,6,5,6]
