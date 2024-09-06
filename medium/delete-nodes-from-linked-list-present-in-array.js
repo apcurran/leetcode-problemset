258eaeed1ab76d3555fc06acb189d1fe2a679173
Alex Curran <alexcurran.va@gmail.com>
2024-09-06 14:25:45 -0500

    Hashset iterative solution for delete-nodes-from-linked-list-present-in-array.js

---
+++ medium/delete-nodes-from-linked-list-present-in-array.js
@@ -0,0 +1,36 @@
+"use strict";
+
+function ListNode(val, next) {
+    this.val = (val === undefined ? 0 : val);
+    this.next = (next === undefined ? null : next);
+}
+
+/**
+ * solution 1 -- hashset
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {number[]} nums
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+function modifiedList(nums, head) {
+    const numsSet = new Set(nums);
+    let dummyNode = new ListNode(0, head);
+
+    let previous = dummyNode;
+    let current = head;
+
+    while (current) {
+        if (numsSet.has(current.val)) {
+            // delete node
+            previous.next = current.next;
+        } else {
+            previous = previous.next;
+        }
+        // always travel forward by 1 node
+        current = current.next;
+    }
+
+    return dummyNode.next; // first node in revised list
+}
