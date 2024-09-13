e8545bcf051537eba45bb859cf437621afb2badd
Alex Curran <alexcurran.va@gmail.com>
2024-09-13 13:49:59 -0500

    Optimal prefix XOR solution

--- medium/xor-queries-of-a-subarray.js
+++ medium/xor-queries-of-a-subarray.js
@@ -1,8 +1,37 @@
 "use strict";
 
+// /**
+//  * solution 1 -- brute force
+//  * time: O(q * a)
+//  * space: O(a) -- including results array
+//  * 
+//  * @param {number[]} arr
+//  * @param {number[][]} queries
+//  * @return {number[]}
+//  */
+// function xorQueries(arr, queries) {
+//     let results = [];
+
+//     for (let [leftStart, rightEnd] of queries) {
+//         // for current query,
+//         // from left index to right index
+//         let xorValue = arr[leftStart];
+
+//         for (let i = leftStart + 1; i <= rightEnd; i++) {
+//             const currentValue = arr[i];
+//             // XOR ^ op on arr value
+//             xorValue ^= currentValue;
+//         }
+//         // add result to results array
+//         results.push(xorValue);
+//     }
+
+//     return results;
+// }
+
 /**
- * solution 1 -- brute force
- * time: O(q * a)
+ * solution 2 -- prefix XOR
+ * time: O(a + q)
  * space: O(a) -- including results array
  * 
  * @param {number[]} arr
@@ -10,19 +39,17 @@
  * @return {number[]}
  */
 function xorQueries(arr, queries) {
+    let prefix = [0];
+
+    for (let value of arr) {
+        const xorValue = prefix.at(-1) ^ value;
+        prefix.push(xorValue);
+    }
+
     let results = [];
 
     for (let [leftStart, rightEnd] of queries) {
-        // for current query,
-        // from left index to right index
-        let xorValue = arr[leftStart];
-
-        for (let i = leftStart + 1; i <= rightEnd; i++) {
-            const currentValue = arr[i];
-            // XOR ^ op on arr value
-            xorValue ^= currentValue;
-        }
-        // add result to results array
+        const xorValue = prefix[rightEnd + 1] ^ prefix[leftStart];
         results.push(xorValue);
     }
 
