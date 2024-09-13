47497e979972a7755e6bfc2a9c9c4a91d201fd95
Alex Curran <alexcurran.va@gmail.com>
2024-09-13 13:33:02 -0500

    Brute force iterative solution for xor-queries-of-a-subarray.js

---
+++ medium/xor-queries-of-a-subarray.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/**
+ * solution 1 -- brute force
+ * time: O(q * a)
+ * space: O(a) -- including results array
+ * 
+ * @param {number[]} arr
+ * @param {number[][]} queries
+ * @return {number[]}
+ */
+function xorQueries(arr, queries) {
+    let results = [];
+
+    for (let [leftStart, rightEnd] of queries) {
+        // for current query,
+        // from left index to right index
+        let xorValue = arr[leftStart];
+
+        for (let i = leftStart + 1; i <= rightEnd; i++) {
+            const currentValue = arr[i];
+            // XOR ^ op on arr value
+            xorValue ^= currentValue;
+        }
+        // add result to results array
+        results.push(xorValue);
+    }
+
+    return results;
+}
+
+console.log(xorQueries([1,3,4,8], [[0,1],[1,2],[0,3],[3,3]]));
+// [2,7,14,8]
+console.log(xorQueries([4,8,2,10], [[2,3],[1,3],[0,0],[0,3]]));
+// [8,0,4,4]
