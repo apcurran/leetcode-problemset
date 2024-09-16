d7a2fe93dcbe17ad94257be1d1f4181df8d2e23f
Alex Curran <alexcurran.va@gmail.com>
2024-09-16 14:13:42 -0500

    Hashset solution, improvement for time complexity -- tradeoff space

--- easy/count-the-num-of-consistent-strs.js
+++ easy/count-the-num-of-consistent-strs.js
@@ -1,12 +1,40 @@
 "use strict";
 
+// /**
+//  * solution 1
+//  * n = len of words arr
+//  * m = len of str in words arr
+//  * k = len of allowed str
+//  * time: O(n * m * k)
+//  * space: O(1)
+//  * 
+//  * @param {string} allowed
+//  * @param {string[]} words
+//  * @return {number}
+//  */
+
+// function countConsistentStrings(allowed, words) {
+//     let consistentStrsCount = 0;
+
+//     outerLoop:
+//     for (let str of words) {
+//         for (let char of str) {
+//             if (!allowed.includes(char)) continue outerLoop;
+//         }
+
+//         consistentStrsCount++;
+//     }
+
+//     return consistentStrsCount;
+// }
+
 /**
- * solution 1
+ * solution 2 -- hashset
  * n = len of words arr
  * m = len of str in words arr
  * k = len of allowed str
- * time: O(n * m * k)
- * space: O(1)
+ * time: O(n * m + k)
+ * space: O(k)
  * 
  * @param {string} allowed
  * @param {string[]} words
@@ -14,18 +42,21 @@
  */
 
 function countConsistentStrings(allowed, words) {
-    let consistentStrsCount = 0;
+    const allowableChars = new Set(allowed);
+    let validStringsCount = words.length; // auto-init to all words being valid
 
-    outerLoop:
-    for (let str of words) {
-        for (let char of str) {
-            if (!allowed.includes(char)) continue outerLoop;
+    for (let word of words) {
+        for (let char of word) {
+            if (!allowableChars.has(char)) {
+                // decrement from valid words count
+                validStringsCount--;
+                // leave loop to prevent decrementing other words
+                break;
+            }
         }
-
-        consistentStrsCount++;
     }
 
-    return consistentStrsCount;
+    return validStringsCount;
 }
 
 console.log( countConsistentStrings("ab", ["ad", "bd", "aaab", "baa", "badab"]) ); // 2
