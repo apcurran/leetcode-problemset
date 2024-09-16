736acd122614c63a7adf2ab0e007c8d14c2e17f3
Alex Curran <alexcurran.va@gmail.com>
2024-09-16 11:56:02 -0500

    One-pass solution

--- medium/longest-subarray-with-max-bitwise-and.js
+++ medium/longest-subarray-with-max-bitwise-and.js
@@ -1,7 +1,33 @@
 "use strict";
 
+// /**
+//  * solution 1 -- two pass
+//  * time: O(n)
+//  * space: O(1)
+//  * 
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// function longestSubarray(nums) {
+//     let maxNum = Math.max.apply(null, nums);
+//     let maxLengthK = 0;
+//     let result = 0;
+
+//     for (let num of nums) {
+//         if (num === maxNum) {
+//             maxLengthK++;
+//         } else {
+//             maxLengthK = 0;
+//         }
+
+//         result = Math.max(result, maxLengthK);
+//     }
+
+//     return result;
+// }
+
 /**
- * solution 1 -- two pass
+ * solution 2 -- one pass
  * time: O(n)
  * space: O(1)
  * 
@@ -9,12 +35,17 @@
  * @return {number}
  */
 function longestSubarray(nums) {
-    let maxNum = Math.max.apply(null, nums);
+    let maxNum = 0;
     let maxLengthK = 0;
     let result = 0;
 
     for (let num of nums) {
-        if (num === maxNum) {
+        if (num > maxNum) {
+            // reset max value and length
+            maxNum = num;
+            maxLengthK = 1;
+            result = 0;
+        } else if (num === maxNum) {
             maxLengthK++;
         } else {
             maxLengthK = 0;
