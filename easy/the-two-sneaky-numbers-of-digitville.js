b66c1912b17ed0887c648ea6d50a3510657b356c
Alex Curran <alexcurran.va@gmail.com>
2024-09-17 11:57:23 -0500

    Hashset solution for the-two-sneaky-numbers-of-digitville.js

---
+++ easy/the-two-sneaky-numbers-of-digitville.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ * solution 1 -- hashset
+ * time: O(n)
+ * space: O(n) -- set
+ * 
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+function getSneakyNumbers(nums) {
+    let seenNums = new Set();
+    let sneakyNums = [];
+
+    for (let num of nums) {
+        if (seenNums.has(num)) {
+            sneakyNums.push(num);
+        }
+
+        seenNums.add(num);
+    }
+
+    return sneakyNums;
+}
+
+console.log(getSneakyNumbers([0,3,2,1,3,2])); // [2, 3]
