a7598e6744c3fbfa6f60dd5b145b51fbbe1afc03
Alex Curran <alexcurran.va@gmail.com>
2024-09-18 16:52:09 -0500

    Working iterative solution for find-indices-of-stable-mountains.js

---
+++ easy/find-indices-of-stable-mountains.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(n) -- if results are included
+ * 
+ * @param {number[]} mountainHeights
+ * @param {number} threshold
+ * @return {number[]}
+ */
+function stableMountains(mountainHeights, threshold) {
+    let stableMountainIndices = [];
+
+    for (let i = 1; i < mountainHeights.length; i++) {
+        const previousMountainHeight = mountainHeights[i - 1];
+
+        if (previousMountainHeight > threshold) {
+            stableMountainIndices.push(i);
+        }
+    }
+
+    return stableMountainIndices;
+}
+
+console.log(stableMountains([1, 2, 3, 4, 5], 2)); // [3, 4]
+console.log(stableMountains([10, 1, 10, 1, 10], 3)); // [1, 3]
