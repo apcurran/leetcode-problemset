316fcdf26f34476c482a9b7a6a5d50d2e0474673
Alex Curran <alexcurran.va@gmail.com>
2024-12-04 14:57:50 -0600

    Two pointer solution for adding-spaces-to-a-string.js

---
+++ medium/adding-spaces-to-a-string.js
@@ -0,0 +1,37 @@
+"use strict";
+
+/**
+ * solution 1 -- two pointers
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} str
+ * @param {number[]} spaces
+ * @return {string}
+ */
+function addSpaces(str, spaces) {
+    let strPointer = 0;
+    let spacesPointer = 0;
+    let results = [];
+
+    while (strPointer < str.length && spacesPointer < spaces.length) {
+        if (strPointer < spaces[spacesPointer]) {
+            // current char
+            results.push(str[strPointer]);
+            strPointer++;
+        } else {
+            // we reached a space addition
+            results.push(" ");
+            spacesPointer++;
+        }
+    }
+
+    if (strPointer < str.length) {
+        // add the rest of the word
+        results.push(str.slice(strPointer));
+    }
+
+    return results.join("");
+}
+
+console.log(addSpaces("LeetcodeHelpsMeLearn", [8,13,15])); // "Leetcode Helps Me Learn"
