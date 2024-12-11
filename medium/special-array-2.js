3ef862501a26f618b98baab88e165a453c20545d
Alex Curran <alexcurran.va@gmail.com>
2024-12-11 14:10:14 -0600

    Shorten comment

--- medium/special-array-2.js
+++ medium/special-array-2.js
@@ -17,7 +17,7 @@ function isArraySpecial(nums, queries) {
 
     for (let i = 0; i < nums.length - 1; i++) {
         // if both nums are even OR
-        // if both nums are odd (no differing parity)
+        // both nums are odd (no differing parity)
         if ((nums[i] % 2 === 0 && nums[i + 1] % 2 === 0) || (nums[i] % 2 !== 0 && nums[i + 1] % 2 !== 0)) {
             badIndices.push(i);
         }
