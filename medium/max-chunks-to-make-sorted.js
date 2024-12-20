3af81eabc8aa5905b43679dcf9c40d135da2e92a
Alex Curran <alexcurran.va@gmail.com>
2024-12-20 13:49:56 -0600

    Add solution type and time/space complexities

---
+++ medium/max-chunks-to-make-sorted.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ * solution 1 -- prefix sum
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number[]} arr
+ * @return {number}
+ */
+function maxChunksToSorted(arr) {
+    let currentMax = -1;
+    let result = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        const num = arr[i];
+        currentMax = Math.max(currentMax, num);
+
+        if (currentMax === i) result++; 
+    }
+
+    return result;
+}
+
+console.log(maxChunksToSorted([4,3,2,1,0])); // 1
+console.log(maxChunksToSorted([1,0,2,3,4])); // 4
