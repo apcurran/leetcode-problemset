0b2f3b095a3cd278eb715c944ec5eb9bb1637f7e
Alex Curran <alexcurran.va@gmail.com>
2024-12-26 13:39:42 -0600

    Brute force recursion solution for target-sum.js

---
+++ medium/target-sum.js
@@ -0,0 +1,38 @@
+"use strict";
+
+/**
+ * solution 1 -- recursion (brute force)
+ * time: O(2^n)
+ * space: O(n)
+ * 
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+function findTargetSumWays(nums, target) {
+    /**
+     * @param {number} i 
+     * @param {number} currSum 
+     * @returns {number}
+     */
+    function waysToTarget(i, currSum) {
+        // reached the end
+        if (i === nums.length) {
+            if (currSum === target) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+
+        // otherwise recursive case
+        return (
+            waysToTarget(i + 1, currSum + nums[i]) +
+            waysToTarget(i + 1, currSum - nums[i])
+        );
+    }
+
+    return waysToTarget(0, 0);
+}
+
+console.log(findTargetSumWays([1,1,1,1,1], 3)); // 5
