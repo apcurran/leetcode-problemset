a4d6bda538d8c2bd4036348c8693baa2e8e835da
Alex Curran <alexcurran.va@gmail.com>
2025-01-02 15:01:39 -0600

    Solution 2 setup

--- medium/count-vowel-strings-in-ranges.js
+++ medium/count-vowel-strings-in-ranges.js
@@ -1,9 +1,44 @@
 "use strict";
 
+// /**
+//  * solution 1 -- brute force (TLE)
+//  * time: O(q * w)
+//  * space: O(1) -- not including results
+//  * 
+//  * @param {string[]} words
+//  * @param {number[][]} queries
+//  * @return {number[]}
+//  */
+// function vowelStrings(words, queries) {
+//     const VOWELS = "aeiou";
+//     let results = [];
+
+//     for (let [startRange, endRange] of queries) {
+//         let counter = 0;
+
+//         for (let i = startRange; i <= endRange; i++) {
+//             const currentWord = words[i];
+//             const wordFirstChar = currentWord[0];
+//             const wordLastChar = currentWord[currentWord.length - 1];
+            
+//             // check that start and end of word is a vowel (aeiou)
+//             if (!VOWELS.includes(wordFirstChar) || !VOWELS.includes(wordLastChar)) {
+//                 continue;
+//             }
+//             // if word is in range and follows start/end with vowel, increment a counter
+//             counter++;
+//         }
+//         // add counter total to results array
+//         results.push(counter);
+//     }
+
+//     return results;
+// }
+
 /**
- * solution 1 -- brute force (TLE)
- * time: O(q * w)
- * space: O(q)
+ * solution 2 -- prefix/suffix
+ * time: O(q + w)
+ * space: O(w)
  * 
  * @param {string[]} words
  * @param {number[][]} queries
@@ -13,25 +48,8 @@ function vowelStrings(words, queries) {
     const VOWELS = "aeiou";
     let results = [];
 
-    for (let [startRange, endRange] of queries) {
-        let counter = 0;
-
-        for (let i = startRange; i <= endRange; i++) {
-            const currentWord = words[i];
-            const wordFirstChar = currentWord[0];
-            const wordLastChar = currentWord[currentWord.length - 1];
-            
-            // check that start and end of word is a vowel (aeiou)
-            if (!VOWELS.includes(wordFirstChar) || !VOWELS.includes(wordLastChar)) {
-                continue;
-            }
-            // if word is in range and follows start/end with vowel, increment a counter
-            counter++;
-        }
-        // add counter total to results array
-        results.push(counter);
-    }
-
+    
+    
     return results;
 }
 
