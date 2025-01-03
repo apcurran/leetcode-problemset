3bf5568ca2a8bb69a44c3a1391f67b05f5e4b9f2
Alex Curran <alexcurran.va@gmail.com>
2025-01-03 16:39:56 -0600

    Slightly optimize solution 1, still TLE

--- medium/num-of-ways-to-split-array.js
+++ medium/num-of-ways-to-split-array.js
@@ -1,9 +1,30 @@
 "use strict";
 
+// /**
+//  * solution 1 -- brute force (TLE)
+//  * time: O(n^2)
+//  * space: O(n)
+//  * 
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// function waysToSplitArray(nums) {
+//     let counter = 0;
+
+//     for (let i = 0; i < nums.length - 1; i++) {
+//         const prefixSum = nums.slice(0, i + 1).reduce((sum, curr) => sum + curr, 0);
+//         const suffixSum = nums.slice(i + 1).reduce((sum, curr) => sum + curr, 0);
+
+//         if (prefixSum >= suffixSum) counter++;
+//     }
+
+//     return counter;
+// }
+
 /**
- * solution 1 -- brute force (TLE)
+ * solution 2 -- brute force (slightly optimized)
  * time: O(n^2)
- * space: O(n)
+ * space: O(1)
  * 
  * @param {number[]} nums
  * @return {number}
@@ -12,8 +33,17 @@ function waysToSplitArray(nums) {
     let counter = 0;
 
     for (let i = 0; i < nums.length - 1; i++) {
-        const prefixSum = nums.slice(0, i + 1).reduce((sum, curr) => sum + curr, 0);
-        const suffixSum = nums.slice(i + 1).reduce((sum, curr) => sum + curr, 0);
+        let prefixSum = 0;
+
+        for (let j = 0; j <= i; j++) {
+            prefixSum += nums[j];
+        }
+
+        let suffixSum = 0;
+
+        for (let j = i + 1; j < nums.length; j++) {
+            suffixSum += nums[j];
+        }
 
         if (prefixSum >= suffixSum) counter++;
     }
