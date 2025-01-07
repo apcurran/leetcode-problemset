b174ff8e4da3a1addf1d2dd8e59a8a5f67941a59
Alex Curran <alexcurran.va@gmail.com>
2025-01-07 14:14:14 -0600

    Working solution for string-matching-in-an-array.js

---
+++ easy/string-matching-in-an-array.js
@@ -0,0 +1,29 @@
+"use strict";
+
+/**
+ * solution 1
+ * n = words length
+ * m = otherWord length
+ * time: O(n^2 * m)
+ * space: O(n)
+ * 
+ * @param {string[]} words
+ * @return {string[]}
+ */
+function stringMatching(words) {
+    return words.filter(function keepSubstrWords(word, i) {
+        for (let j = 0; j < words.length; j++) {
+            if (j === i) continue;
+
+            const otherWord = words[j];
+
+            if (otherWord.includes(word)) {
+                return true;
+            }
+        }
+
+        return false;
+    });
+}
+
+console.log(stringMatching(["mass", "as", "hero", "superhero"])); // ["as", "hero"]
