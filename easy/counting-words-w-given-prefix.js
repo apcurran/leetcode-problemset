94fe2d94cebe5eec85b2521c3015502d9676d000
Alex Curran <alexcurran.va@gmail.com>
2025-01-09 10:04:54 -0600

    String startsWith method on prototype solution

--- easy/counting-words-w-given-prefix.js
+++ easy/counting-words-w-given-prefix.js
@@ -1,7 +1,30 @@
 "use strict";
 
+// /**
+//  * solution 1 -- regex
+//  * time: O(n * m)
+//  * space: O(1)
+//  * 
+//  * @param {string[]} words
+//  * @param {string} pref
+//  * @return {number}
+//  */
+// function prefixCount(words, pref) {
+//     // use ^WORD regex to start searching from beginning of each word
+//     const prefixRegex = new RegExp(`^${pref}`);
+//     let prefixCount = 0;
+
+//     for (let word of words) {
+//         if (prefixRegex.test(word)) prefixCount++;
+//     }
+
+//     return prefixCount;
+// }
+
 /**
- * solution 1 -- regex
+ * solution 2 -- string method
+ * n = words length
+ * m = word length
  * time: O(n * m)
  * space: O(1)
  * 
@@ -10,12 +33,10 @@
  * @return {number}
  */
 function prefixCount(words, pref) {
-    // use ^WORD regex to start searching from beginning of each word
-    const prefixRegex = new RegExp(`^${pref}`);
     let prefixCount = 0;
 
     for (let word of words) {
-        if (prefixRegex.test(word)) prefixCount++;
+        if (word.startsWith(pref)) prefixCount++;
     }
 
     return prefixCount;
