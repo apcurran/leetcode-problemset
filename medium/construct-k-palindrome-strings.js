97482c5e1599c6d7190a79881d6c43b42b79cd4a
Alex Curran <alexcurran.va@gmail.com>
2025-01-13 17:00:17 -0600

    Hashmap linear time/space solution for construct-k-palindrome-strings.js

---
+++ medium/construct-k-palindrome-strings.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/**
+ * solution 1 -- hashmap
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} s
+ * @param {number} k
+ * @return {boolean}
+ */
+function canConstruct(s, k) {
+    if (k > s.length) return false;
+
+    // count odd char counts
+    // make sure odd counts are <= k
+    let charCounts = new Map();
+
+    for (let char of s) {
+        const previousCharCount = charCounts.get(char) || 0;
+        charCounts.set(char, previousCharCount + 1);
+    }
+
+    let oddCharCounts = 0;
+
+    for (let [, currentCharCount] of charCounts) {
+        oddCharCounts += currentCharCount % 2;
+    }
+
+    return oddCharCounts <= k;
+}
+
+console.log(canConstruct("annabelle", 2)); // true
+console.log(canConstruct("leetcode", 3)); // false
