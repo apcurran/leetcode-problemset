1fe5aaf5e044f6003a2ffe1889429e62e0e9c5f6
Alex Curran <alexcurran.va@gmail.com>
2025-01-14 16:35:45 -0600

    Hashmap solution for min-length-of-string-after-operations.js

---
+++ medium/min-length-of-string-after-operations.js
@@ -0,0 +1,38 @@
+"use strict";
+
+/**
+ * solution 1 -- hashmap
+ * n = s length
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} s
+ * @return {number}
+ */
+function minimumLength(s) {
+    // count all char frequencies
+    let charCounts = new Map();
+
+    for (let char of s) {
+        const previousCharCount = charCounts.get(char) || 0;
+        charCounts.set(char, previousCharCount + 1);
+    }
+
+    let strLength = 0;
+
+    for (let charTuple of charCounts) {
+        const charCount = charTuple[1];
+
+        if (charCount % 2 === 0) {
+            // if char freq is even -> add 2
+            strLength += 2;
+        } else {
+            // if char freq is odd -> add 1
+            strLength += 1;
+        }
+    }
+
+    return strLength;
+}
+
+console.log(minimumLength("abaacbcbb")); // 5
