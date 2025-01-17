973988b606181c331bcb5b585f8d06552b55dc2d
Alex Curran <alexcurran.va@gmail.com>
2025-01-17 14:55:11 -0600

    Direct simulation solution for neighboring-bitwise-xor.js

---
+++ medium/neighboring-bitwise-xor.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ * solution 1 (simulation)
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number[]} derived
+ * @return {boolean}
+ */
+function doesValidArrayExist(derived) {
+    let first = 0;
+    let last = 0;
+
+    for (let num of derived) {
+        if (num === 1) {
+            last = ~last;
+        }
+    }
+
+    return first === last;
+}
+
+console.log(doesValidArrayExist([1,1,0])); // true
+console.log(doesValidArrayExist([1,1])); // true
+console.log(doesValidArrayExist([1,0])); // false
