2ca864199982cfb842bc85b35084415671b91914
Alex Curran <alexcurran.va@gmail.com>
2025-01-29 15:14:30 -0600

    Accepted brute force solution with linear time/constant space for search-in-rotated-sorted-array-2.js

---
+++ medium/search-in-rotated-sorted-array-2.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/**
+ * solution 1 -- brute force
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {boolean}
+ */
+function search(nums, target) {
+    return nums.includes(target);
+}
+
+console.log(search([2,5,6,0,0,1,2], 0)); // true
+console.log(search([2,5,6,0,0,1,2], 3)); // false
