9cc3b396442161c4e05849b4f1c7129f777b43de
Alex Curran <alexcurran.va@gmail.com>
2025-02-05 13:35:21 -0600

    O(n^2) time and constant space solution for check-if-array-is-sorted-and-rotated.js

---
+++ easy/check-if-array-is-sorted-and-rotated.js
@@ -0,0 +1,42 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n^2) -- array.shift() and isSortedAsc func called during loop
+ * space: O(1)
+ * 
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+function check(nums) {
+    for (let i = 0; i < nums.length; i++) {
+        // shift off front and add to back of arr
+        const top = nums.shift();
+        nums.push(top);
+        
+        if (isSortedAsc(nums)) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+console.log(check([3,4,5,1,2])); // true
+console.log(check([2,1,3,4])); // false
+console.log(check([1,2,3])); // true
+
+/**
+ * @param {number[]} nums 
+ * @returns {boolean}
+ */
+function isSortedAsc(nums) {
+    for (let i = 0; i < nums.length - 1; i++) {
+        const currentNum = nums[i];
+        const nextNum = nums[i + 1];
+
+        if (currentNum > nextNum) return false;
+    }
+
+    return true;
+}
