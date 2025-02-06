a84b44a0768882d2267293b36c276d2e952d333f
Alex Curran <alexcurran.va@gmail.com>
2025-02-06 16:33:21 -0600

    Prefix and suffix sums solution 2

--- easy/count-partitions-with-even-sum-difference.js
+++ easy/count-partitions-with-even-sum-difference.js
@@ -1,22 +1,52 @@
 "use strict";
 
+// /**
+//  * solution 1 -- brute force
+//  * time: O(n^2)
+//  * space: O(n)
+//  * 
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// function countPartitions(nums) {
+//     let count = 0;
+
+//     for (let i = 0; i < nums.length - 1; i++) {
+//         // get subarray sums
+//         const leftSubarraySum = nums.slice(0, i + 1).reduce((sum, curr) => sum + curr, 0);
+//         const rightSubarraySum = nums.slice(i + 1).reduce((sum, curr) => sum + curr, 0);
+//         // get diff between sums
+//         const subarraysDifference = leftSubarraySum - rightSubarraySum;
+
+//         // check if diff is even
+//         if (subarraysDifference % 2 === 0) {
+//             count++;
+//         }
+//     }
+
+//     return count;
+// }
+
 /**
- * solution 1 -- brute force
- * time: O(n^2)
- * space: O(n)
+ * solution 2 -- prefix and suffix sums
+ * time: O(n)
+ * space: O(1)
  * 
  * @param {number[]} nums
  * @return {number}
  */
 function countPartitions(nums) {
+    let leftSum = 0;
+    let rightSum = nums.reduce((sum, current) => sum + current, 0);
     let count = 0;
 
     for (let i = 0; i < nums.length - 1; i++) {
         // get subarray sums
-        const leftSubarraySum = nums.slice(0, i + 1).reduce((sum, curr) => sum + curr, 0);
-        const rightSubarraySum = nums.slice(i + 1).reduce((sum, curr) => sum + curr, 0);
+        const currentLeftValue = nums[i];
+        leftSum += currentLeftValue;
+        rightSum -= currentLeftValue;
         // get diff between sums
-        const subarraysDifference = leftSubarraySum - rightSubarraySum;
+        const subarraysDifference = leftSum - rightSum;
 
         // check if diff is even
         if (subarraysDifference % 2 === 0) {
