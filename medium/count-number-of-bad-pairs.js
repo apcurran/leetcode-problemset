9f6e3ab0a42e2cc7970abb7b446198eb8a8d5224
Alex Curran <alexcurran.va@gmail.com>
2025-02-10 16:46:44 -0600

    Optimal sequences approach for count-number-of-bad-pairs.js using hashmap

--- medium/count-number-of-bad-pairs.js
+++ medium/count-number-of-bad-pairs.js
@@ -1,28 +1,53 @@
 "use strict";
 
+// /**
+//  * solution 1 -- brute force (TLE)
+//  * time: O(n^2)
+//  * space: O(1)
+//  * 
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// function countBadPairs(nums) {
+//     let badPairsCounter = 0;
+
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             // i < j satisfied
+//             // j - i !== nums[j] - nums[i]
+
+//             if (j - i !== nums[j] - nums[i]) {
+//                 badPairsCounter++;
+//             }
+//         }
+//     }
+
+//     return badPairsCounter;
+// }
+
 /**
- * solution 1 -- brute force (TLE)
- * time: O(n^2)
- * space: O(1)
+ * solution 2 -- sequences
+ * time: O(n)
+ * space: O(n)
  * 
  * @param {number[]} nums
  * @return {number}
  */
 function countBadPairs(nums) {
-    let badPairsCounter = 0;
+    let totalPairs = 0;
+    let goodPairs = 0;
+    let countMap = new Map();
 
     for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            // i < j satisfied
-            // j - i !== nums[j] - nums[i]
-
-            if (j - i !== nums[j] - nums[i]) {
-                badPairsCounter++;
-            }
-        }
+        // update good pairs
+        totalPairs += i;
+        const currentCount = countMap.get(nums[i] - i) || 0;
+        goodPairs += currentCount;
+        // update countMap
+        countMap.set(nums[i] - i, currentCount + 1);
     }
-
-    return badPairsCounter;
+    // bad pairs count
+    return totalPairs - goodPairs;
 }
 
 console.log(countBadPairs([4,1,3,3])); // 5
