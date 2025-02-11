52f6ce0fffffe45416f18986aba3566e5c465623
Alex Curran <alexcurran.va@gmail.com>
2025-02-11 11:51:12 -0600

    Stack-based solution with linear time/space complexity

--- easy/clear-digits.js
+++ easy/clear-digits.js
@@ -1,23 +1,49 @@
 "use strict";
 
+// /**
+//  * solution 1 -- regex
+//  * time: O(n^2)
+//  * space: O(n)
+//  * 
+//  * @param {string} s
+//  * @return {string}
+//  */
+// function clearDigits(s) {
+//     let result = s;
+
+//     // as long as there are still digits
+//     while (/\d/g.test(result)) {
+//         // remove the first digit encountered and the char to the left
+//         result = result.replace(/.\d/, "");
+//     }
+
+//     return result;
+// }
+
 /**
- * solution 1 -- regex
- * time: O(n^2)
+ * solution 2 -- stack
+ * time: O(n)
  * space: O(n)
  * 
- * @param {string} s
+ * @param {string} str
  * @return {string}
  */
-function clearDigits(s) {
-    let result = s;
+function clearDigits(str) {
+    const digits = "0123456789";
+    let stack = [];
 
-    // as long as there are still digits
-    while (/\d/g.test(result)) {
-        // remove the first digit encountered and the char to the left
-        result = result.replace(/.\d/, "");
+    for (let char of str) {
+        if (digits.includes(char)) {
+            // digit found
+                // delete previous stack top item
+                // skip adding this current char
+            stack.pop();
+        } else {
+            stack.push(char);
+        }
     }
 
-    return result;
+    return stack.join("");
 }
 
 console.log(clearDigits("abc")); // "abc"
