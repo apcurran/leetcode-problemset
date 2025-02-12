54090355dc018eba74a513715b5e87a78aa65919
Alex Curran <alexcurran.va@gmail.com>
2025-02-12 11:57:30 -0600

    Minor optimization leads to constant space improvement but still TLE

--- medium/max-sum-of-a-pair-with-equal-sum-of-digits.js
+++ medium/max-sum-of-a-pair-with-equal-sum-of-digits.js
@@ -5,7 +5,7 @@
  * x = nums[i]
  * y = nums[y]
  * time: O(n^2 * max(x, y))
- * space: O(max(x, y)) -- split creates a new array
+ * space: O(1) -- split creates a new array
  * 
  * @param {number[]} nums
  * @return {number}
@@ -15,12 +15,9 @@ function maximumSum(nums) {
 
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
-            const iDigitsSum = String(nums[i])
-                                .split("")
-                                .reduce((sum, current) => sum + Number(current), 0);
-            const jDigitsSum = String(nums[j])
-                                .split("")
-                                .reduce((sum, current) => sum + Number(current), 0);
+            const iDigitsSum = sumNumDigits(nums[i]);
+            const jDigitsSum = sumNumDigits(nums[j]);
+
             if (iDigitsSum === jDigitsSum) {
                 const numsSum = nums[i] + nums[j];
                 max = Math.max(max, numsSum);
@@ -29,6 +26,21 @@ function maximumSum(nums) {
     }
 
     return max;
+}
+
+/**
+ * @param {number} num 
+ * @returns {number}
+ */
+function sumNumDigits(num) {
+    let sum = 0;
+
+    while (num > 0) {
+        sum += num % 10; // get last digit and add to sum
+        num = Math.floor(num / 10); // remove last digit, before going to next iteration
+    }
+
+    return sum;
 }
 
 console.log(maximumSum([18,43,36,13,7])); // 54
