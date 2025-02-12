3e6b646dc4a48b00ad933a8206d458400d618975
Alex Curran <alexcurran.va@gmail.com>
2025-02-12 13:28:36 -0600

    Sort, then evaluate maxSum solution 2

--- medium/max-sum-of-a-pair-with-equal-sum-of-digits.js
+++ medium/max-sum-of-a-pair-with-equal-sum-of-digits.js
@@ -1,31 +1,86 @@
 "use strict";
 
+// /**
+//  * solution 1 -- brute force (TLE)
+//  * x = nums[i] value
+//  * y = nums[y] value
+//  * time: O(n^2 * max(log x, log y))
+//  * space: O(1)
+//  * 
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// function maximumSum(nums) {
+//     let max = -1;
+
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             const iDigitsSum = sumNumDigits(nums[i]);
+//             const jDigitsSum = sumNumDigits(nums[j]);
+
+//             if (iDigitsSum === jDigitsSum) {
+//                 const numsSum = nums[i] + nums[j];
+//                 max = Math.max(max, numsSum);
+//             }
+//         }
+//     }
+
+//     return max;
+// }
+
+// /**
+//  * @param {number} num 
+//  * @returns {number}
+//  */
+// function sumNumDigits(num) {
+//     let sum = 0;
+
+//     while (num > 0) {
+//         sum += num % 10; // get last digit and add to sum
+//         num = Math.floor(num / 10); // remove last digit, before going to next iteration
+//     }
+
+//     return sum;
+// }
+
 /**
- * solution 1 -- brute force (TLE)
+ * solution 2 -- sort
  * x = nums[i] value
  * y = nums[y] value
- * time: O(n^2 * max(log x, log y))
- * space: O(1)
+ * time: O(n * log n) -- sorting
+ * space: O(n)
  * 
  * @param {number[]} nums
  * @return {number}
  */
 function maximumSum(nums) {
-    let max = -1;
+    let digitSumPairs = nums.map((num) => {
+        const digitSum = sumNumDigits(num);
 
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            const iDigitsSum = sumNumDigits(nums[i]);
-            const jDigitsSum = sumNumDigits(nums[j]);
+        return [digitSum, num];
+    }).sort(function sortByDigitSumThenNumValue([aDigitSum, aNum], [bDigitSum, bNum]) {
+        if (aDigitSum === bDigitSum) {
+            return aNum - bNum;
+        } else {
+            return aDigitSum - bDigitSum;
+        }
+    });    
 
-            if (iDigitsSum === jDigitsSum) {
-                const numsSum = nums[i] + nums[j];
-                max = Math.max(max, numsSum);
-            }
+    let maxPairSum = -1;
+
+    for (let i = 0; i < digitSumPairs.length - 1; i++) {
+        const currentDigitSum = digitSumPairs[i][0];
+        const nextDigitSum = digitSumPairs[i + 1][0];
+
+        if (currentDigitSum === nextDigitSum) {
+            const currentNumValue = digitSumPairs[i][1];
+            const nextNumValue = digitSumPairs[i + 1][1];
+            const pairSum = currentNumValue + nextNumValue;
+            maxPairSum = Math.max(maxPairSum, pairSum);
         }
     }
 
-    return max;
+    return maxPairSum;
 }
 
 /**
