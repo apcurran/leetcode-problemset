e0cdd7a59398640011b27c4d88b0ec32ff4d3097
Alex Curran <alexcurran.va@gmail.com>
2025-02-19 11:30:32 -0600

    Stack-based approach to construct-smallest-number-from-DI-string.js

---
+++ medium/construct-smallest-number-from-DI-string.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ * solution 1 -- stack
+ * time: O(n)
+ * space: O(n)
+ * 
+ * @param {string} pattern
+ * @return {string}
+ */
+function smallestNumber(pattern) {
+    let result = [];
+    let stack = [];
+
+    for (let i = 0; i <= pattern.length; i++) {
+        stack.push(i + 1);
+
+        while (stack.length > 0 && (i === pattern.length || pattern[i] === "I")) {
+            result.push(String(stack.pop()));
+        }
+    }
+
+    return result.join("");
+}
+
+console.log(smallestNumber("IIIDIDDD")); // "123549876"
