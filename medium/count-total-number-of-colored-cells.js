1ff6abea8a65e8b130ae2cf147b61d2493b438df
Alex Curran <alexcurran.va@gmail.com>
2025-03-05 15:49:08 -0600

    Constant time/space math formula inspired by Gauss

--- medium/count-total-number-of-colored-cells.js
+++ medium/count-total-number-of-colored-cells.js
@@ -1,21 +1,33 @@
 "use strict";
 
+// /**
+//  * solution 1 -- iterative
+//  * time: O(n)
+//  * space: O(1)
+//  * 
+//  * @param {number} n
+//  * @return {number}
+//  */
+// function coloredCells(n) {
+//     let result = 1;
+
+//     for (let i = 0; i < n; i++) {
+//         result += 4 * i;
+//     }
+
+//     return result;
+// }
+
 /**
- * solution 1 -- iterative
- * time: O(n)
+ * solution 2 -- math formula (gauss-inspired)
+ * time: O(1)
  * space: O(1)
  * 
  * @param {number} n
  * @return {number}
  */
 function coloredCells(n) {
-    let result = 1;
-
-    for (let i = 0; i < n; i++) {
-        result += 4 * i;
-    }
-
-    return result;
+    return 1 + ((4 * (n - 1)) / 2) * n;
 }
 
 console.log(coloredCells(1)); // 1
