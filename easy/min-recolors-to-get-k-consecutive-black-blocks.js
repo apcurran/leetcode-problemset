57e917b698b69bd01d6e3534d98928d3b13ac2ef
Alex Curran <alexcurran.va@gmail.com>
2025-03-12 14:32:28 -0500

    Second 'brute force' solution for min-recolors-to-get-k-consecutive-black-blocks.js

--- easy/min-recolors-to-get-k-consecutive-black-blocks.js
+++ easy/min-recolors-to-get-k-consecutive-black-blocks.js
@@ -1,8 +1,45 @@
 "use strict";
 
+// /**
+//  * solution 1 -- sliding window
+//  * time: O(n)
+//  * space: O(1)
+//  * 
+//  * @param {string} blocks
+//  * @param {number} k
+//  * @return {number}
+//  */
+// function minimumRecolors(blocks, k) {
+//     let currentWindowRecolorsCount = 0;
+//     let minRecolorsCountResult = k;
+//     let left = 0;
+
+//     for (let right = 0; right < blocks.length; right++) {
+//         // White blocks need to be flipped to get k black blocks in a row
+//         if (blocks[right] === "W") {
+//             currentWindowRecolorsCount++;
+//         }
+
+//         // window size is equal to k
+//         if (right - left + 1 === k) {
+//             // record current recolor count
+//             minRecolorsCountResult = Math.min(minRecolorsCountResult, currentWindowRecolorsCount);
+
+//             if (blocks[left] === "W") {
+//                 currentWindowRecolorsCount--;
+//             }
+
+//             left++;
+//         }
+//         // always move right pointer forwards
+//     }
+
+//     return minRecolorsCountResult;
+// }
+
 /**
- * solution 1 -- sliding window
- * time: O(n)
+ * solution 2 -- brute force
+ * time: O(n^2)
  * space: O(1)
  * 
  * @param {string} blocks
@@ -10,28 +47,21 @@
  * @return {number}
  */
 function minimumRecolors(blocks, k) {
-    let currentWindowRecolorsCount = 0;
     let minRecolorsCountResult = k;
     let left = 0;
 
-    for (let right = 0; right < blocks.length; right++) {
-        // White blocks need to be flipped to get k black blocks in a row
-        if (blocks[right] === "W") {
-            currentWindowRecolorsCount++;
-        }
+    for (let right = k - 1; right < blocks.length; right++) {
+        let recolorCount = 0;
 
-        // window size is equal to k
-        if (right - left + 1 === k) {
-            // record current recolor count
-            minRecolorsCountResult = Math.min(minRecolorsCountResult, currentWindowRecolorsCount);
-
-            if (blocks[left] === "W") {
-                currentWindowRecolorsCount--;
+        for (let l = left; l <= right; l++) {
+            if (blocks[l] === "W") {
+                recolorCount++;
             }
-
-            left++;
         }
-        // always move right pointer forwards
+
+        minRecolorsCountResult = Math.min(minRecolorsCountResult, recolorCount);
+        // move left pointer forwards
+        left++;
     }
 
     return minRecolorsCountResult;
