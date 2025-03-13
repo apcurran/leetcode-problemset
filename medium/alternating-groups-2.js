a5b7be091928358d8b357a9ac42d875db519bb83
Alex Curran <alexcurran.va@gmail.com>
2025-03-13 16:49:22 -0500

    Sliding window approach to alternating-groups-2.js

---
+++ medium/alternating-groups-2.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/**
+ * solution 1 -- sliding window
+ * time: O(n + k)
+ * space: O(1)
+ * 
+ * @param {number[]} colors
+ * @param {number} k
+ * @return {number}
+ */
+function numberOfAlternatingGroups(colors, k) {
+    const n = colors.length;
+    let left = 0;
+    let result = 0;
+
+    for (let right = 1; right < n + k - 1; right++) {
+        // check alternating status
+        if (colors[right % n] === colors[(right - 1) % n]) {
+            left = right;
+        }
+
+        if (right - left + 1 > k) {
+            left++;
+        }
+
+        if (right - left + 1 === k) {
+            result++;
+        }
+    }
+
+    return result;
+}
+
+console.log(numberOfAlternatingGroups([0,1,0,1,0], 3)); // 3
