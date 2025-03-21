5bd1d5d073ce48c4fe45f94e554041344af17e47
Alex Curran <alexcurran.va@gmail.com>
2025-03-21 16:54:30 -0500

    Change JSDoc description, add solution 2 for hashset -- WIP

--- easy/divide-arr-into-equal-pairs.js
+++ easy/divide-arr-into-equal-pairs.js
@@ -1,7 +1,36 @@
 "use strict";
 
+// /**
+//  * solution 1 -- hashmap
+//  * time: O(n)
+//  * space: O(n)
+//  * 
+//  * @param {number[]} nums
+//  * @return {boolean}
+//  */
+// function divideArray(nums) {
+//     if (nums.length % 2 !== 0) return false;
+
+//     let numCounts = new Map();
+
+//     for (let num of nums) {
+//         const prevCount = numCounts.get(num) || 0;
+//         numCounts.set(num, prevCount + 1);
+//     }
+
+//     // // iterate cache and check if occurences val is even
+//     for (let [, numCount] of numCounts) {
+//         if (numCount % 2 !== 0) {
+//             // odd count -> can't make all even pairs!
+//             return false;
+//         }
+//     }
+
+//     return true;
+// }
+
 /**
- * solution 1 -- cache
+ * solution 2 -- hashset
  * time: O(n)
  * space: O(n)
  * 
@@ -10,7 +39,7 @@
  */
 function divideArray(nums) {
     if (nums.length % 2 !== 0) return false;
-    
+
     let numCounts = new Map();
 
     for (let num of nums) {
