8dcb2baad2c29b9427403f0f07883c9effc91631
Alex Curran <alexcurran.va@gmail.com>
2025-03-24 10:54:37 -0500

    Install latest version of ESLint and separate formatting with stylistic package

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import js from "@eslint/js";
+import stylistic from "@stylistic/eslint-plugin";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default defineConfig([
+    {
+        files: ["**/*.{js,mjs,cjs}"],
+        languageOptions: {
+            sourceType: "module",
+            globals: {
+                ...globals.node,
+                ...globals.browser,
+            },
+        },
+        plugins: {
+            js,
+            "@stylistic": stylistic,
+        },
+        extends: ["js/recommended"],
+        rules: {
+            "@stylistic/indent": ["error", 4],
+            "@stylistic/semi": "error",
+            "@stylistic/arrow-parens": ["error", "always"],
+            "@stylistic/quotes": ["error", "double"],
+            "@stylistic/brace-style": "error",
+            "@stylistic/comma-dangle": ["error", "always-multiline"],
+            "@stylistic/eol-last": ["error", "always"],
+        },
+    },
+]);
