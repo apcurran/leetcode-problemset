5cad1d6e153205f0e005f546b469e74739c0df2f
Alex Curran <alexcurran.va@gmail.com>
2025-03-25 16:36:44 -0500

    Bit flip linear solution for min-operations-to-make-binary-array-elements-equal-to-one-1.js

---
+++ medium/min-operations-to-make-binary-array-elements-equal-to-one-1.js
@@ -0,0 +1,45 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
+function minOperations(nums) {
+    /**
+     * mutate array in-place
+     * 
+     * @param {number[]} nums 
+     * @param {number} i 
+     * @returns {void}
+     */
+    function flipBit(nums, i) {
+        nums[i] === 0 ? nums[i] = 1 : nums[i] = 0;
+    }
+    
+    let result = 0;
+
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (nums[i] === 1) {
+            continue;
+        }
+
+        // flip next 3 consecutive bits
+        flipBit(nums, i);
+        flipBit(nums, i + 1);
+        flipBit(nums, i + 2);
+
+        result++;
+    }
+
+    if (nums.at(-1) === 0 || nums.at(-2) === 0) {
+        return -1;
+    }
+
+    return result;
+}
+
+console.log(minOperations([0,1,1,1,0,0])); // 3
