abb717a309ad49ee9bb4cff2549665e2efbb2dfd
Alex Curran <alexcurran.va@gmail.com>
2025-03-28 15:51:15 -0500

    Sliding window-based solution for find-k-closest-elements.js

---
+++ medium/find-k-closest-elements.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/**
+ * solution 1 -- sliding window
+ * time: O(n)
+ * space: O(1) -- not including results
+ * 
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} x
+ * @return {number[]}
+ */
+function findClosestElements(arr, k, x) {
+    let left = 0;
+    let right = arr.length - k;
+
+    while (left < right) {
+        const mid = Math.floor((left + right) / 2);
+
+        if (x - arr[mid] > arr[mid + k] - x) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+
+    return arr.slice(left, left + k);
+}
+
+console.log(findClosestElements([1,2,3,4,5], 4, 3)); // [1,2,3,4]
