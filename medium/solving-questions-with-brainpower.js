87ba079283362a74cddc0c501439ddbf58a277e7
Alex Curran <alexcurran.va@gmail.com>
2025-04-01 17:02:52 -0500

    Array-based cache

--- medium/solving-questions-with-brainpower.js
+++ medium/solving-questions-with-brainpower.js
@@ -1,25 +1,51 @@
 "use strict";
 
+// /**
+//  * solution 1 -- dynamic programming (top down)
+//  * time: O(n) -- hashmap
+//  * space: O(n)
+//  * 
+//  * @param {number[][]} questions
+//  * @return {number}
+//  */
+// function mostPoints(questions, i = 0, cache = new Map()) {
+//     if (i >= questions.length) return 0;
+
+//     if (cache.has(i)) {
+//         return cache.get(i);
+//     }
+
+//     const [pointsEarned, questionsToSkip] = questions[i];
+//     const path1 = mostPoints(questions, i + 1, cache); // skip current question
+//     const path2 = pointsEarned + mostPoints(questions, i + 1 + questionsToSkip, cache); // solve current question and take score
+//     const mostPossiblePoints = Math.max(path1, path2);
+//     cache.set(i, mostPossiblePoints);
+
+//     return mostPossiblePoints;
+// }
+
 /**
- * solution 1 -- dynamic programming (top down)
- * time: O(n)
+ * solution 2 -- dynamic programming (top down)
+ * time: O(n) -- array cache
  * space: O(n)
  * 
- * @param {number[][]} questions
+ * @param {Array<[number, number]>} questions
+ * @param {number} [i] - optional index
+ * @param {Array<number>} [cache] - optional cache
  * @return {number}
  */
-function mostPoints(questions, i = 0, cache = new Map()) {
+function mostPoints(questions, i = 0, cache = new Array(questions.length)) {
     if (i >= questions.length) return 0;
 
-    if (cache.has(i)) {
-        return cache.get(i);
+    if (cache[i]) {
+        return cache[i];
     }
 
     const [pointsEarned, questionsToSkip] = questions[i];
     const path1 = mostPoints(questions, i + 1, cache); // skip current question
     const path2 = pointsEarned + mostPoints(questions, i + 1 + questionsToSkip, cache); // solve current question and take score
     const mostPossiblePoints = Math.max(path1, path2);
-    cache.set(i, mostPossiblePoints);
+    cache[i] = mostPossiblePoints;
 
     return mostPossiblePoints;
 }
