1208b258c12e709e062fbca536ca19fa28228972
Alex Curran <alexcurran.va@gmail.com>
2025-04-02 14:13:31 -0500

    Greedy n^2 solution 2

--- easy/max-value-of-an-ordered-triplet-1.js
+++ easy/max-value-of-an-ordered-triplet-1.js
@@ -1,8 +1,31 @@
 "use strict";
 
+// /**
+//  * solution 1 -- brute force
+//  * time: O(n^3)
+//  * space: O(1)
+//  * 
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// function maximumTripletValue(nums) {
+//     let maxValue = 0;
+
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             for (let k = j + 1; k < nums.length; k++) {
+//                 const currentValue = (nums[i] - nums[j]) * nums[k];
+//                 maxValue = Math.max(maxValue, currentValue);
+//             }
+//         }
+//     }
+
+//     return maxValue;
+// }
+
 /**
- * solution 1 -- brute force
- * time: O(n^3)
+ * solution 2 -- greedy
+ * time: O(n^2)
  * space: O(1)
  * 
  * @param {number[]} nums
@@ -10,13 +33,18 @@
  */
 function maximumTripletValue(nums) {
     let maxValue = 0;
+    let leftValue = nums[0];
+    
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[j] > leftValue) {
+            leftValue = nums[j];
 
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            for (let k = j + 1; k < nums.length; k++) {
-                const currentValue = (nums[i] - nums[j]) * nums[k];
-                maxValue = Math.max(maxValue, currentValue);
-            }
+            continue;
+        }
+
+        for (let k = j + 1; k < nums.length; k++) {
+            const currentValue = (leftValue - nums[j]) * nums[k];
+            maxValue = Math.max(maxValue, currentValue);
         }
     }
 
