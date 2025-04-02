d69d55d4553d5c3618cf35ee2d1102d24294bfa9
Alex Curran <alexcurran.va@gmail.com>
2025-04-02 12:01:21 -0500

    Brute force solution with nested loops for max-value-of-an-ordered-triplet-1.js

---
+++ easy/max-value-of-an-ordered-triplet-1.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/**
+ * solution 1 -- nested loops
+ * time: O(n^3)
+ * space: O(1)
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
+function maximumTripletValue(nums) {
+    let maxValue = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            for (let k = j + 1; k < nums.length; k++) {
+                const currentValue = (nums[i] - nums[j]) * nums[k];
+                maxValue = Math.max(maxValue, currentValue);
+            }
+        }
+    }
+
+    return maxValue;
+}
+
+console.log(maximumTripletValue([12,6,1,2,7])); // 77
