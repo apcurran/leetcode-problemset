d0a00c227fc06fda3a8fbfd4933e1de6f0ee244c
Alex Curran <alexcurran.va@gmail.com>
2025-04-03 16:26:24 -0500

    Greedy approach for max-value-of-an-ordered-triplet-2.js

---
+++ medium/max-value-of-an-ordered-triplet-2.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * solution 1 -- greedy
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
+function maximumTripletValue(nums) {
+    let prefixMax = nums[0];
+    let maxDiff = 0;
+    let result = 0;
+
+    for (let k = 1; k < nums.length; k++) {
+        result = Math.max(result, maxDiff * nums[k]);
+        maxDiff = Math.max(maxDiff, prefixMax - nums[k]);
+        prefixMax = Math.max(prefixMax, nums[k]);
+    }
+    
+    return result;
+}
+
+console.log(maximumTripletValue([12,6,1,2,7])); // 77
