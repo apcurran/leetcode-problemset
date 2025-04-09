e7a1ac24cc96c14b08cc7fce17ae5c54abc54bc6
Alex Curran <alexcurran.va@gmail.com>
2025-04-09 16:18:44 -0500

    Working solution for reverse-degree-of-a-string.js

---
+++ easy/reverse-degree-of-a-string.js
@@ -0,0 +1,24 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {string} str
+ * @return {number}
+ */
+function reverseDegree(str) {
+    let sum = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        const reversedAlphaIndex = 26 - (str.charCodeAt(i) - 97);
+        const oneIndexedPosition = i + 1;
+        const charDegree = oneIndexedPosition * reversedAlphaIndex;
+        sum += charDegree;
+    }
+
+    return sum;
+}
+
+console.log(reverseDegree("abc")); // 148
