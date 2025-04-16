d9deaee8dc199ac6acb2e36d458e2a0148d8c87e
Alex Curran <alexcurran.va@gmail.com>
2025-04-16 14:19:45 -0500

    Hashmap solution in O(n^2) time, but TLE

--- medium/count-the-number-of-good-subarrays.js
+++ medium/count-the-number-of-good-subarrays.js
@@ -1,9 +1,46 @@
 "use strict";
 
+// /**
+//  * solution 1 -- brute force
+//  * time: O(n^4)
+//  * space: O(1)
+//  * 
+//  * @param {number[]} nums
+//  * @param {number} k
+//  * @return {number}
+//  */
+// function countGood(nums, k) {
+//     let resultsCount = 0;
+
+//     for (let i = 0; i < nums.length; i++) {
+//         secondLoop: for (let j = i; j < nums.length; j++) {
+//             let goodPairsSubarrayCounter = 0;
+
+//             for (let m = i; m < j; m++) {
+//                 for (let n = m + 1; n <= j; n++) {
+//                     if (nums[m] === nums[n]) {
+//                         goodPairsSubarrayCounter++;
+//                     }
+//                 }
+//             }
+
+//             if (goodPairsSubarrayCounter >= k) {
+//                 resultsCount++;
+
+//                 // small optimization
+//                 continue secondLoop;
+//             }
+//         }
+
+//     }
+
+//     return resultsCount;
+// }
+
 /**
- * solution 1 -- brute force
- * time: O(n^4)
- * space: O(1)
+ * solution 2 -- hashmap
+ * time: O(n^2)
+ * space: O(n)
  * 
  * @param {number[]} nums
  * @param {number} k
@@ -13,25 +50,20 @@ function countGood(nums, k) {
     let resultsCount = 0;
 
     for (let i = 0; i < nums.length; i++) {
-        secondLoop: for (let j = i; j < nums.length; j++) {
-            let goodPairsSubarrayCounter = 0;
+        let hashmap = new Map();
+        let goodPairsSubarrayCounter = 0;
 
-            for (let m = i; m < j; m++) {
-                for (let n = m + 1; n <= j; n++) {
-                    if (nums[m] === nums[n]) {
-                        goodPairsSubarrayCounter++;
-                    }
-                }
-            }
+        for (let j = i; j < nums.length; j++) {
+            const current = hashmap.get(nums[j]) || 0;
+            // update counter
+            goodPairsSubarrayCounter += current;
+            // update hashmap
+            hashmap.set(nums[j], current + 1);
 
             if (goodPairsSubarrayCounter >= k) {
                 resultsCount++;
-
-                // small optimization
-                continue secondLoop;
             }
         }
-
     }
 
     return resultsCount;
