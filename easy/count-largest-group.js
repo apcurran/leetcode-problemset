fd28b626dba4ae2b21d79524a4bd42c2b3b8ffe8
Alex Curran <alexcurran.va@gmail.com>
2025-04-23 12:03:55 -0500

    Refactor to remove unnecessary loop over map

--- easy/count-largest-group.js
+++ easy/count-largest-group.js
@@ -10,17 +10,15 @@
  */
 function countLargestGroup(n) {
     let map = new Map();
+    let largestArrsLength = 0;
 
     for (let i = 1; i <= n; i++) {
         const digitSum = String(i).split("").reduce((sum, curr) => sum + Number(curr), 0);
         const previousDigitSumValues = map.get(digitSum) || [];
         previousDigitSumValues.push(i);
         map.set(digitSum, previousDigitSumValues);
-    }
 
-    let largestArrsLength = 0;
-
-    for (let [, previousDigitSumValues] of map) {
+        // update largest arr length
         largestArrsLength = Math.max(largestArrsLength, previousDigitSumValues.length);
     }
 
