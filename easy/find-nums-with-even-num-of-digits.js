0433b11e7e063dd02a72c915944b192bf9f4b669
Alex Curran <alexcurran.va@gmail.com>
2025-04-30 17:34:10 -0500

    Improve space complexity which yielded large perf diff in submission runtime

--- easy/find-nums-with-even-num-of-digits.js
+++ easy/find-nums-with-even-num-of-digits.js
@@ -16,30 +16,59 @@
 //     for (let num of nums) {
 //         const numDigitsCount = String(num).length;
 //         const isDigitCountEven = numDigitsCount % 2 === 0;
-        
+
 //         if (isDigitCountEven) evenDigitAmtCounter++;
 //     }
 
 //     return evenDigitAmtCounter;
 // }
 
+// /**
+//  * solution 2 -- .filter() method
+//  * n = len of nums arr
+//  * time: O(n * m)
+//  * space: O(n + m)
+//  * 
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// function findNumbers(nums) {
+//     const evenDigitsNums = nums.filter((num) => {
+//         const numDigitsCount = String(num).length;
+
+//         return numDigitsCount % 2 === 0;
+//     });
+
+//     return evenDigitsNums.length;
+// }
+
 /**
- * solution 2 -- .filter() method
+ * solution 3
  * n = len of nums arr
+ * m = num digits count
  * time: O(n * m)
- * space: O(n + m)
+ * space: O(1)
  * 
  * @param {number[]} nums
  * @return {number}
  */
 function findNumbers(nums) {
-    const evenDigitsNums = nums.filter((num) => {
-        const numDigitsCount = String(num).length;
+    let evenDigitNumsCounter = 0;
 
-        return numDigitsCount % 2 === 0;
-    });
+    for (let num of nums) {
+        let digitsCounter = 0;
 
-    return evenDigitsNums.length;
+        while (num > 0) {
+            num = Math.floor(num / 10);
+            digitsCounter++;
+        }
+
+        if (digitsCounter % 2 === 0) {
+            evenDigitNumsCounter++;
+        }
+    }
+
+    return evenDigitNumsCounter;
 }
 
-console.log( findNumbers([12, 345, 2, 6, 7896]) ); // 2
+console.log(findNumbers([12, 345, 2, 6, 7896])); // 2
