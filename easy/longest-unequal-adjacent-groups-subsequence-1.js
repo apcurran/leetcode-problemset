b8b8c1b2829903a97f5165227dcb4cdd2c71b08d
Alex Curran <alexcurran.va@gmail.com>
2025-05-15 16:34:12 -0500

    Greedy solution for longest-unequal-adjacent-groups-subsequence-1.js

---
+++ easy/longest-unequal-adjacent-groups-subsequence-1.js
@@ -0,0 +1,25 @@
+"use strict";
+
+/**
+ * solution 1 -- greedy
+ * time: O(n)
+ * space: O(1) -- without results array included
+ * 
+ * @param {string[]} words
+ * @param {number[]} groups
+ * @return {string[]}
+ */
+function getLongestSubsequence(words, groups) {
+    let results = [];
+
+    for (let i = 0; i < words.length; i++) {
+        if (i === 0 || groups[i] !== groups[i - 1]) {
+            results.push(words[i]);
+        }
+    }
+
+    return results;
+}
+
+console.log(getLongestSubsequence(["e", "a", "b"], [0, 0, 1])); // ["e", "b"]
+console.log(getLongestSubsequence(["a", "b", "c", "d"], [1, 0, 1, 1])); // ["a", "b", "c"]
