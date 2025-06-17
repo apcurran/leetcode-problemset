bc958b2769098f03e01cc157503200c1f05bc09b
Alex Curran <alexcurran.va@gmail.com>
2025-06-17 13:34:48 -0500

    Working linear time solution for max-difference-between-adjacent-elements-in-a-circular-array.js

---
+++ easy/max-difference-between-adjacent-elements-in-a-circular-array.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ * solution 1
+ * time: O(n)
+ * space: O(1)
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
+function maxAdjacentDistance(nums) {
+    // take care of circular array case first
+    let maxAbsoluteDiff = Math.abs(nums.at(-1) - nums[0]);
+
+    // skip last case (already handled above first)
+    for (let i = 0; i < nums.length - 1; i++) {
+        maxAbsoluteDiff = Math.max(maxAbsoluteDiff, Math.abs(nums[i] - nums[i + 1]));
+    }
+    
+    return maxAbsoluteDiff;
+}
+
+console.log(maxAdjacentDistance([-5, -10, -5])); // 5
