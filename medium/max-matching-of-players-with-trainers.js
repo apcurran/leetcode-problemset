2044d350241bb34e4223d716406a03e4577ddae5
Alex Curran <alexcurran.va@gmail.com>
2025-07-17 11:55:37 -0500

    Sort, then two pointer solution

--- medium/max-matching-of-players-with-trainers.js
+++ medium/max-matching-of-players-with-trainers.js
@@ -1,10 +1,44 @@
 "use strict";
 
+// /**
+//  * solution 1 -- correct, but TLE
+//  * n = players
+//  * m = trainers
+//  * time: O(n * m)
+//  * space: O(n + m) -- sorting both arrays
+//  *
+//  * @param {number[]} players
+//  * @param {number[]} trainers
+//  * @return {number}
+//  */
+// function matchPlayersAndTrainers(players, trainers) {
+//     players.sort(sortAsc);
+//     trainers.sort(sortAsc);
+//     let counter = 0;
+
+//     for (let player of players) {
+//         // iterate trainers arr
+//         for (let j = 0; j < trainers.length; j++) {
+//             const trainer = trainers[j];
+
+//             if (player <= trainer) {
+//                 counter++;
+//                 // zero out current trainer value
+//                 trainers[j] = 0;
+//                 // exit early
+//                 break;
+//             }
+//         }
+//     }
+
+//     return counter;
+// }
+
 /**
- * solution 1 -- correct, but TLE
+ * solution 2 -- sort, then two pointers
  * n = players
  * m = trainers
- * time: O(n * m)
+ * time: O(n * log n + m * log m)
  * space: O(n + m) -- sorting both arrays
  *
  * @param {number[]} players
@@ -14,27 +48,27 @@
 function matchPlayersAndTrainers(players, trainers) {
     players.sort(sortAsc);
     trainers.sort(sortAsc);
-    let counter = 0;
+    let matchesCount = 0;
 
-    for (let player of players) {
-        // iterate trainers arr
-        for (let j = 0; j < trainers.length; j++) {
-            const trainer = trainers[j];
+    for (
+        let pIndex = 0, tIndex = 0;
+        pIndex < players.length && tIndex < trainers.length;
+        pIndex++, tIndex++
+    ) {
+        while (tIndex < trainers.length && players[pIndex] > trainers[tIndex]) {
+            tIndex++;
+        }
 
-            if (player <= trainer) {
-                counter++;
-                // zero out current trainer value
-                trainers[j] = 0;
-                // exit early
-                break;
-            }
+        if (tIndex < trainers.length) {
+            matchesCount++;
         }
     }
 
-    return counter;
+    return matchesCount;
 }
 
 console.log(matchPlayersAndTrainers([4, 7, 9], [8, 2, 5, 8])); // 2
+console.log(matchPlayersAndTrainers([1, 1, 1], [10])); // 1
 
 /**
  * @param {number} a
