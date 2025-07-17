c510b3afc2280bfe8b503f09a0cb7f7a427e9a10
Alex Curran <alexcurran.va@gmail.com>
2025-07-17 14:25:22 -0500

    Regex-based solution with multiple constraints

---
+++ easy/valid-word.js
@@ -0,0 +1,22 @@
+"use strict";
+
+/**
+ * solution 1 -- regex
+ * time: O(n)
+ * space: O(1)
+ *
+ * @param {string} word
+ * @return {boolean}
+ */
+function isValid(word) {
+    if (word.length < 3) return false;
+
+    const regex =
+        /^(?=.*[aeiouAEIOU])(?=.*[bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ])[a-zA-Z0-9]+$/gi;
+
+    return regex.test(word);
+}
+
+console.log(isValid("234Adas")); // true
+console.log(isValid("b3")); // false
+console.log(isValid("a3$e")); // false
