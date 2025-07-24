94146e3d975db9bee059bfa317a7354cb47c865f
Alex Curran <alexcurran.va@gmail.com>
2025-07-24 16:26:01 -0500

    Optimize with 8-bit typed array

--- medium/max-erasure-value.js
+++ medium/max-erasure-value.js
@@ -1,31 +1,62 @@
 "use strict";
 
+// /**
+//  * solution 1 -- sliding window with hashset
+//  * time: O(n)
+//  * space: O(n)
+//  *
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// function maximumUniqueSubarray(nums) {
+//     let seenValues = new Set();
+//     let maxSum = 0;
+//     let currentSum = 0;
+//     let left = 0;
+
+//     for (let right = 0; right < nums.length; right++) {
+//         while (seenValues.has(nums[right])) {
+//             // shrink window
+//             seenValues.delete(nums[left]);
+//             currentSum -= nums[left];
+//             left++;
+//         }
+
+//         // expand window
+//         seenValues.add(nums[right]);
+//         currentSum += nums[right];
+//         // possible max update
+//         maxSum = Math.max(maxSum, currentSum);
+//     }
+
+//     return maxSum;
+// }
+
 /**
- * solution 1 -- sliding window with hashset
+ * solution 2 -- sliding window with typed array (slight optimization)
  * time: O(n)
- * space: O(n)
+ * space: O(1) -- fixed array size
  *
  * @param {number[]} nums
  * @return {number}
  */
 function maximumUniqueSubarray(nums) {
-    let seenValues = new Set();
+    let seenValues = new Uint8Array(10_001); // max possible by constraint
     let maxSum = 0;
     let currentSum = 0;
     let left = 0;
 
     for (let right = 0; right < nums.length; right++) {
-        while (seenValues.has(nums[right])) {
-            // shrink window
-            seenValues.delete(nums[left]);
+        const num = nums[right];
+
+        while (seenValues[num]) {
+            seenValues[nums[left]] = 0;
             currentSum -= nums[left];
             left++;
         }
 
-        // expand window
-        seenValues.add(nums[right]);
-        currentSum += nums[right];
-        // possible max update
+        seenValues[num] = 1;
+        currentSum += num;
         maxSum = Math.max(maxSum, currentSum);
     }
 
