bd9dfe80d292116be4a716c595b8cb6c55b8d6af
Alex Curran <alexcurran.va@gmail.com>
2025-08-15 11:39:02 -0500

    Adjust to mod 4 from mod 2

--- easy/power-of-four.js
+++ easy/power-of-four.js
@@ -1,22 +1,22 @@
 "use strict";
 
-// /**
-//  * solution 1 -- recursion
-//  * time: O(log n)
-//  * space: O(log n)
-//  *
-//  * @param {number} n
-//  * @return {boolean}
-//  */
-// function isPowerOfFour(n) {
-//     // base case
-//     if (n === 1) return true;
+/**
+ * solution 1 -- recursion
+ * time: O(log n)
+ * space: O(log n)
+ *
+ * @param {number} n
+ * @return {boolean}
+ */
+function isPowerOfFour(n) {
+    // base case
+    if (n === 1) return true;
 
-//     // base case
-//     if (n === 0 || n % 2 !== 0) return false;
+    // base case
+    if (n === 0 || n % 4 !== 0) return false;
 
-//     return isPowerOfFour(n / 4);
-// }
+    return isPowerOfFour(n / 4);
+}
 
 // /**
 //  * solution 2 -- iterative
@@ -38,23 +38,23 @@
 //     return true;
 // }
 
-/**
- * solution 3 -- math logarithms
- * time: O(1)
- * space: O(1)
- *
- * @param {number} n
- * @return {boolean}
- */
-function isPowerOfFour(n) {
-    if (n === 0) return false;
+// /**
+//  * solution 3 -- math logarithms
+//  * time: O(1)
+//  * space: O(1)
+//  *
+//  * @param {number} n
+//  * @return {boolean}
+//  */
+// function isPowerOfFour(n) {
+//     if (n === 0) return false;
 
-    const baseFourLog = Math.log(n) / Math.log(4);
+//     const baseFourLog = Math.log(n) / Math.log(4);
 
-    if (!Number.isInteger(baseFourLog)) return false;
+//     if (!Number.isInteger(baseFourLog)) return false;
 
-    return n === 4 ** baseFourLog;
-}
+//     return n === 4 ** baseFourLog;
+// }
 
 console.log(isPowerOfFour(16)); // true
 console.log(isPowerOfFour(5)); // false
