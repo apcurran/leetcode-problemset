2fda0cb6e0a8228b4879e03515c78d4fa8c32987
Alex Curran <alexcurran.va@gmail.com>
2025-09-08 15:19:31 -0500

    Easy solution with more space tradeoff

--- easy/convert-integer-to-the-sum-of-two-no-zero-integers.js
+++ easy/convert-integer-to-the-sum-of-two-no-zero-integers.js
@@ -1,7 +1,43 @@
+// /**
+//  * solution 1
+//  * time: O(n * log n)
+//  * space: O(1)
+//  *
+//  * @param {number} n
+//  * @return {number[]}
+//  */
+// function getNoZeroIntegers(n) {
+//     for (let a = 1; a <= n; a++) {
+//         const b = n - a;
+
+//         if (checkNoZeros(a) && checkNoZeros(b)) {
+//             return [a, b];
+//         }
+//     }
+
+//     return [-1, -1];
+// }
+
+// /**
+//  * @param {number} n
+//  * @returns {boolean}
+//  */
+// function checkNoZeros(n) {
+//     while (n > 0) {
+//         const digit = n % 10;
+
+//         if (digit === 0) return false;
+
+//         n = Math.floor(n / 10);
+//     }
+
+//     return true;
+// }
+
 /**
- * solution 1
+ * solution 2
  * time: O(n * log n)
- * space: O(1)
+ * space: O(maxlen(a, b)) -- string conversion space
  *
  * @param {number} n
  * @return {number[]}
@@ -10,28 +46,12 @@ function getNoZeroIntegers(n) {
     for (let a = 1; a <= n; a++) {
         const b = n - a;
 
-        if (checkNoZeros(a) && checkNoZeros(b)) {
+        if (!String(a).includes("0") && !String(b).includes("0")) {
             return [a, b];
         }
     }
 
     return [-1, -1];
-}
-
-/**
- * @param {number} n
- * @returns {boolean}
- */
-function checkNoZeros(n) {
-    while (n > 0) {
-        const digit = n % 10;
-
-        if (digit === 0) return false;
-
-        n = Math.floor(n / 10);
-    }
-
-    return true;
 }
 
 console.log(getNoZeroIntegers(11)); // [2, 9]
