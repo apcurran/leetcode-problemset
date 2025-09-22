ec8d77c8a6a18202234085077fec64642467815d
Alex Curran <alexcurran.va@gmail.com>
2025-09-22 10:45:52 -0500

    Brute force solution for restore-finishing-order.js

---
+++ easy/restore-finishing-order.js
@@ -0,0 +1,20 @@
+/**
+ * solution 1
+ * n = order length
+ * m = friends length
+ * time: O(n * m)
+ * space: O(n)
+ *
+ * @param {number[]} order
+ * @param {number[]} friends
+ * @return {number[]}
+ */
+function recoverOrder(order, friends) {
+    // remove all values in order that do not appear in friends
+    return order.filter(function removeNonFriends(value) {
+        return friends.includes(value);
+    });
+}
+
+console.log(recoverOrder([3, 1, 2, 5, 4], [1, 3, 4])); // [3, 1, 4]
+console.log(recoverOrder([1, 4, 5, 3, 2], [2, 5])); // [5, 2]
