d53b36b854b33078456f7f7875f7c7ddcac037a9
Alex Curran <alexcurran.va@gmail.com>
2025-09-22 10:47:54 -0500

    Hashset solution for O(n + m) time

--- easy/restore-finishing-order.js
+++ easy/restore-finishing-order.js
@@ -1,18 +1,37 @@
+// /**
+//  * solution 1
+//  * n = order length
+//  * m = friends length
+//  * time: O(n * m)
+//  * space: O(n)
+//  *
+//  * @param {number[]} order
+//  * @param {number[]} friends
+//  * @return {number[]}
+//  */
+// function recoverOrder(order, friends) {
+//     // remove all values in order that do not appear in friends
+//     return order.filter(function removeNonFriends(value) {
+//         return friends.includes(value);
+//     });
+// }
+
 /**
- * solution 1
+ * solution 2 -- hashset
  * n = order length
  * m = friends length
- * time: O(n * m)
- * space: O(n)
+ * time: O(n + m)
+ * space: O(n + m)
  *
  * @param {number[]} order
  * @param {number[]} friends
  * @return {number[]}
  */
 function recoverOrder(order, friends) {
-    // remove all values in order that do not appear in friends
+    const friendsSet = new Set(friends);
+
     return order.filter(function removeNonFriends(value) {
-        return friends.includes(value);
+        return friendsSet.has(value);
     });
 }
 
