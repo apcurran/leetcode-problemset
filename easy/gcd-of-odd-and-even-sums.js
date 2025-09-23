c84c33a8c9ddd84b494506afa961c17073c4244b
Alex Curran <alexcurran.va@gmail.com>
2025-09-23 13:42:05 -0500

    Log m time solution (removes linear iteration of n first)

--- easy/gcd-of-odd-and-even-sums.js
+++ easy/gcd-of-odd-and-even-sums.js
@@ -1,24 +1,57 @@
+// /**
+//  * solution 1
+//  * time: O(n + log m)
+//  * space: O(1)
+//  *
+//  * @param {number} n
+//  * @return {number}
+//  */
+// function gcdOfOddEvenSums(n) {
+//     let oddSum = 0;
+//     let evenSum = 0;
+
+//     for (let i = 1; i <= n * 2; i++) {
+//         if (i % 2 === 1) {
+//             // if odd
+//             oddSum += i;
+//         } else {
+//             // if even
+//             evenSum += i;
+//         }
+//     }
+
+//     return getGCD(oddSum, evenSum);
+// }
+
+// console.log(gcdOfOddEvenSums(4)); // 4
+// console.log(gcdOfOddEvenSums(5)); // 5
+
+// /**
+//  * @param {number} a
+//  * @param {number} b
+//  * @returns {number}
+//  */
+// function getGCD(a, b) {
+//     while (b !== 0) {
+//         const temp = b;
+//         b = a % b;
+//         a = temp;
+//     }
+
+//     return Math.abs(a);
+// }
+
 /**
- * solution 1
- * time: O(n + log m)
+ * solution 2
+ * time: O(log m)
  * space: O(1)
  *
  * @param {number} n
  * @return {number}
  */
 function gcdOfOddEvenSums(n) {
-    let oddSum = 0;
-    let evenSum = 0;
-
-    for (let i = 1; i <= n * 2; i++) {
-        if (i % 2 === 1) {
-            // if odd
-            oddSum += i;
-        } else {
-            // if even
-            evenSum += i;
-        }
-    }
+    const oddSum = n * n;
+    const evenSum = n * (n + 1);
 
     return getGCD(oddSum, evenSum);
 }
