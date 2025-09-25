3f27712a732a3323f7219b3842e1703d6e23ec56
Alex Curran <alexcurran.va@gmail.com>
2025-09-25 11:22:55 -0500

    Correct, but time exceeded DFS recursive solution without memo

---
+++ medium/triangle.js
@@ -0,0 +1,33 @@
+/**
+ * solution 1 -- DFS recursion (no memo)
+ * time: O(2^n) -- each call spawns two more recursive calls
+ * space: O(height) -> O(n)
+ *
+ * @param {number[][]} triangle
+ * @return {number} min path sum from top to bottom
+ */
+function minimumTotal(triangle) {
+    /**
+     * @param {number} row
+     * @param {number} col
+     * @returns {number}
+     */
+    function dfs(row, col) {
+        // base case: end at final row in triangle
+        if (row === triangle.length - 1) {
+            return triangle[row][col];
+        }
+
+        // can travel to next row, current column
+        const down = dfs(row + 1, col);
+        // OR next row, next column
+        const downRight = dfs(row + 1, col + 1);
+
+        return triangle[row][col] + Math.min(down, downRight);
+    }
+
+    return dfs(0, 0);
+}
+
+console.log(minimumTotal([[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]])); // 11
+console.log(minimumTotal([[-10]])); // -10
