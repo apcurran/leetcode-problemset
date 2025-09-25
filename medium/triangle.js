2e798801d2e259ed7848c3e36ac8da9b4088342c
Alex Curran <alexcurran.va@gmail.com>
2025-09-25 11:50:16 -0500

    Iterative bottom-up approach with memo typed array cache

--- medium/triangle.js
+++ medium/triangle.js
@@ -29,44 +29,72 @@
 //     return dfs(0, 0);
 // }
 
+// /**
+//  * solution 2 -- DFS recursion (memo)
+//  * time: O(n^2)
+//  * space: O(n^2)
+//  *
+//  * @param {number[][]} triangle
+//  * @return {number} min path sum from top to bottom
+//  */
+// function minimumTotal(triangle) {
+//     let memo = new Map();
+
+//     /**
+//      * @param {number} row
+//      * @param {number} col
+//      * @returns {number}
+//      */
+//     function dfs(row, col) {
+//         // base case: end at final row in triangle
+//         if (row === triangle.length - 1) {
+//             return triangle[row][col];
+//         }
+
+//         if (memo.has(`${row}:${col}`)) {
+//             return memo.get(`${row}:${col}`);
+//         }
+
+//         // can travel to next row, current column
+//         const down = dfs(row + 1, col);
+//         // OR next row, next column
+//         const downRight = dfs(row + 1, col + 1);
+//         const result = triangle[row][col] + Math.min(down, downRight);
+//         // add to memo hashmap
+//         memo.set(`${row}:${col}`, result);
+
+//         return result;
+//     }
+
+//     return dfs(0, 0);
+// }
+
 /**
- * solution 2 -- DFS recursion (memo)
+ * solution 3 -- iterative bottom-up approach (memo)
  * time: O(n^2)
- * space: O(n^2)
+ * space: O(n)
  *
  * @param {number[][]} triangle
  * @return {number} min path sum from top to bottom
  */
 function minimumTotal(triangle) {
-    let memo = new Map();
+    const h = triangle.length;
+    // copy final row of triangle
+    let dpCache = new Int16Array(triangle[h - 1].length);
 
-    /**
-     * @param {number} row
-     * @param {number} col
-     * @returns {number}
-     */
-    function dfs(row, col) {
-        // base case: end at final row in triangle
-        if (row === triangle.length - 1) {
-            return triangle[row][col];
-        }
-
-        if (memo.has(`${row}:${col}`)) {
-            return memo.get(`${row}:${col}`);
-        }
-
-        // can travel to next row, current column
-        const down = dfs(row + 1, col);
-        // OR next row, next column
-        const downRight = dfs(row + 1, col + 1);
-        const result = triangle[row][col] + Math.min(down, downRight);
-        // add to memo hashmap
-        memo.set(`${row}:${col}`, result);
-
-        return result;
+    for (let i = 0; i < dpCache.length; i++) {
+        dpCache[i] = triangle[h - 1][i];
     }
 
-    return dfs(0, 0);
+    // iterate from next to last row upwards
+    for (let row = h - 2; row >= 0; row--) {
+        for (let col = 0; col <= row; col++) {
+            dpCache[col] =
+                triangle[row][col] + Math.min(dpCache[col], dpCache[col + 1]);
+        }
+    }
+
+    return dpCache[0];
 }
 
 console.log(minimumTotal([[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]])); // 11
