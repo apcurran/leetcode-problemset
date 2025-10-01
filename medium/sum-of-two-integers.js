e1fde8674db90367af8952464935f52644cf403d
Alex Curran <alexcurran.va@gmail.com>
2025-10-01 16:56:00 -0500

    Bit manipulation constant time/space

---
+++ medium/sum-of-two-integers.js
@@ -0,0 +1,21 @@
+/**
+ * solution 1 -- bit manipulation
+ * time: O(1)
+ * space: O(1)
+ *
+ * @param {number} a
+ * @param {number} b
+ * @return {number}
+ */
+function getSum(a, b) {
+    while (b !== 0) {
+        const temp = (a & b) << 1;
+        a ^= b;
+        b = temp;
+    }
+
+    return a;
+}
+
+console.log(getSum(1, 2)); // 3
+console.log(getSum(2, 3)); // 5
