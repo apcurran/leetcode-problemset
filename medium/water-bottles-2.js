822479c0547ed9c77d53ed3a7f88c90651195a46
Alex Curran <alexcurran.va@gmail.com>
2025-10-02 10:57:20 -0500

    Show more primitive (but logical) O(n) time solution

--- medium/water-bottles-2.js
+++ medium/water-bottles-2.js
@@ -1,5 +1,39 @@
+// /**
+//  * solution 1 -- simulation
+//  * time: O(n)
+//  * space: O(1)
+//  *
+//  * @param {number} fullBottles
+//  * @param {number} numExchange
+//  * @return {number}
+//  */
+// function maxBottlesDrunk(fullBottles, numExchange) {
+//     let emptyBottles = 0;
+//     let totalBottlesDrunk = 0;
+
+//     while (fullBottles > 0) {
+//         totalBottlesDrunk += fullBottles;
+//         emptyBottles += fullBottles;
+//         fullBottles = 0;
+
+//         // attempt exchange of old for 1 new bottle
+//         if (emptyBottles >= numExchange) {
+//             // pay exchange cost with empty bottles
+//             emptyBottles -= numExchange;
+//             // add 1 full bottle
+//             fullBottles++;
+//             // exchange rate increments by 1
+//             numExchange++;
+//         } else {
+//             break;
+//         }
+//     }
+
+//     return totalBottlesDrunk;
+// }
+
 /**
- * solution 1 -- simulation
+ * solution 2 -- simulation (enhanced!)
  * time: O(sqrt(n))
  * space: O(1)
  *
