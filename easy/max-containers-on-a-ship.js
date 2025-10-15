33983649d8b511a1630a436077093087bacceeb6
Alex Curran <alexcurran.va@gmail.com>
2025-10-15 15:31:00 -0500

    Math solution

--- easy/max-containers-on-a-ship.js
+++ easy/max-containers-on-a-ship.js
@@ -1,6 +1,33 @@
+// /**
+//  * solution 1 -- iterative
+//  * time: O(n)
+//  * space: O(1)
+//  *
+//  * @param {number} n
+//  * @param {number} w
+//  * @param {number} maxWeight
+//  * @return {number}
+//  */
+// function maxContainers(n, w, maxWeight) {
+//     let sumWeight = 0;
+//     let totalContainers = 0;
+
+//     for (let i = 1; i <= n * n; i++) {
+//         if (sumWeight + w <= maxWeight) {
+//             // you may add another container
+//             sumWeight += w;
+//             totalContainers++;
+//         } else {
+//             break;
+//         }
+//     }
+
+//     return totalContainers;
+// }
+
 /**
- * solution 1 -- iterative
- * time: O(n)
+ * solution 2 -- math
+ * time: O(1)
  * space: O(1)
  *
  * @param {number} n
@@ -9,20 +36,7 @@
  * @return {number}
  */
 function maxContainers(n, w, maxWeight) {
-    let sumWeight = 0;
-    let totalContainers = 0;
-
-    for (let i = 1; i <= n * n; i++) {
-        if (sumWeight + w <= maxWeight) {
-            // you may add another container
-            sumWeight += w;
-            totalContainers++;
-        } else {
-            break;
-        }
-    }
-
-    return totalContainers;
+    return Math.min(n * n, Math.floor(maxWeight / w));
 }
 
 console.log(maxContainers(2, 3, 4)); // 1
