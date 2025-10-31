7ab03b00d1de7cf8382e8c40be6c4d426cea9586
Alex Curran <alexcurran.va@gmail.com>
2025-10-31 10:54:17 -0500

    Solution 2 for the brute force, but constant space solution

--- easy/the-two-sneaky-numbers-of-digitville.js
+++ easy/the-two-sneaky-numbers-of-digitville.js
@@ -1,23 +1,49 @@
 "use strict";
 
+// /**
+//  * solution 1 -- hashset
+//  * time: O(n)
+//  * space: O(n) -- set
+//  *
+//  * @param {number[]} nums
+//  * @return {number[]}
+//  */
+// function getSneakyNumbers(nums) {
+//     let seenNums = new Set();
+//     let sneakyNums = [];
+
+//     for (let num of nums) {
+//         if (seenNums.has(num)) {
+//             sneakyNums.push(num);
+//         }
+
+//         seenNums.add(num);
+//     }
+
+//     return sneakyNums;
+// }
+
 /**
- * solution 1 -- hashset
- * time: O(n)
- * space: O(n) -- set
+ * solution 2 -- brute force
+ * time: O(n^2)
+ * space: O(1)
  *
  * @param {number[]} nums
  * @return {number[]}
  */
 function getSneakyNumbers(nums) {
-    let seenNums = new Set();
     let sneakyNums = [];
 
-    for (let num of nums) {
-        if (seenNums.has(num)) {
-            sneakyNums.push(num);
-        }
+    for (let i = 0; i < nums.length; i++) {
+        const currentNum = nums[i];
 
-        seenNums.add(num);
+        for (let j = i + 1; j < nums.length; j++) {
+            const otherNum = nums[j];
+
+            if (currentNum === otherNum) {
+                sneakyNums.push(currentNum);
+            }
+        }
     }
 
     return sneakyNums;
