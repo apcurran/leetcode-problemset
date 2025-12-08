35a8c1cf2b32feb7535360d7e723f0b49d96c955
Alex Curran <alexcurran.va@gmail.com>
2025-12-08 10:31:53 -0600

    Optimize sqrt calculation approach using n^2 - a^2 to derive maxBSquared and maxB -> O(n * sqrt(n)) time

--- easy/count-square-sum-triples.js
+++ easy/count-square-sum-triples.js
@@ -24,9 +24,36 @@
 //     return triplesCount;
 // }
 
+// /**
+//  * solution 2
+//  * time: O(n^2)
+//  * space: O(1)
+//  *
+//  * @param {number} n
+//  * @return {number}
+//  */
+// function countTriples(n) {
+//     let triplesCount = 0;
+
+//     for (let a = 1; a <= n; a++) {
+//         for (let b = 2; b <= n; b++) {
+//             const aSquared = a * a;
+//             const bSquared = b * b;
+//             const c = Math.trunc(Math.sqrt(aSquared + bSquared));
+//             const cSquared = c * c;
+
+//             if (aSquared + bSquared === cSquared && c <= n) {
+//                 triplesCount++;
+//             }
+//         }
+//     }
+
+//     return triplesCount;
+// }
+
 /**
- * solution 2
- * time: O(n^2)
+ * solution 3 -- sqrt calculation approach
+ * time: O(n * sqrt(n))
  * space: O(1)
  *
  * @param {number} n
@@ -36,13 +63,18 @@ function countTriples(n) {
     let triplesCount = 0;
 
     for (let a = 1; a <= n; a++) {
-        for (let b = 2; b <= n; b++) {
-            const aSquared = a * a;
-            const bSquared = b * b;
-            const c = Math.trunc(Math.sqrt(aSquared + bSquared));
-            const cSquared = c * c;
+        const aSquared = a * a;
+        // c^2 = a^2 + b^2 and c <= n, therefore:
+        // b^2 = c^2 - a^2 <= n^2 - a^2
+        const maxBSquared = n * n - aSquared;
+        const maxB = Math.trunc(Math.sqrt(maxBSquared));
 
-            if (aSquared + bSquared === cSquared && c <= n) {
+        for (let b = 1; b <= maxB; b++) {
+            const bSquared = b * b;
+            const cSquared = aSquared + bSquared;
+            const c = Math.sqrt(cSquared);
+
+            if (Number.isInteger(c)) {
                 triplesCount++;
             }
         }
