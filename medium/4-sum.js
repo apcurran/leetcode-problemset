011854a682d3f87bc3d41dc01ccac62b20bc088a
Alex Curran <alexcurran.va@gmail.com>
2026-02-12 15:53:47 -0600

    Working solution for 4-sum.js

--- medium/4-sum.js
+++ medium/4-sum.js
@@ -1,9 +1,57 @@
+// /**
+//  * solution 1 -- brute force, TLE
+//  * n = nums length
+//  * m = combos length
+//  * time: O(n^4)
+//  * space: O(n + m) -- due to sorting and hashset
+//  *
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number[][]}
+//  */
+// function fourSum(nums, target) {
+//     nums.sort(function compareAsc(a, b) {
+//         return a - b;
+//     });
+//     let seen = new Set();
+//     let results = [];
+
+//     for (let i = 0; i < nums.length; i++) {
+//         for (let j = i + 1; j < nums.length; j++) {
+//             for (let k = j + 1; k < nums.length; k++) {
+//                 for (let l = k + 1; l < nums.length; l++) {
+//                     const a = nums[i];
+//                     const b = nums[j];
+//                     const c = nums[k];
+//                     const d = nums[l];
+//                     const sum = a + b + c + d;
+
+//                     if (sum !== target) {
+//                         continue; // skip rest
+//                     }
+
+//                     // check for uniqueness
+//                     const numsGroup = [nums[i], nums[j], nums[k], nums[l]];
+//                     const key = numsGroup.join(",");
+
+//                     if (!seen.has(key)) {
+//                         seen.add(key);
+//                         results.push(numsGroup);
+//                     }
+//                 }
+//             }
+//         }
+//     }
+
+//     return results;
+// }
+
 /**
- * solution 1 -- brute force, TLE
+ * solution 2
  * n = nums length
  * m = combos length
- * time: O(n^4)
- * space: O(n + m) -- due to sorting and hashset
+ * time: O(n^3)
+ * space: O(k)
  *
  * @param {number[]} nums
  * @param {number} target
@@ -13,35 +61,53 @@ function fourSum(nums, target) {
     nums.sort(function compareAsc(a, b) {
         return a - b;
     });
-    let seen = new Set();
+    let quad = [];
     let results = [];
 
-    for (let i = 0; i < nums.length; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            for (let k = j + 1; k < nums.length; k++) {
-                for (let l = k + 1; l < nums.length; l++) {
-                    const a = nums[i];
-                    const b = nums[j];
-                    const c = nums[k];
-                    const d = nums[l];
-                    const sum = a + b + c + d;
+    /**
+     * @param {number} k
+     * @param {number} start
+     * @param {number} target
+     * @returns {void}
+     */
+    function kSum(k, start, target) {
+        if (k !== 2) {
+            for (let i = start; i <= nums.length - k; i++) {
+                if (i > start && nums[i] === nums[i - 1]) {
+                    // duplicate
+                    continue;
+                }
 
-                    if (sum !== target) {
-                        continue; // skip rest
-                    }
+                quad.push(nums[i]);
+                kSum(k - 1, i + 1, target - nums[i]);
+                quad.pop();
+            }
 
-                    // check for uniqueness
-                    const numsGroup = [nums[i], nums[j], nums[k], nums[l]];
-                    const key = numsGroup.join(",");
+            return;
+        }
 
-                    if (!seen.has(key)) {
-                        seen.add(key);
-                        results.push(numsGroup);
-                    }
+        let left = start;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            if (nums[left] + nums[right] < target) {
+                // incr
+                left++;
+            } else if (nums[left] + nums[right] > target) {
+                // decr
+                right--;
+            } else {
+                results.push([...quad, nums[left], nums[right]]);
+                left++;
+
+                while (left < right && nums[left] === nums[left - 1]) {
+                    left++;
                 }
             }
         }
     }
+
+    kSum(4, 0, target);
 
     return results;
 }
