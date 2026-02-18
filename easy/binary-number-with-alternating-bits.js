ca885655d83f3545898e0d4962f23ed3528279c6
Alex Curran <alexcurran.va@gmail.com>
2026-02-18 15:47:06 -0600

    Solution 3: bitwise operators

--- easy/binary-number-with-alternating-bits.js
+++ easy/binary-number-with-alternating-bits.js
@@ -21,8 +21,35 @@
 //     return true;
 // }
 
+// /**
+//  * solution 2 -- remainder and modulo operators for bit checking
+//  * w <= 32 bits
+//  * time: O(w)
+//  * space: O(1)
+//  *
+//  * @param {number} n
+//  * @return {boolean}
+//  */
+// function hasAlternatingBits(n) {
+//     let lastBit = n % 2;
+//     n = Math.floor(n / 2);
+
+//     while (n > 0) {
+//         const currentBit = n % 2;
+
+//         if (currentBit === lastBit) {
+//             return false;
+//         }
+
+//         lastBit = currentBit;
+//         n = Math.floor(n / 2);
+//     }
+
+//     return true;
+// }
+
 /**
- * solution 2 -- remainder and modulo operators for bit checking
+ * solution 3 -- bitwise operators
  * w <= 32 bits
  * time: O(w)
  * space: O(1)
@@ -31,18 +58,18 @@
  * @return {boolean}
  */
 function hasAlternatingBits(n) {
-    let lastBit = n % 2;
-    n = Math.floor(n / 2);
+    let lastBit = n & 1; // rightmost bit
+    n >>= 1; // shift n right 1 place
 
     while (n > 0) {
-        const currentBit = n % 2;
+        const currentBit = n & 1;
 
         if (currentBit === lastBit) {
             return false;
         }
 
         lastBit = currentBit;
-        n = Math.floor(n / 2);
+        n >>= 1; // get the next bit into checking position
     }
 
     return true;
