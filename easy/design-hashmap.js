f3900993a8b0b3d5b62fe7dc7212490f16791f79
Alex Curran <alexcurran.va@gmail.com>
2026-03-05 13:37:58 -0600

    Working implementation of hashmap

---
+++ easy/design-hashmap.js
@@ -0,0 +1,51 @@
+class MyHashMap {
+    constructor() {
+        this.hash = {};
+    }
+
+    /**
+     * inserts a (key, value) pair into the HashMap. If the key already exists in the map, update the corresponding value
+     *
+     * @param {number} key
+     * @param {number} value
+     * @return {void}
+     */
+    put(key, value) {
+        // otherwise, add new key/val pair
+        this.hash[key] = value;
+    }
+
+    /**
+     * returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
+     *
+     * @param {number} key
+     * @return {number}
+     */
+    get(key) {
+        if (Object.hasOwn(this.hash, key)) {
+            return this.hash[key];
+        } else {
+            return -1;
+        }
+    }
+
+    /**
+     * removes the key and its corresponding value if the map contains the mapping for the key
+     *
+     * @param {number} key
+     * @return {void}
+     */
+    remove(key) {
+        delete this.hash[key];
+
+        return;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * var obj = new MyHashMap()
+ * obj.put(key,value)
+ * var param_2 = obj.get(key)
+ * obj.remove(key)
+ */
