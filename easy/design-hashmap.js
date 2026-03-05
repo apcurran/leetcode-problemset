ac34e16abeb5109a24fa9bc6d1e8f027b53389e1
Alex Curran <alexcurran.va@gmail.com>
2026-03-05 13:44:30 -0600

    Array-based solution 2

--- easy/design-hashmap.js
+++ easy/design-hashmap.js
@@ -1,6 +1,52 @@
+// solution 1 -- object
+// class MyHashMap {
+//     constructor() {
+//         this.hash = {};
+//     }
+
+//     /**
+//      * inserts a (key, value) pair into the HashMap. If the key already exists in the map, update the corresponding value
+//      *
+//      * @param {number} key
+//      * @param {number} value
+//      * @return {void}
+//      */
+//     put(key, value) {
+//         // otherwise, add new key/val pair
+//         this.hash[key] = value;
+//     }
+
+//     /**
+//      * returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
+//      *
+//      * @param {number} key
+//      * @return {number}
+//      */
+//     get(key) {
+//         if (Object.hasOwn(this.hash, key)) {
+//             return this.hash[key];
+//         } else {
+//             return -1;
+//         }
+//     }
+
+//     /**
+//      * removes the key and its corresponding value if the map contains the mapping for the key
+//      *
+//      * @param {number} key
+//      * @return {void}
+//      */
+//     remove(key) {
+//         delete this.hash[key];
+
+//         return;
+//     }
+// }
+
+// solution 2 -- array
 class MyHashMap {
     constructor() {
-        this.hash = {};
+        this.data = Array.from({ length: 1_000_001 });
     }
 
     /**
@@ -12,7 +58,7 @@ class MyHashMap {
      */
     put(key, value) {
         // otherwise, add new key/val pair
-        this.hash[key] = value;
+        this.data[key] = value;
     }
 
     /**
@@ -22,8 +68,10 @@ class MyHashMap {
      * @return {number}
      */
     get(key) {
-        if (Object.hasOwn(this.hash, key)) {
-            return this.hash[key];
+        const value = this.data[key];
+
+        if (value !== undefined) {
+            return value;
         } else {
             return -1;
         }
@@ -36,9 +84,7 @@ class MyHashMap {
      * @return {void}
      */
     remove(key) {
-        delete this.hash[key];
-
-        return;
+        delete this.data[key];
     }
 }
 
