62bdee38a79e98285ee2f9085ff14e153933d942
Alex Curran <alexcurran.va@gmail.com>
2026-03-10 16:01:36 -0500

    Binary search solution 2

--- medium/search-in-rotated-sorted-array-2.js
+++ medium/search-in-rotated-sorted-array-2.js
@@ -1,8 +1,21 @@
 "use strict";
 
+// /**
+//  * solution 1 -- brute force
+//  * time: O(n)
+//  * space: O(1)
+//  *
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {boolean}
+//  */
+// function search(nums, target) {
+//     return nums.includes(target);
+// }
+
 /**
- * solution 1 -- brute force
- * time: O(n)
+ * solution 2 -- binary search
+ * time: O(log n)
  * space: O(1)
  *
  * @param {number[]} nums
@@ -10,7 +23,35 @@
  * @return {boolean}
  */
 function search(nums, target) {
-    return nums.includes(target);
+    let left = 0;
+    let right = nums.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor(left + (right - left) / 2);
+
+        if (nums[mid] === target) {
+            return true;
+        }
+
+        // otherwise, keep looking
+        if (nums[left] < nums[mid]) {
+            if (nums[left] <= target && target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } else if (nums[left] > nums[mid]) {
+            if (nums[mid] < target && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        } else {
+            left++;
+        }
+    }
+
+    return false;
 }
 
 console.log(search([2, 5, 6, 0, 0, 1, 2], 0)); // true
