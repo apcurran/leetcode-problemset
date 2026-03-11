b7d0f110b513cc6186d2d2c3da0cd96f3fbb1da9
Alex Curran <alexcurran.va@gmail.com>
2026-03-11 12:02:53 -0500

    Bitwise operators with bitmask solution 2

--- easy/complement-of-base-10-integer.js
+++ easy/complement-of-base-10-integer.js
@@ -1,24 +1,49 @@
+// /**
+//  * solution 1 -- iterative
+//  * time: O(n)
+//  * space: O(n)
+//  *
+//  * @param {number} n
+//  * @return {number}
+//  */
+// function bitwiseComplement(n) {
+//     const nBinaryStr = n.toString(2);
+//     let resultBinaryStr = "";
+
+//     for (let bitStr of nBinaryStr) {
+//         if (bitStr === "0") {
+//             resultBinaryStr += "1";
+//         } else {
+//             resultBinaryStr += "0";
+//         }
+//     }
+
+//     return parseInt(resultBinaryStr, 2);
+// }
+
 /**
- * solution 1 -- iterative
- * time: O(n)
- * space: O(n)
+ * solution 2 -- bitwise operators and bitmask
+ * time: O(1)
+ * space: O(1)
  *
  * @param {number} n
  * @return {number}
  */
 function bitwiseComplement(n) {
-    const nBinaryStr = n.toString(2);
-    let resultBinaryStr = "";
-
-    for (let bitStr of nBinaryStr) {
-        if (bitStr === "0") {
-            resultBinaryStr += "1";
-        } else {
-            resultBinaryStr += "0";
-        }
+    if (n === 0) {
+        return 1;
     }
 
-    return parseInt(resultBinaryStr, 2);
+    let mask = 1;
+
+    // shift mask left until it covers all bits of binary n
+    while (mask <= n) {
+        mask = mask << 1;
+    }
+
+    const allOnesMask = mask - 1;
+
+    return n ^ allOnesMask; // flip bits with mask
 }
 
 console.log(bitwiseComplement(5)); // 2
