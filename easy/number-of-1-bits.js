71d2ff9f74003f86c0ce94b8e0c576a2746ebdf5
Alex Curran <alexcurran.va@gmail.com>
2026-03-11 16:08:15 -0500

    Brute force stringify n to binary str solution

--- easy/number-of-1-bits.js
+++ easy/number-of-1-bits.js
@@ -17,20 +17,41 @@
 //     return oneBitsCounter;
 // }
 
+// /**
+//  * solution 2
+//  * time: O(1) - constant since 32-bits won't ever change in size
+//  * space: O(1)
+//  *
+//  * @param {number} n - a positive int
+//  * @return {number}
+//  */
+// function hammingWeight(n) {
+//     let oneBitsCounter = 0;
+
+//     while (n) {
+//         n &= n - 1;
+//         oneBitsCounter++;
+//     }
+
+//     return oneBitsCounter;
+// }
+
 /**
- * solution 2
- * time: O(1) - constant since 32-bits won't ever change in size
- * space: O(1)
+ * solution 3 -- brute force
+ * time: O(n) stringify n
+ * space: O(n) stringify n
  *
  * @param {number} n - a positive int
  * @return {number}
  */
 function hammingWeight(n) {
+    const nBinStr = n.toString(2);
     let oneBitsCounter = 0;
 
-    while (n) {
-        n &= n - 1;
-        oneBitsCounter++;
+    for (let bitStr of nBinStr) {
+        if (bitStr === "1") {
+            oneBitsCounter++;
+        }
     }
 
     return oneBitsCounter;
