afa24992d64c910be467ccb44c6e05a5c528c02e
Alex Curran <alexcurran.va@gmail.com>
2026-03-12 12:12:52 -0500

    O(n * log n) solution with bitwise operator

--- easy/counting-bits.js
+++ easy/counting-bits.js
@@ -1,31 +1,59 @@
-"use strict";
+// /**
+//  * solution 1
+//  * n = input n number
+//  * m = bin ones count for each bin number i of arr
+//  *
+//  * time: O(n * m)
+//  * space: O(m)
+//  *
+//  * @param {number} n
+//  * @return {number[]}
+//  */
+// function countBits(n) {
+//     let resArr = [];
+
+//     for (let i = 0; i <= n; i++) {
+//         const binNum = i.toString(2);
+//         let onesBitsCount = 0;
+
+//         for (let binDigitStr of binNum) {
+//             if (binDigitStr === "1") onesBitsCount++;
+//         }
+
+//         resArr.push(onesBitsCount);
+//     }
+
+//     return resArr;
+// }
 
 /**
- * solution 1
+ * solution 2
  * n = input n number
  * m = bin ones count for each bin number i of arr
  *
- * time: O(n * m)
- * space: O(m)
+ * time: O(n * log n)
+ * space: O(1) -- not including results
  *
  * @param {number} n
  * @return {number[]}
  */
 function countBits(n) {
-    let resArr = [];
+    let results = [];
 
-    for (let i = 0; i <= n; i++) {
-        const binNum = i.toString(2);
-        let onesBitsCount = 0;
+    for (let num = 0; num <= n; num++) {
+        let numOnesBitsCount = 0;
 
-        for (let binDigitStr of binNum) {
-            if (binDigitStr === "1") onesBitsCount++;
+        for (let j = 0; j < 32; j++) {
+            // check for ith bit set (1 bit)
+            if ((num & (1 << j)) !== 0) {
+                numOnesBitsCount++;
+            }
         }
 
-        resArr.push(onesBitsCount);
+        results.push(numOnesBitsCount);
     }
 
-    return resArr;
+    return results;
 }
 
 console.log(countBits(2)); // [0,1,1]
