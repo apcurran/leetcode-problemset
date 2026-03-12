957212f873d1b2155d658300e488f0b9ee5cd7d8
Alex Curran <alexcurran.va@gmail.com>
2026-03-12 13:52:20 -0500

    Kerningham's Algorithm solution

--- easy/counting-bits.js
+++ easy/counting-bits.js
@@ -26,8 +26,38 @@
 //     return resArr;
 // }
 
+// /**
+//  * solution 2 -- bit manipulation
+//  * time: O(n * log n)
+//  * space: O(1) -- not including results
+//  *
+//  * @param {number} n
+//  * @return {number[]}
+//  */
+// function countBits(n) {
+//     let results = [];
+
+//     for (let num = 0; num <= n; num++) {
+//         let numOnesBitsCount = 0;
+
+//         for (let j = 0; j < 32; j++) {
+//             // shift 1 bit left by j places to check current bit value
+//             const bitPowerOfTwoMask = 1 << j;
+//             const bitValue = num & bitPowerOfTwoMask;
+//             // check for ith bit set (1 bit -> always non-zero)
+//             if (bitValue !== 0) {
+//                 numOnesBitsCount++;
+//             }
+//         }
+
+//         results.push(numOnesBitsCount);
+//     }
+
+//     return results;
+// }
+
 /**
- * solution 2 -- bit manipulation
+ * solution 3 -- Kernighan’s Algorithm (bit manipulation)
  * time: O(n * log n)
  * space: O(1) -- not including results
  *
@@ -35,22 +65,16 @@
  * @return {number[]}
  */
 function countBits(n) {
-    let results = [];
+    let results = Array.from({ length: n + 1 }).fill(0);
 
-    for (let num = 0; num <= n; num++) {
-        let numOnesBitsCount = 0;
+    for (let i = 0; i <= n; i++) {
+        let num = i;
 
-        for (let j = 0; j < 32; j++) {
-            // shift 1 bit left by j places to check current bit value
-            const bitPowerOfTwoMask = 1 << j;
-            const bitValue = num & bitPowerOfTwoMask;
-            // check for ith bit set (1 bit -> always non-zero)
-            if (bitValue !== 0) {
-                numOnesBitsCount++;
-            }
+        while (num > 0) {
+            // increment 1 bits count
+            results[i]++;
+            num &= num - 1;
         }
-
-        results.push(numOnesBitsCount);
     }
 
     return results;
