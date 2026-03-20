f85450ffd5654a150a304ac6b630c3e5194894ac
Alex Curran <alexcurran.va@gmail.com>
2026-03-20 14:57:14 -0500

    For loop version that tests slower than string methods

--- easy/reverse-string-prefix.js
+++ easy/reverse-string-prefix.js
@@ -1,5 +1,21 @@
+// /**
+//  * solution 1 -- string methods
+//  * time: O(n)
+//  * space: O(n)
+//  *
+//  * @param {string} s
+//  * @param {number} k
+//  * @return {string}
+//  */
+// function reversePrefix(s, k) {
+//     const reversedChunk = s.slice(0, k).split("").reverse("").join("");
+//     const remainingChunk = s.slice(k);
+
+//     return reversedChunk + remainingChunk;
+// }
+
 /**
- * solution 1 -- string methods
+ * solution 2 -- for loop
  * time: O(n)
  * space: O(n)
  *
@@ -8,10 +24,17 @@
  * @return {string}
  */
 function reversePrefix(s, k) {
-    const reversedChunk = s.slice(0, k).split("").reverse("").join("");
-    const remainingChunk = s.slice(k);
+    let reversedResult = "";
 
-    return reversedChunk + remainingChunk;
+    for (let i = k - 1; i >= 0; i--) {
+        reversedResult += s[i];
+    }
+
+    for (let i = k; i < s.length; i++) {
+        reversedResult += s[i];
+    }
+
+    return reversedResult;
 }
 
 console.log(reversePrefix("abcd", 2)); // "bacd"
