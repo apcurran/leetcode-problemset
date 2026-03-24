02b01f4258ef7964232b1e70d7221aa463625eef
Alex Curran <alexcurran.va@gmail.com>
2026-03-24 15:45:40 -0500

    Math-based solution using arithmetic producing a O(log n) time complexity and O(1) space

--- easy/mirror-distance-of-an-integer.js
+++ easy/mirror-distance-of-an-integer.js
@@ -1,13 +1,36 @@
+// /**
+//  * solution 1 -- string methods
+//  * time: O(n)
+//  * space: O(n)
+//  *
+//  * @param {number} n
+//  * @return {number}
+//  */
+// function mirrorDistance(n) {
+//     const nReversed = Number(String(n).split("").reverse().join(""));
+
+//     return Math.abs(n - nReversed);
+// }
+
 /**
- * solution 1 -- string methods
- * time: O(n)
- * space: O(n)
+ * solution 2 -- math
+ * time: O(log n)
+ * space: O(1)
  *
  * @param {number} n
  * @return {number}
  */
 function mirrorDistance(n) {
-    const nReversed = Number(String(n).split("").reverse().join(""));
+    let nCopy = n;
+    let nReversed = 0;
+
+    while (nCopy > 0) {
+        const lastDigit = nCopy % 10;
+        // shift and add (move over left)
+        nReversed = nReversed * 10 + lastDigit;
+        // drop last digit
+        nCopy = Math.floor(nCopy / 10);
+    }
 
     return Math.abs(n - nReversed);
 }
