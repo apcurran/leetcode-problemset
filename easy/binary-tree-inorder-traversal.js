bbf35d42c694faaa4cf691eaa9ea13a3d4ee7f37
Alex Curran <alexcurran.va@gmail.com>
2026-03-30 15:54:50 -0500

    Stack-based DFS with inorder (iterative)

--- easy/binary-tree-inorder-traversal.js
+++ easy/binary-tree-inorder-traversal.js
@@ -4,25 +4,61 @@ function TreeNode(val, left, right) {
     this.right = right === undefined ? null : right;
 }
 
+// /**
+//  * solution 1 -- DFS recursion
+//  * time: O(n)
+//  * space: O(n)
+//  *
+//  * @param {TreeNode} root
+//  * @return {number[]}
+//  */
+// function inorderTraversal(root, results = []) {
+//     if (root === null) {
+//         return results;
+//     }
+
+//     // handle left subtree
+//     inorderTraversal(root.left, results);
+//     // handle current node
+//     results.push(root.val);
+//     // handle right subtree
+//     inorderTraversal(root.right, results);
+
+//     return results;
+// }
+
 /**
- * solution 1 -- DFS recursion
+ * solution 2 -- DFS iterative
  * time: O(n)
  * space: O(n)
  *
  * @param {TreeNode} root
  * @return {number[]}
  */
-function inorderTraversal(root, results = []) {
+function inorderTraversal(root) {
+    let results = [];
+
     if (root === null) {
         return results;
     }
 
-    // handle left subtree
-    inorderTraversal(root.left, results);
-    // handle current node
-    results.push(root.val);
-    // handle right subtree
-    inorderTraversal(root.right, results);
+    let stack = [];
+    let current = root;
+
+    while (current !== null || stack.length > 0) {
+        if (current !== null) {
+            // exhaust left subtree before checking current node's value
+            stack.push(current);
+            current = current.left;
+        } else {
+            // check current node's value
+            current = stack.pop();
+            // add to results array
+            results.push(current.val);
+            // explore right subtree
+            current = current.right;
+        }
+    }
 
     return results;
 }
