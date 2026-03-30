48e1bd1122f1c42ff99ab79b9a9459282c2e45b8
Alex Curran <alexcurran.va@gmail.com>
2026-03-30 16:37:34 -0500

    Iterative postorder trav

--- easy/binary-tree-postorder-traversal.js
+++ easy/binary-tree-postorder-traversal.js
@@ -4,8 +4,32 @@ function TreeNode(val, left, right) {
     this.right = right === undefined ? null : right;
 }
 
+// /**
+//  * solution 1 -- DFS recursion
+//  * time: O(n)
+//  * space: O(n)
+//  *
+//  * @param {TreeNode} root
+//  * @param {number[]} [results]
+//  * @return {number[]}
+//  */
+// function postorderTraversal(root, results = []) {
+//     if (root === null) {
+//         return results;
+//     }
+
+//     // handle left subtree
+//     postorderTraversal(root.left, results);
+//     // handle right subtree
+//     postorderTraversal(root.right, results);
+//     // handle current node value
+//     results.push(root.val);
+
+//     return results;
+// }
+
 /**
- * solution 1 -- DFS recursion
+ * solution 2 -- DFS iterative
  * time: O(n)
  * space: O(n)
  *
@@ -13,17 +37,29 @@ function TreeNode(val, left, right) {
  * @param {number[]} [results]
  * @return {number[]}
  */
-function postorderTraversal(root, results = []) {
+function postorderTraversal(root) {
+    let results = [];
+
     if (root === null) {
         return results;
     }
 
-    // handle left subtree
-    postorderTraversal(root.left, results);
-    // handle right subtree
-    postorderTraversal(root.right, results);
-    // handle current node value
-    results.push(root.val);
+    let stack = [root];
+
+    while (stack.length > 0) {
+        const current = stack.pop();
+        results.push(current.val);
+
+        if (current.left !== null) {
+            stack.push(current.left);
+        }
+
+        if (current.right !== null) {
+            stack.push(current.right);
+        }
+    }
+
+    results.reverse();
 
     return results;
 }
