9facc61e917055e8da343fed6e1c9e3438be8abf
Alex Curran <alexcurran.va@gmail.com>
2026-03-31 10:15:34 -0500

    Single pointer solution

--- easy/first-matching-character-from-both-ends.js
+++ easy/first-matching-character-from-both-ends.js
@@ -1,5 +1,35 @@
+// /**
+//  * solution 1 -- two pointers
+//  * time: O(n)
+//  * space: O(1)
+//  *
+//  * @param {string} s
+//  * @return {number}
+//  */
+// function firstMatchingIndex(s) {
+//     let matchingIndex = -1;
+//     let leftPointer = 0;
+//     let rightPointer = s.length - 1;
+
+//     while (leftPointer <= rightPointer) {
+//         const leftChar = s[leftPointer];
+//         const rightChar = s[rightPointer];
+
+//         if (leftChar === rightChar) {
+//             matchingIndex = leftPointer;
+
+//             break;
+//         }
+
+//         leftPointer++;
+//         rightPointer--;
+//     }
+
+//     return matchingIndex;
+// }
+
 /**
- * solution 1 -- two pointers
+ * solution 2 -- single pointer
  * time: O(n)
  * space: O(1)
  *
@@ -7,25 +37,15 @@
  * @return {number}
  */
 function firstMatchingIndex(s) {
-    let matchingIndex = -1;
-    let leftPointer = 0;
-    let rightPointer = s.length - 1;
+    const middle = Math.ceil(s.length / 2);
 
-    while (leftPointer <= rightPointer) {
-        const leftChar = s[leftPointer];
-        const rightChar = s[rightPointer];
-
-        if (leftChar === rightChar) {
-            matchingIndex = leftPointer;
-
-            break;
+    for (let i = 0; i < middle; i++) {
+        if (s[i] === s[s.length - 1 - i]) {
+            return i;
         }
-
-        leftPointer++;
-        rightPointer--;
     }
 
-    return matchingIndex;
+    return -1;
 }
 
 console.log(firstMatchingIndex("abcacbd")); // 1
