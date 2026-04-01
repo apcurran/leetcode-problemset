a986e679ecedd205994d9cfb111ef32ca8ee19a5
Alex Curran <alexcurran.va@gmail.com>
2026-04-01 11:57:52 -0500

    Single pointer solution with linear time/space

--- easy/trim-trailing-vowels.js
+++ easy/trim-trailing-vowels.js
@@ -18,8 +18,22 @@
 //     return s;
 // }
 
+// /**
+//  * solution 2 -- regex (optimized time)
+//  * time: O(n)
+//  * space: O(n)
+//  *
+//  * @param {string} s
+//  * @return {string}
+//  */
+// function trimTrailingVowels(s) {
+//     const vowelRegex = /[aeiou]+$/;
+
+//     return s.replace(vowelRegex, "");
+// }
+
 /**
- * solution 1 -- regex (optimized time)
+ * solution 3 -- single pointer
  * time: O(n)
  * space: O(n)
  *
@@ -27,9 +41,15 @@
  * @return {string}
  */
 function trimTrailingVowels(s) {
-    const vowelRegex = /[aeiou]+$/;
+    const VOWELS = ["a", "e", "i", "o", "u"];
+    let pointer = s.length - 1;
 
-    return s.replace(vowelRegex, "");
+    while (pointer >= 0 && VOWELS.includes(s[pointer])) {
+        // move pointer
+        pointer--;
+    }
+
+    return s.slice(0, pointer + 1);
 }
 
 console.log(trimTrailingVowels("idea")); // "id"
