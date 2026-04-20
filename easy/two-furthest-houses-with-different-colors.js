1b2e635543e4348690b9d9cabbbe0ab0cc858832
Alex Curran <alexcurran.va@gmail.com>
2026-04-20 13:20:00 -0500

    Brute force solution -- try every combo

---
+++ easy/two-furthest-houses-with-different-colors.js
@@ -0,0 +1,30 @@
+/**
+ * solution 1 -- brute force
+ * time: O(n^2)
+ * space: O(1)
+ *
+ * @param {number[]} colors
+ * @return {number}
+ */
+function maxDistance(colors) {
+    let max = 0;
+
+    // try every house color combo
+    for (let i = 0; i < colors.length; i++) {
+        const house1 = colors[i];
+
+        for (let j = i + 1; j < colors.length; j++) {
+            const house2 = colors[j];
+            if (house1 !== house2) {
+                const currentDistance = j - i;
+                max = Math.max(max, currentDistance);
+            }
+        }
+    }
+
+    return max;
+}
+
+console.log(maxDistance([1, 1, 1, 6, 1, 1, 1])); // 3
+console.log(maxDistance([1, 8, 3, 8, 3])); // 4
+console.log(maxDistance([0, 1])); // 1
