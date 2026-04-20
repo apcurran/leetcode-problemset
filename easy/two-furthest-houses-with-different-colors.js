c3d03ca8c2420d931dea662d34fe27e7e4624d9d
Alex Curran <alexcurran.va@gmail.com>
2026-04-20 13:38:21 -0500

    Two-pass solution linear time

--- easy/two-furthest-houses-with-different-colors.js
+++ easy/two-furthest-houses-with-different-colors.js
@@ -1,6 +1,33 @@
+// /**
+//  * solution 1 -- brute force
+//  * time: O(n^2)
+//  * space: O(1)
+//  *
+//  * @param {number[]} colors
+//  * @return {number}
+//  */
+// function maxDistance(colors) {
+//     let max = 0;
+
+//     // try every house color combo
+//     for (let i = 0; i < colors.length; i++) {
+//         const house1 = colors[i];
+
+//         for (let j = i + 1; j < colors.length; j++) {
+//             const house2 = colors[j];
+//             if (house1 !== house2) {
+//                 const currentDistance = j - i;
+//                 max = Math.max(max, currentDistance);
+//             }
+//         }
+//     }
+
+//     return max;
+// }
+
 /**
- * solution 1 -- brute force
- * time: O(n^2)
+ * solution 2 -- two-pass
+ * time: O(n)
  * space: O(1)
  *
  * @param {number[]} colors
@@ -9,16 +36,23 @@
 function maxDistance(colors) {
     let max = 0;
 
-    // try every house color combo
-    for (let i = 0; i < colors.length; i++) {
-        const house1 = colors[i];
+    // static first house, find the furthest house with a diff color
+    for (let i = colors.length - 1; i >= 0; i--) {
+        if (colors[i] !== colors[0]) {
+            // current dist (i - 0)
+            max = Math.max(max, i);
 
-        for (let j = i + 1; j < colors.length; j++) {
-            const house2 = colors[j];
-            if (house1 !== house2) {
-                const currentDistance = j - i;
-                max = Math.max(max, currentDistance);
-            }
+            break;
+        }
+    }
+
+    // static last house, find the furthest house with a diff color
+    for (let i = 0; i < colors.length; i++) {
+        if (colors[i] !== colors.at(-1)) {
+            const currentDistance = colors.length - 1 - i;
+            max = Math.max(max, currentDistance);
+
+            break;
         }
     }
 
