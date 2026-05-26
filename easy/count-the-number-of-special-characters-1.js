40ebe15f21ba560a2df18a079e6e1040b3a97a64
Alex Curran <alexcurran.va@gmail.com>
2026-05-26 09:24:33 -0500

    Set solution, slightly optimize from prior version

--- easy/count-the-number-of-special-characters-1.js
+++ easy/count-the-number-of-special-characters-1.js
@@ -1,26 +1,52 @@
+// /**
+//  * solution 1 -- map
+//  * time: O(n^2) - string.includes() nested inside of for loop
+//  * space: O(n)
+//  *
+//  * @param {string} word
+//  * @return {number}
+//  */
+// function numberOfSpecialChars(word) {
+//     let isLowerAndUpperChar = new Map();
+
+//     for (let char of word) {
+//         if (char.toLowerCase() === char) {
+//             // if lowercase letter,
+//             // check if uppercase version exists,
+//             // if so, add true to letter in map
+//             if (word.includes(char.toUpperCase())) {
+//                 isLowerAndUpperChar.set(char, true);
+//             }
+//         }
+//     }
+
+//     return isLowerAndUpperChar.size;
+// }
+
 /**
- * solution 1 -- map
- * time: O(n^2) - string.includes() nested inside of for loop
- * space: O(n)
+ * solution 2 -- set
+ * time: O(n)
+ * space: O(u) -- 52 unique chars max
  *
  * @param {string} word
  * @return {number}
  */
 function numberOfSpecialChars(word) {
-    let isLowerAndUpperChar = new Map();
+    const aCharCodeLower = 97;
+    const aCharCodeUpper = 65;
+    let charSet = new Set(word);
+    let specialCharsCount = 0;
 
-    for (let char of word) {
-        if (char.toLowerCase() === char) {
-            // if lowercase letter,
-            // check if uppercase version exists,
-            // if so, add true to letter in map
-            if (word.includes(char.toUpperCase())) {
-                isLowerAndUpperChar.set(char, true);
-            }
+    for (let i = 0; i < 26; i++) {
+        const lowerChar = String.fromCharCode(aCharCodeLower + i);
+        const upperChar = String.fromCharCode(aCharCodeUpper + i);
+
+        if (charSet.has(lowerChar) && charSet.has(upperChar)) {
+            specialCharsCount++;
         }
     }
 
-    return isLowerAndUpperChar.size;
+    return specialCharsCount;
 }
 
 console.log(numberOfSpecialChars("aaAbcBC")); // 3
