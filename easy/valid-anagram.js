47db93bc8f59ba1d7a5e4dc1b20cc42e005aebf9
Alex Curran <alexcurran.va@gmail.com>
2026-06-10 14:36:54 -0500

    Two hashmap solution 3

--- easy/valid-anagram.js
+++ easy/valid-anagram.js
@@ -22,8 +22,42 @@
 //     return sSorted === tSorted;
 // }
 
+// /**
+//  * solution 2 -- hashmap
+//  * time: O(s)
+//  * space: O(26) -> O(1) -- max of 26 alphabet chars
+//  *
+//  * @param {string} s
+//  * @param {string} t
+//  * @return {boolean}
+//  */
+// function isAnagram(s, t) {
+//     if (s.length !== t.length) {
+//         return false;
+//     }
+
+//     let charsCounts = new Map();
+
+//     for (let char of s) {
+//         const previousCount = charsCounts.get(char) || 0;
+//         charsCounts.set(char, previousCount + 1);
+//     }
+
+//     for (let char of t) {
+//         const currCharCount = charsCounts.get(char);
+//         // character we don't already have OR char count ran out already
+//         if (!charsCounts.has(char) || currCharCount === 0) {
+//             return false;
+//         }
+
+//         charsCounts.set(char, currCharCount - 1); // decrement this char's frequency
+//     }
+
+//     return true;
+// }
+
 /**
- * solution 1 -- hashmap
+ * solution 3 -- two hashmaps comparison
  * time: O(s)
  * space: O(26) -> O(1) -- max of 26 alphabet chars
  *
@@ -36,24 +70,32 @@ function isAnagram(s, t) {
         return false;
     }
 
-    let charsCounts = new Map();
+    const sMap = getCount(s);
+    const tMap = getCount(t);
 
-    for (let char of s) {
-        const previousCount = charsCounts.get(char) || 0;
-        charsCounts.set(char, previousCount + 1);
-    }
-
-    for (let char of t) {
-        const currCharCount = charsCounts.get(char);
-        // character we don't already have OR char count ran out already
-        if (!charsCounts.has(char) || currCharCount === 0) {
+    for (let [sChar, sCharCount] of sMap) {
+        // char from sChar does not exist in tMap OR tMap char count is different
+        if (!tMap.has(sChar) || tMap.get(sChar) !== sCharCount) {
             return false;
         }
-
-        charsCounts.set(char, currCharCount - 1); // decrement this char's frequency
     }
 
     return true;
+}
+
+/**
+ * @param {string} str
+ * @returns {Map}
+ */
+function getCount(str) {
+    let hash = new Map();
+
+    for (let char of str) {
+        const previousCount = hash.get(char) || 0;
+        hash.set(char, previousCount + 1);
+    }
+
+    return hash;
 }
 
 console.log(isAnagram("anagram", "nagaram")); // true
