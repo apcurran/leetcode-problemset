8d6cad2edf6235da9d6210bd1664dc076ae2cdb2
Alex Curran <alexcurran.va@gmail.com>
2026-07-01 15:46:41 -0500

    Math-based solution 2

--- easy/digit-frequency-score.js
+++ easy/digit-frequency-score.js
@@ -1,5 +1,33 @@
+// /**
+//  * solution 1 -- hash map
+//  * time: O(n)
+//  * space: O(n)
+//  *
+//  * @param {number} n
+//  * @return {number}
+//  */
+// function digitFrequencyScore(n) {
+//     const nStr = String(n);
+//     let digitsFrequencies = new Map();
+
+//     for (let digitStr of nStr) {
+//         const digit = Number(digitStr);
+//         const previousCount = digitsFrequencies.get(digit) || 0;
+//         digitsFrequencies.set(digit, previousCount + 1);
+//     }
+
+//     let sum = 0;
+
+//     for (let [digit, digitCount] of digitsFrequencies) {
+//         const digitProduct = digit * digitCount;
+//         sum += digitProduct;
+//     }
+
+//     return sum;
+// }
+
 /**
- * solution 1 -- hash map
+ * solution 2 -- math
  * time: O(n)
  * space: O(n)
  *
@@ -7,20 +35,10 @@
  * @return {number}
  */
 function digitFrequencyScore(n) {
-    const nStr = String(n);
-    let digitsFrequencies = new Map();
-
-    for (let digitStr of nStr) {
-        const digit = Number(digitStr);
-        const previousCount = digitsFrequencies.get(digit) || 0;
-        digitsFrequencies.set(digit, previousCount + 1);
-    }
-
     let sum = 0;
 
-    for (let [digit, digitCount] of digitsFrequencies) {
-        const digitProduct = digit * digitCount;
-        sum += digitProduct;
+    for (let digitStr of String(n)) {
+        sum += Number(digitStr);
     }
 
     return sum;
