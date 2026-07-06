605da3126a02f4907892dd651822ff68bd84cc0e
Alex Curran <alexcurran.va@gmail.com>
2026-07-06 11:40:33 -0500

    Sort solution 2
    Sort, then iterate intervals array is big-O more optimal,
    yet LeetCode shows it is slower (space is also larger).

--- medium/remove-covered-intervals.js
+++ medium/remove-covered-intervals.js
@@ -1,33 +1,67 @@
+// /**
+//  * solution 1 -- brute force
+//  * time: O(n^2)
+//  * space: O(1)
+//  *
+//  * @param {number[][]} intervals
+//  * @return {number}
+//  */
+// function removeCoveredIntervals(intervals) {
+//     let count = 0;
+
+//     outerLoop: for (let i = 0; i < intervals.length; i++) {
+//         const currentInterval = intervals[i];
+
+//         for (let j = 0; j < intervals.length; j++) {
+//             if (j === i) continue;
+
+//             // interate again skipping current interval
+//             const otherInterval = intervals[j];
+
+//             // given [a, b] [c, d] check that c <= a and b <= d
+//             if (otherInterval[0] <= currentInterval[0] && currentInterval[1] <= otherInterval[1]) {
+//                 continue outerLoop;
+//             }
+//         }
+
+//         count++;
+//     }
+
+//     return count;
+// }
+
 /**
- * solution 1 -- brute force
- * time: O(n^2)
- * space: O(1)
+ * solution 2 -- sorting
+ * time: O(n * log n)
+ * space: O(n) -- due to array.sort() call
  *
  * @param {number[][]} intervals
  * @return {number}
  */
 function removeCoveredIntervals(intervals) {
-    let count = 0;
+    intervals.sort(function startAscEndDesc(a, b) {
+        if (a[0] !== b[0]) {
+            return a[0] - b[0];
+        } else {
+            return b[1] - a[1];
+        }
+    });
 
-    outerLoop: for (let i = 0; i < intervals.length; i++) {
-        const currentInterval = intervals[i];
+    let remaining = 0;
+    let maxEnd = -Infinity;
 
-        for (let j = 0; j < intervals.length; j++) {
-            if (j === i) continue;
+    for (let interval of intervals) {
+        const end = interval[1];
 
-            // interate again skipping current interval
-            const otherInterval = intervals[j];
-
-            // given [a, b] [c, d] check that c <= a and b <= d
-            if (otherInterval[0] <= currentInterval[0] && currentInterval[1] <= otherInterval[1]) {
-                continue outerLoop;
-            }
+        if (end <= maxEnd) {
+            continue;
         }
 
-        count++;
+        remaining++;
+        maxEnd = end;
     }
 
-    return count;
+    return remaining;
 }
 
 console.log(
