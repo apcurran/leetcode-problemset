397d2f64a3d282e396f576d647a1823c13596ae0
Alex Curran <alexcurran.va@gmail.com>
2026-07-27 17:13:04 -0500

    Optimal log n time and constant space solution 2

--- easy/max-product-of-two-digits.js
+++ easy/max-product-of-two-digits.js
@@ -1,26 +1,55 @@
+// /**
+//  * solution 1
+//  * time: O(n^2)
+//  * space: O(n) -- string conversion of n
+//  *
+//  * @param {number} n
+//  * @return {number}
+//  */
+// function maxProduct(n) {
+//     const strN = String(n);
+//     let largestProduct = -Infinity;
+
+//     for (let i = 0; i < strN.length; i++) {
+//         const currentDigit = Number(strN[i]);
+
+//         for (let j = i + 1; j < strN.length; j++) {
+//             const nextDigit = Number(strN[j]);
+//             const currentProduct = currentDigit * nextDigit;
+//             largestProduct = Math.max(largestProduct, currentProduct);
+//         }
+//     }
+
+//     return largestProduct;
+// }
+
 /**
- * solution 1
- * time: O(n^2)
- * space: O(n) -- string conversion of n
+ * solution 2
+ * time: O(log n)
+ * space: O(1)
  *
  * @param {number} n
  * @return {number}
  */
 function maxProduct(n) {
-    const strN = String(n);
-    let largestProduct = -Infinity;
+    let digitA = 0;
+    let digitB = 0;
 
-    for (let i = 0; i < strN.length; i++) {
-        const currentDigit = Number(strN[i]);
+    while (n > 0) {
+        let x = n % 10;
 
-        for (let j = i + 1; j < strN.length; j++) {
-            const nextDigit = Number(strN[j]);
-            const currentProduct = currentDigit * nextDigit;
-            largestProduct = Math.max(largestProduct, currentProduct);
+        if (x > digitA) {
+            digitB = digitA;
+            digitA = x;
+        } else if (x > digitB) {
+            digitB = x;
         }
+
+        // either way, move to the next digit
+        n = Math.floor(n / 10);
     }
 
-    return largestProduct;
+    return digitA * digitB;
 }
 
 console.log(maxProduct(31)); // 3
