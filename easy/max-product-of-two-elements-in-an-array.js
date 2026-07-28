01be84910155ddb028d9e3562d644b2450179613
Alex Curran <alexcurran.va@gmail.com>
2026-07-28 13:25:54 -0500

    Optimal linear scan solution 2

--- easy/max-product-of-two-elements-in-an-array.js
+++ easy/max-product-of-two-elements-in-an-array.js
@@ -1,20 +1,46 @@
+// /**
+//  * solution 1 -- sort
+//  * time: O(n * log n)
+//  * space: O(n)
+//  *
+//  * @param {number[]} nums
+//  * @return {number}
+//  */
+// function maxProduct(nums) {
+//     nums.sort(function sortDesc(a, b) {
+//         return b - a;
+//     });
+
+//     const a = nums[0];
+//     const b = nums[1];
+
+//     return (a - 1) * (b - 1);
+// }
+
 /**
- * solution 1 -- sort
- * time: O(n * log n)
- * space: O(n)
+ * solution 2 -- linear iteration with tracking
+ * time: O(n)
+ * space: O(1)
  *
  * @param {number[]} nums
  * @return {number}
  */
 function maxProduct(nums) {
-    nums.sort(function sortDesc(a, b) {
-        return b - a;
-    });
+    let biggest = 0;
+    let secondBiggest = 0;
 
-    const a = nums[0];
-    const b = nums[1];
+    for (let num of nums) {
+        if (num > biggest) {
+            secondBiggest = biggest;
+            biggest = num;
+        } else {
+            // only replace secondBiggest if the current num
+            // is indeed larger
+            secondBiggest = Math.max(secondBiggest, num);
+        }
+    }
 
-    return (a - 1) * (b - 1);
+    return (biggest - 1) * (secondBiggest - 1);
 }
 
 console.log(maxProduct([3, 4, 5, 2])); // 12
