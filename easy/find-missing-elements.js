0c74ad0df9b468c11c15752e367ebf0c6bed0da5
Alex Curran <alexcurran.va@gmail.com>
2026-08-04 11:14:01 -0500

    Sort + enumerate solution 3

--- easy/find-missing-elements.js
+++ easy/find-missing-elements.js
@@ -26,33 +26,59 @@
 //     return results;
 // }
 
+// /**
+//  * solution 2 -- hashset
+//  * time: O(n)
+//  * space: O(n) -- not including results array
+//  *
+//  * @param {number[]} nums
+//  * @return {number[]}
+//  */
+// function findMissingElements(nums) {
+//     let min = Infinity;
+//     let max = -Infinity;
+
+//     for (let num of nums) {
+//         min = Math.min(min, num);
+//         max = Math.max(max, num);
+//     }
+
+//     let fullRange = new Set();
+
+//     for (let current = min; current <= max; current++) {
+//         fullRange.add(current);
+//     }
+
+//     const numsSet = new Set(nums);
+//     const diff = fullRange.difference(numsSet);
+
+//     return [...diff];
+// }
+
 /**
- * solution 2 -- hashset
- * time: O(n)
- * space: O(n) -- not including results array
+ * solution 3 -- sort + enumerate
+ * time: O(n * log n + d)
+ * space: O(n) -- due to sorting, not including results array
  *
  * @param {number[]} nums
  * @return {number[]}
  */
 function findMissingElements(nums) {
-    let min = Infinity;
-    let max = -Infinity;
+    nums.sort(function sortAsc(a, b) {
+        return a - b;
+    });
 
-    for (let num of nums) {
-        min = Math.min(min, num);
-        max = Math.max(max, num);
+    let results = [];
+
+    // from beginning to num before end
+    for (let i = 0; i < nums.length - 1; i++) {
+        // iterate all nums less than next num in list (missing values)
+        for (let j = nums[i] + 1; j < nums[i + 1]; j++) {
+            results.push(j);
+        }
     }
 
-    let fullRange = new Set();
-
-    for (let current = min; current <= max; current++) {
-        fullRange.add(current);
-    }
-
-    const numsSet = new Set(nums);
-    const diff = fullRange.difference(numsSet);
-
-    return [...diff];
+    return results;
 }
 
 console.log(findMissingElements([1, 4, 2, 5])); // [3]
