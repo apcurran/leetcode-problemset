3b9bcf86bbd2d5b3bf2c66aceda0e1e8d09b2423
Alex Curran <alexcurran.va@gmail.com>
2026-08-07 14:29:06 -0500

    Optimal O(n) time reverse traversal solution

--- easy/count-indices-with-opposite-parity.js
+++ easy/count-indices-with-opposite-parity.js
@@ -1,33 +1,66 @@
+// /**
+//  * solution 1 -- brute force
+//  * time: O(n^2)
+//  * space: O(1) -- not including required results array
+//  *
+//  * @param {number[]} nums
+//  * @return {number[]}
+//  */
+// function countOppositeParity(nums) {
+//     let results = [];
+
+//     for (let i = 0; i < nums.length; i++) {
+//         const outerNum = nums[i];
+//         let counter = 0;
+
+//         for (let j = i + 1; j < nums.length; j++) {
+//             const innerNum = nums[j];
+
+//             if (outerNum % 2 === innerNum % 2) {
+//                 // check innerNum even/odd (they must be diff)
+//                 continue;
+//             }
+
+//             // if diff, incr counter
+//             counter++;
+//         }
+
+//         results.push(counter);
+//         // reset counter state
+//         counter = 0;
+//     }
+
+//     return results;
+// }
+
 /**
- * solution 1 -- brute force
- * time: O(n^2)
+ * solution 2 -- reverse traversal with counters
+ * time: O(n)
  * space: O(1) -- not including required results array
  *
  * @param {number[]} nums
  * @return {number[]}
  */
 function countOppositeParity(nums) {
-    let results = [];
+    let results = new Array(nums.length);
 
-    for (let i = 0; i < nums.length; i++) {
-        const outerNum = nums[i];
-        let counter = 0;
+    let evenCount = 0;
+    let oddCount = 0;
 
-        for (let j = i + 1; j < nums.length; j++) {
-            const innerNum = nums[j];
+    // reverse iteration
+    for (let i = nums.length - 1; i >= 0; i--) {
+        const currNumIsEven = nums[i] % 2 === 0;
 
-            if (outerNum % 2 === innerNum % 2) {
-                // check innerNum even/odd (they must be diff)
-                continue;
-            }
-
-            // if diff, incr counter
-            counter++;
+        if (currNumIsEven) {
+            // set oddCount to result
+            results[i] = oddCount;
+            // increment even counter
+            evenCount++;
+        } else {
+            results[i] = evenCount;
+            // increment odd counter
+            oddCount++;
         }
-
-        results.push(counter);
-        // reset counter state
-        counter = 0;
     }
 
     return results;
