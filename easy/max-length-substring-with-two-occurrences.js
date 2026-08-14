2b6b4e336d3bc92269dd432fc1064746abe7a0e1
Alex Curran <alexcurran.va@gmail.com>
2026-08-14 13:34:12 -0500

    Optimal sliding window and hash map approach 2

--- easy/max-length-substring-with-two-occurrences.js
+++ easy/max-length-substring-with-two-occurrences.js
@@ -1,6 +1,41 @@
+// /**
+//  * solution 1 -- brute force hash map
+//  * time: O(n^2)
+//  * space: O(n)
+//  *
+//  * @param {string} s
+//  * @return {number}
+//  */
+// function maximumLengthSubstring(s) {
+//     let maxWindowLength = 0;
+
+//     for (let i = 0; i < s.length; i++) {
+//         let charsCounts = new Map();
+
+//         for (let j = i; j < s.length; j++) {
+//             const char = s[j];
+//             const previousCharCount = charsCounts.get(char) || 0;
+
+//             // If adding this character would make its count 3,
+//             // stop expanding from this starting point
+//             if (previousCharCount === 2) {
+//                 break;
+//             }
+
+//             charsCounts.set(char, previousCharCount + 1);
+//             const currentWindowLength = j - i + 1;
+//             maxWindowLength = Math.max(maxWindowLength, currentWindowLength);
+//         }
+
+//         charsCounts.clear();
+//     }
+
+//     return maxWindowLength;
+// }
+
 /**
- * solution 1 -- brute force hash map
- * time: O(n^2)
+ * solution 2 -- sliding window with hash map
+ * time: O(n)
  * space: O(n)
  *
  * @param {string} s
@@ -8,26 +43,23 @@
  */
 function maximumLengthSubstring(s) {
     let maxWindowLength = 0;
+    let left = 0;
+    let charCounts = new Map();
 
-    for (let i = 0; i < s.length; i++) {
-        let charsCounts = new Map();
+    for (let right = 0; right < s.length; right++) {
+        const char = s[right];
+        charCounts.set(char, (charCounts.get(char) || 0) + 1);
 
-        for (let j = i; j < s.length; j++) {
-            const char = s[j];
-            const previousCharCount = charsCounts.get(char) || 0;
-
-            // If adding this character would make its count 3,
-            // stop expanding from this starting point
-            if (previousCharCount === 2) {
-                break;
-            }
-
-            charsCounts.set(char, previousCharCount + 1);
-            const currentWindowLength = j - i + 1;
-            maxWindowLength = Math.max(maxWindowLength, currentWindowLength);
+        // shrink window when more than 2 same chars within it
+        while (charCounts.get(char) > 2) {
+            const leftChar = s[left];
+            charCounts.set(leftChar, charCounts.get(leftChar) - 1);
+            // move left pointer forward to point to next char i string
+            left++;
         }
 
-        charsCounts.clear();
+        const currentWindowLength = right - left + 1;
+        maxWindowLength = Math.max(maxWindowLength, currentWindowLength);
     }
 
     return maxWindowLength;
